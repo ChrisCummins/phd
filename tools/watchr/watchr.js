4f20b5c20aad5192e5b7e363939f48064b3080ca
Chris Cummins <chrisc.101@gmail.com>
2014-04-24 03:13:32 +0100

    Add GPL v3 license headers to sources
    Issue #350.

--- tools/watchr/watchr.js
+++ tools/watchr/watchr.js
@@ -1,7 +1,25 @@
 #!/usr/bin/env node
 
 /*
- * watchr.js - Deploy a development server and automatically rebuild sources
+ * watchr.js - Deploy a development server and automatically rebuild
+ * sources.
+ *
+ * Copyright 2014 Chris Cummins.
+ *
+ * This file is part of pip-db.
+ *
+ * pip-db is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pip-db is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pip-db.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var colors = require('colors');
