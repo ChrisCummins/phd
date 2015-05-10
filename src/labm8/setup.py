f6cfad6d831794fa907b6691d81c11f17ff813bc
Chris Cummins <chrisc.101@gmail.com>
2015-05-10 14:49:13 +0100

    Add python setup files

---
+++ src/labm8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='labm8',
+      version='0.0.1',
+      description='A collection of functions and utilities for scientific computing',
+      url='',
+      author='Chris Cummins',
+      author_email='chrisc.101@gmail.com',
+      license='GPL v3',
+      packages=['labm8'],
+      test_suite='nose.collector',
+      tests_require=[
+          'coverage',
+          'nose'
+      ],
+      install_requires=[
+          'scipy'
+      ],
+      zip_safe=False)
