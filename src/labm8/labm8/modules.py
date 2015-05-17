519c94405343b56021a752502afaa416cc394036
Chris Cummins <chrisc.101@gmail.com>
2015-05-17 02:55:26 +0100

    modules: Rename function

--- src/labm8/labm8/modules.py
+++ src/labm8/labm8/modules.py
@@ -16,7 +16,7 @@
 # along with labm8.  If not, see <http://www.gnu.org/licenses/>.
 
 
-def import_foreign_module(name, custom_name=None):
+def import_foreign(name, custom_name=None):
     """
     Import a module with "name", using the optional imported name
     "custom_name". See: http://stackoverflow.com/a/6032023
