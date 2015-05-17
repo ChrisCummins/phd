6c3f28cca3dcd0f9f0f87b118ec739bb82d472c6
Chris Cummins <chrisc.101@gmail.com>
2015-05-17 01:37:40 +0100

    docs/example_progs: Add license headers

--- src/labm8/docs/example_prog.py
+++ src/labm8/docs/example_prog.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2015 Chris Cummins.
+#
+# Labm8 is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Labm8 is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with labm8.  If not, see <http://www.gnu.org/licenses/>.
 """
 In this example script, the user wants to record performance data
 for a program compiled at various different optimisations levels using
@@ -14,7 +29,7 @@ import labm8 as lab
 from labm8 import git
 from labm8 import make
 from labm8 import result
-from labm8 import static
+from labm8 import sampler
 from labm8 import system
 from labm8 import testcase
 from labm8 import variables
