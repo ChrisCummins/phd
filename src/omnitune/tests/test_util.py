0418c01afca45abaa62778e4f56dad2d83896df0
Chris Cummins <chrisc.101@gmail.com>
2015-05-22 15:21:55 +0100

    omnitune: Add parse_str() tests

--- src/omnitune/tests/test_util.py
+++ src/omnitune/tests/test_util.py
@@ -8,7 +8,9 @@ class TestUtil(TestCase):
 
     # parse_str()
     def test_parse_str(self):
-        self._test("abd", util.parse_str("abc"))
+        self._test("abc", util.parse_str("abc"))
+        self._test("a\nc", util.parse_str("a\nc"))
+        self._test("123", util.parse_str("123"))
 
 
 if __name__ == '__main__':
