098dd1a2713cef1bbb30816c512f85613cc02baa
Chris Cummins <chrisc.101@gmail.com>
2015-05-28 12:06:16 +0100

    modules: Add documentation and tests

--- src/labm8/labm8/modules.py
+++ src/labm8/labm8/modules.py
@@ -15,13 +15,46 @@
 # You should have received a copy of the GNU General Public License
 # along with labm8.  If not, see <http://www.gnu.org/licenses/>.
 
+import imp
+import sys
+
+import labm8
+from labm8 import io
+
 
 def import_foreign(name, custom_name=None):
     """
-    Import a module with "name", using the optional imported name
-    "custom_name". See: http://stackoverflow.com/a/6032023
+    Import a module with a custom name.
+
+    NOTE this is only needed for Python2. For Python3, import the
+    module using the "as" keyword to declare the custom name.
+
+    For implementation details, see:
+    http://stackoverflow.com/a/6032023
+
+    Example:
+
+      To import the standard module "math" as "std_math":
+
+          if sys.version_info >= (3, 0):
+            import math as std_math
+          else:
+            std_math = modules.import_foreign("math", "std_math")
+
+    Arguments:
+
+        name (str): The name of the module to import.
+        custom_name (str, optional): The custom name to assign the module to.
+
+    Raises:
+        ImportError: If the module is not found.
     """
-    import imp, sys
+    if sys.version_info >= (3, 0):
+        io.error(("Ignoring attempt to import foreign module '{mod}' "
+                  "using python version {major}.{minor}"
+                  .format(mod=name, major=sys.version_info[0],
+                          minor=sys.version_info[1])))
+        return
 
     custom_name = custom_name or name
 
