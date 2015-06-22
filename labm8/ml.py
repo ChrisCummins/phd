b093789a836b01fa892a33fb9231f5f1f177b5f0
Chris Cummins <chrisc.101@gmail.com>
2015-06-22 17:00:06 +0100

    ml: Implement Dataset and Classifier sub-class

--- labm8/ml.py
+++ labm8/ml.py
@@ -19,22 +19,32 @@ from __future__ import division
 
 import atexit
 
+from random import randint
+
 import labm8 as lab
 from labm8 import fs
 from labm8 import io
 from labm8 import math as labmath
 from labm8 import system
 
-#
+
 MODULE_SUPPORTED = system.which("weka") and not lab.is_python3()
+
 
 if MODULE_SUPPORTED:
     import weka
     import weka.core
     import weka.core.jvm as jvm
+    import weka.classifiers
     from weka.classifiers import Classifier as WekaClassifier
+    from weka.core.dataset import Instances as WekaInstances
     from weka.core.converters import Loader as WekaLoader
     from weka.core.converters import Saver as WekaSaver
+    from weka.core.classes import Random as WekaRandom
+else:
+    # Add dummy variables as required.
+    WekaClassifier = object
+    WekaInstances = object
 
 
 class Error(Exception):
@@ -73,7 +83,7 @@ def stop():
 def load(src, loader="weka.core.converters.ArffLoader", **kwargs):
     if not MODULE_SUPPORTED: return
     loader = WekaLoader(classname=loader, **kwargs)
-    return loader.load_file(src)
+    return loader.load_file(fs.path(src))
 
 
 def load_csv(src, **kwargs):
@@ -83,7 +93,7 @@ def load_csv(src, **kwargs):
 def save(data, dst, saver="weka.core.converters.ArffSaver", **kwargs):
     if not MODULE_SUPPORTED: return
     saver = WekaSaver(classname=saver, **kwargs)
-    saver.save_file(data, dst)
+    saver.save_file(data, fs.path(dst))
 
 
 def save_csv(src, **kwargs):
@@ -92,8 +102,8 @@ def save_csv(src, **kwargs):
 
 def load_and_save(src, dst, loader, saver, loader_args={}, saver_args={}):
     if not MODULE_SUPPORTED: return
-    data = load(src, loader, **loader_args)
-    save(data, dst, saver, **saver_args)
+    data = load(src, loader=loader, **loader_args)
+    save(data, dst, saver=saver, **saver_args)
 
 
 def csv2arff(csv_path, arff_path):
@@ -114,42 +124,114 @@ def arff2csv(arff_path, csv_path):
                   "weka.core.converters.CSVSaver")
 
 
-class Classifier(object):
+class Dataset(object):
+    """
+    Dataset object, wrapper around weka.core.dataset.Instances.
+    """
+
+    def __init__(self, instances):
+        self.instances = instances
+
+    def __repr__(self):
+        return self.instances.__repr__()
+
+    def __self__(self):
+        return self.instances.__str__()
+
+    def __len__(self):
+        return self.instances.num_instances
+
+    def folds(self, nfolds=10, seed=None):
+        """
+        Get (training,testing) datasets for cross-validation.
+
+        Arguments:
+
+            nfolds (int, optional): Number of folds. Default value is
+              10.
+            seed (int, optional): Seed value for shuffling
+              dataset. Default value is random int 0 <= x <= 10000.
+
+        Returns:
+
+            list of (Instances,Instances) tuples: Each list element is
+              a pair of (training,testing) datasets, respectively.
+        """
+        seed = seed or randint(0, 10000)
+        rnd = WekaRandom(seed)
+
+        fold_size = labmath.ceil(self.instances.num_instances / nfolds)
+
+        # Shuffle the dataset.
+        instances = WekaInstances.copy_instances(self.instances)
+        instances.randomize(rnd)
+
+        folds = []
+        for i in range(nfolds):
+            offset = i * fold_size
+            testing_end = min(offset + fold_size, instances.num_instances - 1)
+
+            # Calculate dataset indices for testing and training data.
+            testing_range = (offset, testing_end - offset)
+            left_range = (0, offset)
+            right_range = (testing_end, instances.num_instances - testing_end)
+
+            # If there's nothing to test, move on.
+            if testing_range[1] < 1: continue
+
+            # Create testing and training folds.
+            testing = WekaInstances.copy_instances(instances, *testing_range)
+            left = WekaInstances.copy_instances(instances, *left_range)
+            right = WekaInstances.copy_instances(instances, *right_range)
+            training = WekaInstances.append_instances(left, right)
+
+            # Add fold to collection.
+            folds.append((training, testing))
+
+        return folds
+
+    @staticmethod
+    def load(src, loader="weka.core.converters.ArffLoader", **kwargs):
+        instances = load(src, loader=loader, **kwargs)
+        return Dataset(instances)
+
+    @staticmethod
+    def save(dst, saver="weka.core.converters.ArffSaver", **kwargs):
+        save(self.instances, dst, saver=saver, **kwargs)
+
+
+class Classifier(WekaClassifier):
     """
     Object representing a classifier.
     """
-    def __init__(self, classname, data, *args):
-        """
-        """
-        self.data = data
-        # Create the classifier.
-        self.classifier = WekaClassifier(classname=classname, options=args)
-        # Train on data.
-        self.classifier.build_classifier(self.data)
-        # Index of class labels.
-        # TODO: Determine from dataset.
-        self.label_index = self.data.num_attributes - 1
 
     def classify(self, instance):
+        """
+        Classify an instance and return the value.
+        """
         value_index = self.classifier.classify_instance(instance)
         return self.data.attribute(self.label_index).value(value_index)
+
+    def __repr__(self):
+        return " ".join([self.classname,] + self.options)
+
+    def __str__(self):
+        return self.__repr__()
 
 
 class J48(Classifier):
     def __init__(self, *args, **kwargs):
         classname = "weka.classifiers.trees.J48"
-        super(J48, self).__init__(classname, *args, **kwargs)
+        super(J48, self).__init__(classname=classname, *args, **kwargs)
 
 
 class NaiveBayes(Classifier):
     def __init__(self, *args, **kwargs):
         classname = "weka.classifiers.bayes.NaiveBayes"
-        super(NaiveBayes, self).__init__(classname, *args, **kwargs)
+        super(NaiveBayes, self).__init__(classname=classname, *args, **kwargs)
 
 
-def evaluate(classifier, testing):
-    """
-    Return a list of speedups for a J48 classifier trained on
-    "training_data" and tested using "testing_data".
-    """
-    return [classify(classifier, instance, testing) for instance in testing]
+class ZeroR(Classifier):
+    def __init__(self, *args, **kwargs):
+        classname = "weka.classifiers.rules.ZeroR"
+        super(ZeroR, self).__init__(classname=classname, *args, **kwargs)
