350dcf599c7eb014ed8268749d07fead5dc8928e
Chris Cummins <chrisc.101@gmail.com>
2015-07-24 11:12:39 +0100

    training: Reduce picking space

--- src/omnitune/omnitune/skelcl/training.py
+++ src/omnitune/omnitune/skelcl/training.py
@@ -8,10 +8,9 @@ from . import hash_params
 
 
 def random_wg_value(max_wg_size):
-    wg_c = random.randrange(2, max_wg_size, 2)
-    wg_r = random.randrange(2, max_wg_size, 2)
-
-    if wg_c * wg_r > max_wg_size:
-        wg_c = random.randrange(2, max_wg_size, 2)
-        wg_r = random.randrange(2, max_wg_size, 2)
+    wg_c = random.randrange(2, max_wg_size / 2, 2)
+    wg_r = random.randrange(2, max_wg_size / 2, 2)
+    while wg_c * wg_r > max_wg_size:
+        wg_c = random.randrange(2, max_wg_size / 2, 2)
+        wg_r = random.randrange(2, max_wg_size / 2, 2)
     return [wg_c, wg_r]
