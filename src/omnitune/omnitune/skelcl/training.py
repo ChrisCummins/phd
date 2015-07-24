3bb4de1543a47184efd953c41f2622918e612896
Chris Cummins <chrisc.101@gmail.com>
2015-07-24 11:04:43 +0100

    training: Use iterative not recursive picker

--- src/omnitune/omnitune/skelcl/training.py
+++ src/omnitune/omnitune/skelcl/training.py
@@ -12,7 +12,6 @@ def random_wg_value(max_wg_size):
     wg_r = random.randrange(2, max_wg_size, 2)
 
     if wg_c * wg_r > max_wg_size:
-        return random_wg_value(max_wg_size)
+        wg_c = random.randrange(2, max_wg_size, 2)
+        wg_r = random.randrange(2, max_wg_size, 2)
     return [wg_c, wg_r]
-
-
