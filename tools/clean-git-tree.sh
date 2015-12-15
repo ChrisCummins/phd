fd1d316f140fb3e13e8cc283be2bc0859fc1e3a6
Chris Cummins <chrisc.101@gmail.com>
2015-12-15 11:55:37 +0000

    tools: Add a script to clean git tree

---
+++ tools/clean-git-tree.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# clean-git-tree - Remove redundant and dead git branches
+#
+
+set -ex
+
+# Prune remote branches
+git remote prune origin
+
+# Prune local branches
+git branch -r | awk '{print $1}' \
+    | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) \
+    | awk '{print $1}' | xargs git branch -d
