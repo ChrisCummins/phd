d27baba3ef3b0c9c29c993fab16cb231d99fbc03
Chris Cummins <chrisc.101@gmail.com>
2016-01-03 10:34:23 +0000

    learn/challenges: Add flight information test

---
+++ learn/challenges/flight-information.sh
@@ -0,0 +1,28 @@
+# Given a string matrix containing the information of running flights
+# (columns like source, destination, arrival time, departure time
+# ...), use any tools known to you to ..  Extract the info of all
+# flights from source X to destination Y.
+#
+# Extract a summary info of how many flights leave from each source
+# and how many flights arrive at each destination.  Justify your
+# choice for the specific tool / language you would use.
+
+echo "Departures:"
+awk '{print $1;}' < flight-information-test.txt | uniq -c | sort -rn
+echo
+echo "Arrivals:"
+awk '{print $2;}' < flight-information-test.txt | uniq -c | sort -rn
+
+# Justification:
+#
+# Use common UNIX tools. Easy and quick to develop and modify. There's
+# no need to over-engineer. If performance becomes an issue, it's easy
+# to profile.
+#
+# Can be pipeline-parallelised using GNU parallel:
+#
+#   echo
+#   echo "Arrivals:"
+#   awk '{print $2;}' < flight-information-test.txt \
+#       | parallel --pipe uniq -c \
+#       | parallel --pipe sort -rn
