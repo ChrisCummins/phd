122a41ff696dc40ec9e90ee184496346da55bcbe
Chris Cummins <chrisc.101@gmail.com>
2016-02-01 14:56:15 +0000

    lab/stl: Add overview script

---
+++ lab/stl/overview.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+files="$(find include/ustl -type f) tests.cpp benchmarks.cpp"
+
+(echo "FILE LINES TODOs"; \
+ for file in $files; do \
+     echo -n "$file "; \
+     set +e; \
+     wc -l "$file" | awk '{printf "%d ", $1;}'; \
+     grep -c 'TODO:' "$file"; \
+     set -e; \
+ done) | sort -nk 3 | column -t
