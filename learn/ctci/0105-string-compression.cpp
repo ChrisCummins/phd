d5f63709ebfc554a660e16732b19c6ed1fb65725
Chris Cummins <chrisc.101@gmail.com>
2016-02-09 09:23:20 +0000

    learn/ctci: Refactor "1.05 string compression"

--- learn/ctci/0105-string-compression.cpp
+++ learn/ctci/0105-string-compression.cpp
@@ -8,78 +8,76 @@
 #include "./ctci.h"
 
 #include <string>
-#include <iostream>
+#include <ostream>
 
-std::string compressString1(std::string &s) {
-    // Output string.
-    std::ostringstream o;
+//
+// A stateful solution which iterates over the string, storing the
+// current character and the current character count. If the new
+// character does not match the current character, output the
+// character and its count.
+//
+// O(n) time, O(n) space.
+//
+std::string compress_str(const std::string &str) {
+  std::ostringstream oss;
+  char curr_c = '\0';
+  size_t count = 0;
 
-    // Current character, and character count.
-    char c = '\0';
-    int cc = 0;
-
-    for (size_t i = 0; i < s.size(); i++) {
-        if (s[i] == c) {
-            cc++;
-        } else {
-            if (c != '\0')
-                o << c << cc;
-            c = s[i];
-            cc = 1;
-        }
+  for (auto &c : str) {
+    if (c == curr_c) {
+      count++;
+    } else {
+      if (curr_c != '\0')
+        oss << curr_c << count;
+      curr_c = c;
+      count = 1;
     }
+  }
 
-    if (c != '\0')
-        o << c << cc;
+  // Output last character:
+  if (curr_c != '\0')
+    oss << curr_c << count;
 
-    // Return the shortest string:
-    std::string ostr = o.str();
-    return ostr.size() < s.size() ? ostr : s;
+  // Return the shortest string:
+  std::string os = oss.str();
+  return os.size() < str.size() ? os : str;
 }
 
-// Unit tests
 
-TEST(Permutation, compressString1) {
-    auto in1 = std::string("aabcccccaaa");
-    auto out1 = compressString1(in1);
-    ASSERT_EQ(std::string("a2b1c5a3"), out1);
+///////////
+// Tests //
+///////////
 
-    auto in2 = std::string("abcde");
-    auto out2 = compressString1(in2);
-    ASSERT_EQ(std::string("abcde"), out2);
+TEST(Permutation, compress_str) {
+  const auto in1 = std::string("aabcccccaaa");
+  auto out1 = compress_str(in1);
+  ASSERT_EQ(std::string("a2b1c5a3"), out1);
+
+  const auto in2 = std::string("abcde");
+  auto out2 = compress_str(in2);
+  ASSERT_EQ(std::string("abcde"), out2);
 }
 
-// Benchmarks
 
-static unsigned int seed = 0xcec;
-static const size_t lengthMin = 8;
-static const size_t lengthMax = 10 << 10;
+////////////////
+// Benchmarks //
+////////////////
 
-void BM_baseline(benchmark::State& state) {
-    const auto strlen = static_cast<size_t>(state.range_x());
-    std::string t;
+static const size_t BM_length_min = 8;
+static const size_t BM_length_max = 10 << 10;
 
-     while (state.KeepRunning()) {
-         for (size_t i = 0; i < strlen; i++)
-             t += static_cast<char>(rand_r(&seed) % 5);
+void BM_compress_str(benchmark::State& state) {
+  const auto strlen = static_cast<size_t>(state.range_x());
+  std::string t;
 
-         benchmark::DoNotOptimize(t[0]);
-     }
+  while (state.KeepRunning()) {
+    for (size_t i = 0; i < strlen; i++)
+      t += static_cast<char>(arc4random() % 5);
+
+    std::string o = compress_str(t);
+    benchmark::DoNotOptimize(o[0]);
+  }
 }
-BENCHMARK(BM_baseline)->Range(lengthMin, lengthMax);
-
-void BM_compressString1(benchmark::State& state) {
-    const auto strlen = static_cast<size_t>(state.range_x());
-    std::string t;
-
-    while (state.KeepRunning()) {
-        for (size_t i = 0; i < strlen; i++)
-            t += static_cast<char>(rand_r(&seed) % 5);
-
-        std::string o = compressString1(t);
-        benchmark::DoNotOptimize(o[0]);
-    }
-}
-BENCHMARK(BM_compressString1)->Range(lengthMin, lengthMax);
+BENCHMARK(BM_compress_str)->Range(BM_length_min, BM_length_max);
 
 CTCI_MAIN();
