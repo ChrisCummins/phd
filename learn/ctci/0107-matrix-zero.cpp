486bd13010defd1ad411601d72bc971cd09493c9
Chris Cummins <chrisc.101@gmail.com>
2016-02-09 09:38:15 +0000

    learn/ctci: Refactor "1.07 matrix zero"

--- learn/ctci/0107-matrix-zero.cpp
+++ learn/ctci/0107-matrix-zero.cpp
@@ -1,125 +1,116 @@
 /*
- * Write a method to perform basic string compression using the counts
- * of repeated characters. For example, the string aabcccccaaa would
- * become a2b1c5a3. If the "compressed" string would not become
- * smaller than the original string, your method should return the
- * original string.
+ * Write an algorithm such that if an element in an MxN matrix 0, its
+ * entire row and column are set to 0.
  */
 #include "./ctci.h"
 
-#include <set>
+#include <unordered_set>
 
-template<typename Element>
-void matrixZero1(Element *const m, const size_t n) {
-    if (n == 0 || m == nullptr)
-        return;
+//
+// Perform two passes. The first, to record the row and column indices
+// to zero. The second, to perform the actual zeroing.
+//
+// O(n^2) time, O(n) space.
+//
+template<typename T>
+void matrix_zero(T *const m, const size_t rows, const size_t cols) {
+  if (!(rows * cols) || !m) return;
 
-    // Determine which rows and columns to zero.
-    std::set<size_t> rows, cols;
-    for (size_t i = 0; i < n * n; i++) {
-        if (m[i] == 0) {
-            auto x = i % n;
-            auto y = i / n;
+  // Determine which rows and columns to zero.
+  std::unordered_set<size_t> rows_s, cols_s;
+  for (size_t i = 0; i < rows * cols; i++) {
+    if (m[i] == 0) {
+      auto x = i % cols;
+      auto y = i / cols;
 
-            rows.insert(y);
-            cols.insert(x);
-        }
+      rows_s.insert(y);
+      cols_s.insert(x);
     }
+  }
 
-    for (auto row : rows)
-        for (size_t i = row * n; i < (row + 1) * n; i++)
-            m[i] = 0;
+  for (auto &row : rows_s)
+    for (size_t i = row * cols; i < (row + 1) * cols; i++)
+      m[i] = 0;
 
-    for (auto col : cols)
-        for (size_t i = col % n; i < n * n; i += n)
-            m[i] = 0;
+  for (auto &col : cols_s)
+    for (size_t i = col % cols; i < rows * cols; i += cols)
+      m[i] = 0;
 }
 
-// Unit tests
 
-TEST(Permutation, matrixZero1) {
-    int m1[] = {
-        1, 2, 3,
-        4, 0, 6,
-        7, 8, 9
-    };
-    const int m2[] = {
-        1, 0, 3,
-        0, 0, 0,
-        7, 0, 9
-    };
+///////////
+// Tests //
+///////////
 
-    matrixZero1(m1, 3);
-    for (size_t i = 0; i < 9; i++)
-        ASSERT_EQ(m2[i], m1[i]);
+TEST(Permutation, matrix_zero) {
+  int m1[] = {
+    1, 2, 3,
+    4, 0, 6,
+    7, 8, 9
+  };
+  const int m2[] = {
+    1, 0, 3,
+    0, 0, 0,
+    7, 0, 9
+  };
 
-    float m3a[] = {
-        1.0, 2.0, 3.0,
-        4.0, 5.0, 6.0,
-        7.0, 8.0, 9.0
-    };
-    const float m3[] = {
-        1.0, 2.0, 3.0,
-        4.0, 5.0, 6.0,
-        7.0, 8.0, 9.0
-    };
+  matrix_zero(m1, 3, 3);
+  for (size_t i = 0; i < 9; i++)
+    ASSERT_EQ(m2[i], m1[i]);
 
-    matrixZero1(m3a, 3);
-    for (size_t i = 0; i < 9; i++)
-        ASSERT_EQ(m3a[i], m3[i]);
+  float m3a[] = {
+    1.0, 2.0, 3.0,
+    4.0, 5.0, 6.0,
+    7.0, 8.0, 9.0
+  };
+  const float m3[] = {
+    1.0, 2.0, 3.0,
+    4.0, 5.0, 6.0,
+    7.0, 8.0, 9.0
+  };
 
-    int m4[] = {
-         0,  2,  3,  4,
-         5,  6,  7,  8,
-         9, 10, 11, 12,
-        13, 14, 15, 16
-    };
-    const int m5[] = {
-         0,  0,  0,  0,
-         0,  6,  7,  8,
-         0, 10, 11, 12,
-         0, 14, 15, 16
-    };
+  matrix_zero(m3a, 3, 3);
+  for (size_t i = 0; i < 9; i++)
+    ASSERT_EQ(m3a[i], m3[i]);
 
-    matrixZero1(m4, 4);
-    for (size_t i = 0; i < 16; i++)
-        ASSERT_EQ(m4[i], m5[i]);
+  int m4[] = {
+    0,  2,  3,  4,
+    5,  6,  7,  8,
+    9, 10, 11, 12,
+  };
+  const int m5[] = {
+    0,  0,  0,  0,
+    0,  6,  7,  8,
+    0, 10, 11, 12,
+  };
+
+  matrix_zero(m4, 3, 4);
+  for (size_t i = 0; i < 12; i++)
+    ASSERT_EQ(m4[i], m5[i]);
 }
 
-// Benchmarks
 
-static const size_t lengthMin = 8;
-static const size_t lengthMax = 10 << 10;
+////////////////
+// Benchmarks //
+////////////////
 
-void BM_baseline(benchmark::State& state) {
-    const auto n = static_cast<size_t>(state.range_x());
-    int *m = new int[n];
+static const size_t BM_length_min = 8;
+static const size_t BM_length_max = 10 << 10;
 
-    while (state.KeepRunning()) {
-        for (size_t i = 0; i < n; i++)
-            m[i] = static_cast<int>(arc4random());
+void BM_matrix_zero(benchmark::State& state) {
+  const auto n = static_cast<size_t>(state.range_x());
+  int *m = new int[n];
 
-        benchmark::DoNotOptimize(*m);
-    }
+  while (state.KeepRunning()) {
+    for (size_t i = 0; i < n * n; i++)
+      m[i] = static_cast<int>(arc4random() % 25);
 
-    delete[] m;
+    matrix_zero(m, n, n);
+    benchmark::DoNotOptimize(m[0]);
+  }
+
+  delete[] m;
 }
-BENCHMARK(BM_baseline)->Range(lengthMin, lengthMax);
-
-void BM_matrixZero1(benchmark::State& state) {
-    const auto n = static_cast<size_t>(state.range_x());
-    int *m = new int[n];
-
-    while (state.KeepRunning()) {
-        for (size_t i = 0; i < n; i++)
-            m[i] = static_cast<int>(arc4random());
-
-        matrixZero1(m, n);
-        benchmark::DoNotOptimize(*m);
-    }
-
-    delete[] m;
-}
-BENCHMARK(BM_matrixZero1)->Range(lengthMin, lengthMax);
+BENCHMARK(BM_matrix_zero)->Range(BM_length_min, BM_length_max);
 
 CTCI_MAIN();
