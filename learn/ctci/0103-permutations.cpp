9a01d15c24681b4c38188888ee9df82f75691bef
Chris Cummins <chrisc.101@gmail.com>
2016-02-09 08:51:23 +0000

    learn/ctci: Refactor "1.03 permutations"

--- learn/ctci/0103-permutations.cpp
+++ learn/ctci/0103-permutations.cpp
@@ -4,80 +4,68 @@
  */
 #include "./ctci.h"
 
-#include <cmath>
-#include <cstdlib>
-#include <limits>
 #include <array>
-#include <iostream>
-#include <unordered_map>
+#include <limits>
 #include <string>
 
-bool isPermutation1(std::string &a, std::string &b) {
-    // Optimization - permutations are the same length.
-    if (a.size() != b.size())
-        return false;
+//
+// First solution. Compare frequency counts for characters in the
+// string.
+//
+// O(n) time, O(1) space. Best case O(1) time and space.
+//
+bool is_permutation(const std::string &a, const std::string &b) {
+  if (a.size() != b.size())
+    return false;  // Permutations must be the same length.
 
-    std::array<int, std::numeric_limits<char>::max()> af = {}, bf = {};
+  // Create char-frequency tables:
+  std::array<size_t, std::numeric_limits<char>::max()> a_freq = {0};
+  std::array<size_t, std::numeric_limits<char>::max()> b_freq = {0};
 
-    // Create char-frequency tables:
-    for (auto &c : a)
-        af[static_cast<size_t>(c)] += 1;
-    for (auto &c : b)
-        bf[static_cast<size_t>(c)] += 1;
+  for (auto &c : a)
+    a_freq[static_cast<size_t>(c)] += 1;
+  for (auto &c : b)
+    b_freq[static_cast<size_t>(c)] += 1;
 
-    // Check char-frequency equivalence.
-    for (size_t i = 0; i < af.size(); i++) {
-        if (af[i] != bf[i])
-            return false;
-    }
-
-    return true;
-}
-
-// Unit tests
-
-TEST(Permutation, isPermutation1) {
-    std::string a1("abcde"), a2("efgh");
-    std::string b1("abcde"), b2("edcba");
-
-    ASSERT_FALSE(isPermutation1(a1, a2));
-    ASSERT_TRUE(isPermutation1(b1, b2));
+  // Strings are permutations if char-frequency tables match:
+  return a_freq == b_freq;
 }
 
 
-static const size_t lengthMin = 8;
-static const size_t lengthMax = 10 << 10;
+///////////
+// Tests //
+///////////
 
-void BM_baseline(benchmark::State& state) {
-    auto len = static_cast<size_t>(state.range_x());
-    std::string t1(len, 'a'), t2(len, 'a');
+TEST(Permutations, is_permutation) {
+  const std::string a1("abcde"), a2("efgh");
+  const std::string b1("abcde"), b2("edcba");
 
-    while (state.KeepRunning()) {
-        for (auto &c : t1)
-            c = arc4random() % std::numeric_limits<char>::max();
-        for (auto &c : t2)
-            c = arc4random() % std::numeric_limits<char>::max();
-
-        benchmark::DoNotOptimize(t1.data());
-        benchmark::DoNotOptimize(t2.data());
-    }
+  ASSERT_FALSE(is_permutation(a1, a2));
+  ASSERT_TRUE(is_permutation(b1, b2));
 }
-BENCHMARK(BM_baseline)->Range(lengthMin, lengthMax);
 
-void BM_isPermutation1(benchmark::State& state) {
-    auto len = static_cast<size_t>(state.range_x());
-    std::string t1(len, 'a'), t2(len, 'a');
 
-    while (state.KeepRunning()) {
-        for (auto &c : t1)
-            c = arc4random() % std::numeric_limits<char>::max();
-        for (auto &c : t2)
-            c = arc4random() % std::numeric_limits<char>::max();
+////////////////
+// Benchmarks //
+////////////////
 
-        auto ret = isPermutation1(t1, t2);
-        benchmark::DoNotOptimize(ret);
-    }
+static const size_t BM_length_min = 8;
+static const size_t BM_length_max = 10 << 10;
+
+void BM_is_permutation(benchmark::State& state) {
+  auto len = static_cast<size_t>(state.range_x());
+  std::string t1(len, 'a'), t2(len, 'a');
+
+  while (state.KeepRunning()) {
+    for (auto &c : t1)
+      c = arc4random() % std::numeric_limits<char>::max();
+    for (auto &c : t2)
+      c = arc4random() % std::numeric_limits<char>::max();
+
+    auto ret = is_permutation(t1, t2);
+    benchmark::DoNotOptimize(ret);
+  }
 }
-BENCHMARK(BM_isPermutation1)->Range(lengthMin, lengthMax);
+BENCHMARK(BM_is_permutation)->Range(BM_length_min, BM_length_max);
 
 CTCI_MAIN();
