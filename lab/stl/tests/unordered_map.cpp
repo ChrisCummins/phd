eae2e830743a898cbe3030b8fba8dc6391328ae1
Chris Cummins <chrisc.101@gmail.com>
2016-02-09 16:24:49 +0000

    lab/stl: unordered_map implementation work

--- lab/stl/tests/unordered_map.cpp
+++ lab/stl/tests/unordered_map.cpp
@@ -45,36 +45,34 @@ TEST(std_unordered_map, constructors) {
 }
 
 
-// TEST(ustl_unordered_map, constructors) {
-//   // Empty:
-//   ustl::unordered_map<int, int> e1;
-//   ustl::unordered_map<int, int> e2(10);
+TEST(ustl_unordered_map, constructors) {
+  // Empty:
+  ustl::unordered_map<int, int> e1;
+  ASSERT_EQ(size_t(0), e1.size());
+  ASSERT_TRUE(e1.empty());
 
-//   ASSERT_EQ(size_t(0), e1.size());
-//   ASSERT_TRUE(e1.empty());
+  ustl::unordered_map<int, int> e2(10);
+  ASSERT_EQ(size_t(0), e2.size());
+  ASSERT_TRUE(e2.empty());
 
-//   ASSERT_EQ(size_t(0), e2.size());
-//   ASSERT_TRUE(e2.empty());
+  // // Range:
+  // ustl::vector<std::pair<int, int>> in1{{1, 2}, {2, 3}, {3, 4}};
+  // ustl::unordered_map<int, int> ra1(in1.begin(), in1.end());
+  // ASSERT_EQ(size_t(3), ra1.size());
+  // for (auto& v : ra1)
+  //   ASSERT_TRUE(v.first == v.second - 1);
 
-//   // Range:
-//   ustl::vector<std::pair<int, int>> in1{{1, 2}, {2, 3}, {3, 4}};
-//   ustl::unordered_map<int, int> ra1(in1.begin(), in1.end());
+  // Initialiazer list:
+  ustl::unordered_map<int, int> il1({{1, 2}, {2, 3}, {3, 4}});
 
-//   ASSERT_EQ(size_t(3), ra1.size());
-//   for (auto& v : ra1)
-//     ASSERT_TRUE(v.first == v.second - 1);
+  // ASSERT_EQ(size_t(3), il1.size());
+  // for (auto& v : il1)
+  //   ASSERT_TRUE(v.first == v.second - 1);
 
-//   // Initialiazer list:
-//   ustl::unordered_map<int, int> il1({{1, 2}, {2, 3}, {3, 4}});
+  // // Copy:
+  // auto cp1 = il1;
 
-//   ASSERT_EQ(size_t(3), il1.size());
-//   for (auto& v : il1)
-//     ASSERT_TRUE(v.first == v.second - 1);
-
-//   // Copy:
-//   auto cp1 = il1;
-
-//   ASSERT_EQ(size_t(3), cp1.size());
-//   for (auto& v : cp1)
-//     ASSERT_TRUE(v.first == v.second - 1);
-// }
+  // ASSERT_EQ(size_t(3), cp1.size());
+  // for (auto& v : cp1)
+  //   ASSERT_TRUE(v.first == v.second - 1);
+}
