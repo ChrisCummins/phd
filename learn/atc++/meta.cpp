6a49906c23d06c0c036e6d2f695f4494cdd4554e
Chris Cummins <chrisc.101@gmail.com>
2016-03-15 22:27:02 +0000

    learn/atc++: Add meta programming examples

---
+++ learn/atc++/meta.cpp
@@ -0,0 +1,38 @@
+// is_integral:
+
+template<typename T>
+struct is_integral {
+  static const bool value = false;
+};
+
+template<>
+struct is_integral<int> {
+  static const bool value = true;
+};
+
+// enable_if:
+
+template<bool B, typename T = void>
+struct enable_if {};
+
+template<typename T>
+struct enable_if<true, T> { using type = T; };
+
+// test:
+
+template<typename T,
+         typename enable_if<is_integral<T>::value>::type* = nullptr>
+int foobar(const T& val) {
+  return 0;
+}
+
+
+int main() {
+  static_assert(is_integral<int>::value);
+  static_assert(is_integral<float>::value == false);
+
+  foobar(3);
+  foobar(-1);
+
+  // Return statement is optional in C++
+}
