82face3bae33380c8f956b9e206916502e6e9df5
Chris Cummins <chrisc.101@gmail.com>
2016-04-07 18:32:37 +0100

    lab/ml: Include forks in queries

--- lab/ml/fetch.py
+++ lab/ml/fetch.py
@@ -195,7 +195,7 @@ def main():
     #
     queries = ['opencl', 'cl', 'khronos', 'gpu']
     for query in queries:
-        repos = g.search_repositories('cl')
+        repos = g.search_repositories('cl fork:true')
 
         for repo in repos:
             repo_modified = handle_repo(repo)
