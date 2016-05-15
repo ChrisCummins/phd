fa18a253c49de5da83b3831c015249dba1bb779a
Chris Cummins <chrisc.101@gmail.com>
2016-05-15 16:36:05 +0100

    lab/ml: train.py only dumps training corpus

--- lab/ml/train.py
+++ lab/ml/train.py
@@ -16,37 +16,13 @@ import os
 import sqlite3
 
 from argparse import ArgumentParser
-from collections import defaultdict
 
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
-
-from pymarkovchain import MarkovChain
-
-markov_db = 'markov.db'
-dst = 'gen.txt'
-
-num_lines = 10000
-
-def _db_factory():
-    return defaultdict(_one_dict)
-
-def _one():
-    return 1.0
-
-def _one_dict():
-    return defaultdict(_one)
-
-
-def dump_training_data(db):
-    print('dump training data ...')
-    out_path = 'input.txt'
+def create_corpus(db, out_path):
+    # Dump all of the rewritten OpenCL files, ordered by the number of
+    # stargazers of the file repo.
+    print('creating training corpus', out_path, '...')
 
     c = db.cursor()
-    # Get all of the rewritten OpenCL files, ordered by the number of
-    # stars in the containing repo.
     c.execute('SELECT PreprocessedFiles.contents FROM PreprocessedFiles '
               'LEFT JOIN ContentMeta ON PreprocessedFiles.id=ContentMeta.id '
               'LEFT JOIN Repositories ON ContentMeta.repo_url=Repositories.url '
@@ -59,8 +35,6 @@ def dump_training_data(db):
             out.write(contents)
             out.write('\n')
 
-    return out_path
-
 
 def main():
     parser = ArgumentParser()
@@ -70,26 +44,7 @@ def main():
     db_path = args.input
 
     db = sqlite3.connect(db_path)
-    data_path = dump_training_data(db)
-
-    # Hack to get MarkovChain to run:
-    if not os.path.exists(markov_db):
-        with open(markov_db, 'wb') as out:
-            pickle.dump(_db_factory(), out)
-
-    mc = MarkovChain(markov_db)
-
-    print('training model ...')
-    with open(data_path, 'r') as infile:
-        mc.generateDatabase(infile.read())
-
-    # To let the markov chain generate some text, execute:
-    print('generating text ...')
-    with open(dst, 'w') as out:
-        i = 0
-        while i < num_lines:
-            out.write(mc.generateString() + '\n')
-            i += 1
+    create_corpus(db, 'input.txt')
 
 
 if __name__ == '__main__':
