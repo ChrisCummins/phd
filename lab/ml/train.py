c2d90db3ec708d30b7e68e40fc211e77a92f1525
Chris Cummins <chrisc.101@gmail.com>
2016-05-16 10:06:24 +0100

    lab/ml: Add output options to train.py

--- lab/ml/train.py
+++ lab/ml/train.py
@@ -17,34 +17,80 @@ import sqlite3
 
 from argparse import ArgumentParser
 
-def create_corpus(db, out_path):
-    # Dump all of the rewritten OpenCL files, ordered by the number of
-    # stargazers of the file repo.
-    print('creating training corpus', out_path, '...')
+
+def table_exists(db, table_name):
+    c = db.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='"
+              + table_name + "'")
+    res = c.fetchone()
+    c.close()
+    return res and res[0]
+
+
+def create_corpus(db, out_path, gh=False, fileid=False, reverse=False,
+                  status=0):
+    # Dump all the preprocessed OpenCL files
+    print('creating DNN corpus', out_path, '...')
+
+    order = 'ASC' if reverse else 'DESC'
 
     c = db.cursor()
-    c.execute('SELECT PreprocessedFiles.contents FROM PreprocessedFiles '
-              'LEFT JOIN ContentMeta ON PreprocessedFiles.id=ContentMeta.id '
-              'LEFT JOIN Repositories ON ContentMeta.repo_url=Repositories.url '
-              'WHERE PreprocessedFiles.status=0 ORDER BY Repositories.stars DESC')
-    query = c.fetchall()
+    if gh:
+        print('ordering by number of GitHub stargazers')
+        c.execute('SELECT PreprocessedFiles.id,PreprocessedFiles.contents FROM PreprocessedFiles '
+                  'LEFT JOIN ContentMeta ON PreprocessedFiles.id=ContentMeta.id '
+                  'LEFT JOIN Repositories ON ContentMeta.repo_url=Repositories.url '
+                  'WHERE PreprocessedFiles.status=' + str(status) + ' '
+                  'ORDER BY Repositories.stars '
+                  + order)
+    else:
+        print('ordering by line count')
+        c.execute('SELECT id,contents FROM PreprocessedFiles '
+                  'WHERE PreprocessedFiles.status=' + str(status) + ' '
+                  'ORDER BY LC(contents) '
+                  + order)
+    rows = c.fetchall()
 
     with open(out_path, 'w') as out:
-        for row in query:
-            contents = row[0]
+        for row in rows:
+            id,contents = row
+            if fileid:
+                out.write('// ID: ' + id + '\n\n')
             out.write(contents)
-            out.write('\n')
+            out.write('\n\n')
+
+
+def linecount(t):
+    return len(t.split('\n'))
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('input', help='path to SQL input dataset')
+    parser.add_argument('output', help='path to output file')
+    parser.add_argument('-i', action='store_true', default=False,
+                        help='include file separators')
+    parser.add_argument('-r', action='store_true', default=False,
+                        help='use reverse order')
+    parser.add_argument('-s', '--status', type=int, default=0,
+                        help='status code to use')
     args = parser.parse_args()
 
     db_path = args.input
+    out_path = args.output
+    opts = {
+        "fileid": args.i,
+        "reverse": args.r,
+        "status": args.status
+    }
 
     db = sqlite3.connect(db_path)
-    create_corpus(db, 'input.txt')
+    db.create_function("LC", 1, linecount)
+
+    # auto-detect whether it's a GitHub repo
+    opts['gh'] = table_exists(db, 'Repositories')
+
+    create_corpus(db, out_path, **opts)
 
 
 if __name__ == '__main__':
