03ee03801fddd543304d56308166a6594eb9e607
Chris Cummins <chrisc.101@gmail.com>
2016-06-01 14:45:47 +0100

    tools/parse_texcount: Remove dependency on labm8

--- tools/parse_texcount.py
+++ tools/parse_texcount.py
@@ -61,6 +61,82 @@ _FILE_RE = compile("^File: (.+)")
 _WORDS_IN_TEXT_RE = compile("^Words in text: (\d+)")
 
 
+def table(rows, columns=None, output=None, data_args={}, **kwargs):
+    """
+    Return a formatted string of "list of list" table data.
+
+    See: http://pandas.pydata.org/pandas-docs/dev/generated/pandas.DataFrame.html
+
+    Examples:
+
+        >>> fmt.print([("foo", 1), ("bar", 2)])
+             0  1
+        0  foo  1
+        1  bar  2
+
+        >>> fmt.print([("foo", 1), ("bar", 2)], columns=("type", "value"))
+          type  value
+        0  foo      1
+        1  bar      2
+
+    Arguments:
+
+        rows (list of list): Data to format, one row per element,
+          multiple columns per row.
+        columns (list of str, optional): Column names.
+        output (str, optional): Path to output file.
+        str_args (dict, optional): Any additional kwargs to pass to
+          pandas.DataFrame constructor.
+        **kwargs: Any additional arguments to pass to
+          pandas.DataFrame.to_string().
+
+    Returns:
+
+        str: Formatted data as table.
+
+    Raises:
+
+        Exception: If number of columns (if provided) does not equal
+          number of columns in rows; or if number of columns is not
+          consistent across all rows.
+    """
+    import pandas
+
+    # Number of columns.
+    num_columns = len(rows[0])
+
+    # Check that each row is the same length.
+    for i,row in enumerate(rows[1:]):
+        if len(row) != num_columns:
+            raise Exception("Number of columns in row {i_row} ({c_row}) does "
+                            "not match number of columns in row 0 ({z_row})"
+                            .format(i_row=i, c_row=len(row), z_row=num_columns))
+
+    if columns is None:
+        # Default parameters.
+        if "header" not in kwargs:
+            kwargs["header"] = False
+    elif len(columns) != num_columns:
+        # Check that number of columns matches number of columns in
+        # rows.
+        raise Exception("Number of columns in header ({c_header}) does not "
+                        "match the number of columns in the data ({c_rows})"
+                        .format(c_header=len(columns), c_rows=num_columns))
+
+    # Default arguments.
+    if "index" not in kwargs:
+        kwargs["index"] = False
+
+    data_args["columns"] = columns
+
+    string = pandas.DataFrame(list(rows), **data_args).to_string(**kwargs)
+    if output is None:
+        return string
+    else:
+        print(string, file=open(output, "w"))
+        io.info("Wrote", output)
+
+
 def parse_wc(input, output=stdout, match_pattern=None):
     match_re = compile(match_pattern) if match_pattern else None
 
@@ -91,7 +167,7 @@ def parse_wc(input, output=stdout, match_pattern=None):
     files.append(("Total", total))
 
     # Print word counts.
-    print(fmt.table(files, justify="left"))
+    print(table(files, justify="left"))
 
 
 def main():
