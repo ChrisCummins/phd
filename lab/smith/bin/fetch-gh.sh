9b468ff4a04374f1999c3ef24f7f167b5aad165a
Chris Cummins <chrisc.101@gmail.com>
2016-07-11 18:31:18 +0200

    src/smith: Add fetch script

---
+++ lab/smith/bin/fetch-gh.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Fetch from Github
+#
+# Requires env variables: $GITHUB_TOKEN $GITHUB_USERNAME $GITHUB_PW
+#
+main() {
+    local db_path="$1"
+
+    local i=0
+
+    while true; do
+        echo "*** Running fetch, iteration $i..."
+
+        fetch-gh "$db_path"
+
+        i=$((i+1))
+    done
+}
+main $@
