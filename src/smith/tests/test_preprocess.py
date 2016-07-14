9c4e41600419e7e13dce9b42a97cca227b635be3
Chris Cummins <chrisc.101@gmail.com>
2016-07-14 16:09:08 +0200

    src/smith: Add preprocessor test

---
+++ src/smith/tests/test_preprocess.py
@@ -0,0 +1,59 @@
+from unittest import TestCase
+import tests
+
+import sys
+import os
+
+import smith
+from smith import preprocess
+from smith import config
+
+# If true, write gold standard files.
+UPDATE_GS_FILES = True
+
+def preprocess_pair(basename, preprocessor=preprocess.preprocess):
+    gs_path = tests.data_path(os.path.join('cl', str(basename) + '.gs'),
+                              exists=not UPDATE_GS_FILES)
+    tin_path = tests.data_path(os.path.join('cl', str(basename) + '.cl'))
+
+    # Run preprocess
+    tin = tests.data_str(tin_path)
+    tout = preprocessor(tin)
+
+    if UPDATE_GS_FILES:
+        gs = tout
+        with open(gs_path, 'w') as outfile:
+            outfile.write(gs)
+            print("\n-> updated gold standard file '{}' ..."
+                  .format(gs_path), file=sys.stderr, end=' ')
+    else:
+        gs = tests.data_str(gs_path)
+
+    return (gs, tout)
+
+
+class TestPreprocess(TestCase):
+    def test_preprocess(self):
+        self.assertEqual(*preprocess_pair('sample-1'))
+
+    def test_strip_attributes(self):
+        self.assertEqual("", preprocess.strip_attributes(
+            "__attribute__((reqd_work_group_size(64,1,1)))"))
+
+        out = "foobar"
+        tin = "foo__attribute__((reqd_work_group_size(WG_SIZE,1,1)))bar"
+        self.assertEqual(out, preprocess.strip_attributes(tin))
+
+        out = "typedef  unsigned char uchar8;"
+        tin = "typedef __attribute__((ext_vector_type(8))) unsigned char uchar8;"
+        self.assertEqual(out, preprocess.strip_attributes(tin))
+
+        out = ("typedef  unsigned char uchar8;\n"
+               "typedef  unsigned char uchar8;")
+        tin = ("typedef __attribute__  ((ext_vector_type(8))) unsigned char uchar8;\n"
+               "typedef __attribute__((reqd_work_group_size(64,1,1))) unsigned char uchar8;")
+        self.assertEqual(out, preprocess.strip_attributes(tin))
+
+
+if __name__ == '__main__':
+    main()
