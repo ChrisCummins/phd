e16979f62d8efe48b04d4def3e94f42655a806fd
Chris Cummins <chrisc.101@gmail.com>
2016-07-28 14:41:17 +0100

    src/smith: Driver work

--- src/smith/smith/drive.py
+++ src/smith/smith/drive.py
@@ -2,46 +2,70 @@ import numpy as np
 import pyopencl as cl
 import os
 
+
 def build_program(ctx, src):
     return cl.Program(ctx, src).build()
 
 
-def get_arg_prop(kernel, idx, prop):
+def get_arg_prop(kernel, idx, prop, prop2str):
     name = cl.kernel_arg_info.to_string(prop)
     try:
-        val = kernel.get_arg_info(idx, prop)
+        val = prop2str(kernel.get_arg_info(idx, prop))
     except RuntimeError as e:
         val = 'FAIL'
-    print('    {}: {}'.format(name, val))
     return (name, val)
 
 
-def get_arg(kernel, idx):
-    print("ARG", idx)
+def get_arg_props(kernel, idx):
     props = [
-        cl.kernel_arg_info.ACCESS_QUALIFIER,
-        cl.kernel_arg_info.ADDRESS_QUALIFIER,
-        cl.kernel_arg_info.NAME,
-        cl.kernel_arg_info.TYPE_NAME,
-        cl.kernel_arg_info.TYPE_QUALIFIER
+        (cl.kernel_arg_info.ACCESS_QUALIFIER,
+         cl.kernel_arg_access_qualifier.to_string),
+        (cl.kernel_arg_info.ADDRESS_QUALIFIER,
+         cl.kernel_arg_address_qualifier.to_string),
+        (cl.kernel_arg_info.NAME, str),
+        (cl.kernel_arg_info.TYPE_NAME, str),
+        (cl.kernel_arg_info.TYPE_QUALIFIER,
+         cl.kernel_arg_type_qualifier.to_string)
     ]
-    return dict(get_arg_prop(kernel, idx, x) for x in props)
+    return dict(get_arg_prop(kernel, idx, *x) for x in props)
 
 
-def get_args(kernel):
+def get_args_props(kernel):
     nargs = kernel.get_info(cl.kernel_info.NUM_ARGS)
-    return [get_arg(kernel, i) for i in range(nargs)]
+    return [get_arg_props(kernel, i) for i in range(nargs)]
+
+
+def args_from_kernel(kernel):
+    return get_args_props(kernel)
+
+
+def is_pointer(props):
+    return props['TYPE_NAME'].endswith('*')
+
+def placeholder_from_props(props):
+    if is_pointer(props):
+        print('pointer')
+    else:
+        print('value')
+
+    return props
 
 
 def source(src, devtype=cl.device_type.GPU, quiet=False):
-    print("Hello, world!")
     a_np = np.random.rand(50000).astype(np.float32)
     b_np = np.random.rand(50000).astype(np.float32)
 
     if not quiet:
         os.environ['PYOPENCL_COMPILER_OUTPUT'] = "1"
 
-    ctx = cl.Context(dev_type=devtype)
+    platforms = cl.get_platforms()
+    try:
+        ctx = cl.Context(
+            dev_type=devtype,
+            properties=[(cl.context_properties.PLATFORM, platforms[0])])
+    except Exception as e:
+        ctx = cl.create_some_context(interactive=False)
+    print("Device:", ctx.get_info(cl.context_info.DEVICES))
     queue = cl.CommandQueue(ctx)
 
     mf = cl.mem_flags
@@ -52,16 +76,13 @@ def source(src, devtype=cl.device_type.GPU, quiet=False):
     program = build_program(ctx, src)
 
     for kernel in program.all_kernels():
-        get_args(kernel)
+        name = kernel.get_info(cl.kernel_info.FUNCTION_NAME)
+        print("Kernel:", name)
+        args = [placeholder_from_props(x) for x in args_from_kernel(kernel)]
+        for arg in args:
+            print("Argument:", arg)
 
-    # program = build_program(ctx, """
-# __kernel void sum(
-#     __global const float *a, __global const float *b_g, __global float *res_g)
-# {
-#   int gid = get_global_id(0);
-#   res_g[gid] = a[gid] + b_g[gid];
-# }
-# """)
+
 
 #     kernels = program.all_kernels()
 #     kernel = kernels[0]
