cc2ccce700e42f92bbe91f58894fbc3e69cbb232
Chris Cummins <chrisc.101@gmail.com>
2016-09-01 22:45:41 +0100

    src/smith: Bump release version

--- src/smith/setup.py
+++ src/smith/setup.py
@@ -11,7 +11,7 @@ deps = [
 ]
 
 setup(name='smith',
-      version='0.0.1',
+      version='0.0.2',
       description='',
       url='https://github.com/ChrisCummins/phd',
       author='Chris Cummins',
