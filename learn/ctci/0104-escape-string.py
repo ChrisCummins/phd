faaf535f357ec995e6699a7a4b12b68d646162f3
Chris Cummins <chrisc.101@gmail.com>
2016-09-18 21:41:07 +0100

    learn/ctci: Import python solutions from cstp
    Source: https://github.com/ChrisCummins/cstp

---
+++ learn/ctci/0104-escape-string.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+
+# Exercise 1.4:
+#
+#     Write a method to replace all spaces in a string with '%20'. You
+#     may assume that the string has sufficient space at the end of
+#     the string to hold the additional characters, and that you given
+#     the "true" length of the string.
+#
+# First off, let's get the obvious way over and done with:
+def escape_spaces_regexp(string, strlen):
+    return string.replace(' ', '%20')
+
+# Of course, this misses the purpose of the question by operating on a
+# string, not a character array. Implementing a proper character array
+# solution requires two passes, and operates in O(n) time, with O(1)
+# space complexity (it operates in place):
+def escape_spaces(string, strlen):
+    # The first pass is to ascertain the number of ' ' characters
+    # which need escaping, which can then be used to calculate the new
+    # length of the escaped string.
+    spaces_count = 0
+    for c in list(string[:strlen]):
+        if c == ' ':
+            spaces_count += 1
+
+    new_strlen = strlen + 2 * spaces_count
+    # Now that we know the new string length, we work from front to
+    # back, copying original string characters into their new
+    # positions. If we come across a ' ' character, it is replaced
+    # with the padded equivalent.
+    #
+    # We can make a cheeky optimisation because we know that if the
+    # escaped string length and the original string length are equal,
+    # then there are no characters which need escaping, so we don't
+    # need to do anything.
+    if new_strlen != strlen:
+        for i in range(strlen - 1, -1, -1):
+            new_strlen -= 1
+            if string[i] == ' ':
+                string[new_strlen - 2] = '%'
+                string[new_strlen - 1] = '2'
+                string[new_strlen] = '0'
+                new_strlen -= 2
+            else:
+                string[new_strlen] = string[i]
+
+    return string
+
+if __name__ == "__main__":
+    assert escape_spaces_regexp("Hello, the World!", 17) == "Hello,%20the%20World!"
+    assert (''.join(escape_spaces(list("Hello, the World!        "), 17)) ==
+            "Hello,%20the%20World!    ")
---
+++ learn/ctci/0201-linked-list-remove-dups.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2.7
+#
+## Linked Lists:
+#
+# Solutions for the code questions posed in Cracking the Code
+# Interview, Chapter 2, page 77.
+#
+from sets import Set
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+    def __str__(self):
+        x = self
+        s = ""
+        while x:
+            s += str(x.key) + " "
+            x = x.next
+        return s[:-1]
+
+# Exercise 2.1:
+#
+#     Write code to remove duplicates from an unsorted linked list.
+#
+# The implemented solution uses a runner pointer (x) to traverse every
+# node after the current, removing duplicates where found. When this
+# is complete, the function recurses to the next node and
+# completes. This means it operates in O(n^2) time, although it only
+# requires O(1) space.
+#
+def remove_duplicates_no_buffer(head):
+    prev, x = head, head.next
+
+    while x:
+        if x.key == head.key:
+            prev.next = x.next  # Delete node
+
+        prev, x = x, x.next
+
+    if head.next:
+        remove_duplicates_no_buffer(head.next)
+#
+# A O(1) time solution is to iterate through the list and add each
+# node's key into a set. If the key already exists within the set,
+# then it is a duplicate and the node may be removed. This operates in
+# O(n) space.
+#
+def remove_duplicates(head):
+    values = Set()
+
+    prev, x = None, head
+    while x:
+        if x.key in values:     # Check if node exists in set
+            prev.next = x.next  # Delete node
+        else:
+            values.add(x.key)
+        prev, x = x, x.next
+
+
+if __name__ == "__main__":
+    # Exercise 2.1
+    a, b, c, d, e = Node(1), Node(2), Node(3), Node(2), Node(4)
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
+
+    assert str(a) == "1 2 3 2 4"
+    remove_duplicates_no_buffer(a)
+    assert str(a) == "1 2 3 4"
+
+    e.next = Node(2)
+    assert str(a) == "1 2 3 4 2"
+    remove_duplicates(a)
+    assert str(a) == "1 2 3 4"
