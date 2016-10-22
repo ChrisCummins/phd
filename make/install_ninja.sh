b182d69471acc51e0a6ef492bde9a8032e24d421
Chris Cummins <chrisc.101@gmail.com>
2016-10-23 00:02:06 +0100

    Print path to ninja

--- make/install_ninja.sh
+++ make/install_ninja.sh
@@ -4,3 +4,6 @@ set -eux
 wget https://github.com/ninja-build/ninja/releases/download/v1.7.1/ninja-linux.zip
 unzip ninja-linux.zip
 mv ninja /usr/bin
+
+ninja_path=$(which ninja)
+echo "path to ninja: $ninja_path"
