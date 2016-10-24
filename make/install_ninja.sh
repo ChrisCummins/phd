de12c8af720c7f2f11a56451bcd4250cd7e38cb3
Chris Cummins <chrisc.101@gmail.com>
2016-10-24 22:35:33 +0100

    Remove unused build file

--- make/install_ninja.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-mkdir -p ninja
-cd ninja
-wget https://github.com/ninja-build/ninja/archive/v1.7.1.tar.gz -O ninja.tar.gz
-tar -xf ninja.tar.gz --strip-components=1
-./configure.py --bootstrap
-mv ninja /usr/bin
