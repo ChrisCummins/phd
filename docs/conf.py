d348a3e091daa70c715ee42489feac90c8cd8b60
Chris Cummins <chrisc.101@gmail.com>
2016-10-27 13:52:04 +0100

    Add markdown support to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+from recommonmark.parser import CommonMarkParser
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -43,8 +44,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_parsers = {'.md': CommonMarkParser}
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #
