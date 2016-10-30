714688aca072fc5f2d17d61294af8d0902ebee5e
Chris Cummins <chrisc.101@gmail.com>
2016-10-30 17:38:51 +0000

    Release 0.0.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 
 setup(name="labm8",
-      version="0.0.5",
+      version="0.0.6",
       description="Utils for manipulating quantitative experimental data",
       url="https://github.com/ChrisCummins/labm8",
       author="Chris Cummins",
