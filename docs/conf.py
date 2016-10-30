1df84dcb257c27a19c33a93a7892c62ecbaf7069
Chris Cummins <chrisc.101@gmail.com>
2016-10-30 22:36:38 +0000

    Release 0.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -160,13 +160,13 @@ html_theme = 'nature'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'assets']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
 #
-# html_extra_path = []
+html_extra_path = ['assets']
 
 # If not None, a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
