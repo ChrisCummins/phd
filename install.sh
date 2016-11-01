6439700e52650d270f603fbeefcfff23598095bd
Chris Cummins <chrisc.101@gmail.com>
2016-11-01 16:30:53 +0000

    Add an install.sh script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# One-liner to install CLgen 0.0.13.
+#
+# Copyright 2016 Chris Cummins <chrisc.101@gmail.com>.
+#
+# This file is part of CLgen.
+#
+# CLgen is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CLgen is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CLgen.  If not, see <http://www.gnu.org/licenses/>.
+#
+set -euv
+
+wget https://github.com/ChrisCummins/clgen/archive/0.0.13.tar.gz -O clgen-0.0.13.tar.gz
+tar xf clgen-0.0.13.tar.gz
+rm clgen-0.0.13.tar.gz
+cd clgen-0.0.13
+
+./configure
+make
+sudo make install
