b941de3ec37d19edc9a3dea4bbe7973933f6d68f
Chris Cummins <chrisc.101@gmail.com>
2016-11-01 23:12:51 +0000

    Release 0.0.20

--- install-cuda.sh
+++ install-cuda.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# One-liner to install CLgen 0.0.19.
+# One-liner to install CLgen 0.0.20.
 #
 # Copyright 2016 Chris Cummins <chrisc.101@gmail.com>.
 #
@@ -21,10 +21,10 @@
 #
 set -euv
 
-wget https://github.com/ChrisCummins/clgen/archive/0.0.19.tar.gz -O clgen-0.0.19.tar.gz
-tar xf clgen-0.0.19.tar.gz
-rm clgen-0.0.19.tar.gz
-cd clgen-0.0.19
+wget https://github.com/ChrisCummins/clgen/archive/0.0.20.tar.gz -O clgen-0.0.20.tar.gz
+tar xf clgen-0.0.20.tar.gz
+rm clgen-0.0.20.tar.gz
+cd clgen-0.0.20
 ./configure --batch --with-opencl --with-cuda
 make
 sudo make install
--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ def all_module_data_files(module, datadir="data"):
 
 setup(
     name='CLgen',
-    version='0.0.19',
+    version='0.0.20',
     description='',
     url='https://github.com/ChrisCummins/clgen',
     author='Chris Cummins',
