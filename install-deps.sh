ac8a5671a0c5391c6c6985e0fcaafe84bac99688
Chris Cummins <chrisc.101@gmail.com>
2016-11-04 11:29:20 +0000

    Release 0.0.30

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# One-liner to install dependencies for CLgen 0.0.29.
+# One-liner to install dependencies for CLgen 0.0.30.
 #
 # Copyright 2016 Chris Cummins <chrisc.101@gmail.com>.
 #
