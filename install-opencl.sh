b0176f8facce1b677745ca991e069278aedd994e
Chris Cummins <chrisc.101@gmail.com>
2016-11-15 23:42:08 +0000

    Release 0.1.4

--- install-opencl.sh
+++ install-opencl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# One-liner to install CLgen 0.1.3.
+# One-liner to install CLgen 0.1.4.
 #
 # Copyright 2016 Chris Cummins <chrisc.101@gmail.com>.
 #
@@ -20,10 +20,10 @@
 # along with CLgen.  If not, see <http://www.gnu.org/licenses/>.
 #
 set -ex
-wget https://github.com/ChrisCummins/clgen/archive/0.1.3.tar.gz -O clgen-0.1.3.tar.gz
-tar xf clgen-0.1.3.tar.gz
-rm clgen-0.1.3.tar.gz
-cd clgen-0.1.3
+wget https://github.com/ChrisCummins/clgen/archive/0.1.4.tar.gz -O clgen-0.1.4.tar.gz
+tar xf clgen-0.1.4.tar.gz
+rm clgen-0.1.4.tar.gz
+cd clgen-0.1.4
 ./configure --batch --with-opencl
 make
 
--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ def all_module_data_files(module, datadir="data"):
 
 setup(
     name='CLgen',
-    version='0.1.3',
+    version='0.1.4',
     description='',
     url='https://github.com/ChrisCummins/clgen',
     author='Chris Cummins',
