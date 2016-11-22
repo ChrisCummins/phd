a910407d88d9cabb866e92a3cf56b025c7d8486d
Chris Cummins <chrisc.101@gmail.com>
2016-11-22 22:18:59 +0000

    Add viz.finalise() test

---
+++ tests/test_viz.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2015, 2016 Chris Cummins.
+#
+# Labm8 is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Labm8 is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with labm8.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import division
+
+from unittest import main
+from tests import TestCase
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import labm8 as lab
+from labm8 import fs
+from labm8 import viz
+
+class TestViz(TestCase):
+    def _mkplot(self):
+        t = np.arange(0.0, 2.0, 0.01)
+        s = np.sin(2*np.pi*t)
+        plt.plot(t, s)
+
+    def test_finalize(self):
+        self._mkplot()
+        viz.finalise("/tmp/labm8.png")
+        self.assertTrue(fs.exists("/tmp/labm8.png"))
+
+    def test_finalize_tight(self):
+        self._mkplot()
+        viz.finalise("/tmp/labm8.png", tight=True)
+        self.assertTrue(fs.exists("/tmp/labm8.png"))
