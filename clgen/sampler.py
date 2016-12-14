992940142a478a4533ad241443dc8edc68aec680
Chris Cummins <chrisc.101@gmail.com>
2016-12-14 12:27:23 +0000

    sampler: Fix max_batches count

--- clgen/sampler.py
+++ clgen/sampler.py
@@ -159,19 +159,19 @@ class Sampler(clgen.CLgenObject):
 
         batch_i = 0
         while True:
-            batch_i += 1
-            print("beginning batch", batch_i, "...")
-
             # stop if we have enough kernels
-            has_max_kernels = self.max_kernels >= 0
+            has_max_kernels = self.max_kernels > 0
             num_good_kernels = dbutil.num_good_kernels(cache["kernels.db"])
             if has_max_kernels and num_good_kernels >= self.max_kernels:
                 return
 
             # stop if we've done enough batches
             has_max_batches = self.max_batches > 0
-            if has_max_batches and batch_i > self.max_batches:
+            if has_max_batches and batch_i >= self.max_batches:
                 return
+
+            batch_i += 1
+            print("beginning batch", batch_i, "...")
 
             self.sample_iteration(model, quiet=quiet)
 
