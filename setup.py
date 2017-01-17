acbce391140b3825346e23025da80e04a7e198b7
Chris Cummins <chrisc.101@gmail.com>
2017-01-17 20:14:46 +0000

    Initial code import

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='shutterbug',
+    version='0.0.1',
+    description='',
+    url='https://github.com/ChrisCummins/shutterbug',
+    author='Chris Cummins',
+    author_email='chrisc.101@gmail.com',
+    license='GNU General Public License, Version 3',
+    packages=['shutterbug'],
+    scripts=['bin/shutterbug'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    install_requires=[],
+    data_files=[],
+    zip_safe=True)
