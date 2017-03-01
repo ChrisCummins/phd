0c6af5e0a42a1c1b79f1a109b2d1ed97696afed8
Chris Cummins <chrisc.101@gmail.com>
2017-03-01 23:51:42 +0000

    lab/clgen: new models

--- lab/clgen/classification/lib/models.py
+++ lab/clgen/classification/lib/models.py
@@ -418,3 +418,321 @@ def rupert():
         "save_fn": save_fn,
         "load_fn": load_fn
     }
+
+def fred():
+    BATCH_SIZE = 64
+    EMBEDDING_VECTOR_LEN = 64
+
+    def create_model(*args, data_desc=None, **kwargs):
+        """ instantiate a model """
+        atomizer = data_desc["atomizer"]
+        seq_length = data_desc["seq_length"]
+        vocab_size = atomizer.vocab_size + 1
+
+        data_in = Input(shape=(2,), name="data_in")
+
+        code_in = Input(shape=(seq_length,), dtype="int32", name="code_in")
+        right = Embedding(output_dim=EMBEDDING_VECTOR_LEN, input_dim=vocab_size, input_length=seq_length)(code_in)
+        right = LSTM(16, consume_less="mem", return_sequences=True)(right)
+        right = LSTM(16, consume_less="mem")(right)
+
+        aux_out = Dense(2, activation="sigmoid", name="aux_out")(right)
+
+        x = merge([data_in, right], mode="concat")
+        x = BatchNormalization(name="norm")(x)
+        x = Dense(18, activation="relu")(x)
+        out = Dense(2, activation="sigmoid", name="out")(x)
+
+        model = Model(input=[data_in, code_in], output=[out, aux_out])
+        model.compile(optimizer="adam",
+                      loss={"out": "categorical_crossentropy", "aux_out": "categorical_crossentropy"},
+                      loss_weights={"out": 1., "aux_out": .2},
+                      metrics=['accuracy'])
+        return model
+
+    def train_fn(model, train, *args, **kwargs):
+        """ train a model """
+        model.fit({"data_in": train["x_2"], "code_in": train["x_seq"]},
+                  {"out": train["y_2"], "aux_out": train["y_2"]},
+                  nb_epoch=50,
+                  batch_size=BATCH_SIZE,
+                  verbose=1,
+                  shuffle=True)
+
+    def test_fn(model, test, seed, *args, **kwargs):
+        """ make predictions for test data """
+        predictions = np.array(model.predict({"data_in": test["x_2"], "code_in": test["x_seq"]},
+                                             batch_size=BATCH_SIZE, verbose=0))
+        clipped = [np.argmax(x) for x in predictions[0]]
+        return clipped
+
+    def save_fn(outpath, model):
+        """ save a trained model """
+        model.save(outpath)
+
+    def load_fn(inpath):
+        """ load a trained model """
+        return load_model(inpath)
+
+    return {
+        "name": "fred",
+        "create_model": create_model,
+        "train_fn": train_fn,
+        "test_fn": test_fn,
+        "save_fn": save_fn,
+        "load_fn": load_fn
+    }
+
+def barry():
+    BATCH_SIZE = 64
+    EMBEDDING_VECTOR_LEN = 64
+
+    def create_model(*args, data_desc=None, **kwargs):
+        """ instantiate a model """
+        atomizer = data_desc["atomizer"]
+        seq_length = data_desc["seq_length"]
+        vocab_size = atomizer.vocab_size + 1
+
+        data_in = Input(shape=(2,), name="data_in")
+
+        code_in = Input(shape=(seq_length,), dtype="int32", name="code_in")
+        right = Embedding(output_dim=EMBEDDING_VECTOR_LEN, input_dim=vocab_size, input_length=seq_length)(code_in)
+        right = LSTM(16, consume_less="mem")(right)
+
+        aux_out = Dense(2, activation="sigmoid", name="aux_out")(right)
+
+        x = merge([data_in, right], mode="concat")
+        x = BatchNormalization(name="norm")(x)
+        x = Dense(18, activation="relu")(x)
+        x = Dense(18, activation="relu")(x)
+        out = Dense(2, activation="sigmoid", name="out")(x)
+
+        model = Model(input=[data_in, code_in], output=[out, aux_out])
+        model.compile(optimizer="adam",
+                      loss={"out": "categorical_crossentropy", "aux_out": "categorical_crossentropy"},
+                      loss_weights={"out": 1., "aux_out": .2},
+                      metrics=['accuracy'])
+        return model
+
+    def train_fn(model, train, *args, **kwargs):
+        """ train a model """
+        model.fit({"data_in": train["x_2"], "code_in": train["x_seq"]},
+                  {"out": train["y_2"], "aux_out": train["y_2"]},
+                  nb_epoch=50,
+                  batch_size=BATCH_SIZE,
+                  verbose=1,
+                  shuffle=True)
+
+    def test_fn(model, test, seed, *args, **kwargs):
+        """ make predictions for test data """
+        predictions = np.array(model.predict({"data_in": test["x_2"], "code_in": test["x_seq"]},
+                                             batch_size=BATCH_SIZE, verbose=0))
+        clipped = [np.argmax(x) for x in predictions[0]]
+        return clipped
+
+    def save_fn(outpath, model):
+        """ save a trained model """
+        model.save(outpath)
+
+    def load_fn(inpath):
+        """ load a trained model """
+        return load_model(inpath)
+
+    return {
+        "name": "barry",
+        "create_model": create_model,
+        "train_fn": train_fn,
+        "test_fn": test_fn,
+        "save_fn": save_fn,
+        "load_fn": load_fn
+    }
+
+def turner():
+    BATCH_SIZE = 64
+    EMBEDDING_VECTOR_LEN = 32
+
+    def create_model(*args, data_desc=None, **kwargs):
+        """ instantiate a model """
+        atomizer = data_desc["atomizer"]
+        seq_length = data_desc["seq_length"]
+        vocab_size = atomizer.vocab_size + 1
+
+        data_in = Input(shape=(2,), name="data_in")
+
+        code_in = Input(shape=(seq_length,), dtype="int32", name="code_in")
+        right = Embedding(output_dim=EMBEDDING_VECTOR_LEN, input_dim=vocab_size, input_length=seq_length)(code_in)
+        right = LSTM(16, consume_less="mem")(right)
+
+        aux_out = Dense(2, activation="sigmoid", name="aux_out")(right)
+
+        x = merge([data_in, right], mode="concat")
+        x = BatchNormalization(name="norm")(x)
+        x = Dense(18, activation="relu")(x)
+        out = Dense(2, activation="sigmoid", name="out")(x)
+
+        model = Model(input=[data_in, code_in], output=[out, aux_out])
+        model.compile(optimizer="adam",
+                      loss={"out": "categorical_crossentropy", "aux_out": "categorical_crossentropy"},
+                      loss_weights={"out": 1., "aux_out": .2},
+                      metrics=['accuracy'])
+        return model
+
+    def train_fn(model, train, *args, **kwargs):
+        """ train a model """
+        model.fit({"data_in": train["x_2"], "code_in": train["x_seq"]},
+                  {"out": train["y_2"], "aux_out": train["y_2"]},
+                  nb_epoch=50,
+                  batch_size=BATCH_SIZE,
+                  verbose=1,
+                  shuffle=True)
+
+    def test_fn(model, test, seed, *args, **kwargs):
+        """ make predictions for test data """
+        predictions = np.array(model.predict({"data_in": test["x_2"], "code_in": test["x_seq"]},
+                                             batch_size=BATCH_SIZE, verbose=0))
+        clipped = [np.argmax(x) for x in predictions[0]]
+        return clipped
+
+    def save_fn(outpath, model):
+        """ save a trained model """
+        model.save(outpath)
+
+    def load_fn(inpath):
+        """ load a trained model """
+        return load_model(inpath)
+
+    return {
+        "name": "turner",
+        "create_model": create_model,
+        "train_fn": train_fn,
+        "test_fn": test_fn,
+        "save_fn": save_fn,
+        "load_fn": load_fn
+    }
+
+def fife():
+    BATCH_SIZE = 64
+    EMBEDDING_VECTOR_LEN = 64
+
+    def create_model(*args, data_desc=None, **kwargs):
+        """ instantiate a model """
+        atomizer = data_desc["atomizer"]
+        seq_length = data_desc["seq_length"]
+        vocab_size = atomizer.vocab_size + 1
+
+        data_in = Input(shape=(2,), name="data_in")
+
+        code_in = Input(shape=(seq_length,), dtype="int32", name="code_in")
+        right = Embedding(output_dim=EMBEDDING_VECTOR_LEN, input_dim=vocab_size, input_length=seq_length)(code_in)
+        right = LSTM(16, consume_less="mem")(right)
+
+        aux_out = Dense(2, activation="sigmoid", name="aux_out")(right)
+
+        x = merge([data_in, right], mode="concat")
+        x = Dense(18, activation="relu")(x)
+        out = Dense(2, activation="sigmoid", name="out")(x)
+
+        model = Model(input=[data_in, code_in], output=[out, aux_out])
+        model.compile(optimizer="adam",
+                      loss={"out": "categorical_crossentropy", "aux_out": "categorical_crossentropy"},
+                      loss_weights={"out": 1., "aux_out": .2},
+                      metrics=['accuracy'])
+        return model
+
+    def train_fn(model, train, *args, **kwargs):
+        """ train a model """
+        model.fit({"data_in": train["x_2"], "code_in": train["x_seq"]},
+                  {"out": train["y_2"], "aux_out": train["y_2"]},
+                  nb_epoch=50,
+                  batch_size=BATCH_SIZE,
+                  verbose=1,
+                  shuffle=True)
+
+    def test_fn(model, test, seed, *args, **kwargs):
+        """ make predictions for test data """
+        predictions = np.array(model.predict({"data_in": test["x_2"], "code_in": test["x_seq"]},
+                                             batch_size=BATCH_SIZE, verbose=0))
+        clipped = [np.argmax(x) for x in predictions[0]]
+        return clipped
+
+    def save_fn(outpath, model):
+        """ save a trained model """
+        model.save(outpath)
+
+    def load_fn(inpath):
+        """ load a trained model """
+        return load_model(inpath)
+
+    return {
+        "name": "fife",
+        "create_model": create_model,
+        "train_fn": train_fn,
+        "test_fn": test_fn,
+        "save_fn": save_fn,
+        "load_fn": load_fn
+    }
+
+
+def bruno():
+    BATCH_SIZE = 64
+    EMBEDDING_VECTOR_LEN = 64
+
+    def create_model(*args, data_desc=None, **kwargs):
+        """ instantiate a model """
+        atomizer = data_desc["atomizer"]
+        seq_length = data_desc["seq_length"]
+        vocab_size = atomizer.vocab_size + 1
+
+        data_in = Input(shape=(2,), name="data_in")
+
+        code_in = Input(shape=(seq_length,), dtype="int32", name="code_in")
+        right = Embedding(output_dim=EMBEDDING_VECTOR_LEN, input_dim=vocab_size, input_length=seq_length)(code_in)
+        right = LSTM(64, consume_less="mem", return_sequences=True)(right)
+        right = LSTM(64, consume_less="mem")(right)
+
+        aux_out = Dense(2, activation="sigmoid", name="aux_out")(right)
+
+        x = merge([data_in, right], mode="concat")
+        x = BatchNormalization(name="norm")(x)
+        x = Dense(32, activation="relu")(x)
+        out = Dense(2, activation="sigmoid", name="out")(x)
+
+        model = Model(input=[data_in, code_in], output=[out, aux_out])
+        model.compile(optimizer="adam",
+                      loss={"out": "categorical_crossentropy", "aux_out": "categorical_crossentropy"},
+                      loss_weights={"out": 1., "aux_out": .2},
+                      metrics=['accuracy'])
+        return model
+
+    def train_fn(model, train, *args, **kwargs):
+        """ train a model """
+        model.fit({"data_in": train["x_2"], "code_in": train["x_seq"]},
+                  {"out": train["y_2"], "aux_out": train["y_2"]},
+                  nb_epoch=50,
+                  batch_size=BATCH_SIZE,
+                  verbose=1,
+                  shuffle=True)
+
+    def test_fn(model, test, seed, *args, **kwargs):
+        """ make predictions for test data """
+        predictions = np.array(model.predict({"data_in": test["x_2"], "code_in": test["x_seq"]},
+                                             batch_size=BATCH_SIZE, verbose=0))
+        clipped = [np.argmax(x) for x in predictions[0]]
+        return clipped
+
+    def save_fn(outpath, model):
+        """ save a trained model """
+        model.save(outpath)
+
+    def load_fn(inpath):
+        """ load a trained model """
+        return load_model(inpath)
+
+    return {
+        "name": "bruno",
+        "create_model": create_model,
+        "train_fn": train_fn,
+        "test_fn": test_fn,
+        "save_fn": save_fn,
+        "load_fn": load_fn
+    }
