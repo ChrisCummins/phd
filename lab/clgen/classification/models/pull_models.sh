22de167fb02f4f997abce246437503ea8968c308
Chris Cummins <chrisc.101@gmail.com>
2017-03-02 11:00:36 +0000

    lab/clgen: distr sync

--- lab/clgen/classification/models/pull_models.sh
+++ lab/clgen/classification/models/pull_models.sh
@@ -1,6 +1,6 @@
 set -ux
 
-test -f models-cc1.tar.bz2 && tar xjvf models-cc1.tar.bz2
-test -f models-cc2.tar.bz2 && tar xjvf models-cc2.tar.bz2
-test -f models-cc3.tar.bz2 && tar xjvf models-cc3.tar.bz2
+test -f models-cc1.tar.bz2 && { test $(hostname) = cc1 || tar xjvf models-cc1.tar.bz2; }
+test -f models-cc2.tar.bz2 && { test $(hostname) = cc2 || tar xjvf models-cc2.tar.bz2; }
+test -f models-cc3.tar.bz2 && { test $(hostname) = cc3 || tar xjvf models-cc3.tar.bz2; }
 true
--- lab/clgen/classification/models/push_models.sh
+++ lab/clgen/classification/models/push_models.sh
@@ -5,4 +5,9 @@ model_file=models-$(hostname).tar.bz2
 
 rm -f $model_file
 tar cjvf $model_file *
-scp $model_file cc3:phd/lab/clgen/classification/models
+
+set +e
+test $(hostname) = cc1 || scp $model_file cc1:phd/lab/clgen/classification/models
+test $(hostname) = cc2 || scp $model_file cc2:phd/lab/clgen/classification/models
+test $(hostname) = cc3 || scp $model_file cc3:phd/lab/clgen/classification/models
+true
