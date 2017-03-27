2aa94a3e39fafa444a6577f5294653bf8776ac44
Chris Cummins <chrisc.101@gmail.com>
2017-03-27 15:27:10 +0100

    test: Add jsonutil tests

--- tests/test_jsonutil.py
+++ tests/test_jsonutil.py
@@ -43,7 +43,7 @@ class TestJsonutil(TestCase):
         with self.assertRaises(ValueError):
             jsonutil.loads(a_str)
 
-    def test_loadf(self):
+    def test_read_file(self):
         a_str = """{
             "a": 1,  // this has comments
             "b": [1, 2, 3]
@@ -51,12 +51,31 @@ class TestJsonutil(TestCase):
         // begin with comment
         """
         system.echo(a_str, "/tmp/labm8.loaf.json")
-        a = jsonutil.loadf("/tmp/labm8.loaf.json")
+        a = jsonutil.read_file("/tmp/labm8.loaf.json")
 
         self.assertEqual(a["a"], 1)
         self.assertEqual(a["b"], [1, 2, 3])
         self.assertFalse("c" in a)
 
-    def test_loadf_bad_path(self):
+    def test_read_file_bad_path(self):
         with self.assertRaises(fs.File404):
-            jsonutil.loadf("/not/a/real/path")
+            jsonutil.read_file("/not/a/real/path")
+
+        self.assertEqual({}, jsonutil.read_file("/not/a/real/path",
+                                                must_exist=False))
+
+    def test_write_file(self):
+        d1 = {
+            "a": "1",
+            "b": "2"
+        }
+        jsonutil.write_file("/tmp/labm8.write_file.json", d1)
+        d2 = jsonutil.read_file("/tmp/labm8.write_file.json")
+        fs.rm("/tmp/labm8.write_file.json")
+
+        jsonutil.write_file("/tmp/labm8.write_file2.json", d1)
+        d3 = jsonutil.read_file("/tmp/labm8.write_file2.json")
+        fs.rm("/tmp/labm8.write_file2.json")
+
+        self.assertEqual(d1, d2)
+        self.assertEqual(d1, d3)
