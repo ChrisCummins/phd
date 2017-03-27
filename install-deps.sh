eb507fe769a86a9237780bd6645d29ad8e4a88ba
Chris Cummins <chrisc.101@gmail.com>
2017-03-27 13:18:11 +0100

    Release 0.2.9

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# One-liner to install dependencies for CLgen 0.2.8.
+# One-liner to install dependencies for CLgen 0.2.9.
 #
 # Copyright 2016, 2017 Chris Cummins <chrisc.101@gmail.com>.
 #
--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ def all_module_data_files(module, datadir="data"):
 
 setup(
     name='CLgen',
-    version='0.2.8',
+    version='0.2.9',
     description='',
     url='https://github.com/ChrisCummins/clgen',
     author='Chris Cummins',
