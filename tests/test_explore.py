8d2530e8c428475e7b6e867602150863668ccd0b
Chris Cummins <chrisc.101@gmail.com>
2017-03-28 20:46:37 +0100

    tests: Add explore tests

---
+++ tests/test_explore.py
@@ -0,0 +1,42 @@
+#
+# Copyright 2016, 2017 Chris Cummins <chrisc.101@gmail.com>.
+#
+# This file is part of CLgen.
+#
+# CLgen is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CLgen is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CLgen.  If not, see <http://www.gnu.org/licenses/>.
+#
+from unittest import TestCase, main, skip
+import tests
+
+from labm8 import fs
+
+from clgen import corpus
+from clgen import explore
+
+class TestExplore(TestCase):
+    def test_explore(self):
+        c = corpus.Corpus.from_json({
+            "path": tests.data_path("tiny", "corpus", exists=False)
+        })
+        explore.explore(c.contentcache["kernels.db"])
+
+    def test_explore_gh(self):
+        db_path = tests.archive("tiny-gh.db")
+        assert(fs.exists(db_path))
+
+        explore.explore(db_path)
+
+
+if __name__ == "__main__":
+    main()
