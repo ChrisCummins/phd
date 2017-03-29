6df82c1d2b62d0010cb81cae895a653bd233aa1c
Chris Cummins <chrisc.101@gmail.com>
2017-03-29 13:51:06 +0100

    Release 0.2.15

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ def all_module_data_files(module, datadir="data"):
 
 setup(
     name='CLgen',
-    version='0.2.14',
+    version='0.2.15',
     description='',
     url='https://github.com/ChrisCummins/clgen',
     author='Chris Cummins',
