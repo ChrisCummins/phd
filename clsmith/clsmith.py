95822af5d35745977f469543cf1e780cbc455e5c
Chris Cummins <chrisc.101@gmail.com>
2017-03-31 16:55:21 +0100

    clsmith: Add timeout and return statuscode

--- clsmith/clsmith.py
+++ clsmith/clsmith.py
@@ -9,14 +9,14 @@ cl_launcher_path = fs.path("~/src/CLSmith/build/cl_launcher")
 include_path = fs.path("~/src/CLSmith/runtime")
 
 
-def clsmith_cli(*args):
-    return [exec_path] + list(args)
+def clsmith_cli(*args, timeout=30):
+    return ["timeout", "--signal=9", str(timeout), exec_path] + list(args)
 
 
 def clsmith(*args):
     """
         Returns:
-            (float, str, str): Runtime, stdout, and stderr.
+            (float, int, str, str): Runtime, status, stdout, and stderr.
     """
     start_time = time()
 
@@ -26,18 +26,22 @@ def clsmith(*args):
 
     runtime = time() - start_time
 
-    return runtime, stdout.decode('utf-8'), stderr.decode('utf-8')
+    return (runtime, process.returncode,
+            stdout.decode('utf-8'), stderr.decode('utf-8'))
 
 
-def cl_launcher_cli(program_path, platform_id, device_id, *args):
-    return [cl_launcher_path, '---debug', '-f', program_path, '-p', platform_id,
-            '-d', device_id, '--include_path', include_path] + list(args)
+def cl_launcher_cli(program_path: str, platform_id: int, device_id: int,
+                    *args, timeout=30) -> str:
+    return ["timeout", "--signal=9", str(timeout),
+            cl_launcher_path, '---debug', '-f', program_path,
+            '-p', str(platform_id), '-d', str(device_id),
+            '--include_path', include_path] + list(args)
 
 
-def cl_launcher(program_path, platform_id, device_id, *args):
+def cl_launcher(program_path: str, platform_id: int, device_id: int, *args):
     """
         Returns:
-            (float, stdout, stderr): Runtime, stdout, and stderr.
+            (float, int, stdout, stderr): Runtime, status, stdout, and stderr.
     """
     start_time = time()
 
@@ -47,4 +51,5 @@ def cl_launcher(program_path, platform_id, device_id, *args):
 
     runtime = time() - start_time
 
-    return runtime, stdout.decode('utf-8'), stderr.decode('utf-8')
+    return (runtime, process.returncode,
+            stdout.decode('utf-8'), stderr.decode('utf-8'))
