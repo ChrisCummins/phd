1c7204b181aba5b4639c38b6808f338e0248f86c
Chris Cummins <chrisc.101@gmail.com>
2017-04-05 14:47:59 +0100

    Add cldrive-info script

---
+++ setup.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2017 Chris Cummins.
+#
+# This file is part of cldrive.
+#
+# Cldrive is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Cldrive is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cldrive.  If not, see <http://www.gnu.org/licenses/>.
+#
+from setuptools import setup
+
+# read from requirements.txt
+with open('./requirements.txt') as infile:
+    requirements = [x.strip() for x in infile.readlines() if x.strip()]
+
+setup(name='cldrive',
+      version='0.0.1',
+      description='Run arbitrary OpenCL kernels',
+      url='https://github.com/ChrisCummins/cldrive',
+      author='Chris Cummins',
+      author_email='chrisc.101@gmail.com',
+      license='GNU General Public License, Version 3',
+      packages=['cldrive'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=[
+            'bin/cldrive',
+            'bin/cldrive-info'
+      ],
+      install_requires=requirements,
+      zip_safe=True)
