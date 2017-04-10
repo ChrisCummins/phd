6bf56abc1e970a26202bb6db3ab2c21789449311
Chris Cummins <chrisc.101@gmail.com>
2017-04-10 15:47:18 +0100

    Work on sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -33,12 +33,14 @@
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
     'sphinx.ext.githubpages',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinx_autodoc_typehints'
+    'sphinx_autodoc_typehints',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -91,7 +93,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
