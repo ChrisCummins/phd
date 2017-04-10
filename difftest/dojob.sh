9f2d3f1e8cd01b7fdc483e0c68877cb940ef8899
Chris Cummins <chrisc.101@gmail.com>
2017-04-10 17:04:26 +0100

    Update dojob script

--- difftest/dojob.sh
+++ difftest/dojob.sh
@@ -6,13 +6,15 @@ set -ux
 # example usage:
 #     PLATFORM='NVIDIA CUDA' DEVICE='GeForce GTX 1080' bash ./dojob.sh
 
+script="./clsmith-runprograms-cldrive.py"
+
 while true; do
-        ./clgen-runprograms.py "$PLATFORM" "$DEVICE" -g 1,1,1 -l 1,1,1 -s 256 -i arange
+        $script "$PLATFORM" "$DEVICE" -g 1,1,1 -l 1,1,1 -s 256 -i arange
         sleep 1
-        ./clgen-runprograms.py "$PLATFORM" "$DEVICE" -g 1,1,1 -l 1,1,1 -s 256 -i arange --no-opts
+        $script "$PLATFORM" "$DEVICE" -g 1,1,1 -l 1,1,1 -s 256 -i arange --no-opts
         sleep 1
-        ./clgen-runprograms.py "$PLATFORM" "$DEVICE" -g 128,16,1 -l 32,1,1 -s 4096 -i arange
+        $script "$PLATFORM" "$DEVICE" -g 128,16,1 -l 32,1,1 -s 4096 -i arange
         sleep 1
-        ./clgen-runprograms.py "$PLATFORM" "$DEVICE" -g 128,16,1 -l 32,1,1 -s 4096 -i arange --no-opts
+        $script "$PLATFORM" "$DEVICE" -g 128,16,1 -l 32,1,1 -s 4096 -i arange --no-opts
         sleep 1
 done
