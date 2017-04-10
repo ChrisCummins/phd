07d66a44bf593d04bc1a5092de2b0381e034aa6a
Chris Cummins <chrisc.101@gmail.com>
2017-04-10 15:41:50 +0100

    Numpy style docstrings for env, no deferred import

--- cldrive/env.py
+++ cldrive/env.py
@@ -5,6 +5,8 @@ import sys
 from collections import namedtuple
 from typing import List, Tuple
 
+import pyopencl as cl
+
 
 class OpenCLEnvironment(namedtuple('OpenCLEnvironment', ['platform', 'device'])):
     __slots__ = ()  # memory saving
@@ -12,22 +14,27 @@ class OpenCLEnvironment(namedtuple('OpenCLEnvironment', ['platform', 'device']))
     def __repr__(self) -> str:
         return f"Device: {self.device}, Platform: {self.platform}"
 
-    def ctx_queue(self, profiling=False) -> Tuple['cl.Context', 'cl.CommandQueue']:
+    def ctx_queue(self, profiling=False) -> Tuple[cl.Context, cl.CommandQueue]:
         """
         Return an OpenCL context and command queue for the given environment.
 
-        Arguments:
-            profiling (bool, optional): If True, enable profiling support.
+        Arguments
+        ---------
+        profiling : bool, optional
+            If True, enable profiling support.
 
-        Raises:
-            LookupError: If a matching OpenCL device cannot be found.
-            RuntimeError: In case of an OpenCL API call failure.
+        Raises
+        ------
+        LookupError
+            If a matching OpenCL device cannot be found.
+        RuntimeError
+            In case of an OpenCL API call failure.
         """
         return _lookup_env(return_cl=True, platform=self.platform,
                            device=self.device, profiling=profiling)
 
 
-def _cl_devtype_from_str(cl, string: str) -> 'cl.device_type':
+def _cl_devtype_from_str(string: str) -> cl.device_type:
     devtypes = {
         "cpu": cl.device_type.CPU,
         "gpu": cl.device_type.GPU,
@@ -40,8 +47,7 @@ def _cl_devtype_from_str(cl, string: str) -> 'cl.device_type':
         raise ValueError(f"unrecognized device type '{string}'")
 
 
-def _devtype_matches(cl, device: 'cl.Device',
-                     devtype: 'cl.device_type') -> bool:
+def _devtype_matches(device: cl.Device, devtype: cl.device_type) -> bool:
     """ check that device type matches """
     if devtype == cl.device_type.ALL:
         return True
@@ -53,9 +59,7 @@ def _devtype_matches(cl, device: 'cl.Device',
 def _lookup_env(return_cl: bool, platform: str=None, device: str=None,
                 devtype: str="all", profiling: bool=False) -> OpenCLEnvironment:
     """ find a matching OpenCL device """
-    import pyopencl as cl
-
-    cl_devtype = _cl_devtype_from_str(cl, devtype)
+    cl_devtype = _cl_devtype_from_str(devtype)
 
     try:
         cl_platforms = cl.get_platforms()
@@ -74,7 +78,7 @@ def _lookup_env(return_cl: bool, platform: str=None, device: str=None,
             cl_devices = ctx.get_info(cl.context_info.DEVICES)
 
             # filter devices on device type
-            cl_devices = [d for d in cl_devices if _devtype_matches(cl, d, cl_devtype)]
+            cl_devices = [d for d in cl_devices if _devtype_matches(d, cl_devtype)]
 
             for cl_device in cl_devices:
                 device_str = cl_device.get_info(cl.device_info.NAME)
@@ -112,10 +116,12 @@ def host_os() -> str:
     """
     Get the type and version of the host operating system.
 
-    Returns:
-        str: Formatted <system> <release> <arch>, where <system> is the
-            operating system type, <release> is the release version, and
-            <arch> is 32 or 64 bit.
+    Returns
+    -------
+    str
+        Formatted <system> <release> <arch>, where <system> is the
+        operating system type, <release> is the release version, and
+        <arch> is 32 or 64 bit.
     """
     if sys.platform == "linux" or sys.platform == "linux2":
         dist = platform.linux_distribution()
@@ -129,8 +135,6 @@ def host_os() -> str:
 
 
 def clinfo(file=sys.stdout) -> None:
-    import pyopencl as cl
-
     print("Host:", host_os(), file=file)
 
     for platform_id, platform in enumerate(cl.get_platforms()):
@@ -161,23 +165,30 @@ def make_env(platform: str=None, device: str=None,
     device type. Constructs and returns an OpenCL context and queue for the
     matching device. Note that OpenCL profiling is enabled.
 
-    Arguments:
-        platform_id (int, optional): OpenCL Platform ID. If not provided, any
-            available platform may be used.
-        device_id (int, optional): OpenCL Device ID. If not provided, any
-            available device may be used. Requires that platform_id is set.
-        devtype (str, optional): OpenCL device type to use, one of:
-            {all,cpu,gpu}.
+    Parameters
+    ----------
+    platform_id : int, optional
+        OpenCL Platform ID. If not provided, any available platform may be
+        used.
+    device_id : int, optional
+        OpenCL Device ID. If not provided, any available device may be used.
+        Requires that platform_id is set.
+    devtype : str, optional
+        OpenCL device type to use, one of: {all,cpu,gpu}.
 
+    Returns
+    -------
+    OpenCLEnvironment
+        A named tuple consisting of the platform and device name.
 
-    Returns:
-        OpenCLEnvironment: A named tuple consisting of the platform and device
-            name.
-
-    Raises:
-        ValueError: If device_id is set, but not platform_id.
-        LookupError: If no matching device found.
-        RuntimeError: In case OpenCL API call fails.
+    Raises
+    ------
+    ValueError
+        If device_id is set, but not platform_id.
+    LookupError
+        If no matching device found.
+    RuntimeError
+        In case OpenCL API call fails.
     """
     return _lookup_env(return_cl=False, platform=platform, device=device,
                        devtype=devtype)
