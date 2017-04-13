671d9fc0d30e993285007aa6b93be6c29310ea53
Chris Cummins <chrisc.101@gmail.com>
2017-04-13 10:32:42 +0100

    Update bootstrap script for bazel

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -1,101 +1,39 @@
 #!/usr/bin/env bash
 #
-# bootstrap.sh - Build the project toolchain
-#
-# Writes the date that it was last run to a "bootstrap file" upon
-# completion. This can be checked using the --check argument.
+# bootstrap.sh - Prepare the toolchain
 #
 # Usage:
 #
-#     ./boostrap.sh [--check]
+#     ./boostrap.sh
 #
 set -eu
 
-# Project sources root:
-ROOT=~/phd
 
-# Number of build threads:
-NPROC=4
-
-# Location of boostrapped file:
-BOOTSTRAP_FILE=$ROOT/.bootstrapped
-
-
-build_llvm() {
-    mkdir -vp $ROOT/tools/llvm/build
-    cd $ROOT/tools/llvm/build
-    cmake .. -DCMAKE_BUILD_TYPE=Release
-    make -j$NPROC
-    cd $ROOT
+bootstrap_macos() {
+    set -x
+    brew list | grep '^gcc$' &>/dev/null || brew install gcc --without-multilib
+    brew cask list | grep '^java$' &>/dev/null || brew cask install java
+    brew list | grep '^bazel$' &>/dev/null || brew install bazel
 }
 
-clean_llvm() {
-    rm -rfv $ROOT/tools/llvm/build
+
+bootstrap_ubuntu() {
+    set -x
+
+    # add bazel APT repositories
+    echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+    curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
+    sudo apt-get update
+    dpkg -s 'bazel' &>/dev/null || sudo apt-get install -y bazel
 }
 
-build_libcxx() {
-    mkdir -vp $ROOT/extern/libcxx/build
-    cd $ROOT/extern/libcxx/build
-    cmake .. -DLLVM_CONFIG=$ROOT/tools/llvm/build/bin/llvm-config
-    make -j$NPROC
-    cd $ROOT
-}
-
-clean_libcxx() {
-    rm -rfv $ROOT/extern/libcxx/build
-}
-
-write_bootstrap_file() {
-    date > $BOOTSTRAP_FILE
-}
-
-remove_bootstrap_file() {
-    rm -fv $BOOTSTRAP_FILE
-}
-
-print_boostrapped_info() {
-    if [[ -f $BOOTSTRAP_FILE ]]; then
-        echo "Boostrapped on: $(cat $BOOTSTRAP_FILE)"
-    else
-        echo "Not bootstrapped."
-    fi
-}
-
-boostrap() {
-    build_llvm
-    build_libcxx
-    write_bootstrap_file
-}
-
-unboostrap() {
-    clean_llvm
-    clean_libcxx
-    remove_bootstrap_file
-}
 
 main() {
-    set +u
-    arg=$1
-    set -u
-
-    if [[ -n "$arg" ]]; then
-        case "$arg" in
-            "--check")
-                print_boostrapped_info
-                exit
-                ;;
-            "clean")
-                unboostrap
-                exit
-                ;;
-            *)
-                echo "boostrap: Unrecognised command '$arg'!" >&2
-                exit 1
-                ;;
-        esac
+    if [[ "$(uname)" != "Darwin" ]]; then
+        bootstrap_macos
+    else
+        bootstrap_ubuntu
     fi
-
-    # No argument, run bootstrap:
-    boostrap
 }
 main $@
