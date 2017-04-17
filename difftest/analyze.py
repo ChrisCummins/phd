627d05db8f227335e9d16c99602dbb9412cb4fe9
Chris Cummins <chrisc.101@gmail.com>
2017-04-17 01:46:20 +0100

    difftest: New results

---
+++ difftest/analyze.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+import sqlalchemy as sql
+
+from collections import Counter
+from progressbar import ProgressBar
+
+import db
+from db import *
+
+def main():
+    db.init("cc1")
+    session = db.make_session()
+
+    # Group outcomes into less-granular classifications, as in the CLSmith paper.
+    # This table maps <outcome>: <classification>
+    CLASSIFICATIONS = {
+        '__NOT_IMPLEMENTED_YET__': 'Wrong code',
+        'CL_BUILD_PROGRAM _FAILURE': 'Build failure',
+        'CL_INVALID_COMMAND_QUEUE': 'Runtime crash',
+        'CL_INVALID_KERNEL_NAME': 'Build failure',
+        'CL_INVALID_VALUE': 'Runtime crash',
+        'CL_OUT_OF_HOST_MEMORY': 'Runtime crash',
+        'CL_OUT_OF_RESOURCES': 'Runtime crash',
+        'cldrive Error': 'Invalid testcase',
+        'clWaitForEvents()': 'Runtime crash',
+        'Multiple OpenCL kernels': 'Invalid testcase',
+        'No OpenCL kernel': 'Invalid testcase',
+        'Preprocessing Failed': 'Invalid testcase',
+        'Segmentation Fault': 'Build failure',
+        'SIGBUS': 'Runtime crash',
+        'Timeout': 'Invalid testcase',
+        'Unsupported Program': 'Invalid testcase',
+        'z_Okay': 'Okay',
+        'INVALID_WORK_GROUP_SIZE': 'Invalid testcase',
+        'UnicodeError': 'Invalid testcase',
+    }
+    print("Possible classifications:", ", ".join(f"'{s}'" for s in sorted(set(CLASSIFICATIONS.values()))))
+
+
+    def lookup_status(status):
+        return {
+            -11: "Segmentation Fault",
+            -9: "Timeout",
+            0: "z_Okay",
+            1024: "Preprocessing Failed",
+            1025: "UnicodeError",
+        }[status]
+
+
+    def get_cl_launcher_outcome(result):
+        if result.status == 1:
+            # exit status of 1 indicates that cl_launcher exited after an OpenCL API call error
+            prev = None
+            for line in reversed(result.stderr.split('\n')):
+                if line == "Error found (callback):":
+                    # Interpret CLSmith error callback messages: the following line describes the error
+                    return prev.split()[0]
+                elif line.startswith("Error"):
+                    # Interpret CLSmith error messages
+                    return {
+                        "Error enqueueing kernel: -6": "CL_OUT_OF_HOST_MEMORY",
+                        "Error sending finish command: -36": "CL_INVALID_COMMAND_QUEUE",
+                        "Error building program: -11": "CL_BUILD_PROGRAM _FAILURE",
+                        "Error creating kernel: -46": "CL_INVALID_KERNEL_NAME",
+                        "Error enqueueing kernel: -5": "CL_OUT_OF_RESOURCES",
+                    }.get(line, line)
+                prev = line
+            else:  # should never happen
+                raise LookupError
+        else:
+            return lookup_status(result.status)
+
+
+    def get_cldrive_outcome(result):
+        if result.status == 1:
+            # exit status of 1
+            line = result.stderr.split('\n')[-2]  # last line contains the Exception line
+            if line.startswith('cldrive.args.OpenCLValueError'):
+                return 'Unsupported Program'
+            elif line.startswith('ValueError: unsupported data type'):
+                return 'cldrive Error'
+            elif line.startswith('TypeError: unsupported operand type'):
+                return 'cldrive Error'
+            elif line.startswith('TypeError: _set_args() takes'):
+                return 'cldrive Error'
+            elif line.startswith('ValueError: could not broadcast input array from shape'):
+                return 'cldrive Error'
+            else:
+                # This table maps <exception line>: <outcome>
+                return {
+                    'AssertionError': 'cldrive Error',
+                    'cldrive.driver.PorcelainError: SIGBUS': 'SIGBUS',
+                    'cldrive.driver.PorcelainError: SIGSEGV': 'Segmentation Fault',
+                    'cldrive.driver.Timeout: 60': 'Timeout',
+                    'EOFError: Ran out of input': 'cldrive Error',
+                    'KeyError: \'outputs\'': 'cldrive Error',
+                    'LookupError: source contains more than one kernel definition': 'Multiple OpenCL kernels',
+                    'LookupError: source contains no kernel definitions.': 'No OpenCL kernel',
+                    'pyopencl.cffi_cl.LogicError: clEnqueueNDRangeKernel failed: INVALID_WORK_GROUP_SIZE': 'INVALID_WORK_GROUP_SIZE',
+                    'pyopencl.cffi_cl.LogicError: clWaitForEvents failed: <unknown error -9999>': 'clWaitForEvents()',
+                    'pyopencl.cffi_cl.RuntimeError: clEnqueueNDRangeKernel failed: OUT_OF_HOST_MEMORY': 'CL_OUT_OF_HOST_MEMORY',
+                    'pyopencl.cffi_cl.RuntimeError: clEnqueueNDRangeKernel failed: OUT_OF_RESOURCES': 'CL_OUT_OF_RESOURCES',
+                    'RuntimeError': 'cldrive Error',
+                    'ValueError: failed to set kernel args': 'cldrive Error',
+                }.get(line, line)
+        else:
+            return lookup_status(result.status)
+
+    session.rollback()
+
+    # cl_launcher result outcomes and classifications
+    print("CLSmith ...")
+    for result in ProgressBar()(session.query(CLSmithResult).all()):
+        result.outcome = get_cl_launcher_outcome(result)
+        result.classification = CLASSIFICATIONS[result.outcome]
+
+        # determine if output differs from the majority (if there is one)
+        if result.status == 0:
+            outputs = [x[0] for x in session.query(CLSmithResult.stdout)\
+                .filter(CLSmithResult.program == result.program,
+                        CLSmithResult.params == result.params,
+                        CLSmithResult.status == 0)]
+            if len(outputs) > 1:
+                majority_output = Counter(outputs).most_common(1)[0][0]
+                if result.stdout != majority_output:
+                    result.classification = "Wrong code"
+    session.commit()
+
+    # cldrive results outcomes and classifications
+    for name, table in zip(CLDRIVE_TABLE_NAMES, CLDRIVE_TABLES):
+        print(f"{name} ...")
+        for result in ProgressBar()(session.query(table).all()):
+            result.outcome = get_cldrive_outcome(result)
+            result.classification = CLASSIFICATIONS[result.outcome]
+
+            # determine if output differs from the majority (if there is one)
+            if result.status == 0:
+                outputs = [x[0] for x in session.query(table.stdout)\
+                    .filter(table.program == result.program,
+                            table.params == result.params,
+                            table.status == 0)]
+                if len(outputs) > 1:
+                    majority_output = Counter(outputs).most_common(1)[0][0]
+                    if result.stdout != majority_output:
+                        result.classification = "Wrong code"
+        session.commit()
+
+
+if __name__ == "__main__":
+    main()
