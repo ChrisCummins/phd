56bdf949bcecbc5f8e9b52445cbd3ed73008c119
Chris Cummins <chrisc.101@gmail.com>
2017-04-19 22:14:34 +0100

    Release 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./requirements.txt') as infile:
     requirements = [x.strip() for x in infile.readlines() if x.strip()]
 
 setup(name='lmk',
-      version='0.0.4.dev0',
+      version='0.0.5',
       description='Email notification when command completes',
       url='https://github.com/ChrisCummins/lmk',
       author='Chris Cummins',
