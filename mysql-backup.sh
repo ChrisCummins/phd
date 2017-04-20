f159bb720a69aa6ae8a938f70733a178d3331158
Chris Cummins <chrisc.101@gmail.com>
2017-04-20 10:20:15 +0100

    Password-less MySQL backup

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,10 +1,9 @@
 host=cc1
-user=cec
 db=project_b
 dstdir=~/src/project_b/data
 
 set -eux
 
-ssh $host "mysqldump -u $user -p $db > /tmp/$db.mysql"
+ssh $host "mysqldump $db > /tmp/$db.mysql"
 ssh $host "tar cjvf $dstdir/$db-$(date '+%Y-%m-%d').mysql.tar.bz2 -C /tmp $db.mysql"
 ssh $host "rm -v /tmp/$db.mysql"
