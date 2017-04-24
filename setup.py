d78f49c40c7d81a057cc73bc8995190ed78b3389
Chris Cummins <chrisc.101@gmail.com>
2017-04-24 10:00:31 +0100

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shutterbug',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     url='https://github.com/ChrisCummins/shutterbug',
     author='Chris Cummins',
