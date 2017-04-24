bb4b2097a12e309beb9ba510343d16760c41febe
Chris Cummins <chrisc.101@gmail.com>
2017-04-24 10:00:39 +0100

    Development version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='shutterbug',
-    version='0.0.2',
+    version='0.0.2.dev0',
     description='',
     url='https://github.com/ChrisCummins/shutterbug',
     author='Chris Cummins',
