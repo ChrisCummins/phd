976b759351c907995559a7dbfddd274765fed558
Chris Cummins <chrisc.101@gmail.com>
2017-05-15 19:15:24 +0100

    Development version bump

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ def all_module_data_files(module, datadir="data", excludes=[]):
 
 setup(
     name='CLgen',
-    version='0.3.7',
+    version='0.3.7.dev0',
     description='',
     url='https://github.com/ChrisCummins/clgen',
     author='Chris Cummins',
