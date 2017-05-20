c38c7ec05edbc4452b2257e0e7f2aefd5f6c618a
Chris Cummins <chrisc.101@gmail.com>
2017-05-20 18:23:10 +0100

    Work on reproducing clsmith wrong code bugs

--- difftest/the-reproducerer.py
+++ difftest/the-reproducerer.py
@@ -22,6 +22,7 @@ What it does
 """
 import faulthandler
 faulthandler.enable()
+
 import cldrive
 import os
 import pyopencl as cl
@@ -29,6 +30,7 @@ import sqlalchemy as sql
 import sys
 
 from argparse import ArgumentParser
+from collections import Counter
 from subprocess import Popen, PIPE
 from labm8 import fs
 from pathlib import Path
@@ -37,224 +39,6 @@ from progressbar import ProgressBar
 import db
 from db import *
 from lib import *
-
-
-def escape_c_string(s):
-    return '\n'.join('"{}\\n"'.format(x) for x in s.split('\n') if len(x))
-
-
-def make_c_source(kernel_src):
-    src = """\
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifdef __APPLE__
-#include <OpenCL/opencl.h>
-#else
-#include <CL/cl.h>
-#endif
-
-
-const char *src = \\
-"""
-    src += escape_c_string(kernel_src)
-
-    src += """;
-const char *kernel_name = "{}";
-""".format(cldrive.kernel_name(kernel_src))
-
-    src += """
-int main() {
-        int err;
-
-        cl_uint num_platforms;
-        cl_platform_id platform_id;
-        err = clGetPlatformIDs(
-                /* cl_uint num_entries */ 1,
-                /* cl_platform_id *platforms */ &platform_id,
-                /* cl_uint *num_platforms */ &num_platforms);
-        switch (err) {
-                case CL_SUCCESS:
-                        break;
-                case CL_INVALID_VALUE:
-                        fprintf(stderr, "clGetPlatformIDs CL_INVALID_VALUE\\n");
-                        return 1;
-        }
-
-        cl_device_id device_id;
-        err = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_ALL, 1, &device_id, NULL);
-        switch (err) {
-                case CL_SUCCESS:
-                        break;
-                case CL_INVALID_PLATFORM:
-                        fprintf(stderr, "clGetDeviceIDs CL_INVALID_PLATFORM\\n");
-                        return 1;
-                case CL_INVALID_DEVICE_TYPE:
-                        fprintf(stderr, "clGetDeviceIDs CL_INVALID_DEVICE_TYPE\\n");
-                        return 1;
-                case CL_INVALID_VALUE:
-                        fprintf(stderr, "clGetDeviceIDs CL_INVALID_VALUE\\n");
-                        return 1;
-                case CL_DEVICE_NOT_FOUND:
-                        fprintf(stderr, "clGetDeviceIDs CL_DEVICE_NOT_FOUND\\n");
-                        return 1;
-                default:
-                        fprintf(stderr, "clGetDeviceIDs %d\\n", err);
-                        return 1;
-        }
-
-        cl_context ctx = clCreateContext(
-                /* cl_context_properties *properties */ NULL,
-                /* cl_uint num_devices */ 1,
-                /* const cl_device_id *devices */ &device_id,
-                /* void *pfn_notify */ NULL,
-                /* void *user_data */ NULL,
-                /* cl_int *errcode_ret */ &err);
-        switch (err) {
-                case CL_SUCCESS:
-                        break;
-                case CL_INVALID_PLATFORM:
-                        fprintf(stderr, "clCreateContext CL_INVALID_PLATFORM\\n");
-                        return 1;
-                case CL_INVALID_DEVICE:
-                        fprintf(stderr, "clCreateContext CL_INVALID_DEVICE\\n");
-                        return 1;
-                case CL_INVALID_VALUE:
-                        fprintf(stderr, "clCreateContext CL_INVALID_VALUE\\n");
-                        return 1;
-                case CL_DEVICE_NOT_AVAILABLE:
-                        fprintf(stderr, "clCreateContext CL_DEVICE_NOT_AVAILABLE\\n");
-                        return 1;
-                case CL_OUT_OF_HOST_MEMORY:
-                        fprintf(stderr, "clCreateContext CL_OUT_OF_HOST_MEMORY\\n");
-                        return 1;
-                default:
-                        fprintf(stderr, "clCreateContext %d\\n", err);
-                        return 1;
-        }
-
-        cl_command_queue queue = clCreateCommandQueue(
-                /* cl_context context */ ctx,
-                /* cl_device_id device */ device_id,
-                /* cl_command_queue_properties properties */ 0,
-                /* cl_int *errcode_ret */ &err);
-        switch (err) {
-                case CL_SUCCESS:
-                        break;
-                case CL_INVALID_CONTEXT:
-                        fprintf(stderr, "clCreateCommandQueue CL_INVALID_CONTEXT\\n");
-                        return 1;
-                case CL_INVALID_DEVICE:
-                        fprintf(stderr, "clCreateCommandQueue CL_INVALID_DEVICE\\n");
-                        return 1;
-                case CL_INVALID_VALUE:
-                        fprintf(stderr, "clCreateCommandQueue CL_INVALID_VALUE\\n");
-                        return 1;
-                case CL_INVALID_QUEUE_PROPERTIES:
-                        fprintf(stderr, "clCreateCommandQueue CL_INVALID_QUEUE_PROPERTIES\\n");
-                        return 1;
-                case CL_OUT_OF_HOST_MEMORY:
-                        fprintf(stderr, "clCreateCommandQueue CL_OUT_OF_HOST_MEMORY\\n");
-                        return 1;
-                default:
-                        fprintf(stderr, "clCreateCommandQueue %d\\n", err);
-                        return 1;
-        }
-
-        cl_program program = clCreateProgramWithSource(ctx, 1, (const char **) &src, NULL, &err);
-        switch (err) {
-                case CL_SUCCESS:
-                        break;
-                case CL_INVALID_CONTEXT:
-                        fprintf(stderr, "clCreateProgramWithSource CL_INVALID_CONTEXT\\n");
-                        return 1;
-                case CL_INVALID_VALUE:
-                        fprintf(stderr, "clCreateProgramWithSource CL_INVALID_VALUE\\n");
-                        return 1;
-                case CL_OUT_OF_HOST_MEMORY:
-                        fprintf(stderr, "clCreateProgramWithSource CL_OUT_OF_HOST_MEMORY\\n");
-                        return 1;
-                default:
-                        fprintf(stderr, "clCreateProgramWithSource %d\\n", err);
-                        return 1;
-        }
-
-        err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-        if (err != CL_SUCCESS) {
-                size_t len;
-                char buffer[2048];
-
-                fprintf(stderr, "clBuildProgram build log:\\n");
-                clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
-                fprintf(stderr, "%s\\n", buffer);
-                exit(1);
-
-                switch (err) {
-                        case CL_INVALID_PROGRAM:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_INVALID_PROGRAM\\n");
-                                return 1;
-                        case CL_INVALID_VALUE:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_INVALID_VALUE\\n");
-                                return 1;
-                        case CL_INVALID_DEVICE:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_INVALID_DEVICE\\n");
-                                return 1;
-                        case CL_INVALID_BINARY:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_INVALID_BINARY\\n");
-                                return 1;
-                        case CL_INVALID_BUILD_OPTIONS:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_INVALID_BUILD_OPTIONS\\n");
-                                return 1;
-                        case CL_INVALID_OPERATION:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_INVALID_OPERATION\\n");
-                                return 1;
-                        case CL_COMPILER_NOT_AVAILABLE:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_COMPILER_NOT_AVAILABLE\\n");
-                                return 1;
-                        case CL_BUILD_PROGRAM_FAILURE:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_BUILD_PROGRAM_FAILURE\\n");
-                                return 1;
-                        case CL_OUT_OF_HOST_MEMORY:
-                                fprintf(stderr, "clGetProgramBuildInfo CL_OUT_OF_HOST_MEMORY\\n");
-                                return 1;
-                        default:
-                                fprintf(stderr, "clGetProgramBuildInfo %d\\n", err);
-                                return 1;
-                }
-        }
-
-        cl_kernel kernel = clCreateKernel(program, kernel_name, &err);
-        switch (err) {
-                case CL_SUCCESS:
-                        break;
-                case CL_INVALID_PROGRAM:
-                        fprintf(stderr, "clCreateContext CL_INVALID_PROGRAM\\n");
-                        return 1;
-                case CL_INVALID_PROGRAM_EXECUTABLE:
-                        fprintf(stderr, "clCreateContext CL_INVALID_PROGRAM_EXECUTABLE\\n");
-                        return 1;
-                case CL_INVALID_VALUE:
-                        fprintf(stderr, "clCreateContext CL_INVALID_VALUE\\n");
-                        return 1;
-                case CL_INVALID_KERNEL_NAME:
-                        fprintf(stderr, "clCreateContext CL_INVALID_KERNEL_NAME\\n");
-                        return 1;
-                case CL_INVALID_KERNEL_DEFINITION:
-                        fprintf(stderr, "clCreateContext CL_INVALID_KERNEL_DEFINITION\\n");
-                        return 1;
-                case CL_OUT_OF_HOST_MEMORY:
-                        fprintf(stderr, "clCreateContext CL_OUT_OF_HOST_MEMORY\\n");
-                        return 1;
-                default:
-                        fprintf(stderr, "clCreateContext %d\\n", err);
-                        return 1;
-        }
-
-        return 0;
-}
-"""
-    return src
-
 
 def reproduce_clgen_build_failures(result):
     import analyze
@@ -285,38 +69,105 @@ def reproduce_clgen_build_failures(result):
     with open(result.program.id + '.cl', 'w') as outfile:
         print(result.program.src, file=outfile)
 
-    src = make_c_source(result.program.src)
-    with open(result.program.id + '.c', 'w') as outfile:
-        print(src, file=outfile)
+    # TODO:
+    # env = cldrive.OpenCLEnvironment(platform=result.testbed.platform,
+    #                                 device=result.testbed.device)
+    # src = cldrive.emit_c(env, result.program.src)
+    # with open(result.program.id + '.c', 'w') as outfile:
+    #     print(src, file=outfile)
 
-    # TODO: portable -I and -l flags
-    cli = ['gcc', '-xc', '-', '-lOpenCL']
-    process = Popen(cli, stdin=PIPE, stdout=PIPE, stderr=PIPE,
-                    universal_newlines=True)
-    stdout, stderr = process.communicate(src)
-    print('stdout:', stdout.rstrip())
-    print('stderr:', stderr.rstrip())
-    print('status:', process.returncode)
-    if process.returncode:
-        print("Failed to compile binary for", result.program.id)
-        sys.exit(1)
+    # # TODO: portable -I and -l flags
+    # cli = ['gcc', '-xc', '-', '-lOpenCL']
+    # process = Popen(cli, stdin=PIPE, stdout=PIPE, stderr=PIPE,
+    #                 universal_newlines=True)
+    # stdout, stderr = process.communicate(src)
+    # print('stdout:', stdout.rstrip())
+    # print('stderr:', stderr.rstrip())
+    # print('status:', process.returncode)
+    # if process.returncode:
+    #     print("Failed to compile binary for", result.program.id)
+    #     sys.exit(1)
 
-    cli = ['./a.out']
-    process = Popen(cli, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    stdout, stderr = process.communicate(src)
-    print('stdout:', stdout.rstrip())
-    print('stderr:', stderr.rstrip())
-    print('status:', process.returncode)
+    # cli = ['./a.out']
+    # process = Popen(cli, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    # stdout, stderr = process.communicate(src)
+    # print('stdout:', stdout.rstrip())
+    # print('stderr:', stderr.rstrip())
+    # print('status:', process.returncode)
 
-    if process.returncode:
-        print(">>> Reproduced using standalone binary")
-        sys.exit(0)
-    else:
-        print(">>> Failed to reproduce using standalone binary")
+    # if process.returncode:
+    #     print(">>> Reproduced using standalone binary")
+    #     sys.exit(0)
+    # else:
+    #     print(">>> Failed to reproduce using standalone binary")
 
 
-def get_available_cldrive_envs():
-    pass
+def generate_report_base(result):
+    return f"""\
+Operating System:  {result.testbed.host}
+
+OpenCL Device:     {result.testbed.device}
+OpenCL Platform:   {result.testbed.platform}
+OpenCL version:    {result.testbed.opencl}
+Driver version:    {result.testbed.driver}
+"""
+
+
+
+
+def generate_wrong_code_report(result):
+
+    def summarize_stdout(stdout):
+        components = [x for x in stdout.split(",") if x != ""]
+        ncomponents = len(components)
+        if len(set(components)) == 1:
+            return f"'{components[0]}' x {ncomponents}"
+        else:
+            return stdout
+
+    results = session.query(CLSmithResult)\
+        .filter(CLSmithResult.program == result.program,
+                CLSmithResult.params == result.params,
+                CLSmithResult.status == 0).all()
+
+    if len(results) > 2:
+        # Use voting to pick oracle.
+        outputs = [r.stdout for r in results]
+        majority_output, majority_count = Counter(outputs).most_common(1)[0]
+        if majority_count == 1:  # no majority
+            result.classification = "Wrong code"
+        elif result.stdout != majority_output:
+            result.classification = "Wrong code"
+    elif len(results) == 2:
+        if results[0].stdout != results[1].stdout:
+            majority_count = 1
+            result.classification = "Wrong code"
+            majority_output = "[UNKNOWN]"
+        else:
+            majority_count = 2
+
+    majority_devices = [
+        r.testbed for r in results if r.stdout == majority_output]
+
+    kernel_nlines = len(result.program.src.split('\n'))
+
+    majority_str = ", ".join(t.device for t in majority_devices)
+
+    program_output = summarize_stdout(result.stdout)
+    expected_output = summarize_stdout(majority_output)
+
+    return generate_report_base(result) + f"""
+OpenCL kernel:     {result.program.id} ({kernel_nlines} lines)
+
+Global size:       {result.params.gsize}
+Workgroup size:    {result.params.lsize}
+Optimizations:     {result.params.optimizations_on_off}
+
+Program output:    {program_output}
+Expected output:   {expected_output}
+Majority devices:  {majority_count}
+Correct devices:   {majority_str}
+"""
 
 
 if __name__ == "__main__":
@@ -328,13 +179,34 @@ if __name__ == "__main__":
     db.init(args.hostname)
     session = db.make_session()
 
-    for env in cldrive.all_envs():
-        testbed = db.get_testbed(session, env.platform, env.device)
+    clsmith_wrong_code_programs = session.query(CLSmithResult)\
+            .filter(CLSmithResult.classification == "Wrong code")
+    fs.mkdir("../data/difftest/unreduced/clsmith/wrong_code")
+    fs.mkdir("../data/difftest/unreduced/clsmith/wrong_code/reports")
+    for result in clsmith_wrong_code_programs:
+        with open(f"../data/difftest/unreduced/clsmith/wrong_code/{result.program.id}.cl", "w") as outfile:
+            print(result.program.src, file=outfile)
 
-        clgen_build_failures = session.query(CLgenResult)\
-            .filter(CLgenResult.testbed == testbed)\
-            .filter(CLgenResult.classification == 'Build failure')
-        for result in clgen_build_failures:
-            reproduce_clgen_build_failures(result)
+        with open(f"../data/difftest/unreduced/clsmith/wrong_code/reports/{result.id}.txt", "w") as outfile:
+            print(outfile.name)
+            print(generate_wrong_code_report(result), file=outfile)
+
+    # for env in cldrive.all_envs():
+    #     testbed = db.get_testbed(session, env.platform, env.device)
+
+    #     clsmith_wrong_code_bugs = session.query(CLSmithResult)\
+    #         .filter(CLSmithResult.testbed == testbed)\
+    #         .filter(CLSmithResult.classification == "Wrong code")
+    #     fs.mkdir("../data/difftest/unreduced/clsmith/wrong_code")
+    #     for result in clsmith_wrong_code_bugs:
+    #         print(result.program.id)
+    #         with open(f"../data/difftest/unreduced/clsmith/wrong_code/{result.program.id}.cl", "w") as outfile:
+    #             print(result.program.src, file=outfile)
+
+        # clgen_build_failures = session.query(CLgenResult)\
+        #     .filter(CLgenResult.testbed == testbed)\
+        #     .filter(CLgenResult.classification == 'Build failure')
+        # for result in clgen_build_failures:
+        #     reproduce_clgen_build_failures(result)
 
     print("done.")
