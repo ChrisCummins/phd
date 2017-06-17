df722a247ea5ad76b8c6bfe774ed24fa2c249192
Chris Cummins <chrisc.101@gmail.com>
2017-06-17 14:00:08 +0100

    learn/challenges: Add string reverse and mult table

---
+++ learn/challenges/021-string-reverse.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+
+void inplace_reverse_string(std::string& str) {
+  for (int i = 0; i < str.size() / 2; ++i) {
+    int j = str.size() - 1 - i;
+    std::swap(str[i], str[j]);
+  }
+}
+
+
+std::string reverse_sring(const std::string& str) {
+  return std::string(str.rbegin(), str.rend());
+}
+
+
+int main(int argc, char **argv) {
+  std::string a = "abc";
+  inplace_reverse_string(a);
+  assert(a == "cba");
+  std::cout << a << std::endl;
+
+  assert(reverse_string("abca") == "cba");
+  std::cout << reverse_sring("abc") << std::endl;
+
+  return 0;
+}
