ea87975cacf40cddf210f53cb67e56ddd86ad535
Chris Cummins <chrisc.101@gmail.com>
2017-06-19 16:10:45 +0100

    learn/challenges: Add first non-repeated char

---
+++ learn/challenges/023-first-nonrepeated-char.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def first_nonrepeated_char(s: str) -> str:
+    d = dict()
+    for c in s:
+        d[c] = d.get(c, 0) + 1
+    for c in s:
+        if d[c] == 1:
+            d[c] -= 1
+            if d[c] == 0:
+                return c
+    return None
+
+assert first_nonrepeated_char('aabbcddee') == 'c'
+assert first_nonrepeated_char('aabbccddee') == None
+assert first_nonrepeated_char('') == None
+assert first_nonrepeated_char('abcde') == 'a'
+assert first_nonrepeated_char('aabcde') == 'b'
+assert first_nonrepeated_char('aabcdebcd') == 'e'
