6c44fe8ca637c8cf319dccfb8e8c9e48c9c779f3
Chris Cummins <chrisc.101@gmail.com>
2017-06-26 09:47:31 +0100

    Non-interactive install

--- install.sh
+++ install.sh
@@ -479,7 +479,7 @@ install_python() {
         _apt_get_install software-properties-common  # provides add-apt-repository
 
         if ! dpkg -s python3.6 &>/dev/null ; then
-            sudo add-apt-repository ppa:jonathonf/python-3.6
+            sudo add-apt-repository -y ppa:jonathonf/python-3.6
             sudo apt-get update
             sudo apt-get install -y python3.6 python3.6-venv python3.6-dev python3-pip
         fi
