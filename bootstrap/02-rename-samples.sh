9e89c7150142b1dc09a3e711c4d664b18676fe38
Chris Cummins <chrisc.101@gmail.com>
2017-06-27 16:32:08 +0100

    Use CLgen with CLSmith test harness

---
+++ bootstrap/02-rename-samples.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eux
+
+samples_db="$(clgen --sampler-dir model.json sampler.json)/kernels.db"
+
+mkdir -pv ../data/bootstrap/
+cp -v "$samples_db" ../data/bootstrap/samples.db
+
+clgen db dump --dir ../data/bootstrap/samples.db ../data/bootstrap/samples
+
+for sample in $(ls ../data/bootstrap/samples/*); do
+    echo $sample
+done
