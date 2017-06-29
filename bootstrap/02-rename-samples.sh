da92e95fc36dfac7cd9a92641eb07e4697e7d113
Chris Cummins <chrisc.101@gmail.com>
2017-06-29 14:11:44 +0100

    bootstrap: Dump samples file

--- bootstrap/02-rename-samples.sh
+++ bootstrap/02-rename-samples.sh
@@ -9,6 +9,7 @@ cp -v "$samples_db" ../data/bootstrap/samples.db
 
 rm -rf ../data/bootstrap/samples
 clgen db dump --dir ../data/bootstrap/samples.db ../data/bootstrap/samples
+clgen db dump --file-sep ../data/bootstrap/samples.db ../data/bootstrap/samples.cl
 
 for sample in $(ls ../data/bootstrap/samples/*); do
     sed -i 's/__kernel void A/__kernel void entry/' "$sample"
