49a8ddce9e30e1508419bb692d30f336fab006c0
Chris Cummins <chrisc.101@gmail.com>
2017-07-02 14:51:14 +0100

    Release 0.0.4

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_requirements(path='requirements.txt'):
 
 setup(
     name='gh-archiver',
-    version='0.0.3.dev0',
+    version='0.0.4',
     description="Clone and update a GitHub user's repos locally.",
     url='https://github.com/ChrisCummins/gh-archiver',
     author='Chris Cummins',
