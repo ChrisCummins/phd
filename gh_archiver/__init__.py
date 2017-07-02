8bbe576c7acec43af6b11cbdee4972330438a726
Chris Cummins <chrisc.101@gmail.com>
2017-07-02 13:58:07 +0100

    Add support for gogs migrations

--- gh_archiver/__init__.py
+++ gh_archiver/__init__.py
@@ -23,10 +23,11 @@ Clone and update a GitHub user's repos locally.
 """
 import calendar
 import os
+import re
+import requests
 import shutil
 import sys
 import time
-import os
 
 from argparse import ArgumentParser, FileType, RawDescriptionHelpFormatter
 from configparser import ConfigParser
@@ -35,7 +36,7 @@ from git import Repo
 from github import Github, GithubException, NamedUser
 from github.Repository import Repository as GithubRepository
 from pathlib import Path
-from typing import Iterator, TextIO
+from typing import Iterator, TextIO, Tuple
 
 __copyright__ = "Copyright (C) 2017 Chris Cummins <chrisc.101@gmail.com>."
 
@@ -61,11 +62,21 @@ def get_argument_parser() -> ArgumentParser:
         "--githubrc", metavar="<file>", type=FileType("r"),
         default=os.path.join(os.path.expanduser("~"), ".githubrc"),
         help="Read GitHub username and password from <file>")
+    parser.add_argument(
+        "--gogs", action="store_true",
+        help="Mirror repsoitories to gogs instance")
+    parser.add_argument(
+        "--gogs-uid", type=int, default=1,
+        help="Gogs UID")
+    parser.add_argument(
+        "--gogsrc", metavar="<file>", type=FileType("r"),
+        default=os.path.join(os.path.expanduser("~"), ".gogsrc"),
+        help="Read Gogs server address and token from <file>")
     return parser
 
 
-def get_github_connection(githubrc: TextIO) -> Github:
-    """ connect to GitHub """
+def get_github_config(githubrc: TextIO) -> Github:
+    """ read GitHub config """
     config = ConfigParser()
     config.read(githubrc.name)
 
@@ -73,11 +84,27 @@ def get_github_connection(githubrc: TextIO) -> Github:
         github_username = config['User']['Username']
         github_pw = config['User']['Password']
     except KeyError as e:
-        print(f'config variable {e} not set. Check {args.githubrc}',
+        print(f'config variable {e} not set. Check ~/.githubrc',
               file=sys.stderr)
         sys.exit(1)
 
-    return Github(github_username, github_pw)
+    return github_username, github_pw
+
+
+def get_gogs_config(gogsrc: TextIO) -> Tuple[str, str]:
+    """ read gogs configuration """
+    config = ConfigParser()
+    config.read(gogsrc.name)
+
+    try:
+        server = config['Server']['Address']
+        token = config['User']['Token']
+    except KeyError as e:
+        print(f"config variable {e} not set. Check ~/.gogsrc",
+              file=sys.stderr)
+        sys.exit(1)
+
+    return server, token
 
 
 def get_repos(g: Github, username: str,
@@ -94,7 +121,11 @@ def main():
     try:
         parser = get_argument_parser()
         args = parser.parse_args()
-        g = get_github_connection(args.githubrc)
+        github_username, github_pw = get_github_config(args.githubrc)
+        g = Github(github_username, github_pw)
+
+        if args.gogs:
+            gogs_server, gogs_token = get_gogs_config(args.gogsrc)
 
         outdir = Path(args.outdir, parents=True, exists_ok=True)
         repos = list(get_repos(g, args.user, args.excludes))
@@ -104,7 +135,14 @@ def main():
             repo_names = [r.name for r in repos]
             for path in outdir.iterdir():
                 basename = os.path.basename(path)
-                if basename not in repo_names:
+
+                # strip .git suffix from gogs mirrors
+                if args.gogs:
+                    local_repo_name = re.sub("\.git$", "", basename)
+                else:
+                    local_repo_name = basename
+
+                if local_repo_name not in repo_names:
                     print(f"removing {basename}")
                     if path.is_dir():
                         shutil.rmtree(path)
@@ -112,24 +150,72 @@ def main():
                         os.remove(path)
 
         for repo in repos:
-            local_path = outdir / repo.name
+            if args.gogs:
+                local_path = outdir / Path(repo.name + ".git")
+            else:
+                local_path = outdir / repo.name
 
             # remove any file of the same name
             if local_path.exists() and not local_path.is_dir():
                 os.remove(local_path)
 
-            if local_path.is_dir():
-                print(f"updating {repo.name}")
-            else:
-                print(f"cloning {repo.name}:{repo.default_branch}")
-                Repo.clone_from(repo.git_url, local_path,
-                                branch=repo.default_branch)
+            if args.gogs:
 
-            local_repo = Repo(local_path)
-            for remote in local_repo.remotes:
-                remote.fetch()
-            for submodule in local_repo.submodules:
-                submodule.update(init=True)
+                def truncate(string: str, maxlen: int, suffix=" ...") -> str:
+                    """ truncate a string to a maximum length """
+                    if len(string) > maxlen:
+                        return string[:maxlen - len(suffix)] + suffix
+                    else:
+                        return string
+
+                # Mirror to gogs instance
+                if not local_path.is_dir():
+                    print(f"mirroring {repo.name} ... ", end="")
+                    sys.stdout.flush()
+                    headers = {
+                        "Authorization": f"token {gogs_token}",
+                    }
+                    data = {
+                        "auth_username": github_username,
+                        "auth_password": github_pw,
+                        "repo_name": repo.name,
+                        "clone_addr": repo.clone_url,
+                        "uid": args.gogs_uid,
+                        "description": truncate(repo.description, 255),
+                        "private": False,
+                        "mirror": True,
+                    }
+
+                    def pretty(d):
+                        import json
+                        print(json.dumps(d, sort_keys=True,
+                                         indent=4, separators=(',', ': ')))
+
+                    r = requests.post(gogs_server + "/api/v1/repos/migrate",
+                                      headers=headers, data=data)
+                    print(r.status_code)
+                    if r.status_code < 200 or r.status_code >= 300:
+                        pretty(headers)
+                        pretty(data)
+                        print()
+                        print("status", r.status_code)
+                        pretty(r.json())
+                        print(len(data["description"]))
+                        sys.exit(1)
+            else:
+                # Local clone
+                if local_path.is_dir():
+                    print(f"updating {repo.name}")
+                else:
+                    print(f"cloning {repo.name}:{repo.default_branch}")
+                    Repo.clone_from(repo.git_url, local_path,
+                                    branch=repo.default_branch)
+
+                local_repo = Repo(local_path)
+                for remote in local_repo.remotes:
+                    remote.fetch()
+                for submodule in local_repo.submodules:
+                    submodule.update(init=True)
     except KeyboardInterrupt:
         print()
         sys.exit(1)
