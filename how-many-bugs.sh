ef8585c0807875edf010b4922be4efb4ca9d3c80
Chris Cummins <chrisc.101@gmail.com>
2017-07-08 15:57:51 +0100

    update how-many-bugs

--- how-many-bugs.sh
+++ how-many-bugs.sh
@@ -2,6 +2,6 @@
 set -eux
 
 logfile=~/src/project_b/data/bug-reports/.logs/$(date -Idate).txt
-find -L ~/src/project_b/data/bug-reports -type f > $logfile
+find -L ~/src/project_b/data/bug-reports -type f | grep -v .logs/ > $logfile
 
 wc -l $logfile
