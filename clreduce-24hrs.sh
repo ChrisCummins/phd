993254b1ae770002c79dfe65a2818ea7f0b7c444
Chris Cummins <chrisc.101@gmail.com>
2017-07-09 18:01:00 +0100

    fix

--- clreduce-24hrs.sh
+++ clreduce-24hrs.sh
@@ -4,7 +4,7 @@ set -eux
 
 export PLATFORM_ID=$1
 export DEVICE_ID=$2
-export DEVICE_NAME=$4
+export DEVICE_NAME=$3
 
 lmk 'timeout -s 9 24h ./clreduce-run.sh $PLATFORM_ID $DEVICE_ID optimised $DEVICE_NAME'
 lmk 'timeout -s 9 24h ./clreduce-run.sh $PLATFORM_ID $DEVICE_ID unoptimised $DEVICE_NAME'
