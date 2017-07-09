d0841bb2a9c258a41c2801786a97f4c6b4576456
Chris Cummins <chrisc.101@gmail.com>
2017-07-09 18:00:08 +0100

    fixes

--- clreduce-run.sh
+++ clreduce-run.sh
@@ -6,8 +6,9 @@ cd ~/src/project_b/lib/clreduce
 
 export PLATFORM_ID=$1
 export DEVICE_ID=$2
-export DEVICE_NAME=$3
+export OPTIMISED=$3
+export DEVICE_NAME=$4
 
 while true; do
-    bash ./run.sh $PLATFORM_ID $DEVICE_ID optimised ~/src/project_b/data/clreduce/$DEVICE_NAME/on/$(date '+%Y-%m-%d-%H:%M')
+    bash ./run.sh $PLATFORM_ID $DEVICE_ID $OPTIMISED ~/src/project_b/data/clreduce/$DEVICE_NAME/$OPTIMISED/$(date '+%Y-%m-%d-%H:%M')
 done
