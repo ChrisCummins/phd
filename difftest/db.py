f74d44f5ec0ac8401fc6645caa0b09bbbf4389e7
Chris Cummins <chrisc.101@gmail.com>
2017-07-25 10:07:01 +0100

    db: Global engine

--- difftest/db.py
+++ difftest/db.py
@@ -14,6 +14,7 @@ from typing import Dict, List, Tuple
 Base = declarative_base()
 
 # must call init() first
+engine = None
 make_session = None
 
 
@@ -26,6 +27,7 @@ def get_mysql_creds() -> Tuple[str, str]:
 
 def init(hostname: str) -> str:
     """ must be called before using anything """
+    global engine
     global make_session
     username, password = get_mysql_creds()
     table = "project_b"
