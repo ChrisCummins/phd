d4b11f531ef049cc2314cfe15895632b115f962d
Chris Cummins <chrisc.101@gmail.com>
2017-07-25 20:48:06 +0100

    learn/challenges: 'start at' #

--- learn/challenges/027-missing-numer.py
+++ learn/challenges/027-missing-numer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-def finding_missing_number(lst):
+def trinum(n):
+    return n * (n + 1) // 2
+
+
+def finding_missing_number(lst, start_at=0):
     """
     Given an unordered list of n numbers in range 0...n, return the missing
     value.
@@ -8,10 +12,9 @@ def finding_missing_number(lst):
     Time: O(n)
     Space: O(1)
     """
-    n = len(lst)
-    expected_sum = (n * (n + 1)) // 2
+    expected_sum = trinum(len(lst) + start_at) - trinum(start_at)
     actual_sum = sum(lst)
-    return expected_sum - actual_sum
+    return expected_sum - actual_sum + start_at
 
 
 def test(actual, expected):
@@ -26,3 +29,7 @@ if __name__ == "__main__":
     test(finding_missing_number([0]), 1)
     test(finding_missing_number([0, 1]), 2)
     test(finding_missing_number([0, 2]), 1)
+
+    test(finding_missing_number([5, 6, 8], start_at=5), 7)
+    test(finding_missing_number([6, 7, 8], start_at=5), 5)
+    test(finding_missing_number([6, 7, 8], start_at=6), 9)
