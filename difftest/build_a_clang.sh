ed0c177761776978e6276ec67b1f0271996bb320
Chris Cummins <chrisc.101@gmail.com>
2017-08-05 18:36:44 +0100

    build a clang

---
+++ difftest/build_a_clang.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+build_a_clang() {
+    local dest=$1
+    git clone http://llvm.org/git/llvm.git $dest
+    cd $dest/tools
+    git clone http://llvm.org/git/clang.git
+    cd $dest/projects
+    git clone http://llvm.org/git/compiler-rt.git
+}
+
+build_a_clang llvm
