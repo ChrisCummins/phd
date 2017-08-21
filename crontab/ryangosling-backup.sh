77c595e6bca91f0ed7ef61dec6b8ecf8745e0513
Chris Cummins <chrisc.101@gmail.com>
2017-08-21 14:19:46 +0100

    Fix logdir creation

--- crontab/ryangosling-backup.sh
+++ crontab/ryangosling-backup.sh
@@ -5,6 +5,7 @@
 # ****************************************************************************
 # *                               Configuration                              *
 # ****************************************************************************
+export USER="cec"
 export JOB_TIMEOUT=21600  # 6 hrs
 export LMK="/usr/local/bin/lmk"
 export LMK_TO="chrisc.101@gmail.com"
@@ -22,6 +23,11 @@ export EMU="/usr/local/bin/emu"
 set -eux
 
 if [[ -z "${1:-}" ]]; then
+    # create log directory, and delete old logs
+    sudo mkdir -pv "$EMU_LOGDIR"
+    sudo chown $USER -R "$EMU_LOGDIR"
+    sudo find "$EMU_LOGDIR" -mtime +$OLDEST_LOG_DAYS -exec rm -v {} \;
+
     # entry point
     $LMK --only-errors "timeout $JOB_TIMEOUT $0 --porcelain" 2>&1 | tee "$EMU_LOGDIR/$(date '+%Y-%m-%d-%H%M%S').txt"
 else
@@ -32,11 +38,7 @@ else
     mkdir -pv ~/.local/etc
     crontab -l > $CRONTAB_BACKUP
 
-    sudo $RM_DSSTORE /home/cec
-
-    # create log directory, and delete old logs
-    sudo mkdir -pv "$EMU_LOGDIR"
-    sudo find "$EMU_LOGDIR" -mtime +$OLDEST_LOG_DAYS -exec rm -v {} \;
+    sudo $RM_DSSTORE "/home/$USER"
 
     # create new backup
     cd /
