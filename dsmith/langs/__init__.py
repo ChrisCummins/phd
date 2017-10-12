5993242a414c8d4ed4f035032cf5f5d189c70a93
Chris Cummins <chrisc.101@gmail.com>
2017-10-12 17:03:02 +0100

    interface work

--- dsmith/langs/__init__.py
+++ dsmith/langs/__init__.py
@@ -1,17 +1,59 @@
+#
+# Copyright 2017 Chris Cummins <chrisc.101@gmail.com>.
+#
+# This file is part of DeepSmith.
+#
+# DeepSmith is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# DeepSmith is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# DeepSmith.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Programming language.
+
+Attributes:
+    __languages__ (Dict[str, Language]): List of all available languages.
+"""
+
+class Generator(object):
+    @property
+    def num_programs(self) -> int:
+        raise NotImplementedError("abstract class")
+
+
 class Language(object):
-    pass
+    """
+    Abstract interface for a programming language.
+
+    Attributes:
+        __generators__ (List[Generator]): List of available generators.
+    """
+    def __init__(self):
+        raise NotImplementedError("abstract class")
+
+    def mkgenerator(self, name: str) -> Generator:
+        raise NotImplementedError("abstract class")
 
 # Deferred importing of languages, since the modules may need to import this
 # file.
 from dsmith.langs.opencl import OpenCL
 
-LANGUAGES = {
+__languages__ = {
     "opencl": OpenCL,
 }
 
+def mklang(name: str) -> Language:
+    """
 
-def mklang(string: str) -> Language:
-    lang = LANGUAGES.get(string)
+    """
+    lang = __languages__.get(name)
     if not lang:
         raise ValueError("Unknown language")
     return lang()
--- dsmith/langs/opencl/__init__.py
+++ dsmith/langs/opencl/__init__.py
@@ -1,9 +1,25 @@
+#
+# Copyright 2017 Chris Cummins <chrisc.101@gmail.com>.
+#
+# This file is part of DeepSmith.
+#
+# DeepSmith is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# DeepSmith is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# DeepSmith.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+The OpenCL programming language.
+"""
 import dsmith
-from dsmith.langs import Language
-
-
-class Generator(object):
-    pass
+from dsmith.langs import Generator, Language
 
 
 class CLSmith(Generator):
@@ -23,8 +39,8 @@ class OpenCL(Language):
         "clsmith": CLSmith,
     }
 
-    def mkgenerator(self, string: str) -> Generator:
-        generator = self.__generators__.get(string)
+    def mkgenerator(self, name: str) -> Generator:
+        generator = self.__generators__.get(name)
         if not generator:
             raise ValueError("Unknown generator")
         return generator()
