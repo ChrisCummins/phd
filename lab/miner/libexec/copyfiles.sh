795bb3913e7bec46e07d5f160d3401e744176542
Chris Cummins <chrisc.101@gmail.com>
2017-10-31 11:28:07 +0000

    wip on miner

---
+++ lab/miner/libexec/copyfiles.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eu
+
+main() {
+    local language="$1"
+    local extension="$2"
+
+    local indir=~/dsmith/miner/repos/$language
+    local outdir=~/dsmith/miner/files/$language
+    mkdir -pv "$outdir"
+
+    find "$indir" -type f -name '*'$extension | grep -v .git/ | while read f; do
+        sha1sum=$(md5sum "$f" | awk '{print $1};')
+        dst="$outdir/$sha1sum$extension"
+        [ -f "$dst" ] || cp -v "$f" "$dst"
+        chmod -x "$dst"
+    done
+}
+main $@
---
+++ lab/miner/mkcorpus-glsl.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# ./mine.py 1000 glsl
+./libexec/copyfiles.sh glsl .glsl
+./libexec/copyfiles.sh glsl .frag
+./libexec/copyfiles.sh glsl .vert
+./libexec/copyfiles.sh glsl .tesc
+./libexec/copyfiles.sh glsl .tese
+./libexec/copyfiles.sh glsl .geom
+./libexec/copyfiles.sh glsl .comp
