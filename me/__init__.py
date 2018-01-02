37d3df29bea09bf6694dfd075d5b075dbcaa61bf
Chris Cummins <chrisc.101@gmail.com>
2018-01-02 15:04:02 +0000

    Tiny whitespace fixes

--- me/__init__.py
+++ me/__init__.py
@@ -3,6 +3,7 @@ me - Aggregate health and time tracking data.
 """
 import datetime
 
+
 def daterange(start_date, end_date, reverse=False):
     """ returns an iterator over the specified date range """
     for n in range(int((end_date - start_date).days)):
