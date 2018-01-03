a893001704b7f0d68ae9176a7e349641ec2c0a8a
Chris Cummins <chrisc.101@gmail.com>
2018-01-03 14:53:42 +0000

    scripts: Update MySQL backup script

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -1,9 +1,13 @@
-host=cc1
-db=DeepSmith_1
-dstdir=~/src/dsmith/data
-
 set -eux
 
-ssh $host "mysqldump $db > /tmp/$db.mysql"
-ssh $host "tar cjvf $dstdir/$db-$(date '+%Y-%m-%d').mysql.tar.bz2 -C /tmp $db.mysql"
-ssh $host "rm -v /tmp/$db.mysql"
+main() {
+    local db=$1
+    local dstdir=$2
+
+    set -eux
+
+    mysqldump $db > /tmp/$db.mysql
+    tar cjvf $dstdir/$db-$(date '+%Y-%m-%d').mysql.tar.bz2 -C /tmp $db.mysql
+    rm -v /tmp/$db.mysql
+}
+main $@
