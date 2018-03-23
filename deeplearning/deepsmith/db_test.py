40994f3b4d2dea4f1da55bd072cd342614de4a80
Chris Cummins <chrisc.101@gmail.com>
2018-03-22 23:06:40 -0700

    Add db.Test abstract test methods.

--- deeplearning/deepsmith/db_test.py
+++ deeplearning/deepsmith/db_test.py
@@ -8,6 +8,50 @@ from deeplearning.deepsmith import db
 from deeplearning.deepsmith import toolchain
 
 
+def test_Table_GetOrAdd_abstract():
+  with pytest.raises(NotImplementedError):
+    db.Table.GetOrAdd('session', 'proto')
+
+
+def test_Table_ToProto_abstract():
+  with pytest.raises(NotImplementedError):
+    db.Table().ToProto()
+
+
+def test_Table_SetProto_abstract():
+  with pytest.raises(NotImplementedError):
+    db.Table().SetProto('proto')
+
+
+def test_Table_ProtoFromFile_abstract():
+  with pytest.raises(NotImplementedError):
+    db.Table.ProtoFromFile('path')
+
+
+def test_Table_FromFile_abstract():
+  with pytest.raises(NotImplementedError):
+    db.Table.FromFile('session', 'path')
+
+
+def test_Table_abstract_methods():
+  table = db.Table()
+  with pytest.raises(NotImplementedError):
+    db.Table.GetOrAdd('session', 'proto')
+  with pytest.raises(NotImplementedError):
+    table.ToProto()
+  with pytest.raises(NotImplementedError):
+    table.SetProto('proto')
+  with pytest.raises(NotImplementedError):
+    db.Table.ProtoFromFile('path')
+  with pytest.raises(NotImplementedError):
+    db.Table.FromFile('session', 'path')
+
+
+def test_Table_repr():
+  string = str(db.Table())
+  assert string == 'TODO: Define Table.ToProto() method'
+
+
 def test_StringTable_GetOrAdd_StringTooLongError(session):
   toolchain.Toolchain.GetOrAdd(session, 'a' * toolchain.Toolchain.maxlen)
   with pytest.raises(db.StringTooLongError):
