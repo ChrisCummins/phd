f97589f820849c69c59aafb132e43e9a37823a5c
Chris Cummins <chrisc.101@gmail.com>
2018-05-04 17:31:58 -1000

    Fix BUILD style.

--- lib/labm8/make_test.py
+++ lib/labm8/make_test.py
@@ -10,33 +10,33 @@ from lib.labm8 import make
 
 # make()
 def test_make():
-  ret, out, err = make.make(dir="lib/labm8/test/data/makeproj")
-  assert 0 == ret
-  assert True == out is not None
-  assert True == fs.isfile("lib/labm8/test/data/makeproj/foo")
-  assert True == fs.isfile("lib/labm8/test/data/makeproj/foo.o")
+  ret, out, err = make.make(dir="lib/labm8/data/test/makeproj")
+  assert not ret
+  assert out
+  assert fs.isfile("lib/labm8/data/test/makeproj/foo")
+  assert fs.isfile("lib/labm8/data/test/makeproj/foo.o")
 
 
 def test_make_bad_target():
   with pytest.raises(make.NoTargetError):
-    make.make(target="bad-target", dir="lib/labm8/test/data/makeproj")
+    make.make(target="bad-target", dir="lib/labm8/data/test/makeproj")
 
 
 def test_make_bad_target():
   with pytest.raises(make.NoMakefileError):
     make.make(dir="/bad/path")
   with pytest.raises(make.NoMakefileError):
-    make.make(target="foo", dir="lib/labm8/test/data")
+    make.make(target="foo", dir="lib/labm8/data/test")
 
 
 def test_make_fail():
   with pytest.raises(make.MakeError):
-    make.make(target="fail", dir="lib/labm8/test/data/makeproj")
+    make.make(target="fail", dir="lib/labm8/data/test/makeproj")
 
 
 # clean()
 def test_make_clean():
-  fs.cd("lib/labm8/test/data/makeproj")
+  fs.cd("lib/labm8/data/test/makeproj")
   make.make()
   assert fs.isfile("foo")
   assert fs.isfile("foo.o")
