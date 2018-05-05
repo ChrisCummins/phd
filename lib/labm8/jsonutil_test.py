7b56c0364131b383698283aee28178595fa1fb54
Chris Cummins <chrisc.101@gmail.com>
2018-05-04 17:17:51 -1000

    Update unit tests.

--- lib/labm8/jsonutil_test.py
+++ lib/labm8/jsonutil_test.py
@@ -1,61 +1,75 @@
+"""Unit tests for //lib/labm8:jsonutil."""
+import sys
+
+import pytest
+from absl import app
+
 from lib.labm8 import fs
 from lib.labm8 import jsonutil
 from lib.labm8 import system
-from lib.labm8.tests.testutil import TestCase
 
 
-class TestJsonutil(TestCase):
-  def test_loads(self):
-    a_str = """{
-            "a": 1,  // this has comments
-            "b": [1, 2, 3]
-        } # end comment
-        // begin with comment
-        """
-    a = jsonutil.loads(a_str)
+def test_loads():
+  a_str = """{
+          "a": 1,  // this has comments
+          "b": [1, 2, 3]
+      } # end comment
+      // begin with comment
+      """
+  a = jsonutil.loads(a_str)
+  assert a == {
+    'a': 1,
+    'b': [1, 2, 3]
+  }
 
-    self.assertEqual(a["a"], 1)
-    self.assertEqual(a["b"], [1, 2, 3])
-    self.assertFalse("c" in a)
 
-  def test_loads_malformed(self):
-    a_str = """bad json {asd,,}"""
-    with self.assertRaises(ValueError):
-      jsonutil.loads(a_str)
+def test_loads_malformed():
+  a_str = """bad json {asd,,}"""
+  with pytest.raises(ValueError):
+    jsonutil.loads(a_str)
 
-  def test_read_file(self):
-    a_str = """{
-            "a": 1,  // this has comments
-            "b": [1, 2, 3]
-        } # end comment
-        // begin with comment
-        """
-    system.echo(a_str, "/tmp/labm8.loaf.json")
-    a = jsonutil.read_file("/tmp/labm8.loaf.json")
 
-    self.assertEqual(a["a"], 1)
-    self.assertEqual(a["b"], [1, 2, 3])
-    self.assertFalse("c" in a)
+def test_read_file():
+  a_str = """{
+          "a": 1,  // this has comments
+          "b": [1, 2, 3]
+      } # end comment
+      // begin with comment
+      """
+  system.echo(a_str, "/tmp/labm8.loaf.json")
+  a = jsonutil.read_file("/tmp/labm8.loaf.json")
+  assert a == {
+    'a': 1,
+    'b': [1, 2, 3]
+  }
 
-  def test_read_file_bad_path(self):
-    with self.assertRaises(fs.File404):
-      jsonutil.read_file("/not/a/real/path")
 
-    self.assertEqual({}, jsonutil.read_file("/not/a/real/path",
-                                            must_exist=False))
+def test_read_file_bad_path():
+  with pytest.raises(fs.File404):
+    jsonutil.read_file("/not/a/real/path")
+  assert not jsonutil.read_file("/not/a/real/path", must_exist=False)
 
-  def test_write_file(self):
-    d1 = {
-      "a": "1",
-      "b": "2"
-    }
-    jsonutil.write_file("/tmp/labm8.write_file.json", d1)
-    d2 = jsonutil.read_file("/tmp/labm8.write_file.json")
-    fs.rm("/tmp/labm8.write_file.json")
 
-    jsonutil.write_file("/tmp/labm8.write_file2.json", d1)
-    d3 = jsonutil.read_file("/tmp/labm8.write_file2.json")
-    fs.rm("/tmp/labm8.write_file2.json")
+def test_write_file():
+  d1 = {
+    "a": "1",
+    "b": "2"
+  }
+  jsonutil.write_file("/tmp/labm8.write_file.json", d1)
+  d2 = jsonutil.read_file("/tmp/labm8.write_file.json")
+  fs.rm("/tmp/labm8.write_file.json")
 
-    self.assertEqual(d1, d2)
-    self.assertEqual(d1, d3)
+  jsonutil.write_file("/tmp/labm8.write_file2.json", d1)
+  d3 = jsonutil.read_file("/tmp/labm8.write_file2.json")
+  fs.rm("/tmp/labm8.write_file2.json")
+
+  assert d1 == d2 == d3
+
+
+def main(argv):  # pylint: disable=missing-docstring
+  del argv
+  sys.exit(pytest.main([__file__, '-v']))
+
+
+if __name__ == '__main__':
+  app.run(main)
