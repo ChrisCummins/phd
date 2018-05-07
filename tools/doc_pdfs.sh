587595288204c39e2b6d1235d2e907fe28eee6e5
Chris Cummins <chrisc.101@gmail.com>
2018-05-07 16:27:58 +0100

    Add a tool to generate in-tree PDFs of docs.

---
+++ tools/doc_pdfs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# doc_pdfs.sh - Build PDFs of documents and track them.
+#
+# Usage:
+#
+#     ./doc_pdfs.sh
+#
+set -eu
+
+# Directory of this script.
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Run from the workspace root directory.
+cd "$DIR/.."
+
+bazel build //docs:all
+
+for name in $(bazel query //docs:all | sed -r 's,^//docs:(.*)$,\1,'); do
+  cp -v bazel-genfiles/docs/$name.pdf docs/$name.pdf
+  chmod 664 docs/$name.pdf
+  git add docs/$name.pdf
+done
