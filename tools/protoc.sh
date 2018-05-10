b9ede32461475f867a7a7db2ceeaf065b42d2816
Chris Cummins <chrisc.101@gmail.com>
2018-05-10 14:52:34 +0100

    Generate GRPC code for protobufs.

--- tools/protoc.sh
+++ tools/protoc.sh
@@ -14,6 +14,14 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT"
 for file in $(git ls-files | grep '\.proto'); do
   dir="$(dirname $file)"
-  python -m grpc_tools.protoc -I"$dir" -I"$ROOT" --python_out="$dir" \
-      --grpc_python_out="$dir" "$file"
+  no_extension=${file%.proto}
+  # Remove previously generated code
+  rm -f ${no_extension}_pb2.py
+  rm -f ${no_extension}_pb2_grpc.py
+  python -m grpc_tools.protoc -I"$ROOT" -I"$dir" \
+      --python_out="$dir" --grpc_python_out="$dir" "$file"
+  # Fix the imports of generated GRPC code. This is a workaround for issue
+  # https://github.com/grpc/grpc/issues/9575#issuecomment-293934506
+  sed -i 's/^import /from . import /' ${no_extension}_pb2_grpc.py
+  sed -i 's/^from . import grpc$/import grpc/' ${no_extension}_pb2_grpc.py
 done
