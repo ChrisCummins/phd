25bc97c8f7ae53a3ebf23adf55dedc9dd89902bb
Chris Cummins <chrisc.101@gmail.com>
2018-05-21 13:58:17 +0100

    Re-use the same clgen cache for all tests.

--- deeplearning/clgen/conftest.py
+++ deeplearning/clgen/conftest.py
@@ -9,9 +9,12 @@ from absl import flags
 FLAGS = flags.FLAGS
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session')
 def clgen_cache_dir() -> str:
   """Creates a temporary directory and sets CLGEN_CACHE to it.
+
+  This fixture has session scope, meaning that the clgen cache directory
+  is shared by all unit tests.
 
   Returns:
     The location of $CLGEN_CACHE.
