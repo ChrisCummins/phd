fc734f3ca79599fbf0c42a376f3770b35b8b0cb6
Chris Cummins <chrisc.101@gmail.com>
2018-05-22 21:36:40 +0100

    Remove license header from unit tests.

--- deeplearning/clgen/preprocess_test.py
+++ deeplearning/clgen/preprocess_test.py
@@ -1,21 +1,4 @@
-#
-# Copyright 2016, 2017, 2018 Chris Cummins <chrisc.101@gmail.com>.
-#
-# This file is part of CLgen.
-#
-# CLgen is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# CLgen is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with CLgen.  If not, see <http://www.gnu.org/licenses/>.
-#
+"""Unit tests for //deeplearning/clgen/preprocess.py."""
 import sys
 
 import pytest
