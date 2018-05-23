2ee681cbb687cce71cfe0d74161d0f21e8061bcd
Chris Cummins <chrisc.101@gmail.com>
2018-05-24 00:56:16 +0100

    Work in progress on clgen module.

--- deeplearning/clgen/preprocessors/clang.py
+++ deeplearning/clgen/preprocessors/clang.py
@@ -1,19 +1,39 @@
 """This file contains utility code for working with clang."""
+import json
+import pathlib
+import re
 import subprocess
-from typing import List
+import typing
 
 from absl import flags
 from absl import logging
 
 from config import getconfig
+from deeplearning.clgen import dbutil
 from deeplearning.clgen import errors
 
 
 FLAGS = flags.FLAGS
 
 _config = getconfig.GetGlobalConfig()
+# Path to clang binary.
 CLANG = _config.paths.cc
+# Path to clang-format binary.
 CLANG_FORMAT = _config.paths.clang_format
+# Path to opt binary.
+OPT = _config.paths.opt
+# The marker used to mark stdin from clang pre-processor output.
+CLANG_STDIN_MARKER = re.compile(r'# \d+ "<stdin>" 2')
+# Options to pass to clang-format.
+# See: http://clang.llvm.org/docs/ClangFormatStyleOptions.html
+CLANG_FORMAT_CONFIG = {'BasedOnStyle': 'Google', 'ColumnLimit': 500,
+                       'IndentWidth': 2, 'AllowShortBlocksOnASingleLine': False,
+                       'AllowShortCaseLabelsOnASingleLine': False,
+                       'AllowShortFunctionsOnASingleLine': False,
+                       'AllowShortLoopsOnASingleLine': False,
+                       'AllowShortIfStatementsOnASingleLine': False,
+                       'DerivePointerAlignment': False,
+                       'PointerAlignment': 'Left'}
 
 
 def StripPreprocessorLines(src: str) -> str:
@@ -29,7 +49,7 @@ def StripPreprocessorLines(src: str) -> str:
   i = 0
   # Determine when the final included file ends.
   for i in range(len(lines) - 1, -1, -1):
-    if lines[i] == '# 1 "<stdin>" 2':
+    if CLANG_STDIN_MARKER.match(lines[i]):
       break
   else:
     return ''
@@ -37,7 +57,7 @@ def StripPreprocessorLines(src: str) -> str:
   return '\n'.join([line for line in lines[i:] if not line.startswith('#')])
 
 
-def Preprocess(src: str, cflags: List[str], timeout_seconds: int = 60,
+def Preprocess(src: str, cflags: typing.List[str], timeout_seconds: int = 60,
                strip_preprocessor_lines: bool = True):
   """Run input code through the compiler frontend to inline macros.
 
@@ -54,7 +74,8 @@ def Preprocess(src: str, cflags: List[str], timeout_seconds: int = 60,
     The preprocessed code.
 
   Raises:
-
+    ClangException: In case of an error.
+    ClangTimeout: If clang does not complete before timeout_seconds.
   """
   builtin_cflags = ['-E', '-c', '-', '-o', '-']
   cmd = ['timeout', '-s9', str(timeout_seconds),
@@ -72,3 +93,217 @@ def Preprocess(src: str, cflags: List[str], timeout_seconds: int = 60,
     return StripPreprocessorLines(stdout)
   else:
     return stdout
+
+
+def CompileLlvmBytecode(src: str, cflags: typing.List[str],
+                        timeout_seconds: int = 60) -> str:
+  """Compile input code into textual LLVM byte code.
+
+  Args:
+    src: The source code to compiler.
+    cflags: A list of flags to be passed to clang.
+    timeout_seconds: The number of seconds to allow before killing clang.
+
+  Returns:
+    The textual LLVM byte code.
+
+  Raises:
+    ClangException: In case of an error.
+    ClangTimeout: If clang does not complete before timeout_seconds.
+  """
+  # TODO(cec): I haven't the willpower to get this work again.
+  builtin_cflags = ['-S', '-emit-llvm', '-o', '-']
+  path = pathlib.Path('/tmp/input')
+  try:
+    with open(path, 'w') as f:
+      f.write(src)
+    with open(path) as f:
+      print(f.read())
+    cmd = ['timeout', '-s9', str(timeout_seconds),
+           CLANG, ] + builtin_cflags + cflags + [str(path)]
+    logging.debug('$ %s', ' '.join(cmd))
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, universal_newlines=True)
+    stdout, stderr = process.communicate()
+  finally:
+    path.unlink()
+  if process.returncode == 9:
+    raise errors.ClangTimeout(f'Clang timed out after {timeout_seconds}s')
+  elif process.returncode != 0:
+    raise errors.ClangException(stderr)
+  return stdout
+
+
+def ClangFormat(src: str, timeout_seconds: int = 60) -> str:
+  """Run clang-format on a source to enforce code style.
+
+  Args:
+    src: The source code to run through clang-format.
+    timeout_seconds: The number of seconds to allow clang-format to run for.
+
+  Returns:
+    The output of clang-format.
+
+  Raises:
+    ClangFormatException: In case of an error.
+    ClangTimeout: If clang-format does not complete before timeout_seconds.
+  """
+  cmd = ["timeout", "-s9", str(timeout_seconds), CLANG_FORMAT,
+         '-style={}'.format(json.dumps(CLANG_FORMAT_CONFIG))]
+  logging.debug('$ %s', ' '.join(cmd))
+  process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, universal_newlines=True)
+  stdout, stderr = process.communicate(src)
+  if process.returncode == 9:
+    raise errors.ClangTimeout(
+      f'Clang-format timed out after {timeout_seconds}s')
+  elif process.returncode != 0:
+    raise errors.ClangFormatException(stderr)
+  return stdout
+
+
+_instcount_re = re.compile(
+  r"^(?P<count>\d+) instcount - Number of (?P<type>.+)")
+
+
+def parse_instcounts(txt: str) -> typing.Dict[str, int]:
+  """Parse LLVM opt instruction counts pass.
+
+  Parameters
+  ----------
+  txt : str
+      LLVM output.
+
+  Returns
+  -------
+  Dict[str, int]
+      key, value pairs, where key is instruction type and value is
+      instruction type count.
+  """
+  lines = [x.strip() for x in txt.split("\n")]
+  counts = {}
+
+  # Build a list of counts for each type.
+  for line in lines:
+    match = re.search(_instcount_re, line)
+    if match:
+      count = int(match.group("count"))
+      key = match.group("type")
+      if key in counts:
+        counts[key].append(count)
+      else:
+        counts[key] = [count]
+
+  # Sum all counts.
+  for key in counts:
+    counts[key] = sum(counts[key])
+
+  return counts
+
+
+def instcounts2ratios(counts: typing.Dict[str, int]) -> typing.Dict[str, float]:
+  """
+  Convert instruction counts to instruction densities.
+
+  If, for example, there are 10 instructions and 2 addition instructions,
+  then the instruction density of add operations is .2.
+
+  Parameters
+  ----------
+  counts : Dict[str, int]
+      Key value pairs of instruction types and counts.
+
+  Returns
+  -------
+  ratios : Dict[str, float]
+      Key value pairs of instruction types and densities.
+  """
+  if not len(counts):
+    return {}
+
+  ratios = {}
+  total_key = "instructions (of all types)"
+  non_ratio_keys = [total_key]
+  total = float(counts[total_key])
+
+  for key in non_ratio_keys:
+    ratios[dbutil.escape_sql_key(key)] = counts[key]
+
+  for key in counts:
+    if key not in non_ratio_keys:
+      # Copy count
+      ratios[dbutil.escape_sql_key(key)] = counts[key]
+      # Insert ratio
+      ratio = float(counts[key]) / total
+      ratios[dbutil.escape_sql_key('ratio_' + key)] = ratio
+
+  return ratios
+
+
+def bytecode_features(bc: str, id: str = 'anon', timeout: int = 60) -> \
+    typing.Dict[str, float]:
+  """
+  Extract features from bytecode.
+
+  Parameters
+  ----------
+  bc : str
+      LLVM bytecode.
+  id : str, optional
+      Name of OpenCL source.
+
+  Returns
+  -------
+  Dict[str, float]
+      Key value pairs of instruction types and densities.
+
+  Raises
+  ------
+  OptException
+      If LLVM opt pass errors.
+  """
+  cmd = ["timeout", "-s9", str(timeout), OPT, '-analyze', '-stats',
+         '-instcount', '-']
+
+  # LLVM pass output pritns to stderr, so we'll pipe stderr to
+  # stdout.
+  logging.debug('$ %s', ' '.join(cmd))
+  process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT, universal_newlines=True)
+  stdout, _ = process.communicate(bc)
+
+  if process.returncode != 0:
+    raise errors.OptException(stdout)
+
+  instcounts = parse_instcounts(stdout)
+  instratios = instcounts2ratios(instcounts)
+
+  return instratios
+
+
+def verify_bytecode_features(bc_features: typing.Dict[str, float],
+                             id: str = 'anon') -> None:
+  """
+  Verify LLVM bytecode features.
+
+  Parameters
+  ----------
+  bc_features : Dict[str, float]
+      Bytecode features.
+  id : str, optional
+      Name of OpenCL source.
+
+  Raises
+  ------
+  InstructionCountException
+      If verification errors.
+  """
+  # The minimum number of instructions before a kernel is discarded
+  # as ugly.
+  min_num_instructions = 1
+  num_instructions = bc_features.get('instructions_of_all_types', 0)
+
+  if num_instructions < min_num_instructions:
+    raise errors.InstructionCountException(
+      'Code contains {} instructions. The minimum allowed is {}'.format(
+        num_instructions, min_num_instructions))
--- deeplearning/clgen/preprocessors/clang_test.py
+++ deeplearning/clgen/preprocessors/clang_test.py
@@ -23,6 +23,8 @@ class MockProcess():
     return '', ''
 
 
+# StripPreprocessorLines() tests.
+
 def test_StripPreprocessorLines_empty_input():
   assert clang.StripPreprocessorLines('') == ''
 
@@ -56,6 +58,8 @@ This will # not be stripped.
 This will # not be stripped."""
 
 
+# Preprocess() tests.
+
 def test_Preprocess_process_command(mocker):
   """Test the process comand which is run."""
   mock_Popen = mocker.patch('subprocess.Popen')
@@ -84,6 +88,11 @@ def test_Preprocess_ClangException(mocker):
   mock_Popen.return_value = MockProcess(1)
   with pytest.raises(errors.ClangException):
     clang.Preprocess('', [])
+
+
+def test_Preprocess_empty_file():
+  """Test the preprocessor output with an empty file."""
+  assert clang.Preprocess('', []) == '\n'
 
 
 def test_Preprocess_simple_c_program():
@@ -152,11 +161,6 @@ int main(int argc, char** argv) { return 0; }
   assert "invalid preprocessing directive" in str(e_info.value)
 
 
-def test_Preprocess_empty_file():
-  """Test the preprocessor output with an empty file."""
-  assert clang.Preprocess('', []) == '\n'
-
-
 def test_Preprocess_no_strip_processor_lines():
   """Test that stdin marker is preserved if no strip_preprocessor_lines."""
   assert '# 1 "<stdin>" 2' in clang.Preprocess("""
@@ -164,15 +168,179 @@ int main(int argc, char** argv) { return 0; }
 """, [], strip_preprocessor_lines=False)
 
 
-@pytest.mark.skip()
 def test_Preprocess_include_stdio_strip():
   """Test that an included file is stripped."""
   assert clang.Preprocess("""
 #include <stdio.h>
 int main(int argc, char** argv) { return NULL; }
-""", []) == """
-int main(int argc, char** argv) { return 0; }
+""", []) == """\
+int main(int argc, char** argv) { return ((void *)0); }
 """
+
+
+# ClangFormat() tests.
+
+def test_ClangFormat_process_command(mocker):
+  """Test the clang-format comand which is run."""
+  mock_Popen = mocker.patch('subprocess.Popen')
+  mock_Popen.return_value = MockProcess(0)
+  clang.ClangFormat('')
+  subprocess.Popen.assert_called_once()
+  cmd = subprocess.Popen.call_args_list[0][0][0]
+  assert cmd[:3] == ['timeout', '-s9', '60']
+
+
+def test_ClangFormat_ClangTimeout(mocker):
+  """Test that ClangTimeout is raised if clang-format returns with SIGKILL."""
+  mock_Popen = mocker.patch('subprocess.Popen')
+  mock_Popen.return_value = MockProcess(9)
+  with pytest.raises(errors.ClangTimeout):
+    clang.ClangFormat('')
+  # ClangTimeout inherits from ClangException.
+  with pytest.raises(errors.ClangException):
+    clang.ClangFormat('')
+
+
+def test_ClangFormat_ClangException(mocker):
+  """Test that ClangException is raised if clang-format returns non-zero."""
+  mock_Popen = mocker.patch('subprocess.Popen')
+  mock_Popen.return_value = MockProcess(1)
+  with pytest.raises(errors.ClangFormatException):
+    clang.ClangFormat('')
+
+
+def test_ClangFormat_empty_file():
+  """Test the preprocessor output with an empty file."""
+  assert clang.ClangFormat('') == ''
+
+
+def test_ClangFormat_simple_c_program():
+  """Test that a simple C program is unchanged."""
+  assert clang.ClangFormat("""
+int main(int argc, char** argv) { return 0; }
+""") == """
+int main(int argc, char** argv) {
+  return 0;
+}
+"""
+
+
+def test_ClangFormat_pointer_alignment():
+  """Test that pointers are positioned left."""
+  assert clang.ClangFormat("""
+int * A(int* a, int * b, int *c);
+""") == """
+int* A(int* a, int* b, int* c);
+"""
+
+
+def test_ClangFormat_undefined_data_type():
+  """Test that an undefined data type does not cause an error."""
+  assert clang.ClangFormat("""
+int main(MY_TYPE argc, char** argv) { return 0; }
+""") == """
+int main(MY_TYPE argc, char** argv) {
+  return 0;
+}
+"""
+
+
+def test_ClangFormat_undefined_variable():
+  """Test that an undefined variable does not cause an error."""
+  assert clang.ClangFormat("""
+int main(int argc, char** argv) { return UNDEFINED_VARIABLE; }
+""") == """
+int main(int argc, char** argv) {
+  return UNDEFINED_VARIABLE;
+}
+"""
+
+
+def test_ClangFormat_undefined_function():
+  """Test that an undefined function does not cause an error."""
+  assert clang.ClangFormat("""
+int main(int argc, char** argv) { return UNDEFINED_FUNCTION(0); }
+""") == """
+int main(int argc, char** argv) {
+  return UNDEFINED_FUNCTION(0);
+}
+"""
+
+
+def test_ClangFormat_invalid_preprocessor_directive():
+  """Test that an invalid preprocessor directive does not raise an error."""
+  assert clang.ClangFormat("""
+#this_is_not_a_valid_directive
+int main(int argc, char** argv) { return 0; }
+""") == """
+#this_is_not_a_valid_directive
+int main(int argc, char** argv) {
+  return 0;
+}
+"""
+
+
+@pytest.mark.skip(reason='TODO(cec): Bytecode compiler')
+def test_CompilerLlvmBytecode_empty_file():
+  """Test that bytecode is produced for good code."""
+  assert not clang.CompileLlvmBytecode("int main() {}", [])
+
+
+@pytest.mark.skip(reason='TODO(cec): Bytecode compiler')
+def test_compile_cl_bytecode_undefined_type():
+  """Test that error is raised when kernel contains undefined type."""
+  with pytest.raises(errors.ClangException):
+    clang.CompileLlvmBytecode("kernel void A(global FLOAT_T* a) {}", "<anon>",
+                              use_shim=False)
+
+
+@pytest.mark.skip(reason='TODO(cec): Bytecode compiler')
+def test_compile_cl_bytecode_shim_type():
+  """Test that bytecode is produced for code with shim type."""
+  assert clang.CompileLlvmBytecode("kernel void A(global FLOAT_T* a) {}",
+                                   "<anon>", use_shim=True)
+
+
+@pytest.mark.skip(reason="TODO(cec): Bytecode compiler")
+def test_bytecode_features_empty_code():
+  # Generated by CompileLlvmBytecode from: 'kernel void A(global float* a) {}'.
+  bc = """\
+; ModuleID = '-'
+source_filename = "-"
+target datalayout = "e-i64:64-v16:16-v32:32-n16:32:64"
+target triple = "nvptx64-nvidia-nvcl"
+
+; Function Attrs: noinline norecurse nounwind readnone
+define spir_kernel void @A(float addrspace(1)* nocapture) local_unnamed_addr 
+#0 !kernel_arg_addr_space !4 !kernel_arg_access_qual !5 !kernel_arg_type !6 
+!kernel_arg_base_type !6 !kernel_arg_type_qual !7 {
+  ret void
+}
+
+attributes #0 = { noinline norecurse nounwind readnone 
+"correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" 
+"less-precise-fpmad"="false" "no-frame-pointer-elim"="true" 
+"no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" 
+"no-jump-tables"="false" "no-nans-fp-math"="false" 
+"no-signed-zeros-fp-math"="false" "no-trapping-math"="false" 
+"stack-protector-buffer-size"="8" "target-features"="-satom" 
+"unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!nvvm.annotations = !{!0}
+!llvm.module.flags = !{!1}
+!opencl.ocl.version = !{!2}
+!llvm.ident = !{!3}
+
+!0 = !{void (float addrspace(1)*)* @A, !"kernel", i32 1}
+!1 = !{i32 1, !"wchar_size", i32 4}
+!2 = !{i32 1, i32 0}
+!3 = !{!"clang version 5.0.0 (tags/RELEASE_500/final)"}
+!4 = !{i32 1}
+!5 = !{!"none"}
+!6 = !{!"float*"}
+!7 = !{!""}
+"""
+  assert clang.bytecode_features(bc, "<anon>")
 
 
 def main(argv):
