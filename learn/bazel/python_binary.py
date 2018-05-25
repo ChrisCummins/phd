5e0f5366d334ee5256c7679f4fc53f25610c1fa9
Chris Cummins <chrisc.101@gmail.com>
2018-05-25 17:23:46 +0100

    Add an experiment in bazel paths.

---
+++ learn/bazel/python_binary.py
@@ -0,0 +1,35 @@
+"""A test python binary which reads a data file and runs a data binary."""
+import os
+import subprocess
+import sys
+
+from absl import app
+from absl import flags
+
+from lib.labm8 import bazelutil
+
+
+FLAGS = flags.FLAGS
+
+DATA_FILE = bazelutil.DataPath('phd/learn/bazel/data_file.txt')
+DATA_BINARY = bazelutil.DataPath('phd/learn/bazel/data_binary')
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError('Unrecognized command line flags.')
+  print('Hello from python', sys.executable)
+  print('Sanboxed?', bazelutil.IsBazelSandbox())
+  print('File location:', __file__)
+  print('Current working directory:', os.getcwd())
+  with open(DATA_FILE) as f:
+    print('Data file:', f.read().rstrip())
+  p = subprocess.Popen([DATA_BINARY], stdout=subprocess.PIPE,
+                       universal_newlines=True)
+  stdout, _ = p.communicate()
+  print('Data binary:', stdout.rstrip())
+
+
+if __name__ == '__main__':
+  app.run(main)
