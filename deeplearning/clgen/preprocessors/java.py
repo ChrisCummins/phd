6cfb6be011a8fa52b6b5408f792dc18f8ba39b6f
Chris Cummins <chrisc.101@gmail.com>
2018-05-25 03:40:46 +0100

    Add a java:Compile preprocessor.

--- deeplearning/clgen/preprocessors/java.py
+++ deeplearning/clgen/preprocessors/java.py
@@ -1,11 +1,22 @@
 """Preprocessor modules for the Java programming language."""
-from absl import flags
 
+import pathlib
+import re
+import subprocess
+import tempfile
+import typing
+
+from absl import flags
+from absl import logging
+
+from deeplearning.clgen import errors
 from deeplearning.clgen.preprocessors import clang
 from deeplearning.clgen.preprocessors import public
 
 
 FLAGS = flags.FLAGS
+
+CLASS_NAME_RE = re.compile(r'public\s+class\s+(\w+)')
 
 
 @public.clgen_preprocessor
@@ -23,3 +34,49 @@ def ClangFormat(text: str) -> str:
     ClangTimeout: If clang-format does not complete before timeout_seconds.
   """
   return clang.ClangFormat(text, '.java')
+
+
+def Javac(text: str, class_name: str, cflags: typing.List[str],
+          timeout_seconds: int = 60) -> str:
+  """Run code through javac.
+
+  Args:
+    text: The code to compile.
+    class_name: The name of the class defined in the file.
+    cflags: Additional options passed to javac.
+    timeout_seconds: The number of seconds to wait before killing javac.
+
+  Returns:
+    The unmodified input code.
+  """
+  with tempfile.TemporaryDirectory('w', prefix='clgen_javac_') as d:
+    path = pathlib.Path(d) / (class_name + '.java')
+    with open(path, 'w') as f:
+      f.write(text)
+    cmd = ['timeout', '-s9', str(timeout_seconds), 'javac', f.name] + cflags
+    logging.info('$ %s', ' '.join(cmd))
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, universal_newlines=True)
+    stdout, stderr = process.communicate()
+  if process.returncode == 9:
+    raise errors.BadCodeException(f'Javac timed out after {timeout_seconds}s')
+  elif process.returncode != 0:
+    raise errors.BadCodeException(stderr)
+  return text
+
+
+@public.clgen_preprocessor
+def Compile(text: str) -> str:
+  """A preprocessor which attempts to compile a class file for the given code.
+
+  Args:
+    text: Code to compile.
+
+  Returns:
+    The input code, unmodified.
+  """
+  match = CLASS_NAME_RE.search(text)
+  if not match:
+    raise errors.BadCodeException('Failed to determine class name')
+  class_name = match.group(1)
+  return Javac(text, class_name, [])
--- deeplearning/clgen/preprocessors/java_test.py
+++ deeplearning/clgen/preprocessors/java_test.py
@@ -5,6 +5,7 @@ import pytest
 from absl import app
 from absl import flags
 
+from deeplearning.clgen import errors
 from deeplearning.clgen.preprocessors import java
 
 
@@ -13,7 +14,8 @@ FLAGS = flags.FLAGS
 
 # ClangFormat() tests.
 
-def test_ClangFormat_java_hello_world():
+def test_ClangFormat_hello_world():
+  """Test formatting of a "hello world" Java program."""
   assert java.ClangFormat("""
 public class HelloWorld {
     public static void main(String [] args) {
@@ -28,7 +30,8 @@ public class HelloWorld {
 """
 
 
-def test_ClangFormat_java_long_line():
+def test_ClangFormat_long_line():
+  """Test that extremely long lines are not wrapped."""
   assert java.ClangFormat("""
 public class VeryVeryLongNameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 extends VeryVeryLongNameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeBase {
@@ -36,6 +39,50 @@ extends VeryVeryLongNameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeBase {
 """) == """
 public class VeryVeryLongNameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee \
 extends VeryVeryLongNameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeBase {}
+"""
+
+
+# Compile() tests.
+
+def test_Compile_empty_input():
+  """That an empty file is rejected."""
+  with pytest.raises(errors.BadCodeException):
+    java.Compile('')
+
+
+def test_Compile_hello_world():
+  """Test compilation of a "hello world" Java program."""
+  assert java.Compile("""
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
+""") == """
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
+"""
+
+
+def test_Compile_class_name_whitespace():
+  """Test that compile can infer the class name despite whitespace."""
+  assert java.Compile("""
+public
+class     HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
+""") == """
+public
+class     HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
 """
 
 
