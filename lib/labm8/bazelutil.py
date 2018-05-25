86a2b04aaabb55469494d6b9bd91c3abeea53dfe
Chris Cummins <chrisc.101@gmail.com>
2018-05-25 17:23:26 +0100

    Add a lib.labm8.bazelutil module.

---
+++ lib/labm8/bazelutil.py
@@ -0,0 +1,68 @@
+"""A module for working within Bazel's hemetic and sandboxed world.
+
+The design goals of Bazel has led it to have a lot of quirks. This module
+contains utility code designed to help live with these quirks.
+"""
+import os
+import pathlib
+import re
+import typing
+
+from config import getconfig
+
+
+# Regular expression to everything in a path up until the '*.runfiles'
+# directory, e.g. for the path '/private/var/bazel/foo/bar.runfiles/a/b/c',
+# this regex will match '/private/var/bazel/foo/bar.runfiles'
+RUNFILES_PATTERN = re.compile(r'^(.*\.runfiles)/')
+
+
+def IsBazelSandbox() -> bool:
+  """Determine if the current process is running in a bazel sandbox.
+
+  Returns:
+    True if the current process is running inside a bazel sandbox, else false.
+  """
+  config = getconfig.GetGlobalConfig()
+  return not __file__.startswith(config.paths.repo_root)
+
+
+def FindRunfilesDirectory() -> typing.Optional[pathlib.Path]:
+  """Find the '.runfiles' directory, if there is one.
+
+  Returns:
+    The absolute path of the runfiles directory, else None if not found.
+  """
+  # Follow symlinks, looking for my module space
+  stub_filename = os.path.abspath(__file__)
+  module_space = stub_filename + '.runfiles'
+  if os.path.isdir(module_space):
+    return pathlib.Path(module_space)
+  match = RUNFILES_PATTERN.match(os.path.abspath(__file__))
+  if match:
+    return pathlib.Path(match.group(1))
+  return None
+
+
+def DataPath(path: typing.Union[str, pathlib.Path]) -> pathlib.Path:
+  """Return the absolute path to a data file.
+
+  This allows you to access files from the 'data' attribute of a Python
+  target in Bazel. This is needed because the path to the file changes depending
+  on whether the current process is executing with a 'bazel run' environment, or
+  as a 'bazel-bin' script.
+
+  Args:
+    path: The path to the data, including the name of the workspace.
+
+  Returns:
+    An absolute file path.
+
+  Raises:
+    FileNotFoundError: If the requested path is not found.
+  """
+  runfiles = FindRunfilesDirectory()
+  real_path = runfiles / path if runfiles else pathlib.Path(path).absolute()
+  if not real_path.is_file() or real_path.is_dir():
+    raise FileNotFoundError(f"No such file or directory: '{path}'")
+  return real_path
---
+++ lib/labm8/bazelutil_test.py
@@ -0,0 +1,72 @@
+"""Unit tests for //lib/labm8/bazelutil.py."""
+import sys
+import tempfile
+
+import pytest
+from absl import app
+
+from lib.labm8 import bazelutil
+from lib.labm8 import fs
+
+
+# IsBazelSandbox() tests.
+
+def test_IsBazelSandbox_different_working_directories():
+  """Test IsBazelSandboxed() returns the same result from different dirs."""
+  sandbox = bazelutil.IsBazelSandbox()
+  # We can't test the expected value of this since we don't know it.
+  assert sandbox or True
+  with tempfile.TemporaryDirectory() as d:
+    with fs.chdir(d):
+      assert bazelutil.IsBazelSandbox() == sandbox
+  with fs.chdir('/tmp'):
+    assert bazelutil.IsBazelSandbox() == sandbox
+
+
+# DataPath() tests.
+
+def test_DataPath_path_not_found():
+  """Test that FileNotFoundError is raised if the file is not found."""
+  with pytest.raises(FileNotFoundError) as e_info:
+    bazelutil.DataPath('')
+  assert f"No such file or directory: ''" in str(e_info)
+
+  with pytest.raises(FileNotFoundError) as e_info:
+    bazelutil.DataPath('/not/a/real/path')
+  assert f"No such file or directory: '/not/a/real/path'" in str(e_info)
+
+
+def test_DataPath_missing_data_dep():
+  """FileNotFoundError is raised if the file exists is not in target data."""
+  # The file //lib/labm8/data/test/diabetes.csv exists, but is not a data
+  # dependency of this test target, so is not found.
+  with pytest.raises(FileNotFoundError) as e_info:
+    bazelutil.DataPath('phd/lib/labm8/data/test/diabetes.csv')
+  assert ("No such file or directory: "
+          "'phd/lib/labm8/data/test/diabetes.csv'") in str(e_info)
+
+
+def test_DataPath_read_file():
+  """Test that DataPath is correct for a known data file."""
+  with open(bazelutil.DataPath('phd/lib/labm8/data/test/hello_world')) as f:
+    assert f.read() == 'Hello, world!\n'
+
+
+def test_DataPath_different_working_dir():
+  """Test that DataPath is not affected by current working dir."""
+  p = bazelutil.DataPath('phd/lib/labm8/data/test/hello_world')
+  with fs.chdir('/tmp'):
+    assert bazelutil.DataPath('phd/lib/labm8/data/test/hello_world') == p
+  with tempfile.TemporaryDirectory() as d:
+    with fs.chdir(d):
+      assert bazelutil.DataPath('phd/lib/labm8/data/test/hello_world') == p
+
+
+def main(argv):
+  if len(argv) > 1:
+    raise app.UsageError('Unrecognized command line flags.')
+  sys.exit(pytest.main([__file__, '-vv']))
+
+
+if __name__ == '__main__':
+  app.run(main)
