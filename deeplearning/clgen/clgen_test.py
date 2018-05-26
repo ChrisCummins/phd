b32e5cb35a49648ba015bd6ae866a8c2b270bf37
Chris Cummins <chrisc.101@gmail.com>
2018-05-26 20:36:57 +0100

    Rename cli module to clgen.

--- deeplearning/clgen/clgen_test.py
+++ deeplearning/clgen/clgen_test.py
@@ -7,7 +7,7 @@ import tempfile
 import pytest
 from absl import app
 
-from deeplearning.clgen import cli
+from deeplearning.clgen import clgen
 from deeplearning.clgen.tests import testlib as tests
 from lib.labm8 import fs
 from lib.labm8 import pbutil
@@ -17,7 +17,7 @@ from lib.labm8 import tar
 def test_run(clgen_cache_dir):
   """Test that cli.run() returns correct value for function call."""
   del clgen_cache_dir
-  assert cli.run(lambda a, b: a // b, 4, 2) == 2
+  assert clgen.run(lambda a, b: a // b, 4, 2) == 2
 
 
 # def test_run_exception_handler(clgen_cache_dir):
@@ -32,50 +32,50 @@ def test_run_exception_debug(clgen_cache_dir):
   del clgen_cache_dir
   os.environ["DEBUG"] = "1"
   with pytest.raises(ZeroDivisionError):
-    cli.run(lambda a, b: a // b, 1, 0)
+    clgen.run(lambda a, b: a // b, 1, 0)
 
 
 def test_cli_test_cache_path(clgen_cache_dir):
   del clgen_cache_dir
   with pytest.raises(SystemExit):
-    cli.main("test --cache-path".split())
+    clgen.main("test --cache-path".split())
 
 
 def test_cli_test_coverage_path(clgen_cache_dir):
   del clgen_cache_dir
   with pytest.raises(SystemExit):
-    cli.main("test --coverage-path".split())
+    clgen.main("test --coverage-path".split())
 
 
 def test_cli_test_coveragerc_path(clgen_cache_dir):
   del clgen_cache_dir
   with pytest.raises(SystemExit):
-    cli.main("test --coveragerc-path".split())
+    clgen.main("test --coveragerc-path".split())
 
 
 def test_cli(clgen_cache_dir):
   del clgen_cache_dir
   fs.rm("kernels.db")
-  cli.main("db init kernels.db".split())
+  clgen.main("db init kernels.db".split())
   assert fs.exists("kernels.db")
 
   corpus_path = tests.archive("tiny", "corpus")
-  cli.main("db explore kernels.db".split())
-  cli.main(f"fetch fs kernels.db {corpus_path}".split())
-  cli.main("preprocess kernels.db".split())
-  cli.main("db explore kernels.db".split())
+  clgen.main("db explore kernels.db".split())
+  clgen.main(f"fetch fs kernels.db {corpus_path}".split())
+  clgen.main("preprocess kernels.db".split())
+  clgen.main("db explore kernels.db".split())
 
   fs.rm("kernels_out")
-  cli.main("db dump kernels.db -d kernels_out".split())
+  clgen.main("db dump kernels.db -d kernels_out".split())
   assert fs.isdir("kernels_out")
   assert len(fs.ls("kernels_out")) >= 1
 
   fs.rm("kernels.cl")
-  cli.main("db dump kernels.db kernels.cl --file-sep --eof --reverse".split())
+  clgen.main("db dump kernels.db kernels.cl --file-sep --eof --reverse".split())
   assert fs.isfile("kernels.cl")
 
   fs.rm("kernels_out")
-  cli.main("db dump kernels.db --input-samples -d kernels_out".split())
+  clgen.main("db dump kernels.db --input-samples -d kernels_out".split())
   assert fs.isdir("kernels_out")
   assert len(fs.ls("kernels_out")) == 250
 
@@ -91,9 +91,9 @@ def test_cli_train(clgen_cache_dir, abc_model_config):
       fs.cp(tests.data_path("pico", "corpus.tar.bz2"), './corpus.tar.bz2')
       tar.unpack_archive('corpus.tar.bz2')
       pbutil.ToFile(abc_model_config, pathlib.Path('model.pbtxt'))
-      cli.main("--corpus-dir model.pbtxt".split())
-      cli.main("--model-dir model.pbtxt".split())
-      cli.main("-v train model.pbtxt".split())
+      clgen.main("--corpus-dir model.pbtxt".split())
+      clgen.main("--model-dir model.pbtxt".split())
+      clgen.main("-v train model.pbtxt".split())
 
 
 def test_cli_sample(clgen_cache_dir, abc_model_config, abc_sampler_config):
@@ -103,16 +103,16 @@ def test_cli_sample(clgen_cache_dir, abc_model_config, abc_sampler_config):
       fs.cp(tests.data_path("pico", "corpus.tar.bz2"), './corpus.tar.bz2')
       pbutil.ToFile(abc_model_config, pathlib.Path('model.pbtxt'))
       pbutil.ToFile(abc_sampler_config, pathlib.Path('sampler.pbtxt'))
-      cli.main("--corpus-dir model.pbtxt".split())
-      cli.main("--model-dir model.pbtxt".split())
-      cli.main("--sampler-dir model.pbtxt sampler.pbtxt".split())
-      cli.main("ls files model.pbtxt sampler.pbtxt".split())
+      clgen.main("--corpus-dir model.pbtxt".split())
+      clgen.main("--model-dir model.pbtxt".split())
+      clgen.main("--sampler-dir model.pbtxt sampler.pbtxt".split())
+      clgen.main("ls files model.pbtxt sampler.pbtxt".split())
 
 
 def test_cli_ls(clgen_cache_dir):
   del clgen_cache_dir
-  cli.main("ls models".split())
-  cli.main("ls samplers".split())
+  clgen.main("ls models".split())
+  clgen.main("ls samplers".split())
 
 
 def main(argv):
