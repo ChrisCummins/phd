c9a49057c48566c3b4d56cdb1bad92f2cc92d3ca
Chris Cummins <chrisc.101@gmail.com>
2018-05-29 16:02:59 +0100

    Update scraper to use raw queries as inputs.

--- datasets/github/scrape_repos/scraper.py
+++ datasets/github/scrape_repos/scraper.py
@@ -20,7 +20,6 @@ from absl import logging
 from github import Repository
 
 from datasets.github.scrape_repos.proto import scrape_repos_pb2
-from lib.labm8 import fs
 from lib.labm8 import labdate
 from lib.labm8 import pbutil
 
@@ -64,20 +63,24 @@ def MakeRepositoryMetas(repos: typing.List[Repository.Repository],
       pbutil.ToFile(meta, meta_path)
 
 
-class LanguageScraper(threading.Thread):
-  """Find popular repositories on GitHub and record their metadata.
+class QueryScraper(threading.Thread):
+  """Scrape repository metadata from the results of GitHub search query.
 
   An instance of this class is executed as a thread.
   """
 
-  def __init__(self, language: scrape_repos_pb2.LanguageToClone):
-    """Instantiate a LanguageScraper.
+  def __init__(self, language: scrape_repos_pb2.LanguageToClone,
+               query: scrape_repos_pb2.GitHubRepositoryQuery):
+    """Instantiate a QueryScraper.
 
     Args:
       language: A LanguageToClone instance.
+      query: The query to run.
     """
     self.language = language
+    self.repo_query = query
     self.destination_directory = pathlib.Path(language.destination_directory)
+    self.i = 0
     credentials = ReadGitHubCredentials()
     github_connection = github.Github(credentials.username,
                                       credentials.password)
@@ -86,28 +89,20 @@ class LanguageScraper(threading.Thread):
     while True:
       try:
         self.query = github_connection.search_repositories(
-            f'language:{language.language} sort:stars fork:false')
-        self.total_num_repos_on_github = self.query.totalCount
+            self.repo_query.string)
+        self.total_result_count = self.query.totalCount
         break
       except github.RateLimitExceededException:
         logging.debug('Pausing on GitHub rate limit')
         time.sleep(3)
     self.next_page_num = 0
-    super(LanguageScraper, self).__init__()
+    super(QueryScraper, self).__init__()
 
-  def GetNumberOfRepoMetas(self) -> int:
-    """Get the number of repositories which have been cloned.
+  def GetNumberOfResultsProcessed(self) -> int:
+    """Get the number of results which have been processed."""
+    return self.i
 
-    Returns:
-      The number of repos which have been cloned.
-    """
-    if self.destination_directory.is_dir():
-      return len([x for x in self.destination_directory.iterdir() if
-                  x.suffix == '.pbtxt'])
-    else:
-      return 0
-
-  def GetNextBatchOfRepositories(self) -> typing.List[Repository.Repository]:
+  def GetNextBatchOfResults(self) -> typing.List[Repository.Repository]:
     """Get the next batch of repositories to clone from the query.
 
     Returns:
@@ -122,7 +117,7 @@ class LanguageScraper(threading.Thread):
         self.next_page_num += 1
         return page
       except github.RateLimitExceededException:
-        logging.info('Pausing on GitHub rate limit')
+        logging.debug('Pausing on GitHub rate limit')
         time.sleep(3)
       except github.GithubException:
         # One possible cause for this exception is when trying to request
@@ -142,43 +137,40 @@ class LanguageScraper(threading.Thread):
     if not repos:
       # An empty list indicates that we've run out of query results.
       return True
-    num_scraped_repos = self.GetNumberOfRepoMetas()
-    if num_scraped_repos >= self.language.num_repos_to_clone:
-      return True
-    if num_scraped_repos >= self.total_num_repos_on_github:
-      logging.warning('Ran out of repositories to scrape')
-      return True
-    return False
+    else:
+      return self.i >= self.repo_query.max_results
 
   def run(self) -> None:
     """Execute the worker thread."""
     self.destination_directory.mkdir(parents=True, exist_ok=True)
-    repos = self.GetNextBatchOfRepositories()
+    repos = self.GetNextBatchOfResults()
     while not self.IsDone(repos):
-      num_remaining = (
-          self.language.num_repos_to_clone - self.GetNumberOfRepoMetas())
+      num_remaining = (self.repo_query.max_results - self.i)
       repos = repos[:num_remaining]
       MakeRepositoryMetas(repos, self.destination_directory)
-      repos = self.GetNextBatchOfRepositories()
+      self.i += len(repos)
+      repos = self.GetNextBatchOfResults()
 
 
-def RunScraper(worker: LanguageScraper) -> None:
+def RunQuery(worker: QueryScraper) -> None:
   """Run a language scraper with an asynchronously updating progress bar.
 
   Args:
-    worker: A LanguageScraper worker instance.
+    worker: A QueryScraper worker instance.
   """
-  logging.info('Scraping %s of %s %s repos on GitHub ...',
-               humanize.intcomma(worker.language.num_repos_to_clone),
-               humanize.intcomma(worker.total_num_repos_on_github),
-               worker.language.language)
-  bar = progressbar.ProgressBar(max_value=worker.language.num_repos_to_clone,
+  sys.stderr.flush()
+  logging.info("Query '%s' returned %s results. Processing first %s ...",
+               worker.repo_query.string,
+               humanize.intcomma(worker.total_result_count),
+               humanize.intcomma(worker.repo_query.max_results))
+  bar = progressbar.ProgressBar(max_value=worker.repo_query.max_results,
                                 redirect_stderr=True)
   worker.start()
   while worker.is_alive():
-    bar.update(worker.GetNumberOfRepoMetas())
+    bar.update(worker.GetNumberOfResultsProcessed())
     worker.join(.5)
-  bar.update(worker.GetNumberOfRepoMetas())
+  bar.update(worker.GetNumberOfResultsProcessed())
+  bar.finish()
   sys.stderr.write('\n')
   sys.stderr.flush()
 
@@ -205,33 +197,17 @@ def GetRepositoryMetadata(
   return meta
 
 
-class RepositoryCloneWorker(object):
-  """An object which clones a git repository from GitHub."""
+def GetNumberOfRepoMetas(language: scrape_repos_pb2.LanguageToClone) -> int:
+  """Get the number of repository metafiles for the language.
 
-  def __init__(self, destination_directory: pathlib.Path):
-    """Instantiate a RepositoryCloneWorker.
-
-    Args:
-      destination_directory: The root directory to clone to.
-    """
-    self.destination_directory = destination_directory
-
-  def __call__(self, repo: Repository.Repository) -> None:
-    """Clone a git repository from GitHub.
-
-    Args:
-      repo: A Repository instance.
-    """
-    concat_name = '_'.join([repo.owner.login, repo.name])
-
-    clone_dir = self.destination_directory / concat_name
-    meta_path = pathlib.Path(str(clone_dir) + '.pbtxt')
-    if not pbutil.ProtoIsReadable(meta_path,
-                                  scrape_repos_pb2.GitHubRepoMetadata()):
-      fs.rm(meta_path)
-      meta = GetRepositoryMetadata(repo)
-      logging.debug('%s', meta)
-      pbutil.ToFile(meta, meta_path)
+  Returns:
+    The number of repos which have been cloned.
+  """
+  path = pathlib.Path(language.destination_directory)
+  if path.is_dir():
+    return len([x for x in path.iterdir() if x.suffix == '.pbtxt'])
+  else:
+    return 0
 
 
 def main(argv) -> None:
@@ -247,7 +223,15 @@ def main(argv) -> None:
                                scrape_repos_pb2.LanguageCloneList())
 
   for language in clone_list.language:
-    RunScraper(LanguageScraper(language))
+    logging.info('Scraping %s repos using %s queries ...',
+                 language.language, humanize.intcomma(len(language.query)))
+    for query in language.query:
+      RunQuery(QueryScraper(language, query))
+
+  logging.info('Finished scraping. Indexed repository counts:')
+  for language in clone_list.language:
+    logging.info('  %s: %s', language.language,
+                 humanize.intcomma(GetNumberOfRepoMetas(language)))
 
 
 if __name__ == '__main__':
