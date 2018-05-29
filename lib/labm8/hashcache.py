50527969e354a5c4fbd87842feb7778dace173f2
Chris Cummins <chrisc.101@gmail.com>
2018-05-29 21:48:45 +0100

    Add support for files to HashCache.

---
+++ lib/labm8/hashcache.py
@@ -0,0 +1,132 @@
+"""A hash cache for the filesystem.
+
+Checksums files and directories and cache results. If a file or directory has
+not been modified, subsequent hashes are cache hits. Hashes are recomputed
+lazily, when a directory (or any of its subdirectories) have been modified.
+"""
+import datetime
+import os
+import pathlib
+import typing
+
+import checksumdir
+import sqlalchemy as sql
+import time
+from absl import flags
+from sqlalchemy.ext import declarative
+
+from lib.labm8 import crypto
+from lib.labm8 import fs
+from lib.labm8 import sqlutil
+
+
+FLAGS = flags.FLAGS
+
+Base = declarative.declarative_base()
+
+
+class HashCacheRecord(Base):
+  """A hashed file or directory."""
+  __tablename__ = 'entries'
+
+  # The absolute path to a file or directory.
+  absolute_path: str = sql.Column(sql.String(4096), primary_key=True)
+  # The timestamp of the cache entry, in seconds since the epoch.
+  date_cached: int = sql.Column(sql.Integer, nullable=False)
+  # The cached hash in hexadecimal encoding. We use the length of the longest
+  # supported hash function: sha256.
+  hash: str = sql.Column(sql.String(40), nullable=False)
+
+
+def GetDirectoryMTime(path: pathlib.Path) -> int:
+  """Get the timestamp of the most recently modified file/dir in directory.
+
+  Recursively checks subdirectory contents.
+
+  Params:
+    abspath: The absolute path to the directory.
+
+  Returns:
+    The seconds since epoch of the last modification.
+  """
+  return int(max(
+      max(os.path.getmtime(os.path.join(root, file)) for file in files) for
+      root, _, files in os.walk(path)))
+
+
+class HashCache(sqlutil.Database):
+
+  def __init__(self, path: pathlib.Path, hash_fn: str):
+    """Instantiate a hash cache.
+
+    Args:
+      path:
+      hash_fn: The name of the hash function. One of: md5, sha1, sha256.
+
+    Raises:
+      ValueError: If hash_fn not recognized.
+    """
+    super(HashCache, self).__init__(path, Base)
+    self.hash_fn_name = hash_fn
+    if hash_fn == 'md5':
+      self.hash_fn_file = crypto.md5_file
+    elif hash_fn == 'sha1':
+      self.hash_fn_file = crypto.sha1_file
+    elif hash_fn == 'sha256':
+      self.hash_fn_file = crypto.sha256_file
+    else:
+      raise ValueError(f"Hash function not recognized: '{hash_fn}'")
+
+  def GetHash(self, path: pathlib.Path) -> str:
+    """Get the hash of a file or directory.
+
+    Args:
+      path: Path to the file or directory.
+
+    Returns:
+      Hexadecimal string hash.
+
+    Raises:
+      FileNotFoundError: If the requested path does not exist.
+    """
+    if path.is_file():
+      return self._HashFile(path)
+    elif path.is_dir():
+      return self._HashDirectory(path)
+    else:
+      raise FileNotFoundError(f"File not found: '{path}'")
+
+  def Clear(self):
+    """Empty the cache."""
+    with self.Session(commit=True) as session:
+      session.query(HashCacheRecord).delete()
+
+  def _HashDirectory(self, absolute_path: pathlib.Path) -> str:
+    if fs.directory_is_empty(absolute_path):
+      last_modified = int(time.time())
+    else:
+      last_modified = GetDirectoryMTime(absolute_path)
+    return self._DoHash(absolute_path, last_modified,
+                        lambda x: checksumdir.dirhash(x, self.hash_fn_name))
+
+  def _HashFile(self, absolute_path: pathlib.Path) -> str:
+    return self._DoHash(absolute_path, os.path.getmtime(absolute_path),
+                        self.hash_fn_file)
+
+  def _DoHash(self, absolute_path: pathlib.Path,
+              last_modified: datetime.datetime,
+              hash_fn: typing.Callable[[pathlib.Path], str]) -> str:
+    with self.Session() as session:
+      cached_entry = session.query(HashCacheRecord).filter(
+          HashCacheRecord.absolute_path == str(absolute_path)).first()
+      if cached_entry and cached_entry.date_cached == last_modified:
+        return cached_entry
+      elif cached_entry:
+        session.delete(cached_entry)
+      new_entry = HashCacheRecord(
+          absolute_path=str(absolute_path),
+          date_cached=last_modified,
+          hash=hash_fn(absolute_path))
+      session.add(new_entry)
+      session.commit()
+      return new_entry.hash
---
+++ lib/labm8/hashcache_test.py
@@ -0,0 +1,73 @@
+"""Unit tests for //lib/labm8/hashcache.py."""
+import pathlib
+import tempfile
+
+import pytest
+import sys
+from absl import app
+from absl import flags
+
+from lib.labm8 import hashcache
+
+
+FLAGS = flags.FLAGS
+
+HASH_FUNCTIONS = ['md5', 'sha1', 'sha256']
+
+
+@pytest.fixture(scope='function')
+def database_path() -> pathlib.Path:
+  with tempfile.TemporaryDirectory(prefix='labm8_hashcache_') as d:
+    yield pathlib.Path(d) / 'hashcache.db'
+
+
+def test_HashCache_unrecognized_hash_fn(database_path):
+  """Test that a non-existent path raises an error."""
+  with pytest.raises(ValueError) as e_info:
+    hashcache.HashCache(database_path, 'null')
+  assert "Hash function not recognized: 'null'" == str(e_info.value)
+
+
+def test_HashCache_GetHash_non_existent_path(database_path):
+  """Test that a non-existent path raises an error."""
+  c = hashcache.HashCache(database_path, 'md5')
+  with tempfile.TemporaryDirectory() as d:
+    with pytest.raises(FileNotFoundError) as e_info:
+      c.GetHash(pathlib.Path(d) / 'a')
+    assert f"File not found: '{d}/a'" == str(e_info.value)
+
+
+def test_HashCache_GetHash_empty_file(database_path):
+  """Test the hash of an empty file."""
+  c = hashcache.HashCache(database_path, 'md5')
+  with tempfile.TemporaryDirectory() as d:
+    (pathlib.Path(d) / 'a').touch()
+    assert 'd41d8cd98f00b204e9800998ecf8427e' == c.GetHash(
+        (pathlib.Path(d) / 'a'))
+
+
+def test_HashCache_GetHash_empty_directory(database_path):
+  """Test the hash of an empty directory."""
+  c = hashcache.HashCache(database_path, 'md5')
+  with tempfile.TemporaryDirectory() as d:
+    assert 'd41d8cd98f00b204e9800998ecf8427e' == c.GetHash(pathlib.Path(d))
+
+
+def test_HashCache_GetHash_modified_directory(database_path):
+  """Test that modifying a directory changes the hash."""
+  c = hashcache.HashCache(database_path, 'md5')
+  with tempfile.TemporaryDirectory() as d:
+    hash_1 = c.GetHash(pathlib.Path(d))
+    (pathlib.Path(d) / 'a').touch()
+    hash_2 = c.GetHash(pathlib.Path(d))
+    assert hash_1 != hash_2
+
+
+def main(argv):
+  """Main entry point."""
+  del argv
+  sys.exit(pytest.main([__file__, '-v']))
+
+
+if __name__ == '__main__':
+  app.run(main)
