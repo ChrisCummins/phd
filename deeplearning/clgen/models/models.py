918b0ff156a38eaeff0f64efef8b3f50dcd835a4
Chris Cummins <chrisc.101@gmail.com>
2018-05-29 11:07:00 +0100

    Automatic code format.

--- deeplearning/clgen/models/models.py
+++ deeplearning/clgen/models/models.py
@@ -298,7 +298,8 @@ class Model(object):
       while True:
         X = np.copy(vectorized_seed)
         sample_in_progress = [sampler.start_text]
-        print('=== BEGIN CLGEN SAMPLE ===', sampler.start_text, sep='\n', end='')
+        print('=== BEGIN CLGEN SAMPLE ===', sampler.start_text, sep='\n',
+              end='')
         start_time = labdate.MillisecondsTimestamp()
         # Save a few cycles by not going via the model() property every time we
         # need to access it.
