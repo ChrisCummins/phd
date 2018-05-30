7220be86fe720762045cdab96640a1c3caeb4184
Chris Cummins <chrisc.101@gmail.com>
2018-05-31 00:10:53 +0100

    Add opencl baseline experiment.

---
+++ experimental/clgen/keras/opencl_baseline.py
@@ -0,0 +1,25 @@
+"""Get a baseline reading of CLgen OpenCL models."""
+from absl import app
+from absl import flags
+
+from deeplearning.clgen import clgen
+from lib.labm8 import bazelutil
+
+
+FLAGS = flags.FLAGS
+
+PROTOS = [
+  bazelutil.DataPath('phd/experimental/clgen/keras/opencl_baseline_a.pbtxt'),
+  bazelutil.DataPath('phd/experimental/clgen/keras/opencl_baseline_b.pbtxt'),
+]
+
+
+def main(argv):
+  del argv
+  for proto in PROTOS:
+    instance = clgen.Instance.FromFile(proto)
+    instance.Sample(min_num_samples=1000)
+
+
+if __name__ == '__main__':
+  app.run(main)
