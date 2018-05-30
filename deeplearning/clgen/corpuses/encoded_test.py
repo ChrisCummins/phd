aa219614df42588cb50e7e35fd798bc287c42f46
Chris Cummins <chrisc.101@gmail.com>
2018-05-30 13:53:16 +0100

    Update encoded tests.

--- deeplearning/clgen/corpuses/encoded_test.py
+++ deeplearning/clgen/corpuses/encoded_test.py
@@ -21,41 +21,41 @@ def abc_atomizer():
 
 @pytest.fixture(scope='function')
 def abc_preprocessed():
-  return preprocessed.PreprocessedContentFile(sha256='12345',
-                                              text='aabbccddee')
+  return preprocessed.PreprocessedContentFile(id=123, text='aabbccddee')
 
 
 # EncodedContentFile.FromPreprocessed() tests.
 
-def test_EncodedContentFile_FromPreprocessed_sha256(
+def test_EncodedContentFile_FromPreprocessed_id(
     abc_atomizer, abc_preprocessed):
-  """Test that sha256 is the same as the preprocessed content file."""
+  """Test that id is the same as the preprocessed content file."""
   enc = encoded.EncodedContentFile.FromPreprocessed(
-      abc_preprocessed, abc_atomizer)
-  assert enc.sha256 == abc_preprocessed.sha256
+      abc_preprocessed, abc_atomizer, eof='a')
+  assert enc.id == abc_preprocessed.id
 
 
 def test_EncodedContentFile_FromPreprocessed_data(
     abc_atomizer, abc_preprocessed):
   """Test that sha256 is the same as the preprocessed content file."""
   enc = encoded.EncodedContentFile.FromPreprocessed(
-      abc_preprocessed, abc_atomizer)
-  assert np.array_equal(np.array([0, 0, 1, 1, 2, 2, 3, 3, 4, 4]), enc.data)
+      abc_preprocessed, abc_atomizer, eof='a')
+  assert np.array_equal(np.array([0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 0]),
+                        enc.data)
 
 
 def test_EncodedContentFile_FromPreprocessed_encoding_time_ms(
     abc_atomizer, abc_preprocessed):
   """Test that encoding time is set."""
   enc = encoded.EncodedContentFile.FromPreprocessed(
-      abc_preprocessed, abc_atomizer)
-  assert enc.encoding_time_ms
+      abc_preprocessed, abc_atomizer, eof='a')
+  assert enc.encoding_time_ms is not None
 
 
 def test_EncodedContentFile_FromPreprocessed_date_added(
     abc_atomizer, abc_preprocessed):
   """Test that date_added is set."""
   enc = encoded.EncodedContentFile.FromPreprocessed(
-      abc_preprocessed, abc_atomizer)
+      abc_preprocessed, abc_atomizer, eof='a')
   assert enc.date_added
 
 
