3ddfdbfb50540bc2f51d98db3348cadf9346812a
Chris Cummins <chrisc.101@gmail.com>
2018-05-30 13:12:00 +0100

    Work in progress on preprocess and encode.

--- deeplearning/clgen/corpuses/encoded.py
+++ deeplearning/clgen/corpuses/encoded.py
@@ -3,16 +3,20 @@ import binascii
 import datetime
 import multiprocessing
 import pathlib
+import pickle
 import time
 
+import humanize
 import numpy as np
 import progressbar
 import sqlalchemy as sql
 from absl import flags
+from absl import logging
 from sqlalchemy.ext import declarative
 
 from deeplearning.clgen.corpuses import atomizers
 from deeplearning.clgen.corpuses import preprocessed
+from deeplearning.clgen.proto import internal_pb2
 from lib.labm8 import sqlutil
 
 
@@ -31,8 +35,8 @@ class Meta(Base):
 class EncodedContentFile(Base):
   __tablename__ = 'encoded_contentfiles'
 
-  # Checksum of the input preprocessed file.
-  sha256: str = sql.Column(sql.Binary(32), primary_key=True)
+  # The ID of the PreprocessedContentFile.
+  id: int = sql.Column(sql.Integer, primary_key=True)
   data: str = sql.Column(sql.Binary(), nullable=False)
   # The number of milliseconds encoding took.
   encoding_time_ms: int = sql.Column(sql.Integer, nullable=False)
@@ -49,14 +53,21 @@ class EncodedContentFile(Base):
 
   @classmethod
   def FromPreprocessed(
-      cls, preprocessed_db: preprocessed.PreprocessedContentFile,
+      cls, preprocessed_cf: preprocessed.PreprocessedContentFile,
       atomizer: atomizers.AtomizerBase) -> 'EncodedContentFile':
     start_time = time.time()
-    data = atomizer.AtomizeString(preprocessed_db.text)
-    encoding_time_ms = (time.time() - start_time) * 1000
-    return EncodedContentFile(sha256=preprocessed_db.sha256, data=data,
+    data = atomizer.AtomizeString(preprocessed_cf.text)
+    encoding_time_ms = int((time.time() - start_time) * 1000)
+    return EncodedContentFile(id=preprocessed_cf.id, data=data,
                               encoding_time_ms=encoding_time_ms,
                               date_added=datetime.datetime.utcnow())
+
+
+def EncoderWorker(job: internal_pb2.EncoderWorker) -> EncodedContentFile:
+  return EncodedContentFile.FromPreprocessed(
+      preprocessed.PreprocessedContentFile(
+          id=job.id, text=job.text),
+      pickle.loads(job.pickled_atomizer))
 
 
 class EncodedContentFiles(sqlutil.Database):
@@ -97,13 +108,32 @@ class EncodedContentFiles(sqlutil.Database):
   def Import(self, session: sqlutil.Database.session_t,
              preprocessed_db: preprocessed.PreprocessedContentFiles,
              atomizer: atomizers.AtomizerBase) -> None:
+    start_time = time.time()
     with preprocessed_db.Session() as p_session:
       query = p_session.query(preprocessed.PreprocessedContentFile).filter(
-          ~preprocessed.PreprocessedContentFile.sha256.in_(
-              session.query(EncodedContentFile).sha256))
+          ~preprocessed.PreprocessedContentFile.id.in_(
+              session.query(EncodedContentFile.id).all()))
+      jobs = [
+        internal_pb2.EncoderWorker(
+            id=x.id, text=x.text, pickled_atomizer=pickle.dumps(atomizer))
+        for x in query
+      ]
+      logging.info('Encoding %s of %s preprocessed files',
+                   humanize.intcomma(query.count()),
+                   humanize.intcomma(p_session.query(
+                       preprocessed.PreprocessedContentFile).count()))
       pool = multiprocessing.Pool()
       bar = progressbar.ProgressBar()
-      for encoded_cfg in bar(pool.imap_unordered(
-          lambda x: EncodedContentFile.FromPreprocessed(x, atomizer),
-          query)):
-        session.add(encoded_cfg)
+      last_commit = time.time()
+
+      for encoded_cf in bar(pool.imap_unordered(
+          EncoderWorker, jobs)):
+        session.add(encoded_cf)
+        current_time = time.time()
+        if current_time - last_commit > 10:
+          session.commit()
+          last_commit = current_time
+
+      logging.info('Encoded %s files in %s ms',
+                   humanize.intcomma(query.count()),
+                   humanize.intcomma(int((time.time() - start_time) * 1000)))
--- deeplearning/clgen/corpuses/preprocessed.py
+++ deeplearning/clgen/corpuses/preprocessed.py
@@ -1,15 +1,28 @@
 """This file defines a database for pre-preprocessed content files."""
 import binascii
+import contextlib
 import datetime
+import hashlib
+import multiprocessing
+import os
 import pathlib
+import subprocess
+import tempfile
+import time
+import typing
 
+import humanize
+import progressbar
 import sqlalchemy as sql
 from absl import flags
+from absl import logging
 from sqlalchemy.ext import declarative
 
+from deeplearning.clgen import errors
 from deeplearning.clgen.preprocessors import preprocessors
 from deeplearning.clgen.proto import corpus_pb2
 from deeplearning.clgen.proto import internal_pb2
+from lib.labm8 import fs
 from lib.labm8 import sqlutil
 
 
@@ -28,8 +41,9 @@ class Meta(Base):
 class PreprocessedContentFile(Base):
   __tablename__ = 'preprocessed_contentfiles'
 
+  id: int = sql.Column(sql.Integer, primary_key=True)
   # Relative path of the input file within the content files.
-  input_relpath: str = sql.Column(sql.String(4096), primary_key=True)
+  input_relpath: str = sql.Column(sql.String(4096), nullable=False, unique=True)
   # Checksum of the input file.
   input_sha256: str = sql.Column(sql.Binary(32), nullable=False)
   # Checksum of the preprocessed file.
@@ -54,12 +68,43 @@ class PreprocessedContentFile(Base):
     """Return the 64 character hexadecimal representation of sha256."""
     return binascii.hexlify(self.sha256).decode('utf-8')
 
+  @classmethod
   def FromContentFile(
-      self,
-      input: internal_pb2.CreatePreprocessedContentFile) -> 'PreprocessedContentFile':
-    # TODO(cec):
-    preprocessors.Preprocess()
-    raise NotImplementedError
+      cls, contentfile_root: pathlib.Path, relpath: pathlib.Path,
+      preprocessors_: typing.List[str]) -> 'PreprocessedContentFile':
+    """Instantiate a PreprocessedContentFile."""
+
+    with open(contentfile_root / relpath) as f:
+      input_text = f.read()
+    start_time = time.time()
+    preprocessing_succeeded = False
+    try:
+      text = preprocessors.Preprocess(input_text, preprocessors_)
+      preprocessing_succeeded = True
+    except errors.BadCodeException as e:
+      text = str(e)
+      # An intentionally broad exception catch here to catch whatever's left.
+    except errors.InternalError as e:
+      text = f'!!INTERNAL ERROR!! {e}'
+    end_time = time.time()
+    preprocess_time_ms = int((end_time - start_time) * 1000)
+    return cls(
+        input_relpath=relpath,
+        input_sha256=GetFileSha256(contentfile_root / relpath),
+        sha256=hashlib.sha256(text.encode('utf-8')).digest(),
+        charcount=len(text),
+        linecount=len(text.split('\n')),
+        text=text,
+        preprocessing_succeeded=preprocessing_succeeded,
+        preprocess_time_ms=preprocess_time_ms,
+        date_added=datetime.datetime.utcnow(),
+    )
+
+
+def PreprocessorWorker(
+    job: internal_pb2.PreprocessorWorker) -> PreprocessedContentFile:
+  return PreprocessedContentFile.FromContentFile(
+      pathlib.Path(job.contentfile_root), job.relpath, job.preprocessors)
 
 
 class PreprocessedContentFiles(sqlutil.Database):
@@ -68,16 +113,83 @@ class PreprocessedContentFiles(sqlutil.Database):
   def __init__(self, path: pathlib.Path):
     super(PreprocessedContentFiles, self).__init__(path, Base)
 
+  def Create(self, config: corpus_pb2.Corpus) -> bool:
+    with self.Session() as session:
+      if self.IsDone(session):
+        return False
+      else:
+        self.Import(session, config)
+        self.SetDone(session)
+        session.commit()
+        return True
+
   def IsDone(self, session: sqlutil.Database.session_t):
     if session.query(Meta).filter(Meta.key == 'done').first():
       return True
     else:
       return False
 
-  def Create(self, config: corpus_pb2.Corpus) -> bool:
-    with self.Session() as session:
-      if self.IsDone(session):
-        return False
-      else:
-        pass  # TODO(cec): Implement!
-        return True
+  def SetDone(self, session: sqlutil.Database.session_t):
+    session.add(Meta(key='done', value='yes'))
+
+  def Import(self, session: sqlutil.Database.session_t,
+             config: corpus_pb2.Corpus) -> None:
+    start_time = time.time()
+    with self.GetContentFileRoot(config) as contentfile_root:
+      relpaths = set(self.GetImportRelpaths(contentfile_root))
+      done = set(
+          [x[0] for x in session.query(PreprocessedContentFile.input_relpath)])
+      todo = relpaths - done
+      logging.info('Preprocessing %s of %s content files',
+                   humanize.intcomma(len(todo)),
+                   humanize.intcomma(len(relpaths)))
+      jobs = [
+        internal_pb2.PreprocessorWorker(
+            contentfile_root=str(contentfile_root),
+            relpath=t, preprocessors=config.preprocessor)
+        for t in todo]
+      pool = multiprocessing.Pool()
+      bar = progressbar.ProgressBar(max_value=len(jobs))
+      last_commit = time.time()
+      for preprocessed_cf in bar(pool.imap_unordered(
+          PreprocessorWorker, jobs)):
+        session.add(preprocessed_cf)
+        current_time = time.time()
+        if current_time - last_commit > 10:
+          session.commit()
+          last_commit = current_time
+      logging.info('Preprocessed %s content files in %s ms',
+                   humanize.intcomma(len(todo)),
+                   humanize.intcomma(int((time.time() - start_time) * 1000)))
+
+  @contextlib.contextmanager
+  def GetContentFileRoot(self, config: corpus_pb2.Corpus) -> pathlib.Path:
+    if config.HasField('local_directory'):
+      yield pathlib.Path(ExpandConfigPath(config.local_directory))
+    elif config.HasField('local_tar_archive'):
+      with tempfile.TemporaryDirectory(prefix='clgen_corpus_') as d:
+        start_time = time.time()
+        cmd = ['tar', '-xf', str(ExpandConfigPath(config.local_tar_archive)),
+               '-C', d]
+        subprocess.check_call(cmd)
+        logging.info('Unpacked %s in %s ms',
+                     ExpandConfigPath(config.local_tar_archive).name,
+                     humanize.intcomma(int((time.time() - start_time) * 1000)))
+        yield pathlib.Path(d)
+    else:
+      raise NotImplementedError
+
+  def GetImportRelpaths(
+      self, contentfile_root: pathlib.Path) -> typing.List[pathlib.Path]:
+    with fs.chdir(contentfile_root):
+      return subprocess.check_output(['find', '.', '-type', 'f']).decode(
+          'utf-8').strip().split('\n')
+
+
+def ExpandConfigPath(path: str) -> pathlib.Path:
+  return pathlib.Path(os.path.expandvars(path)).expanduser().absolute()
+
+
+def GetFileSha256(path: pathlib.Path):
+  with open(path, 'rb') as f:
+    return hashlib.sha256(f.read()).digest()
