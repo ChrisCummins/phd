62d561cab9f212c186e322d371934f0b20186eaa
Chris Cummins <chrisc.101@gmail.com>
2018-05-31 17:52:18 +0100

    Fix polyglot tests.

--- experimental/polyglot/baselines/test_protos_are_valid.py
+++ experimental/polyglot/baselines/test_protos_are_valid.py
@@ -1,7 +1,6 @@
 """Test that //experimental/polyglot/baselines:protos are valid."""
-import sys
-
 import pytest
+import sys
 from absl import app
 
 from datasets.github.scrape_repos.proto import scrape_repos_pb2
@@ -12,7 +11,8 @@ from lib.labm8 import bazelutil
 from lib.labm8 import pbutil
 
 
-def DirContainsProtos(data_path: str, proto_class):
+def DirContainsProtos(data_path: str, proto_class) -> None:
+  """Assert that contains protos of the given class."""
   for path in bazelutil.DataPath(data_path).iterdir():
     assert pbutil.ProtoIsReadable(
         bazelutil.DataPath(data_path) / path, proto_class())
@@ -20,25 +20,25 @@ def DirContainsProtos(data_path: str, proto_class):
 
 def test_clone_lists_are_valid():
   """Test that clone_lists are valid."""
-  DirContainsProtos('phd/experimental/polyglot/baseline/clone_lists',
+  DirContainsProtos('phd/experimental/polyglot/baselines/clone_lists',
                     scrape_repos_pb2.LanguageCloneList)
 
 
 def test_corpuses_are_valid():
   """Test that corpuses are valid."""
-  DirContainsProtos('phd/experimental/polyglot/baseline/corpuses',
+  DirContainsProtos('phd/experimental/polyglot/baselines/corpuses',
                     corpus_pb2.Corpus)
 
 
 def test_models_are_valid():
   """Test that models are valid."""
-  DirContainsProtos('phd/experimental/polyglot/baseline/models',
+  DirContainsProtos('phd/experimental/polyglot/baselines/models',
                     model_pb2.Model)
 
 
 def test_samplers_are_valid():
   """Test that samplers are valid."""
-  DirContainsProtos('phd/experimental/polyglot/baseline/samplers',
+  DirContainsProtos('phd/experimental/polyglot/baselines/samplers',
                     sampler_pb2.Sampler)
 
 
