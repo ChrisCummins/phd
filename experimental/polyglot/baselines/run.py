1f17be5d723237ceb2b25cf6f10f619c5eb32ce7
Chris Cummins <chrisc.101@gmail.com>
2018-06-02 21:50:17 +0100

    One more logging call.

--- experimental/polyglot/baselines/run.py
+++ experimental/polyglot/baselines/run.py
@@ -58,6 +58,7 @@ def SampleModel(instance: clgen.Instance) -> None:
   sample_dir = instance.model.SamplerCache(instance.sampler)
   if sample_dir.is_dir():
     num_samples = len(list(sample_dir.iterdir()))
+  logging.info('Need to generate %d samples', target_samples - num_samples)
   while num_samples < target_samples:
     instance.Sample(min_num_samples=target_samples - num_samples)
     num_samples = len(list(sample_dir.iterdir()))
