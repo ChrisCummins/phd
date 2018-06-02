1dcab57d5850f041f1328b81ca1a9732c968fcc1
Chris Cummins <chrisc.101@gmail.com>
2018-06-02 12:05:54 +0100

    Add rebase guards around git hooks.

--- tools/git/pre-commit.sh
+++ tools/git/pre-commit.sh
@@ -3,11 +3,15 @@ set -e
 
 # Directory of the root of this repository.
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-
+# The name of the current branch, or '(no branch)' if currently headless (such
+# as while rebasing).
+BRANCH_NAME="$(git -C "$ROOT" branch | grep '*' | sed 's/* //')"
 
 main() {
   source "$ROOT/.env"
   "$PHD/tools/buildifier.sh"
 }
 
-main $@
+if [[ "$BRANCH_NAME" != '(no branch)' ]]; then
+  main $@
+fi
