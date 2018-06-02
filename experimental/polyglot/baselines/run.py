3cb4ecbc88cbab7de08f096dc9c085098de0af17
Chris Cummins <chrisc.101@gmail.com>
2018-06-02 20:26:28 +0100

    Fix output corpus.

--- experimental/polyglot/baselines/run.py
+++ experimental/polyglot/baselines/run.py
@@ -40,6 +40,11 @@ def CreateOutputCorpus(instance: clgen.Instance) -> corpuses.Corpus:
       str(instance.model.SamplerCache(instance.sampler)) + '.postprocessed')
   logging.info('Creating output contentfiles at %s', out_dir)
   out_dir.mkdir(exist_ok=True)
+  sample_dir = instance.model.SamplerCache(instance.sampler)
+  for sample_path in sample_dir.iterdir():
+    sample = pbutil.FromFile(sample_dir / sample_path, model_pb2.Sample())
+    with open(out_dir / sample_path.stem, 'w') as f:
+      f.write(sample.text)
   output_corpus_config = corpus_pb2.Corpus()
   output_corpus_config.CopyFrom(instance.model.corpus.config)
   output_corpus_config.local_directory = str(out_dir)
