3f4320a06cdee14adc37f77c829cfbf814ebfcb0
Chris Cummins <chrisc.101@gmail.com>
2018-06-02 11:55:05 +0100

    Add a standalone pre-commit file.

---
+++ tools/git/pre-commit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# Directory of the root of this repository.
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+
+
+main() {
+  source "$ROOT/.env"
+  "$ROOT/tools/buildifier.sh"
+}
+
+main $@
