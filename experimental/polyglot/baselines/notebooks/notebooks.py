48dd75bb979efc51f0d8942dd9fbf6e503c6d90c
Chris Cummins <chrisc.101@gmail.com>
2018-06-04 20:18:47 +0100

    Tidy up notebooks target.

--- experimental/polyglot/baselines/notebooks/notebooks.py
+++ experimental/polyglot/baselines/notebooks/notebooks.py
@@ -1,2 +1,18 @@
+"""Launch a Jupyter notebook server. This process never terminates."""
+from absl import app
+from absl import flags
 from notebook import notebookapp
-notebookapp.main()
+
+
+FLAGS = flags.FLAGS
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+  notebookapp.main()
+
+
+if __name__ == '__main__':
+  app.run(main)
