e4aee1fcb99320a1d6488b20349c5efb40951deb
Chris Cummins <chrisc.101@gmail.com>
2018-06-06 17:55:10 +0100

    Work on dataset preparation.Work on dataset preparation.Work on dataset preparation.Work on dataset preparation.Work on dataset preparation.Work on dataset preparation.Work on dataset preparation.Work on dataset preparation.

--- experimental/deeplearning/fish/prepare_discriminator_dataset.py
+++ experimental/deeplearning/fish/prepare_discriminator_dataset.py
@@ -27,6 +27,9 @@ flags.DEFINE_float(
     'testing_ratio', 0.1, 'Ratio of dataset to use for testing.')
 flags.DEFINE_integer(
     'max_protos', 100000, 'The maximum number of protos per class to read')
+flags.DEFINE_boolean(
+    'assertions_only', False, 'If set, load only positive protos which raise '
+    'compiler assertions.')
 
 DatasetRatios = collections.namedtuple(
     'DataSetRatios', ['train', 'val', 'test'])
@@ -52,14 +55,20 @@ def main(argv):
   dataset_root.mkdir(parents=True, exist_ok=True)
 
   ratios = DatasetRatios(
-      FLAGS.training_ratio, FLAGS.validation_ratio, FLAGS.test_ratio)
+      FLAGS.training_ratio, FLAGS.validation_ratio, FLAGS.testing_ratio)
   assert sum(ratios) <= 1
 
-  positive_protos = [
-    pbutil.FromFile(path, fish_pb2.CompilerCrashDiscriminatorTrainingExample())
-    for path in
-    sorted(list((export_path / 'build_crash').iterdir())[:FLAGS.max_protos])
-  ]
+  positive_protos = []
+  i = 0
+  for path in sorted(list((export_path / 'build_crash').iterdir())):
+    proto = pbutil.FromFile(
+        path, fish_pb2.CompilerCrashDiscriminatorTrainingExample())
+    if FLAGS.assertions_only and not proto.raised_assertion:
+      continue
+    i += 1
+    positive_protos.append(proto)
+    if i >= FLAGS.max_protos:
+      break
   logging.info('Loaded %s positive data protos',
                humanize.intcomma(len(positive_protos)))
   # Load an equal number of negative protos, sorted by result ID.
@@ -73,7 +82,7 @@ def main(argv):
 
   training_size = int(len(positive_protos) * FLAGS.training_ratio)
   validation_size = int(len(positive_protos) * FLAGS.validation_ratio)
-  testing_size = int(len(positive_protos) * FLAGS.testing_size)
+  testing_size = int(len(positive_protos) * FLAGS.testing_ratio)
 
   assert training_size + validation_size + testing_size <= len(positive_protos)
 
@@ -82,39 +91,33 @@ def main(argv):
   (dataset_root / 'testing').mkdir(exist_ok=True, parents=True)
 
   for i, proto in enumerate(positive_protos[:training_size]):
-    pbutil.ToFile(fish_pb2.DiscriminatorDatapoint(
-        input=proto.src, label=fish_pb2.DiscriminatorDatapoint.POSITIVE),
-        (dataset_root / 'training' / f'positive-{i:04d}.pbtxt'))
+    pbutil.ToFile(
+        proto, (dataset_root / 'training' / f'positive-{i:04d}.pbtxt'))
   for i, proto in enumerate(negative_protos[:training_size]):
-    pbutil.ToFile(fish_pb2.DiscriminatorDatapoint(
-        input=proto.src, label=fish_pb2.DiscriminatorDatapoint.POSITIVE),
-        (dataset_root / 'training' / f'negative-{i:04d}.pbtxt'))
+    pbutil.ToFile(
+        proto, (dataset_root / 'training' / f'negative-{i:04d}.pbtxt'))
   logging.info('Wrote %s training examples',
                humanize.intcomma(2 * training_size))
   positive_protos = positive_protos[training_size:]
   negative_protos = negative_protos[training_size:]
 
   for i, proto in enumerate(positive_protos[:validation_size]):
-    pbutil.ToFile(fish_pb2.DiscriminatorDatapoint(
-        input=proto.src, label=fish_pb2.DiscriminatorDatapoint.POSITIVE),
-        (dataset_root / 'validation' / f'positive-{i:04d}.pbtxt'))
+    pbutil.ToFile(
+        proto, (dataset_root / 'validation' / f'positive-{i:04d}.pbtxt'))
   for i, proto in enumerate(negative_protos[:validation_size]):
-    pbutil.ToFile(fish_pb2.DiscriminatorDatapoint(
-        input=proto.src, label=fish_pb2.DiscriminatorDatapoint.POSITIVE),
-        (dataset_root / 'validation' / f'negative-{i:04d}.pbtxt'))
+    pbutil.ToFile(
+        proto, (dataset_root / 'validation' / f'negative-{i:04d}.pbtxt'))
   logging.info('Wrote %s validation examples',
                humanize.intcomma(2 * validation_size))
   positive_protos = positive_protos[validation_size:]
   negative_protos = negative_protos[validation_size:]
 
   for i, proto in enumerate(positive_protos[:testing_size]):
-    pbutil.ToFile(fish_pb2.DiscriminatorDatapoint(
-        input=proto.src, label=fish_pb2.DiscriminatorDatapoint.POSITIVE),
-        (dataset_root / 'testing' / f'positive-{i:04d}.pbtxt'))
+    pbutil.ToFile(
+        proto, (dataset_root / 'testing' / f'positive-{i:04d}.pbtxt'))
   for i, proto in enumerate(negative_protos[:testing_size]):
-    pbutil.ToFile(fish_pb2.DiscriminatorDatapoint(
-        input=proto.src, label=fish_pb2.DiscriminatorDatapoint.POSITIVE),
-        (dataset_root / 'testing' / f'negative-{i:04d}.pbtxt'))
+    pbutil.ToFile(
+        proto, (dataset_root / 'testing' / f'negative-{i:04d}.pbtxt'))
   logging.info('Wrote %s testing examples',
                humanize.intcomma(2 * testing_size))
 
