7654fd975213e0299db5ee6503fe0ccbe86603e2
Chris Cummins <chrisc.101@gmail.com>
2018-06-11 23:24:51 +0100

    Work in progress on CLgen binary release.

--- learn/bazel/make_clgen_release.sh
+++ learn/bazel/make_clgen_release.sh
@@ -1,14 +1,30 @@
-# GOAL: Make a standalone binary release of CLgen for macOS and linux.
+#!/usr/bin/env bash
+# Make a standalone binary release of CLgen.
+# This uses whatever the configuration of the host repository/machine. E.g.
+# a mac/linux build, with/without CUDA support.
+set -eux
+
+source ~/phd/.env
 
 # Requirements: python3.6
-# TODO: pip packages
 # TODO: homebrew llvm, until config.pbtxt is dropped.
 
-bazel build //deeplearning/clgen
-cd bazel-phd/bazel-out/darwin-py3-opt/bin/deeplearning/clgen
-tar cjvfh clgen_darwn.tar.bz2 \
+if [[ "$(uname)" == "Darwin" ]]; then
+  uname="darwin"
+else
+  uname="k8"
+fi
+
+date="$(date +%s)"
+
+tarball="clgen_""$uname""_$date"
+
+bazel build //deeplearning/clgen:clgen_test
+cd bazel-phd/bazel-out/"$uname"-py3-opt/bin/deeplearning/clgen
+tar cjvfh "$tarball".tar.bz2 \
   --exclude '*.runfiles_manifest' \
   --exclude '*.intellij-info.txt' \
   --exclude 'MANIFEST' \
   --exclude '__pycache__' \
   clgen_test clgen_test.runfiles
+mv "$tarball".tar.bz2 $PHD
