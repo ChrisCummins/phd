47f786699b53b931c1bcbb05791bc6f510c9c4e3
Chris Cummins <chrisc.101@gmail.com>
2018-06-12 01:14:29 +0100

    Begin work on CLgen docker image.

---
+++ learn/docker/clgen/make_image.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Make a standalone binary release of CLgen.
+# This uses whatever the configuration of the host repository/machine. E.g.
+# a mac/linux build, with/without CUDA support.
+set -eux
+
+source ~/phd/.env
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  uname="darwin"
+else
+  uname="k8"
+fi
+
+cd "$PHD"
+package="deeplearning/clgen/preprocessors"
+target="clang_test"
+
+bazel build "$package":"$target"
+
+cd bazel-phd/bazel-out/"$uname"-py3-opt/bin/"$package"
+tar cjvfh "$target".tar.bz2 \
+  --exclude '*.runfiles_manifest' \
+  --exclude '*.intellij-info.txt' \
+  --exclude 'MANIFEST' \
+  --exclude '__pycache__' \
+  "$target" "$target".runfiles
+mv "$target".tar.bz2 $PHD/learn/docker/clgen
+
+cd $PHD/learn/docker/clgen
+
+sudo docker build -t clgen .
+
+sudo docker run -it clgen /bin/bash
