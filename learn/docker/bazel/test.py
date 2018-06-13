62965b450e81ad8226b5b36513e8665c09ae5b60
Chris Cummins <chrisc.101@gmail.com>
2018-06-13 23:06:50 +0100

    Add a runfile test to docker image.

--- learn/docker/bazel/test.py
+++ learn/docker/bazel/test.py
@@ -6,17 +6,23 @@ from absl import app
 from absl import flags
 from absl import logging
 
+from lib.labm8 import bazelutil
+
 
 FLAGS = flags.FLAGS
 
 
-def test_hello_world():
-  """Test always passes."""
-  assert True
+def test_datafile_read():
+  """Test reading a data file."""
+  with open(bazelutil.DataPath('phd/learn/docker/bazel/datafile.txt')) as f:
+    assert f.read() == 'Hello, Docker!\n'
 
 
 def main(argv):
   """Main entry point."""
+  logging.info('Platform: %s', sys.platform)
+  logging.info('Exec:     %s', sys.executable)
+  logging.info('Args:     %s', ' '.join(argv))
   if len(argv) > 1:
     logging.warning("Unknown arguments: '%s'", ' '.join(argv[1:]))
   sys.exit(pytest.main([__file__, '-vv']))
