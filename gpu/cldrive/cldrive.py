cac64630e2af8c956f1d63fc4b69d775338897a2
Chris Cummins <chrisc.101@gmail.com>
2018-06-19 15:00:15 +0100

    Add a file to list opencl environments.

---
+++ gpu/cldrive/cldrive.py
@@ -0,0 +1,49 @@
+"""Main entry point for CLdrive script."""
+import typing
+
+from absl import app
+from absl import flags
+
+from gpu import cldrive
+
+
+FLAGS = flags.FLAGS
+flags.DEFINE_boolean(
+    'ls_env', False,
+    'If set, list the names and details of available OpenCL environments, and'
+    'exit.')
+
+
+def GetOpenClEnvironments() -> typing.List[cldrive.OpenCLEnvironment]:
+  """Get a list of available OpenCL testbeds."""
+  return sorted(cldrive.all_envs(), key=lambda x: x.name)
+
+
+def GetTestbedNames() -> typing.List[str]:
+  """Get a list of available OpenCL testbed names."""
+  return [env.name for env in GetOpenClEnvironments()]
+
+
+def PrintOpenClEnvironments() -> None:
+  """List the names and details of available OpenCL testbeds."""
+  for i, env in enumerate(GetOpenClEnvironments()):
+    if i:
+      print()
+    print(env.name)
+    print(f'    Platform:     {env.platform_name}')
+    print(f'    Device:       {env.device_name}')
+    print(f'    Driver:       {env.driver_version}')
+    print(f'    Device Type:  {env.device_type}')
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+
+  if FLAGS.ls_env:
+    PrintOpenClEnvironments()
+
+
+if __name__ == '__main__':
+  app.run(main)
