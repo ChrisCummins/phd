c2f65c655184ef34689c5b945dd3b6b8eb24a476
Chris Cummins <chrisc.101@gmail.com>
2018-06-20 15:39:09 +0100

    Begin work on opencl_fuzzer Docker image.

---
+++ experimental/deeplearning/deepsmith/opencl_fuzz/README.md
@@ -0,0 +1,35 @@
+# OpenCL fuzzing through DeepSmith
+
+
+## Usage
+
+Build the binary:
+
+```sh
+$ bazel build //experimental/deeplearning/deepsmith/opencl_fuzz
+```
+
+Test a device using:
+
+```sh
+$ bazel-bin/experimental/deeplearning/deepsmith/opencl_fuzz/opencl_fuzz \
+    --batch_size 10 --max_testing_time_seconds 60 \
+    --generator $PWD/experimental/deeplearning/deepsmith/opencl_fuzz/generator.txt \
+    --dut 'CPU|ComputeAorta|Codeplay_Software_Ltd._-_host_CPU|1.14|1.2'
+```
+
+## Usage: Docker Image
+
+Build the docker image:
+
+```sh
+$ ./experimental/deeplearning/deepsmith/opencl_fuzz/make_image.sh
+```
+
+Test a device using:
+
+```sh
+$ docker run opencl \
+    /app/experimental/deeplearning/deepsmith/opencl_fuzz/opencl_fuzz_image.binary \
+    --generator /datasets/generator.pbtxt
+```
---
+++ experimental/deeplearning/deepsmith/opencl_fuzz/make_image.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# Make a standalone binary release of CLgen.
+# This uses whatever the configuration of the host repository/machine. E.g.
+# a mac/linux build, with/without CUDA support.
+set -eux
+
+source ~/phd/.env
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  uname="darwin"
+else
+  uname="k8"
+fi
+
+cd "$PHD"
+
+# Export pre-trained CLgen cache.
+mkdir -pv $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/model
+mkdir -pv $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/corpus/encoded
+mkdir -pv $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/corpus/preprocessed
+
+MODEL_SRC="/var/phd/clgen/tiny/model/e549e3374ed531acc1f9c397e7006bda59906d6a"
+ENCODED_SRC="$(readlink $MODEL_SRC/corpus)"
+PREPROCESSED_SRC="$(readlink $ENCODED_SRC/preprocessed)"
+
+rsync -avh --delete --no-links "$ENCODED_SRC" \
+    $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/corpus/encoded
+rsync -avh --delete --no-links "$PREPROCESSED_SRC" \
+    $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/corpus/preprocessed
+rsync -avh --delete --no-links "$MODEL_SRC" --exclude samples \
+    $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/model
+
+# TODO(cec): Once support for Contentfiles ID is implemented, remove this.
+CONTENTFILES_SRC="$PHD/deeplearning/clgen/tests/data/tiny/corpus.tar.bz2"
+cp -v "$CONTENTFILES_SRC" $PHD/experimental/deeplearning/deepsmith/opencl_fuzz
+
+
+# The name of the package, *without* leading slashes.
+package="experimental/deeplearning/deepsmith/opencl_fuzz"
+target="opencl_fuzz_image"
+
+# Build and package the app
+bazel build -c opt "//$package":"$target"
+
+rm -f "$PHD/experimental/deeplearning/deepsmith/opencl_fuzz/$target-layer.tar"
+cp "$PHD/bazel-bin/$package/$target-layer.tar" \
+    $PHD/experimental/deeplearning/deepsmith/opencl_fuzz
+
+cd $PHD/experimental/deeplearning/deepsmith/opencl_fuzz
+sudo docker build . -t opencl_fuzz
+
+docker run opencl_fuzz \
+    /app/experimental/deeplearning/deepsmith/opencl_fuzz/opencl_fuzz_image.binary \
+    --generator /datasets/generator.pbtxt
