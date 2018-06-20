d49506423a4370bb0cfabee996a82403ea3c4606
Chris Cummins <chrisc.101@gmail.com>
2018-06-20 12:39:40 +0100

    Add a dummy generator.

---
+++ deeplearning/deepsmith/services/dummy_generator.py
@@ -0,0 +1,86 @@
+"""A very basic "generator" which always returns the same testcase."""
+import time
+from concurrent import futures
+
+import grpc
+from absl import app
+from absl import flags
+from absl import logging
+
+from deeplearning.deepsmith.proto import deepsmith_pb2
+from deeplearning.deepsmith.proto import generator_pb2
+from deeplearning.deepsmith.proto import generator_pb2_grpc
+from deeplearning.deepsmith.services import generator
+from deeplearning.deepsmith.services import services
+from lib.labm8 import labdate
+
+
+FLAGS = flags.FLAGS
+
+
+class DummyGenerator(generator.GeneratorBase,
+                     generator_pb2_grpc.GeneratorServiceServicer):
+
+  def __init__(self, config: generator_pb2.RandCharGenerator):
+    super(DummyGenerator, self).__init__(config)
+    self.generator = deepsmith_pb2.Generator()
+    self.generator.name = 'dummy_generator'
+    logging.info('Dummy generator:\n%s', self.generator)
+
+  def GetGeneratorCapabilities(
+      self, request: generator_pb2.GetGeneratorCapabilitiesRequest,
+      context) -> generator_pb2.GetGeneratorCapabilitiesResponse:
+    """Get the generator capabilities."""
+    del context
+    response = services.BuildDefaultResponse(
+        generator_pb2.GetGeneratorCapabilitiesRequest)
+    response.toolchain = self.config.model.corpus.language
+    response.generator = self.generator
+    return response
+
+  def GenerateTestcases(self, request: generator_pb2.GenerateTestcasesRequest,
+                        context) -> generator_pb2.GenerateTestcasesResponse:
+    """Generate testcases."""
+    del context
+    response = services.BuildDefaultResponse(
+        generator_pb2.GenerateTestcasesResponse)
+
+    # Generate random strings.
+    for _ in range(request.num_testcases):
+      # Instantiate a testcase.
+      testcase = response.testcases.add()
+      testcase.CopyFrom(self.config.testcase_to_generate)
+      testcase.generator.CopyFrom(self.generator)
+      start_time = labdate.MillisecondsTimestamp()
+      end_time = labdate.MillisecondsTimestamp()
+      p = testcase.profiling_events.add()
+      p.type = 'generation'
+      p.event_start_epoch_ms = start_time
+      p.duration_ms = end_time - start_time
+
+    return response
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError('Unrecognized arguments')
+  generator_config = services.ServiceConfigFromFlag(
+      'generator_config', generator_pb2.RandCharGenerator())
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+  services.AssertLocalServiceHostname(generator_config.service)
+  service = DummyGenerator(generator_config)
+  generator_pb2_grpc.add_GeneratorServiceServicer_to_server(service, server)
+  server.add_insecure_port(f'[::]:{generator_config.service.port}')
+  logging.info('%s listening on %s:%s', type(service).__name__,
+               generator_config.service.hostname, generator_config.service.port)
+  server.start()
+  try:
+    while True:
+      time.sleep(3600 * 24)
+  except KeyboardInterrupt:
+    server.stop(0)
+
+
+if __name__ == '__main__':
+  app.run(main)
