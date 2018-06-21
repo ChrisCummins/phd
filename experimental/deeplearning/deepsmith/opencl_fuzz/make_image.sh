5fd83e3006c81e9a5dc0a1c6bd45baceb83cf02d
Chris Cummins <chrisc.101@gmail.com>
2018-06-21 12:15:16 +0100

    Switch to inference-only clgen.

--- experimental/deeplearning/deepsmith/opencl_fuzz/make_image.sh
+++ experimental/deeplearning/deepsmith/opencl_fuzz/make_image.sh
@@ -14,22 +14,6 @@ fi
 
 cd "$PHD"
 
-# Export pre-trained CLgen cache.
-mkdir -pv $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/model
-mkdir -pv $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/corpus/encoded
-mkdir -pv $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/corpus/preprocessed
-
-MODEL_SRC="/mnt/cc/data/experimental/deeplearning/polyglot/clgen/model/71aef8088a45cd5e63c800bc025187a090c77103"
-ENCODED_SRC="$(readlink $MODEL_SRC/corpus)"
-PREPROCESSED_SRC="$(readlink $ENCODED_SRC/preprocessed)"
-
-rsync -avh --delete --no-links "$ENCODED_SRC" \
-    $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/corpus/encoded
-rsync -avh --delete --no-links "$PREPROCESSED_SRC" \
-    $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/corpus/preprocessed
-rsync -avh --delete --no-links "$MODEL_SRC" --exclude samples \
-    $PHD/experimental/deeplearning/deepsmith/opencl_fuzz/cache/model
-
 # The name of the package, *without* leading slashes.
 package="experimental/deeplearning/deepsmith/opencl_fuzz"
 target="opencl_fuzz_image"
