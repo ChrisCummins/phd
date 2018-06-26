75955ea777bbcaf33d0576d338dedc0c045fdd09
Chris Cummins <chrisc.101@gmail.com>
2018-06-27 00:00:42 +0100

    Execute cldrive harness in oclgrind env.

--- deeplearning/deepsmith/services/cldrive_test.py
+++ deeplearning/deepsmith/services/cldrive_test.py
@@ -11,6 +11,7 @@ from absl import logging
 
 FLAGS = flags.FLAGS
 
+from gpu.oclgrind import oclgrind
 from deeplearning.deepsmith.proto import deepsmith_pb2
 from deeplearning.deepsmith.services import cldrive
 
@@ -36,7 +37,7 @@ def test_CompileDriver_hello_world_c():
   with tempfile.TemporaryDirectory() as d:
     p = cldrive.CompileDriver("""
 #include <stdio.h>
-    
+
 int main() {
   printf("Hello, world!\\n");
   return 0;
@@ -124,14 +125,13 @@ def test_MakeDriver_CompileDriver_hello_world():
   with tempfile.TemporaryDirectory() as d:
     binary = cldrive.CompileDriver(
         driver, pathlib.Path(d) / 'exe', 0, 0, timeout_seconds=60)
-    output = subprocess.check_output([binary], universal_newlines=True,
-                                     stderr=subprocess.STDOUT)
-  assert '[cldrive] Platform:' in output
-  assert '[cldrive] Device:' in output
-  assert '[cldrive] OpenCL optimizations: on\n' in output
-  assert '[cldrive] Kernel: "A"\n' in output
-  assert 'done.\n' in output
-  assert output.split('\n')[-2] == (
+    proc = oclgrind.Exec([binary])
+  assert '[cldrive] Platform:' in
+  assert '[cldrive] Device:' in proc.stderr
+  assert '[cldrive] OpenCL optimizations: on\n' in proc.stderr
+  assert '[cldrive] Kernel: "A"\n' in proc.stderr
+  assert 'done.\n' in proc.stderr
+  assert proc.stdout.split('\n')[-2] == (
     'global int * a: 10 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 '
     '22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 '
     '46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 '
