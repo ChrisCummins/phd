4400152dd0a8cbc3ee5d46bdcb266d2d36a1cb41
Chris Cummins <chrisc.101@gmail.com>
2018-07-12 05:32:24 +0200

    Create testbeds attribute on base harness.

--- deeplearning/deepsmith/harnesses/harness.py
+++ deeplearning/deepsmith/harnesses/harness.py
@@ -16,6 +16,7 @@ class HarnessBase(services.ServiceBase):
 
   def __init__(self, config: pbutil.ProtocolBuffer):
     self.config = config
+    self.testbeds = []
 
   def GetHarnessCapabilities(self,
                              request: harness_pb2.GetHarnessCapabilitiesRequest,
