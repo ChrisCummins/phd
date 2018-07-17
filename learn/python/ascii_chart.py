92cbd3ae9843e79f70c48ffbba30f87ec885a45a
Chris Cummins <chrisc.101@gmail.com>
2018-07-17 17:04:12 +0200

    Add ascii_art package.

---
+++ learn/python/ascii_chart.py
@@ -0,0 +1,58 @@
+"""Simple demo using ascii art package."""
+import ascii_art
+import pytest
+import sys
+from absl import app
+from absl import flags
+
+
+FLAGS = flags.FLAGS
+
+
+def MultiLineRightStrip(s):
+  """Right strip all lines of a multi-line string."""
+  return '\n'.join(x.rstrip() for x in str(s).split('\n'))
+
+
+def test_Chart():
+  """Test that Chart produces text."""
+  data = [
+    5.2,
+    2.1,
+    2.3,
+    -10,
+  ]
+  c = ascii_art.Chart(
+      data,
+      width=30, height=20, padding=2, axis_char=u'|'
+  )
+
+  assert MultiLineRightStrip(c.render()) == """
+  5.2 |       ░
+      | █     ░
+      | █     ░
+      | █     ░
+      | █     ░
+      | █     ░
+      | █     ░
+      | █     ░
+      | █     ░
+      | █ █ █ ░
+      | █ █ █ ░
+      | █ █ █ ░
+      | █ █ █ ░
+      | █ █ █ ░
+      | █ █ █ ░
+    0 . . . . ░ . . . . .
+"""
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError('Unrecognized command line flags.')
+  sys.exit(pytest.main([__file__, '-vv']))
+
+
+if __name__ == '__main__':
+  app.run(main)
