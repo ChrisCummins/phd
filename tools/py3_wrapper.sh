cfb98edb4c9f54301616ed600df180337eb2d01b
Chris Cummins <chrisc.101@gmail.com>
2018-07-25 14:51:31 +0200

    Bump rules_docker to a new version.

--- tools/py3_wrapper.sh
+++ tools/py3_wrapper.sh
@@ -2,12 +2,12 @@
 # A Python3 wrapper which selects python2 for bazel tools.
 #
 # This is to workaround a known bug in the rules_docker bazel package, which
-# depends on code which is not Python3 compatible.
+# depends on code which is not Python 3 compatible.
 # See: https://github.com/bazelbuild/rules_docker/issues/293
 
 if [[ $1 = *"/bazel_tools/"* ]] || \
    [[ $1 = *"/containerregistry/"* ]] || \
-   [[ $1 = *"/external/puller/file/puller.par" ]] || \
+   [[ $1 = *"/external/puller/file/"* ]] || \
    [[ $1 = *"/io_bazel_rules_docker/"* ]]; then
     PYTHON_BIN=python2
 else
