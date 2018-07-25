aa8aaf3987a03f5730675dbb4ad017ea0281b227
Chris Cummins <chrisc.101@gmail.com>
2018-07-25 22:51:07 +0200

    Add a CLSmith program generator.

---
+++ deeplearning/deepsmith/generators/clsmith.py
@@ -0,0 +1,131 @@
+"""A CLSmith program generator."""
+import grpc
+import math
+import os
+import pathlib
+import subprocess
+import tempfile
+import time
+import typing
+from absl import app
+from absl import flags
+from absl import logging
+from concurrent import futures
+
+from deeplearning.deepsmith import services
+from deeplearning.deepsmith.generators import generator
+from deeplearning.deepsmith.proto import deepsmith_pb2
+from deeplearning.deepsmith.proto import generator_pb2
+from deeplearning.deepsmith.proto import generator_pb2_grpc
+from deeplearning.deepsmith.proto import service_pb2
+from lib.labm8 import bazelutil
+from lib.labm8 import labdate
+
+
+FLAGS = flags.FLAGS
+
+
+class CLSmithError(EnvironmentError):
+  """Error thrown in case CLSmith fails."""
+  pass
+
+
+def ConfigToGenerator(
+    config: generator_pb2.ClsmithGenerator) -> deepsmith_pb2.Generator:
+  """Convert a config proto to a DeepSmith generator proto."""
+  g = deepsmith_pb2.Generator()
+  g.name = 'clsmith'
+  g.opts['opts'] = ' '.join(config.opt)
+  return g
+
+
+class ClsmithGenerator(generator.GeneratorBase,
+                       generator_pb2_grpc.GeneratorServiceServicer):
+
+  def __init__(self, config: generator_pb2.ClgenGenerator):
+    super(ClsmithGenerator, self).__init__(config)
+    self.config = config
+    self.generator = ConfigToGenerator(self.config)
+    if not self.config.testcase_skeleton:
+      raise ValueError('No testcase skeletons provided')
+
+  def GetGeneratorCapabilities(
+      self, request: generator_pb2.GetGeneratorCapabilitiesRequest,
+      context) -> generator_pb2.GetGeneratorCapabilitiesResponse:
+    del context
+    response = services.BuildDefaultResponse(
+        generator_pb2.GetGeneratorCapabilitiesRequest)
+    response.toolchain = 'opencl'
+    response.generator = self.generator
+    return response
+
+  def GenerateTestcases(self, request: generator_pb2.GenerateTestcasesRequest,
+                        context) -> generator_pb2.GenerateTestcasesResponse:
+    del context
+    num_programs = math.ceil(
+        request.num_testcases / len(self.config.testcase_skeleton))
+    response = services.BuildDefaultResponse(
+        generator_pb2.GenerateTestcasesResponse)
+    with tempfile.TemporaryDirectory(prefix='clsmith_') as d:
+      os.chdir(d)
+      try:
+        for _ in range(num_programs):
+          response.testcases.extend(
+              self.FileToTestcases(*self.GenerateOneFile()))
+      except CLSmithError as e:
+        response.status.returncode = service_pb2.ServiceStatus.ERROR
+        response.status.error_message = str(e)
+    return response
+
+  def GenerateOneFile(self) -> typing.Tuple[pathlib.Path, int, int]:
+    start_epoch_ms_utc = labdate.MillisecondsTimestamp()
+    proc = subprocess.Popen(
+        [bazelutil.DataPath('clsmith/CLSmith')] + list(self.config.opt))
+    proc.communicate()
+    if proc.returncode:
+      raise CLSmithError(f'CLSmith exited with returncode: {proc.returncode}')
+    wall_time_ms = labdate.MillisecondsTimestamp() - start_epoch_ms_utc
+    return (
+      pathlib.Path(os.getcwd()) / 'CLProg.c', wall_time_ms, start_epoch_ms_utc)
+
+  def FileToTestcases(self, path: pathlib.Path,
+                      wall_time_ms: int,
+                      start_epoch_ms_utc: int) -> typing.List[
+    deepsmith_pb2.Testcase]:
+    """Make testcases from a CLSmith generated file."""
+    testcases = []
+    for skeleton in self.config.testcase_skeleton:
+      t = deepsmith_pb2.Testcase()
+      t.CopyFrom(skeleton)
+      p = t.profiling_events.add()
+      p.type = 'generation'
+      p.duration_ms = wall_time_ms
+      p.event_start_epoch_ms = start_epoch_ms_utc
+      with open(path) as f:
+        t.inputs['src'] = f.read().strip()
+      testcases.append(t)
+    return testcases
+
+
+def main(argv):
+  if len(argv) > 1:
+    raise app.UsageError('Unrecognized arguments')
+  generator_config = services.ServiceConfigFromFlag(
+      'generator_config', generator_pb2.ClsmithGenerator())
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+  services.AssertLocalServiceHostname(generator_config.service)
+  service = ClsmithGenerator(generator_config)
+  generator_pb2_grpc.add_GeneratorServiceServicer_to_server(service, server)
+  server.add_insecure_port(f'[::]:{generator_config.service.port}')
+  logging.info('%s listening on %s:%s', type(service).__name__,
+               generator_config.service.hostname, generator_config.service.port)
+  server.start()
+  try:
+    while True:
+      time.sleep(3600 * 24)
+  except KeyboardInterrupt:
+    server.stop(0)
+
+
+if __name__ == '__main__':
+  app.run(main)
---
+++ deeplearning/deepsmith/generators/clsmith_test.py
@@ -0,0 +1,100 @@
+"""Unit tests for //deeplearning/deepsmith/generators/clsmith.py."""
+import os
+import pathlib
+import pytest
+import sys
+import tempfile
+from absl import app
+from absl import flags
+
+from deeplearning.deepsmith.generators import clsmith
+from deeplearning.deepsmith.proto import deepsmith_pb2
+from deeplearning.deepsmith.proto import generator_pb2
+
+
+FLAGS = flags.FLAGS
+
+
+@pytest.fixture(scope='function')
+def abc_config() -> generator_pb2.ClsmithGenerator:
+  return generator_pb2.ClsmithGenerator(
+      testcase_skeleton=[
+        deepsmith_pb2.Testcase(
+            toolchain='opencl',
+            inputs={
+              'gsize': '1,1,1',
+              'lsize': '1,1,1',
+            },
+            harness=deepsmith_pb2.Harness(name='cl_launcher')
+        ),
+        deepsmith_pb2.Testcase(
+            toolchain='opencl',
+            inputs={
+              'gsize': '128,16,1',
+              'lsize': '8,4,1',
+            },
+            harness=deepsmith_pb2.Harness(name='cl_launcher')
+        ),
+      ]
+  )
+
+
+@pytest.fixture(scope='function')
+def abc_generator(
+    abc_config: generator_pb2.ClsmithGenerator) -> clsmith.ClsmithGenerator:
+  return clsmith.ClsmithGenerator(abc_config)
+
+
+def test_ClsmithGenerator_GenerateOneFile(
+    abc_generator: clsmith.ClsmithGenerator):
+  """Test that CLSmith generates a source file."""
+  with tempfile.TemporaryDirectory(prefix='clsmith_') as d:
+    os.chdir(d)
+    path, wall_time, start_time = abc_generator.GenerateOneFile()
+    assert isinstance(path, pathlib.Path)
+    assert path.is_file()
+    # We can't compare abspath because of bazel sandboxing.
+    assert str(path).endswith(str(pathlib.Path(d) / 'CLProg.c'))
+    assert isinstance(wall_time, int)
+    assert wall_time
+    assert isinstance(start_time, int)
+    assert start_time
+    with open(path) as f:
+      src = f.read()
+
+    # Check the basic structure of the generated file.
+    assert src.startswith('// -g ')
+    assert 'kernel void ' in src
+
+
+def test_ClsmithGenerator_GenerateTestcases(
+    abc_generator: clsmith.ClsmithGenerator):
+  """End-to-end test of testcase generation."""
+  req = generator_pb2.GenerateTestcasesRequest(num_testcases=10)
+  res = abc_generator.GenerateTestcases(req, None)
+  assert len(res.testcases) == 10
+  for i in range(0, 10, 2):
+    assert res.testcases[i].inputs['gsize'] == '1,1,1'
+    assert res.testcases[i + 1].inputs['gsize'] == '128,16,1'
+    assert res.testcases[i].inputs['lsize'] == '1,1,1'
+    assert res.testcases[i + 1].inputs['lsize'] == '8,4,1'
+    assert res.testcases[i].inputs['src'] == res.testcases[i + 1].inputs['src']
+  # It is unlikely that the same program is generated 10 times, but this is
+  # technically flaky.
+  assert (res.testcases[0].inputs['src'] !=
+          res.testcases[2].inputs['src'] !=
+          res.testcases[4].inputs['src'] !=
+          res.testcases[6].inputs['src'] !=
+          res.testcases[8].inputs['src'])
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+  sys.exit(pytest.main([__file__, '-vv']))
+
+
+if __name__ == '__main__':
+  flags.FLAGS(['argv[0]', '-v=1'])
+  app.run(main)
