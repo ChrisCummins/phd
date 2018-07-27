c036009ca9b5335f66318f9d849a502f8076d7f9
Chris Cummins <chrisc.101@gmail.com>
2018-07-27 22:59:55 +0200

    Add a python wrapper around cl_launcher.

---
+++ compilers/clsmith/cl_launcher.py
@@ -0,0 +1,110 @@
+"""A python wrapper around cl_launcher, the CLSmith program driver.
+
+CLSmith is developed by Chris Lidbury <christopher.lidbury10imperial.ac.uk>.
+See https://github.com/ChrisLidbury/CLSmith.
+
+This file can be executed as a binary in order to invoke CLSmith. Note you must
+use '--' to prevent this script from attempting to parse the args, and a second
+'--' if invoked using bazel, to prevent bazel from parsing the args.
+
+Usage:
+
+  bazel run //compilers/clsmith:cl_launcher [-- -- <args>]
+"""
+import os
+import subprocess
+import sys
+import typing
+from absl import app
+from absl import flags
+
+from gpu.cldrive import driver
+from gpu.cldrive import env
+from lib.labm8 import bazelutil
+from lib.labm8 import fs
+
+
+FLAGS = flags.FLAGS
+
+# The path to cl_launcher.
+CL_LAUNCHER = bazelutil.DataPath('CLSmith/cl_launcher')
+
+# The header files required by generated CLProg.c files.
+CL_LAUNCHER_RUN_FILES = [
+  bazelutil.DataPath('CLSmith/cl_safe_math_macros.h'),
+  bazelutil.DataPath('CLSmith/safe_math_macros.h'),
+  bazelutil.DataPath('CLSmith/runtime/CLSmith.h'),
+  bazelutil.DataPath('CLSmith/runtime/csmith.h'),
+  bazelutil.DataPath('CLSmith/runtime/csmith_minimal.h'),
+  bazelutil.DataPath('CLSmith/runtime/custom_limits.h'),
+  bazelutil.DataPath('CLSmith/runtime/custom_stdint_x86.h'),
+  bazelutil.DataPath('CLSmith/runtime/platform_avr.h'),
+  bazelutil.DataPath('CLSmith/runtime/platform_generic.h'),
+  bazelutil.DataPath('CLSmith/runtime/platform_msp430.h'),
+  bazelutil.DataPath('CLSmith/runtime/random_inc.h'),
+  bazelutil.DataPath('CLSmith/runtime/safe_abbrev.h'),
+  bazelutil.DataPath('CLSmith/runtime/stdint_avr.h'),
+  bazelutil.DataPath('CLSmith/runtime/stdint_ia32.h'),
+  bazelutil.DataPath('CLSmith/runtime/stdint_ia64.h'),
+  bazelutil.DataPath('CLSmith/runtime/stdint_msp430.h'),
+  bazelutil.DataPath('CLSmith/runtime/volatile_runtime.h')
+]
+
+
+def Exec(*opts, timeout_seconds: int = 60,
+         env: typing.Dict[str, str] = None) -> subprocess.Popen:
+  with fs.TemporaryWorkingDir(prefix='cl_launcher_') as d:
+    for src in CL_LAUNCHER_RUN_FILES:
+      os.symlink(src, str(d / src.name))
+    cmd = ['timeout', '-s9', str(timeout_seconds), str(CL_LAUNCHER),
+           '-i', str(d)] + list(opts)
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        universal_newlines=True, env=env)
+    stdout, stderr = process.communicate()
+    process.stdout, process.stderr = stdout, stderr
+  return process
+
+
+def ExecClsmithSource(
+    opencl_environment: env.OpenCLEnvironment, src: str, gsize: driver.NDRange,
+    lsize: driver.NDRange, *opts, timeout_seconds: int = 60,
+    env: typing.Dict[str, str] = None) -> subprocess.Popen:
+  """Execute a command using cl_launcher.
+
+  This creates a Popen process, executes it, and sets the stdout and stderr
+  attributes to the process output.
+
+  Args:
+    opencl_environment: The OpenCL environment to execute.
+    src: The CLSmith program src.
+    gsize: Kernel global size.
+    lsize: Kernel local size.
+    opts: A list of optional command line flags.
+    timeout_seconds: The maximum number of seconds to execute a program for.
+    env: An optional environment to run the program under.
+
+  Returns:
+    A Popen instance, with string stdout and stderr attributes set.
+  """
+  platform_id, device_id = opencl_environment.ids()
+  with fs.TemporaryWorkingDir(prefix='cl_launcher_') as d:
+    with open(d / 'CLProg.c', 'w') as f:
+      f.write(src)
+    proc = Exec('-f', str(d / 'CLProg.c'), '-g', gsize.ToString(),
+                '-l', lsize.ToString(), '-p', str(platform_id),
+                '-d', str(device_id), *opts, timeout_seconds=timeout_seconds,
+                env=env)
+  return proc
+
+
+def main(argv):
+  """Main entry point."""
+  proc = Exec(*argv[1:])
+  print(proc.stdout)
+  print(proc.stderr, file=sys.stderr)
+  sys.exit(proc.returncode)
+
+
+if __name__ == '__main__':
+  app.run(main)
---
+++ compilers/clsmith/cl_launcher_test.py
@@ -0,0 +1,113 @@
+"""Unit tests for //compilers/clsmith/cl_launcher.py."""
+import pytest
+import sys
+from absl import app
+from absl import flags
+
+from compilers.clsmith import cl_launcher
+from gpu.cldrive import driver
+from gpu.cldrive import env
+
+
+FLAGS = flags.FLAGS
+
+# A bare-bones CLSmith program.
+CLSMITH_EXAMPLE_SRC = """
+// -g 1,1,1 -l 1,1,1
+#define int64_t long
+#define uint64_t ulong
+#define int_least64_t long
+#define uint_least64_t ulong
+#define int_fast64_t long
+#define uint_fast64_t ulong
+#define intmax_t long
+#define uintmax_t ulong
+#define int32_t int
+#define uint32_t uint
+#define int16_t short
+#define uint16_t ushort
+#define int8_t char
+#define uint8_t uchar
+
+#define INT64_MIN LONG_MIN
+#define INT64_MAX LONG_MAX
+#define INT32_MIN INT_MIN
+#define INT32_MAX INT_MAX
+#define INT16_MIN SHRT_MIN
+#define INT16_MAX SHRT_MAX
+#define INT8_MIN CHAR_MIN
+#define INT8_MAX CHAR_MAX
+#define UINT64_MIN ULONG_MIN
+#define UINT64_MAX ULONG_MAX
+#define UINT32_MIN UINT_MIN
+#define UINT32_MAX UINT_MAX
+#define UINT16_MIN USHRT_MIN
+#define UINT16_MAX USHRT_MAX
+#define UINT8_MIN UCHAR_MIN
+#define UINT8_MAX UCHAR_MAX
+
+#define transparent_crc(X, Y, Z) transparent_crc_(&crc64_context, X, Y, Z)
+
+#define VECTOR(X , Y) VECTOR_(X, Y)
+#define VECTOR_(X, Y) X##Y
+
+#ifndef NO_GROUP_DIVERGENCE
+#define GROUP_DIVERGE(x, y) get_group_id(x)
+#else
+#define GROUP_DIVERGE(x, y) (y)
+#endif
+
+#ifndef NO_FAKE_DIVERGENCE
+#define FAKE_DIVERGE(x, y, z) (x - y)
+#else
+#define FAKE_DIVERGE(x, y, z) (z)
+#endif
+
+#include "CLSmith.h"
+
+__kernel void entry(__global ulong *result) {
+    uint64_t crc64_context = 0xFFFFFFFFFFFFFFFFUL;
+    result[get_linear_global_id()] = crc64_context ^ 0xFFFFFFFFFFFFFFFFUL;
+}
+"""
+
+
+def test_ExecClsmithSource_pass():
+  """And end-to-end test of executing a CLSmith source."""
+  env_ = env.OclgrindOpenCLEnvironment()
+  proc = cl_launcher.ExecClsmithSource(
+      env_, CLSMITH_EXAMPLE_SRC,
+      driver.NDRange(1, 1, 1), driver.NDRange(1, 1, 1), '---debug')
+
+  assert not proc.returncode
+  assert '3-D global size 1 = [1, 1, 1]' in proc.stderr
+  assert '3-D local size 1 = [1, 1, 1]' in proc.stderr
+  assert 'OpenCL optimizations: on' in proc.stderr
+  assert 'Platform: ' in proc.stderr
+  assert 'Device: ' in proc.stderr
+  assert 'Compilation terminated successfully...'
+  assert proc.stdout == '0,'
+
+
+def test_ExecClsmithSource_syntax_error():
+  """Test outcome of kernel with syntax error."""
+  env_ = env.OclgrindOpenCLEnvironment()
+  proc = cl_launcher.ExecClsmithSource(
+      env_, "!@!###syntax error!",
+      driver.NDRange(1, 1, 1), driver.NDRange(1, 1, 1), '---debug')
+
+  assert proc.returncode == 1
+  assert proc.stdout == ''
+  assert 'Error building program: -11' in proc.stderr
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+  sys.exit(pytest.main([__file__, '-vv']))
+
+
+if __name__ == '__main__':
+  flags.FLAGS(['argv[0]', '-v=1'])
+  app.run(main)
