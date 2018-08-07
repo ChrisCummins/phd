347f5c59e4db4bf7709be2ca528c9e41d58eef67
Chris Cummins <chrisc.101@gmail.com>
2018-08-07 11:23:58 +0100

    Add a python bzip2 module.

---
+++ datasets/benchmarks/bzip2.py
@@ -0,0 +1,80 @@
+"""A python wrapper around bzip2 benchmark."""
+import subprocess
+import sys
+import typing
+from absl import app
+from absl import flags
+from absl import logging
+
+from lib.labm8 import bazelutil
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer(
+    'bzip2_timeout_seconds', 60,
+    'The maximum number of seconds to allow process to run.')
+
+# Path to bzip2.
+BZIP2 = bazelutil.DataPath('bzip2/bzip2')
+
+# Source files for bzip2.
+BZIP2_SRCS = [
+  bazelutil.DataPath('bzip2/blocksort.c'),
+  bazelutil.DataPath('bzip2/bzlib.c'),
+  bazelutil.DataPath('bzip2/bzlib.h'),
+  bazelutil.DataPath('bzip2/bzlib_private.h'),
+  bazelutil.DataPath('bzip2/compress.c'),
+  bazelutil.DataPath('bzip2/crctable.c'),
+  bazelutil.DataPath('bzip2/decompress.c'),
+  bazelutil.DataPath('bzip2/huffman.c'),
+  bazelutil.DataPath('bzip2/randtable.c'),
+  bazelutil.DataPath('bzip2/bzip2.c'),
+]
+
+
+class Bzip2Timeout(EnvironmentError):
+  """Eror raised in case of time out."""
+  pass
+
+
+def Exec(data: str, args: typing.List[str],
+         timeout_seconds: int = 60) -> subprocess.Popen:
+  """Run bzip2.
+
+  Args:
+    args: A list of arguments to pass to binary.
+    timeout_seconds: The number of seconds to allow clang-format to run for.
+
+  Returns:
+    A Popen instance with stdout and stderr set to strings.
+  """
+  cmd = ['timeout', '-s9', str(timeout_seconds), str(BZIP2)] + args
+  logging.debug('$ %s', ' '.join(cmd))
+  process = subprocess.Popen(
+      cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+      stdin=subprocess.PIPE)
+  stdout, stderr = process.communicate(data)
+  if process.returncode == 9:
+    raise Bzip2Timeout(f'bzip2 timed out after {timeout_seconds}s')
+  process.stdout = stdout
+  process.stderr = stderr
+  return process
+
+
+def main(argv):
+  """Main entry point."""
+  try:
+    proc = Exec(argv[1:], timeout_seconds=FLAGS.opt_timeout_seconds)
+    if proc.stdout:
+      print(proc.stdout)
+    if proc.stderr:
+      print(proc.stderr, file=sys.stderr)
+    sys.exit(proc.returncode)
+  except Bzip2Timeout as e:
+    print(e, file=sys.stderr)
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+  app.run(main)
---
+++ datasets/benchmarks/bzip2_test.py
@@ -0,0 +1,42 @@
+"""Unit tests for //datasets/benchmarks/bzip2.py."""
+import pytest
+import sys
+import typing
+from absl import app
+from absl import flags
+
+from datasets.benchmarks import bzip2
+
+
+FLAGS = flags.FLAGS
+
+
+def test_Exec_compress_decompress():
+  """Test compressing and de-compressing a string."""
+  compress = bzip2.Exec('Hello, bzip2!'.encode('utf-8'), ['-z'])
+  assert not compress.stderr
+  assert not compress.returncode
+  assert compress.stdout != 'Hello, bzip2!'.encode('utf-8')
+
+  decompress = bzip2.Exec(compress.stdout, ['-d'])
+  assert not decompress.stderr
+  assert not decompress.returncode
+  assert decompress.stdout == 'Hello, bzip2!'.encode('utf-8')
+
+
+def test_BZIP2_SRCS():
+  """Test source files for bzip2."""
+  for path in bzip2.BZIP2_SRCS:
+    assert path.is_file()
+
+
+def main(argv: typing.List[str]):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+  sys.exit(pytest.main([__file__, '-vv']))
+
+
+if __name__ == '__main__':
+  flags.FLAGS(['argv[0]', '-v=1'])
+  app.run(main)
