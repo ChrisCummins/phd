7fe7a9f52a6e7945b3ce18df6bcd51a437d41b87
Chris Cummins <chrisc.101@gmail.com>
2018-08-08 20:47:01 +0100

    Add more LLVM benchmarks.

--- datasets/benchmarks/llvm_test_suite.py
+++ datasets/benchmarks/llvm_test_suite.py
@@ -2,22 +2,165 @@
 
 See: https://llvm.org/docs/TestingGuide.html#test-suite-overview
 """
-
+import typing
 from absl import flags
 
+from datasets.benchmarks.proto import benchmarks_pb2
 from lib.labm8 import bazelutil
 
 
 FLAGS = flags.FLAGS
 
-BENCHMARKS = {
-  'SingleSource': {
-    'McGill': {
-      'queens': {
-        'srcs': [str(bazelutil.DataPath(
-            'llvm_test_suite/SingleSource/Benchmarks/McGill/queens.c'))],
-        'binary': [bazelutil.DataPath('llvm_test_suite/queens')],
-      }
-    }
-  }
-}
+
+class McGillBenchmarks(object):
+  """The McGill Benchmarks."""
+  queens = benchmarks_pb2.Benchmark(
+      name='queens',
+      usage='queens [-ac] <n>',
+      binary=[bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_McGill_queens')],
+      srcs=[str(bazelutil.DataPath(
+          'llvm_test_suite/SingleSource/Benchmarks/McGill/queens.c'))],
+  )
+
+
+class ShootoutBenchmarks(object):
+  """The Programming Language Shootout benchmarks."""
+  ackermann = benchmarks_pb2.Benchmark(
+      name='ackermann',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_ackermann'),
+      srcs=[str(bazelutil.DataPath(
+          'llvm_test_suite/SingleSource/Benchmarks/Shootout/ackermann.c'))]
+  )
+  ary3 = benchmarks_pb2.Benchmark(
+      name='ary3',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_ary3'),
+      srcs=[
+        str(bazelutil.DataPath(
+            'llvm_test_suite/SingleSource/Benchmarks/Shootout/ary3.c'))]
+  )
+  fib2 = benchmarks_pb2.Benchmark(
+      name='fib2',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_fib2'),
+      srcs=[
+        str(bazelutil.DataPath(
+            'llvm_test_suite/SingleSource/Benchmarks/Shootout/fib2.c'))]
+  )
+  hash = benchmarks_pb2.Benchmark(
+      name='hash',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_hash'),
+      srcs=[
+        str(bazelutil.DataPath(
+            'llvm_test_suite/SingleSource/Benchmarks/Shootout/hash.c'))]
+  )
+  heapsort = benchmarks_pb2.Benchmark(
+      name='heapsort',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_heapsort'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/heapsort.c'))]
+  )
+  hello = benchmarks_pb2.Benchmark(
+      name='hello',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_hello'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/hello.c'))]
+  )
+  lists = benchmarks_pb2.Benchmark(
+      name='lists',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_lists'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/lists.c'))]
+  )
+  matrix = benchmarks_pb2.Benchmark(
+      name='matrix',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_matrix'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/matrix.c'))]
+  )
+  methcall = benchmarks_pb2.Benchmark(
+      name='methcall',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_methcall'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/methcall.c'))]
+  )
+  nestedloop = benchmarks_pb2.Benchmark(
+      name='nestedloop',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_nestedloop'),
+      srcs=[str(bazelutil.DataPath(
+          'llvm_test_suite/SingleSource/Benchmarks/Shootout/nestedloop.c'))]
+  )
+  objinst = benchmarks_pb2.Benchmark(
+      name='objinst',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_objinst'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/objinst.c'))]
+  )
+  random = benchmarks_pb2.Benchmark(
+      name='random',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_random'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/random.c'))]
+  )
+  sieve = benchmarks_pb2.Benchmark(
+      name='sieve',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_sieve'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/sieve.c'))]
+  )
+  strcat = benchmarks_pb2.Benchmark(
+      name='strcat',
+      binary=bazelutil.DataPath(
+          'llvm_test_suite/SingleSource_Benchmarks_Shootout_strcat'),
+      srcs=[str(
+          bazelutil.DataPath(
+              'llvm_test_suite/SingleSource/Benchmarks/Shootout/strcat.c'))]
+  )
+
+
+class SingleSourceBenchmarks(object):
+  """The single source benchmarks."""
+  McGill = McGillBenchmarks()
+  Shootout = ShootoutBenchmarks()
+
+
+SingleSource = SingleSourceBenchmarks()
+
+# A list of all benchmarks defined in this file.
+BENCHMARKS: typing.List[benchmarks_pb2.Benchmark] = [
+  SingleSource.McGill.queens,
+  SingleSource.Shootout.ackermann,
+  SingleSource.Shootout.ary3,
+  SingleSource.Shootout.fib2,
+  SingleSource.Shootout.hash,
+  SingleSource.Shootout.heapsort,
+  SingleSource.Shootout.hello,
+  SingleSource.Shootout.lists,
+  SingleSource.Shootout.matrix,
+  SingleSource.Shootout.methcall,
+  SingleSource.Shootout.nestedloop,
+  SingleSource.Shootout.objinst,
+  SingleSource.Shootout.random,
+  SingleSource.Shootout.sieve,
+  SingleSource.Shootout.strcat,
+]
---
+++ datasets/benchmarks/llvm_test_suite_test.py
@@ -0,0 +1,33 @@
+"""Unit tests for //datasets/benchmarks/llvm_test_suite.py."""
+import pathlib
+import pytest
+import sys
+import typing
+from absl import app
+from absl import flags
+
+from datasets.benchmarks import llvm_test_suite
+from datasets.benchmarks.proto import benchmark_pb2
+
+
+FLAGS = flags.FLAGS
+
+
+@pytest.mark.parametrize('benchmark', llvm_test_suite.BENCHMARKS)
+def test_benchmarks(benchmark: benchmark_pb2.Benchmark):
+  """Simple black-box tests."""
+  assert pathlib.Path(benchmark.binary).is_file()
+  for path in benchmark.srcs:
+    assert pathlib.Path(path).is_file()
+
+
+def main(argv: typing.List[str]):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+  sys.exit(pytest.main([__file__, '-vv']))
+
+
+if __name__ == '__main__':
+  flags.FLAGS(['argv[0]', '-v=1'])
+  app.run(main)
