2375824f5253aa132d4237650cc594a64b64b5c8
Chris Cummins <chrisc.101@gmail.com>
2018-09-15 00:23:46 +0100

    Add a freefocus main entry point.

--- util/freefocus/freefocus.py
+++ util/freefocus/freefocus.py
@@ -1,5 +1,36 @@
+import pathlib
+import typing
+from absl import app
+from absl import flags
+from absl import logging
+
+from util.freefocus import sql
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string(
+    'database_path', '/tmp/phd/util/freefocus/freefocus.db',
+    'Path to database.')
+
 SPEC_MAJOR = 1
 SPEC_MINOR = 0
 SPEC_MICRO = 0
 
 __freefocus_spec__ = f"{SPEC_MAJOR}.{SPEC_MINOR}.{SPEC_MICRO}"
+
+
+def main(argv: typing.List[str]):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+
+  database_path = pathlib.Path(FLAGS.database_path)
+
+  database_path.parent.mkdir(parents=True, exist_ok=True)
+  db = sql.Database(database_path)
+  logging.info(db)
+
+
+if __name__ == '__main__':
+  app.run(main)
