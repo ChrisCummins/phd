f0c63843d8ac25ed49e7c95b0a0085d4f7f58aa8
Chris Cummins <chrisc.101@gmail.com>
2018-09-15 15:44:39 +0100

    Add ipdb to third_party libs.

---
+++ learn/python/debugger.py
@@ -0,0 +1,20 @@
+"""Experiments in using ipdb debugger."""
+import ipdb
+import typing
+from absl import app
+from absl import flags
+
+
+FLAGS = flags.FLAGS
+
+
+def main(argv: typing.List[str]):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+  ipdb.run('x[0] = 3')
+  ipdb.set_trace()
+
+
+if __name__ == '__main__':
+  app.run(main)
