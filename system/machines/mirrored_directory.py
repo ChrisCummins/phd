1b2665b612be92219583fd25b403eae304476741
Chris Cummins <chrisc.101@gmail.com>
2018-11-18 19:32:45 +0000

    Add a library for mirrored directories.

---
+++ system/machines/mirrored_directory.py
@@ -0,0 +1,98 @@
+"""Class for a mirrored directory.
+
+This library depends on 'rsync' and 'ssh' being on the system path.
+"""
+import pathlib
+import subprocess
+import typing
+from absl import flags
+from absl import logging
+
+from lib.labm8 import labtypes
+from system.machines.proto import machine_spec_pb2
+
+
+FLAGS = flags.FLAGS
+
+
+class MirroredDirectory(object):
+  """Provides interface for a mirrored directory.
+
+  See //system/machines/proto:machine_spec.proto for the spec.
+  """
+
+  def __init__(self, host: machine_spec_pb2.Host,
+               spec: machine_spec_pb2.MirroredDirectory):
+    self.host = host
+    self.spec = spec
+    self.rsync_remote_path = f'{self.host.host}:{self.remote_path}'
+
+  @property
+  def name(self) -> str:
+    return self.spec.name
+
+  @property
+  def local_path(self) -> str:
+    """Return the local path."""
+    if self.spec.local_path[-1] == '/':
+      return self.spec.local_path
+    else:
+      return self.spec.local_path + '/'
+
+  @property
+  def remote_path(self) -> str:
+    """Return the remote path."""
+    if self.spec.remote_path[-1] == '/':
+      return self.spec.remote_path
+    else:
+      return self.spec.remote_path + '/'
+
+  def RemoteExists(self) -> bool:
+    """Test if remote directory exists."""
+    try:
+      subprocess.check_output(['ssh', '-p', str(self.host.port), self.host.host,
+                               '-t', f'test -d "{self.remote_path}"'])
+      return True
+    except subprocess.SubprocessError:
+      return False
+
+  def LocalExists(self) -> bool:
+    """Test if local path exists."""
+    return pathlib.Path(self.local_path).is_dir()
+
+  def PushFromLocalToRemote(self, dry_run: bool = False,
+                            verbose: bool = False) -> None:
+    """Push from local to remote paths."""
+    self.Rsync(self.local_path,
+               self.rsync_remote_path, self.host.port, self.spec.rsync_exclude,
+               dry_run, verbose)
+
+  def PullFromRemoteToLocal(self, dry_run: bool = False,
+                            verbose: bool = False) -> None:
+    """Pull from remote to local paths."""
+    self.Rsync(self.rsync_remote_path, self.local_path, self.host.port,
+               self.spec.rsync_exclude, dry_run, verbose)
+
+  def __repr__(self) -> str:
+    return (f"MirroredDirectory(name={self.name}, "
+            f"local_path='{self.local_path}', "
+            f"remote_path='{self.rsync_remote_path}')")
+
+  @staticmethod
+  def Rsync(src: str, dst: str, host_port: int, excludes: typing.List[str],
+            dry_run: bool, verbose: bool):
+    """Private helper method to invoke rsync with appropriate arguments."""
+    cmd = [
+            'rsync', '-ah', '--delete', str(src),
+            str(dst), '-e', f'ssh -p {host_port}',
+          ] + labtypes.flatten([['--exclude', p] for p in excludes])
+    if dry_run:
+      cmd.append('--dry-run')
+    if verbose:
+      cmd.append('--verbose')
+    logging.info(' '.join(cmd))
+    p = subprocess.Popen(cmd)
+    p.communicate()
+    if p.returncode:
+      raise subprocess.SubprocessError(
+          f'rsync failed with returncode {p.returncode}')
---
+++ system/machines/mirrored_directory_test.py
@@ -0,0 +1,120 @@
+"""Unit tests for //system/machines:mirrored_directory.py."""
+import pathlib
+import pytest
+import subprocess
+import sys
+import tempfile
+import typing
+from absl import app
+from absl import flags
+from absl import logging
+
+from lib.labm8 import fs
+from lib.labm8 import labtypes
+from system.machines import mirrored_directory
+from system.machines.proto import machine_spec_pb2
+
+
+FLAGS = flags.FLAGS
+
+
+@pytest.fixture(scope='module')
+def test_host() -> machine_spec_pb2.Host:
+  return machine_spec_pb2.Host(host='localhost', port=22)
+
+
+@pytest.fixture(scope='function')
+def test_mirrored_directory() -> machine_spec_pb2.MirroredDirectory:
+  with tempfile.TemporaryDirectory(
+      prefix='phd_mirrored_directory_remote_path_') as d1:
+    with tempfile.TemporaryDirectory(
+        prefix='phd_mirrored_directory_local_path_') as d2:
+      yield machine_spec_pb2.MirroredDirectory(
+          name='test_mirrored_directory',
+          remote_path=d1,
+          local_path=d2)
+
+
+class LocalMirroredDirectory(mirrored_directory.MirroredDirectory):
+  """Mirrored directory which overrides rsync to support local remote paths."""
+
+  @staticmethod
+  def Rsync(src: str, dst: str, host_port: int, excludes: typing.List[str],
+            dry_run: bool, verbose: bool):
+    """Private helper method to invoke rsync with appropriate arguments."""
+    del host_port
+    src = str(src).replace('localhost:', '')
+    dst = str(dst).replace('localhost:', '')
+    cmd = ['rsync', '-ah', '--delete', str(src), str(dst)] + labtypes.flatten(
+        [['--exclude', p] for p in excludes])
+    if dry_run:
+      cmd.append('--dry-run')
+    if verbose:
+      cmd.append('--verbose')
+    logging.info(' '.join(cmd))
+    p = subprocess.Popen(cmd)
+    p.communicate()
+    if p.returncode:
+      raise subprocess.SubprocessError(
+          f'rsync failed with returncode {p.returncode}')
+
+
+# rsync_exclude
+
+def test_PushLocalToRemote(
+    test_host: machine_spec_pb2.Host,
+    test_mirrored_directory: machine_spec_pb2.MirroredDirectory):
+  """Test pushing a file to remote."""
+  m = LocalMirroredDirectory(test_host, test_mirrored_directory)
+  with open(fs.path(m.local_path, 'a'), 'w') as f:
+    f.write('Hello, world!')
+
+  m.PushFromLocalToRemote()
+
+  assert pathlib.Path(m.remote_path).is_dir()
+  assert (pathlib.Path(m.remote_path) / 'a').is_file()
+  with open(fs.path(m.remote_path, 'a')) as f:
+    assert f.read() == 'Hello, world!'
+
+
+def test_PullFromRemoteToLocal(
+    test_host: machine_spec_pb2.Host,
+    test_mirrored_directory: machine_spec_pb2.MirroredDirectory):
+  """Test pulling a file from remote."""
+  m = LocalMirroredDirectory(test_host, test_mirrored_directory)
+  with open(fs.path(m.remote_path, 'a'), 'w') as f:
+    f.write('Hello, world!')
+
+  m.PullFromRemoteToLocal()
+
+  assert pathlib.Path(m.local_path).is_dir()
+  assert (pathlib.Path(m.local_path) / 'a').is_file()
+  with open(fs.path(m.local_path, 'a')) as f:
+    assert f.read() == 'Hello, world!'
+
+
+def test_PushLocalToRemote_exclude(
+    test_host: machine_spec_pb2.Host,
+    test_mirrored_directory: machine_spec_pb2.MirroredDirectory):
+  """Test pushing with an excluded file."""
+  m = LocalMirroredDirectory(test_host, test_mirrored_directory)
+  with open(fs.path(m.local_path, 'a'), 'w') as f:
+    f.write('Hello, world!')
+
+  # Exclude /a from transfer.
+  m.spec.rsync_exclude.extend(['/a'])
+  m.PushFromLocalToRemote()
+
+  assert not (pathlib.Path(m.remote_path) / 'a').is_file()
+
+
+def main(argv: typing.List[str]):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+  sys.exit(pytest.main([__file__, '-vv']))
+
+
+if __name__ == '__main__':
+  flags.FLAGS(['argv[0]', '-v=1'])
+  app.run(main)
