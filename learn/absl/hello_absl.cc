9745a38afdeacd5c533c05c0915dabf1efdb1070
Chris Cummins <chrisc.101@gmail.com>
2018-11-28 00:51:54 +0000

    Add //learn/absl package.

---
+++ learn/absl/hello_absl.cc
@@ -0,0 +1,40 @@
+#include "absl/strings/str_format.h"
+#include "absl/container/flat_hash_map.h"
+#include "learn/absl/message.pb.h"
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using std::string;
+
+#define FATAL(...) \
+  std::cerr << absl::StrFormat(__VA_ARGS__); \
+  exit(1);
+
+template<typename Container, typename K>
+auto FindInMap(const Container& container, const K& key) {
+  auto it = container.find(key);
+  if (it == container.end()) {
+    FATAL("Key not found");
+  }
+  return (*it).second;
+}
+
+int main() {
+  absl::flat_hash_map<string, int> map;
+
+  map.insert({"Answer", 42});
+
+  std::cout << absl::StrFormat("Hello, %s!\n", "world");
+
+  auto value = FindInMap(map, "Answer");
+
+  std::cout << absl::StrFormat("The answer is %d\n", value);
+
+  MyMessage proto;
+  proto.set_name("foo");
+  std::cout << "Proto: " << proto.DebugString();
+
+  return 0;
+}
