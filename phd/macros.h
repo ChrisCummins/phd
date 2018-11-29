5f935056a65fa4687d9c292603b6bda79287e7b8
Chris Cummins <chrisc.101@gmail.com>
2018-11-29 22:02:08 +0000

    Add logging macros.

--- phd/macros.h
+++ phd/macros.h
@@ -1,23 +1,38 @@
 // This file defines utility macros for working with C++.
 #pragma once
 
-#include "absl/strings/str_format.h"
+#include "phd/private/macros_impl.h"
 
-#include <iostream>
+#ifdef DEBUG
+#error "C preprocessor macro DEBUG() already defined!"
+#endif
+
+#ifdef INFO
+#error "C preprocessor macro INFO() already defined!"
+#endif
+
+#ifdef WARN
+#error "C preprocessor macro WARN() already defined!"
+#endif
+
+#ifdef ERROR
+#error "C preprocessor macro ERROR() already defined!"
+#endif
 
 #ifdef FATAL
 #error "C preprocessor macro FATAL() already defined!"
 #endif
 
+// Log the given message with varying levels of severity. The arguments should
+// be a format string. A newline is appended to the message when printed.
+#define DEBUG(...) LOG_WITH_PREFIX("D", __VA_ARGS__)
+#define LOG(...) LOG_WITH_PREFIX("I", __VA_ARGS__)
+#define WARN(...) LOG_WITH_PREFIX("W", __VA_ARGS__)
+#define ERROR(...) LOG_WITH_PREFIX("E", __VA_ARGS__)
 // Terminate the program with exit code 1, printing the given message to
 // stderr. The arguments should be a format string. A newline is appended
 // to the message when printed.
-#define FATAL(...) \
-  { \
-    std::cerr << "fatal: [" __FILE__ ":" << __LINE__ << "] " \
-              << absl::StrFormat(__VA_ARGS__) << std::endl; \
-    exit(1); \
-  }
+#define FATAL(...) LOG_WITH_PREFIX("F", __VA_ARGS__); exit(1);
 
 #ifdef CHECK
 #error "C preprocessor macro CHECK() already defined!"
