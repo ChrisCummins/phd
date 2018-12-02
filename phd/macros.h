5366d1a6316f9fff8d52145e3ca88e3b5860614e
Chris Cummins <chrisc.101@gmail.com>
2018-12-02 03:04:14 +0000

    Dump stack trace on FATAL().

--- phd/macros.h
+++ phd/macros.h
@@ -3,6 +3,12 @@
 
 #include "phd/private/macros_impl.h"
 
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #ifdef DEBUG
 #error "C preprocessor macro DEBUG() already defined!"
 #endif
@@ -30,9 +36,18 @@
 #define WARN(...) LOG_WITH_PREFIX("W", __VA_ARGS__)
 #define ERROR(...) LOG_WITH_PREFIX("E", __VA_ARGS__)
 // Terminate the program with exit code 1, printing the given message to
-// stderr. The arguments should be a format string. A newline is appended
-// to the message when printed.
-#define FATAL(...) LOG_WITH_PREFIX("F", __VA_ARGS__); exit(1);
+// stderr, followed by a stack trace. The arguments should be a format string.
+// A newline is appended to the message when printed. Code to dump a stack
+// trace by @tgamblin at: https://stackoverflow.com/a/77336
+#define FATAL(...) \
+  { \
+    LOG_WITH_PREFIX("F", __VA_ARGS__); \
+    void *array[10]; \
+    size_t size; \
+    size = backtrace(array, 10); \
+    backtrace_symbols_fd(array, size, STDERR_FILENO); \
+    exit(1); \
+  }
 
 #ifdef CHECK
 #error "C preprocessor macro CHECK() already defined!"
