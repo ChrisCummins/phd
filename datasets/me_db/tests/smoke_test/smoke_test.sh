f257581083baf5a5af1c13929944161ff5a5033b
Chris Cummins <chrisc.101@gmail.com>
2018-12-05 19:51:02 +0000

    Add a me_db smoke test.

---
+++ datasets/me_db/tests/smoke_test/smoke_test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# A smoke test to ensure that program runs without crashing. This doesn't test
+# the results of execution, other than the return code.
+
+ME_DB="datasets/me_db/me_db"
+INBOX_PATH="datasets/me_db/tests/test_inbox"
+DB_PATH="/tmp/me_db_smoke_test.db"
+
+set -eux
+
+# Run it once.
+"$ME_DB" --inbox "$INBOX_PATH" --v=1 --db="$DB_PATH"
+# Run it again, replacing the existing database.
+"$ME_DB" --inbox "$INBOX_PATH" --v=1 --db="$DB_PATH" --replace_existing
+
+# Tidy up.
+# TODO(cec): Put this in an exist signal handler so that if the test fails,
+# the database is still deleted.
+rm -f "$DB_PATH"
