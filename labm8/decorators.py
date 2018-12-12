a9fb34be037f2101a202e08d26ba9cf176d87d57
Chris Cummins <chrisc.101@gmail.com>
2018-12-12 15:20:27 +0000

    Don’t shadow builtin name.

--- labm8/decorators.py
+++ labm8/decorators.py
@@ -12,27 +12,27 @@ FLAGS = flags.FLAGS
 AnyFunction = typing.Callable[..., typing.Any]
 
 
-def memoized_property(function: AnyFunction) -> AnyFunction:
+def memoized_property(func: AnyFunction) -> AnyFunction:
   """A property decorator that memoizes the result.
 
   This is used to memoize the results of class properties, to be used when
   computing the property value is expensive.
 
   Args:
-    function: The function which should be made to a property.
+    func: The function which should be made to a property.
 
   Returns:
     The decorated property function.
   """
   # Based on Danijar Hafner's implementation of a lazy property, available at:
   # https://danijar.com/structuring-your-tensorflow-models/
-  attribute_name = '_memoized_property_' + function.__name__
+  attribute_name = '_memoized_property_' + func.__name__
 
   @property
-  @functools.wraps(function)
+  @functools.wraps(func)
   def decorator(self):
     if not hasattr(self, attribute_name):
-      setattr(self, attribute_name, function(self))
+      setattr(self, attribute_name, func(self))
     return getattr(self, attribute_name)
 
   return decorator
