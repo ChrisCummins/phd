33f146385ca7aac85c20424612d55d0cef6e958d
Chris Cummins <chrisc.101@gmail.com>
2018-12-12 17:29:13 +0000

    Add shortest path function.

--- learn/daily/d181212_graph_nets_shortest_path/graph_util.py
+++ learn/daily/d181212_graph_nets_shortest_path/graph_util.py
@@ -1,9 +1,12 @@
 """Graph utilities for the shortest path model."""
+import collections
 
 import networkx as nx
 import numpy as np
 from absl import flags
 from scipy import spatial
+
+from labm8 import labtypes
 
 
 FLAGS = flags.FLAGS
@@ -12,7 +15,7 @@ FLAGS = flags.FLAGS
 def GenerateGraph(
     rand: np.random.RandomState,
     num_nodes_min_max, dimensions=2,
-    theta=1000.0, rate=1.0, distance_weight_name: str = "distance") -> nx.Graph:
+    theta=1000.0, rate=1.0, weight_name: str = "distance") -> nx.Graph:
   """Creates a connected graph.
 
   The graphs are geographic threshold graphs, but with added edges via a
@@ -28,7 +31,7 @@ def GenerateGraph(
       20-60 for good non-trees. Default=1000.0.
     rate: (optional) A rate parameter for the node weight exponential sampling
       distribution. Default= 1.0.
-    distance_weight_name: The name for the distance edge attribute.
+    weight_name: The name for the weight edge attribute.
 
   Returns:
     The graph.
@@ -48,8 +51,8 @@ def GenerateGraph(
   i_, j_ = np.meshgrid(range(num_nodes), range(num_nodes), indexing="ij")
   weighted_edges = list(zip(i_.ravel(), j_.ravel(), distances.ravel()))
   mst_graph = nx.Graph()
-  mst_graph.add_weighted_edges_from(weighted_edges, weight=distance_weight_name)
-  mst_graph = nx.minimum_spanning_tree(mst_graph, weight=distance_weight_name)
+  mst_graph.add_weighted_edges_from(weighted_edges, weight=weight_name)
+  mst_graph = nx.minimum_spanning_tree(mst_graph, weight=weight_name)
   # Put geo_graph's node attributes into the mst_graph.
   for i in mst_graph.nodes():
     mst_graph.node[i].update(geo_graph.node[i])
@@ -58,6 +61,73 @@ def GenerateGraph(
   combined_graph = nx.compose_all((mst_graph, geo_graph.copy()))
   # Put all distance weights into edge attributes.
   for i, j in combined_graph.edges():
-    combined_graph.get_edge_data(i, j).setdefault(distance_weight_name,
+    combined_graph.get_edge_data(i, j).setdefault(weight_name,
                                                   distances[i, j])
   return combined_graph
+
+
+def AddShortestPath(rand, graph, min_length=1, weight_name: str = "distance"):
+  """Samples a shortest path from A to B and adds attributes to indicate it.
+
+  Args:
+    rand: A random seed for the graph generator. Default= None.
+    graph: A `nx.Graph`.
+    min_length: (optional) An `int` minimum number of edges in the shortest
+      path. Default= 1.
+
+  Returns:
+    The `nx.DiGraph` with the shortest path added.
+
+  Raises:
+    ValueError: All shortest paths are below the minimum length
+  """
+  # Map from node pairs to the length of their shortest path.
+  pair_to_length_dict = {}
+  lengths = list(nx.all_pairs_shortest_path_length(graph))
+  for x, yy in lengths:
+    for y, l in yy.items():
+      if l >= min_length:
+        pair_to_length_dict[x, y] = l
+  if not pair_to_length_dict:
+    raise ValueError("All shortest paths are below the minimum length")
+  # The node pairs which exceed the minimum length.
+  node_pairs = list(pair_to_length_dict)
+
+  # Computes probabilities per pair, to enforce uniform sampling of each
+  # shortest path lengths.
+  # The counts of pairs per length.
+  counts = collections.Counter(pair_to_length_dict.values())
+  prob_per_length = 1.0 / len(counts)
+  probabilities = [
+    prob_per_length / counts[pair_to_length_dict[x]] for x in node_pairs
+  ]
+
+  # Choose the start and end points.
+  i = rand.choice(len(node_pairs), p=probabilities)
+  start, end = node_pairs[i]
+  path = nx.shortest_path(
+      graph, source=start, target=end, weight=weight_name)
+  print(path)
+
+  # Creates a directed graph, to store the directed path from start to end.
+  directed_graph = graph.to_directed()
+
+  # Add the "start", "end", and "solution" attributes to the nodes.
+  directed_graph.add_node(start, start=True)
+  directed_graph.add_node(end, end=True)
+  directed_graph.add_nodes_from(
+      list(labtypes.SetDiff(directed_graph.nodes(), [start])), start=False)
+  directed_graph.add_nodes_from(
+      list(labtypes.SetDiff(directed_graph.nodes(), [end])), end=False)
+  directed_graph.add_nodes_from(
+      list(labtypes.SetDiff(directed_graph.nodes(), path)), solution=False)
+  directed_graph.add_nodes_from(path, solution=True)
+
+  # Now do the same for the edges.
+  path_edges = list(labtypes.PairwiseIterator(path))
+  directed_graph.add_edges_from(
+      list(labtypes.SetDiff(directed_graph.edges(), path_edges)),
+      solution=False)
+  directed_graph.add_edges_from(path_edges, solution=True)
+
+  return directed_graph
--- learn/daily/d181212_graph_nets_shortest_path/graph_util_test.py
+++ learn/daily/d181212_graph_nets_shortest_path/graph_util_test.py
@@ -46,6 +46,97 @@ def test_GenerateGraph_is_connected():
   assert nx.is_connected(graph)
 
 
+def test_AddShortestPath_empty_graph():
+  """Test that error raised if graph is empty."""
+  g = nx.Graph()
+  with pytest.raises(ValueError) as e_ctx:
+    graph_util.AddShortestPath(np.random.RandomState(seed=1), g)
+  assert str(e_ctx.value) == "All shortest paths are below the minimum length"
+
+
+def test_AddShortestPath_min_length_too_large():
+  """Test that error raised if min_length too large."""
+  g = nx.Graph()
+  g.add_edge('A', 'B')
+  with pytest.raises(ValueError) as e_ctx:
+    graph_util.AddShortestPath(np.random.RandomState(seed=1), g, min_length=5)
+  assert str(e_ctx.value) == "All shortest paths are below the minimum length"
+
+
+def test_AddShortestPath_result_is_digraph():
+  """Test that result is a directed graph."""
+  g = nx.Graph()
+  g.add_edge('A', 'B')
+  g.add_edge('B', 'C')
+  g.add_edge('C', 'D')
+  digraph = graph_util.AddShortestPath(np.random.RandomState(seed=1), g)
+  assert isinstance(digraph, nx.DiGraph)
+
+
+def test_AddShortestPath_num_nodes():
+  """Test that result graph has same number of nodes."""
+  g = nx.Graph()
+  g.add_edge('A', 'B')
+  g.add_edge('B', 'C')
+  g.add_edge('C', 'D')
+  digraph = graph_util.AddShortestPath(np.random.RandomState(seed=1), g)
+  assert digraph.number_of_nodes() == 4
+
+
+def test_AddShortestPath_num_edges():
+  """Test that result graph has same number of edges."""
+  g = nx.Graph()
+  g.add_edge('A', 'B')
+  g.add_edge('B', 'C')
+  g.add_edge('C', 'D')
+  digraph = graph_util.AddShortestPath(np.random.RandomState(seed=1), g)
+  assert digraph.number_of_edges() == 6  # 3 undirected edges * 2
+
+
+def test_AddShortestPath_simple_graph_path():
+  """Test result path for a simple graph."""
+  # Graph:
+  #     A -- B -- C -- D -- E -- F
+  #                 \_ G
+  g = nx.Graph()
+  g.add_edge('A', 'B')
+  g.add_edge('B', 'C')
+  g.add_edge('C', 'D')
+  g.add_edge('C', 'G')
+  g.add_edge('D', 'E')
+  g.add_edge('E', 'F')
+  # Use min_length 3 so that the path is A -> B -> C -> D
+  digraph = graph_util.AddShortestPath(np.random.RandomState(seed=1), g,
+                                       min_length=5)
+  assert digraph.node['A']['start']
+  assert digraph.node['A']['solution']
+  assert not digraph.node['A']['end']
+
+  assert not digraph.node['B']['start']
+  assert digraph.node['B']['solution']
+  assert not digraph.node['B']['end']
+
+  assert not digraph.node['C']['start']
+  assert digraph.node['C']['solution']
+  assert not digraph.node['C']['end']
+
+  assert not digraph.node['D']['start']
+  assert digraph.node['D']['solution']
+  assert not digraph.node['D']['end']
+
+  assert not digraph.node['E']['start']
+  assert digraph.node['E']['solution']
+  assert not digraph.node['E']['end']
+
+  assert not digraph.node['F']['start']
+  assert digraph.node['F']['solution']
+  assert digraph.node['F']['end']
+
+  assert not digraph.node['G']['start']
+  assert not digraph.node['G']['solution']
+  assert not digraph.node['G']['end']
+
+
 def main(argv: typing.List[str]):
   """Main entry point."""
   if len(argv) > 1:
