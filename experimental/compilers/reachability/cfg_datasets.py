4b06c410e3ef1b7af0b8289aec0c6889f9087630
Chris Cummins <chrisc.101@gmail.com>
2018-12-18 13:43:48 +0000

    Work in progress on Linux dataset.

--- experimental/compilers/reachability/cfg_datasets.py
+++ experimental/compilers/reachability/cfg_datasets.py
@@ -1,13 +1,16 @@
 """Reachability analysis datasets."""
 import multiprocessing
 import pathlib
+import tempfile
 import typing
 
+import pyparsing
 import pandas as pd
 from absl import app
 from absl import flags
 
 from compilers.llvm import clang
+from compilers.llvm import opt
 from datasets.linux import linux
 from datasets.opencl.device_mapping import \
   opencl_device_mapping_dataset as ocl_dataset
@@ -164,7 +167,7 @@ class OpenClDeviceMappingsDataset(ocl_dataset.OpenClDeviceMappingsDataset):
 
 
 def BytecodeFromLinuxSrc(path: pathlib.Path) -> str:
-  """Create bytecode from OpenCL source string.
+  """Create bytecode from a Linux source file.
 
   Args:
     path: The path of the source file.
@@ -175,11 +178,50 @@ def BytecodeFromLinuxSrc(path: pathlib.Path) -> str:
   Raises:
     ClangException: If compiling to bytecode fails.
   """
-  # Use -O3 to reduce CFGs.
-  linux_include_dir = linux.LinuxSourcesDataset().src_tree_root / 'include'
+  root = linux.LinuxSourcesDataset().src_tree_root
+  genroot = linux.LinuxSourcesDataset().generated_hdrs_root
+  # A subset of the arguments found by running `make V=1` in the linux
+  # build and grabbing a random C compile target.
+  # The build I took this from: Wp,-MD,arch/x86/kernel/.asm-offsets.s.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/5/include -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -include ./include/linux/compiler_types.h -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -Werror-implicit-function-declaration -Wno-format-security -std=gnu89 -fno-PIE -DCC_HAVE_ASM_GOTO -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -funit-at-a-time -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_FXSAVEQ=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -DRETPOLINE -fno-delete-null-pointer-checks -O2 --param=allow-store-data-races=0 -Wframe-larger-than=1024 -fstack-protector-strong -Wno-unused-but-set-variable -fno-var-tracking-assignments -g -gdwarf-4 -pg -mrecord-mcount -mfentry -DCC_USING_FENTRY -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init    -DKBUILD_BASENAME='"asm_offsets"' -DKBUILD_MODNAME='"asm_offsets"'  -fverbose-asm -S -o arch/x86/kernel/asm-offsets.s arch/x86/kernel/asm-offsets.c
   clang_args = [
-    '-O3', '-S', '-emit-llvm', '-o', '-', '-I', str(linux_include_dir),
-    str(path)]
+    '-S', '-emit-llvm', '-o', '-',
+    '-O3',  # Use -O3 to reduce CFGs.
+    '-Wno-everything',  # No warnings please.
+    '-I', str(root / 'arch/x86/include'),
+    '-I', str(genroot / 'arch/x86/include/generated'),
+    '-I', str(root / 'include'),
+    '-I', str(root / 'arch/x86/include/uapi'),
+    '-I', str(genroot / 'arch/x86/include/generated/uapi'),
+    '-I', str(root / 'include/uapi'),
+    '-I', str(genroot / 'include/generated/uapi'),
+    '-I', str(genroot / 'arch/x86/include'),
+    '-I', str(genroot / 'arch/x86/include/generated'),
+    '-I', str(genroot / 'arch/x86/include/generated/uapi'),
+    '-I', str(genroot / 'include'),
+    '-I', str(genroot / 'include/generated'),
+    '-include', str(genroot / 'include/linux/kconfig.h'),
+    '-include', str(genroot / 'include/linux/compiler_types.h'),
+    '-D__KERNEL__',
+    '-m64',
+    '-DCONFIG_X86_X32_ABI',
+    '-DCONFIG_AS_CFI=1',
+    '-DCONFIG_AS_CFI_SIGNAL_FRAME=1',
+    '-DCONFIG_AS_CFI_SECTIONS=1',
+    '-DCONFIG_AS_FXSAVEQ=1',
+    '-DCONFIG_AS_SSSE3=1',
+    '-DCONFIG_AS_CRC32=1',
+    '-DCONFIG_AS_AVX=1',
+    '-DCONFIG_AS_AVX2=1',
+    '-DCONFIG_AS_AVX512=1',
+    '-DCONFIG_AS_SHA1_NI=1',
+    '-DCONFIG_AS_SHA256_NI=1',
+    '-pipe',
+    '-DRETPOLINE',
+    '-DCC_USING_FENTRY',
+    "-DKBUILD_BASENAME='\"asm_offsets\"'",
+    "-DKBUILD_MODNAME='\"asm_offsets\"'",
+    str(path),
+  ]
   process = clang.Exec(clang_args)
   if process.returncode:
     raise clang.ClangException("clang failed with returncode "
@@ -187,47 +229,55 @@ def BytecodeFromLinuxSrc(path: pathlib.Path) -> str:
   return process.stdout
 
 
-def CreateControlFlowGraphFromLinuxSrc(
+def TryToCreateControlFlowGraphsFromLinuxSrc(
     path: pathlib.Path) -> typing.Optional[cfg.ControlFlowGraph]:
-  """Try to create a CFG proto from a Linux C source file.
+  """Try to create CFGs from a Linux C source file.
+
+  On failure, an empty list is returned.
 
   Args:
     path: The path of the source file.
 
   Returns:
-    A ControlFlowGraph instance, or None on failure.
+    A list of ControlFlowGraph instances.
 
   Raises:
     ClangException: If compiling to bytecode fails.
   """
-  bytecode = BytecodeFromLinuxSrc(path)
-
-  # Extract a single dot source from the bytecode.
-  dot_generator = llvm_util.DotCfgsFromBytecode(bytecode)
   graphs = []
-  for dot in dot_generator:
-    # Instantiate a CFG from the dot source.
+
+  try:
+    bytecode = BytecodeFromLinuxSrc(path)
+  except clang.ClangException:
+    return graphs
+
+  # Extract a dot sources from the bytecode.
+  dot_generator = llvm_util.DotCfgsFromBytecode(bytecode)
+  while True:
     try:
-      graphs.append(llvm_util.ControlFlowGraphFromDotSource(dot))
-    except cfg.MalformedControlFlowGraphError:
-      continue
+      dot = next(dot_generator)
+      # Instantiate a CFG from the dot source.
+      graph = llvm_util.ControlFlowGraphFromDotSource(dot)
+      graph.ValidateControlFlowGraph(strict=False)
+      graphs.append(graph)
+    except (UnicodeDecodeError, cfg.MalformedControlFlowGraphError,
+            ValueError, opt.OptException, pyparsing.ParseException):
+      pass
+    except StopIteration:
+      break
 
   return graphs
 
 
 def ProcessLinuxSrc(
     path: pathlib.Path) -> typing.Optional[typing.Dict[str, typing.Any]]:
-  try:
-    graphs = CreateControlFlowGraphFromLinuxSrc(
-        path).ValidateControlFlowGraph(strict=False)
-  except clang.ClangException:
-    return None
+  graphs = TryToCreateControlFlowGraphsFromLinuxSrc(path)
 
   rows = []
   for graph in graphs:
     row = CfgDfRowFromControlFlowGraph(graph)
     row.update({
-      'program:'
+      'program:src_relpath': str(path),
     })
     rows.append(row)
 
--- experimental/compilers/reachability/cfg_datasets_test.py
+++ experimental/compilers/reachability/cfg_datasets_test.py
@@ -7,6 +7,7 @@ from absl import flags
 
 from datasets.linux import linux
 from experimental.compilers.reachability import cfg_datasets as datasets
+from labm8 import system
 
 
 FLAGS = flags.FLAGS
@@ -37,12 +38,35 @@ def test_OpenClDeviceMappingsDataset_cfgs_df_contains_valid_graphs(
 
 def test_BytecodeFromLinuxSrc_known_file():
   """Test that a known file produces bytecode."""
-  path = linux.LinuxSourcesDataset().src_tree_root / 'kmod.c'
-
+  path = linux.LinuxSourcesDataset().src_tree_root / 'kernel' / 'kmod.c'
   assert path.is_file()  # If this fails, the linux source tree is broken.
 
   bytecode = datasets.BytecodeFromLinuxSrc(path)
-  assert len(bytecode)
+  assert bytecode
+
+
+def test_TryToCreateControlFlowGraphsFromLinuxSrc_known_file():
+  """Test that a known file produces graphs."""
+  path = linux.LinuxSourcesDataset().src_tree_root / 'kernel' / 'kmod.c'
+  assert path.is_file()  # If this fails, the linux source tree is broken.
+
+  # TODO(cec): Debug why this file doesn't produce graphs.
+  assert len(datasets.TryToCreateControlFlowGraphsFromLinuxSrc(path)) == 0
+
+def test_TryToCreateControlFlowGraphsFromLinuxSrc_graphs_are_valid():
+  """Test that a known file produces valid graphs."""
+  path = linux.LinuxSourcesDataset().src_tree_root / 'kernel' / 'kmod.c'
+  assert path.is_file()  # If this fails, the linux source tree is broken.
+
+  for graph in datasets.TryToCreateControlFlowGraphsFromLinuxSrc(path):
+    assert graph.IsValidControlFlowGraph(strict=False)
+
+
+def test_LinuxSourcesDataset_df_count(
+    linux_dataset: datasets.LinuxSourcesDataset):
+  """Test that dataset has expected number of rows."""
+  # TODO(cec): This doesn't seem to be deterministic.
+  assert len(linux_dataset.cfgs_df) >= 1600
 
 
 def main(argv):
