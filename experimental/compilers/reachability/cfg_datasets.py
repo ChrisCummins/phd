ec2d971d0a4e3af27716f9dabeab52eae836ecec
Chris Cummins <chrisc.101@gmail.com>
2018-12-20 15:37:06 +0000

    Create bytecode datasets with -O0.

--- experimental/compilers/reachability/cfg_datasets.py
+++ experimental/compilers/reachability/cfg_datasets.py
@@ -22,11 +22,14 @@ from labm8 import decorators
 FLAGS = flags.FLAGS
 
 
-def BytecodeFromOpenClString(opencl_string: str) -> str:
+def BytecodeFromOpenClString(opencl_string: str,
+                             optimization_level: str) -> str:
   """Create bytecode from OpenCL source string.
 
   Args:
     opencl_string: A string of OpenCL code.
+    optimization_level: The optimization level to use, one of
+        {-O0,-O1,-O2,-O3,-Ofast,-Os,-Oz}.
 
   Returns:
     The bytecode as a string.
@@ -36,7 +39,10 @@ def BytecodeFromOpenClString(opencl_string: str) -> str:
   """
   # Use -O3 to reduce CFGs.
   clang_args = opencl.GetClangArgs(use_shim=False) + [
-    '-O3', '-S', '-emit-llvm', '-o', '-', '-i', '-']
+    clang.ValidateOptimizationLevel(optimization_level),
+    '-S', '-emit-llvm', '-o', '-', '-i', '-',
+    '-Wno-everything',  # No warnings please.
+  ]
   process = clang.Exec(clang_args, stdin=opencl_string)
   if process.returncode:
     raise clang.ClangException("clang failed with returncode "
@@ -61,7 +67,7 @@ def CreateControlFlowGraphFromOpenClKernel(
     ClangException: If compiling to bytecode fails.
     ValueError: If opencl_kernel contains multiple functions.
   """
-  bytecode = BytecodeFromOpenClString(opencl_kernel)
+  bytecode = BytecodeFromOpenClString(opencl_kernel, '-O0')
 
   # Extract a single dot source from the bytecode.
   dot_generator = llvm_util.DotCfgsFromBytecode(bytecode)
@@ -167,11 +173,13 @@ class OpenClDeviceMappingsDataset(ocl_dataset.OpenClDeviceMappingsDataset):
     return df
 
 
-def BytecodeFromLinuxSrc(path: pathlib.Path) -> str:
+def BytecodeFromLinuxSrc(path: pathlib.Path, optimization_level: str) -> str:
   """Create bytecode from a Linux source file.
 
   Args:
     path: The path of the source file.
+    optimization_level: The clang optimization level to use, one of
+        {-O0,-O1,-O2,-O3,-Ofast,-Os,-Oz}.
 
   Returns:
     The bytecode as a string.
@@ -186,7 +194,7 @@ def BytecodeFromLinuxSrc(path: pathlib.Path) -> str:
   # The build I took this from: Wp,-MD,arch/x86/kernel/.asm-offsets.s.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/5/include -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -include ./include/linux/compiler_types.h -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -Werror-implicit-function-declaration -Wno-format-security -std=gnu89 -fno-PIE -DCC_HAVE_ASM_GOTO -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -funit-at-a-time -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_FXSAVEQ=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -DRETPOLINE -fno-delete-null-pointer-checks -O2 --param=allow-store-data-races=0 -Wframe-larger-than=1024 -fstack-protector-strong -Wno-unused-but-set-variable -fno-var-tracking-assignments -g -gdwarf-4 -pg -mrecord-mcount -mfentry -DCC_USING_FENTRY -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init    -DKBUILD_BASENAME='"asm_offsets"' -DKBUILD_MODNAME='"asm_offsets"'  -fverbose-asm -S -o arch/x86/kernel/asm-offsets.s arch/x86/kernel/asm-offsets.c
   clang_args = [
     '-S', '-emit-llvm', '-o', '-',
-    '-O3',  # Use -O3 to reduce CFGs.
+    clang.ValidateOptimizationLevel(optimization_level),
     '-Wno-everything',  # No warnings please.
     '-I', str(root / 'arch/x86/include'),
     '-I', str(genroot / 'arch/x86/include/generated'),
@@ -248,7 +256,7 @@ def TryToCreateControlFlowGraphsFromLinuxSrc(
   graphs = []
 
   try:
-    bytecode = BytecodeFromLinuxSrc(path)
+    bytecode = BytecodeFromLinuxSrc(path, '-O0')
   except clang.ClangException:
     return graphs
 
