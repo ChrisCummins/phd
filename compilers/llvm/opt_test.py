abf76faeb082a8e39c46527137845a951a7d0135
Chris Cummins <chrisc.101@gmail.com>
2018-12-20 15:55:08 +0000

    Fix opt test.

--- compilers/llvm/opt_test.py
+++ compilers/llvm/opt_test.py
@@ -13,16 +13,17 @@ FLAGS = flags.FLAGS
 
 
 @pytest.mark.parametrize(
-    "opt", ("-O0", "-O1", "-O2", "-O3", "-Ofast", "-Os", "-Oz"))
+    "o", ("-O0", "-O1", "-O2", "-O3", "-Os", "-Oz"))
 def test_ValidateOptimizationLevel_valid(o: str):
   """Test that valid optimization levels are returned."""
   assert opt.ValidateOptimizationLevel(o) == o
 
 
 @pytest.mark.parametrize(
-    "opt", ("O0",  # missing leading '-'
-            "-O4",  # not a real value
-            "foo"))  # not a real value
+    "o", ("O0",  # missing leading '-'
+          "-Ofast",  # valid for clang, not for opt
+          "-O4",  # not a real value
+          "foo"))  # not a real value
 def test_ValidateOptimizationLevel_invalid(o: str):
   """Test that invalid optimization levels raise an error."""
   with pytest.raises(ValueError) as e_ctx:
