bd203baaabd906542ff571e681b0010b2e4351e6
Chris Cummins <chrisc.101@gmail.com>
2018-12-21 02:18:35 +0000

    Test all files.

--- datasets/linux/linux_test.py
+++ datasets/linux/linux_test.py
@@ -30,6 +30,28 @@ def test_known_file_locations(dataset: linux.LinuxSourcesDataset):
   assert (dataset.src_tree_root / 'kernel' / 'smpboot.h').is_file()
 
 
+def test_all_srcs_count(dataset: linux.LinuxSourcesDataset):
+  """Test the number of kernel sources."""
+  assert len(dataset.all_srcs) == 26091
+
+
+def test_all_srcs_are_files(dataset: linux.LinuxSourcesDataset):
+  """Test that files exist."""
+  for path in dataset.all_srcs:
+    assert path.is_file()
+
+
+def test_all_hdrs_count(dataset: linux.LinuxSourcesDataset):
+  """Test the number of kernel headers."""
+  assert len(dataset.all_hdrs) == 19459
+
+
+def test_all_hdrs_are_files(dataset: linux.LinuxSourcesDataset):
+  """Test that files exist."""
+  for path in dataset.all_hdrs:
+    assert path.is_file()
+
+
 def test_kernel_srcs_count(dataset: linux.LinuxSourcesDataset):
   """Test the number of kernel sources."""
   assert len(dataset.kernel_srcs) == 310
@@ -54,7 +76,7 @@ def test_kernel_hdrs_are_files(dataset: linux.LinuxSourcesDataset):
 
 def test_include_headers_count(dataset: linux.LinuxSourcesDataset):
   """Test the number of include headers."""
-  assert len(dataset.ListFiles('include', '*.h')) == 4882
+  assert len(dataset.ListFiles('include', '*.h')) == 4890
 
 
 def main(argv):
