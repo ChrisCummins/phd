7c35cf1d5f239fb95b793eb1848ab02a827651bc
Chris Cummins <chrisc.101@gmail.com>
2018-12-30 01:06:50 +0000

    Run the new mirror script.

--- system/dotfiles/usr/share/crontab/run-gh-archiver.sh
+++ system/dotfiles/usr/share/crontab/run-gh-archiver.sh
@@ -9,13 +9,20 @@ export JOB_TIMEOUT=1800  # 30 minutes
 export LMK="/usr/local/bin/lmk -e"
 export LMK_TO="chrisc.101@gmail.com"
 
-export GH_ARCHIVER="/usr/local/bin/gh-archiver"
-export GH_USERNAME="ChrisCummins"
-export GOGS_CLONE_DIR="$HOME/git/cec"
 
 # ****************************************************************************
 # *                                  Program                                 *
 # ****************************************************************************
 set -eux
 
-$LMK --only-errors "timeout -s9 $JOB_TIMEOUT $GH_ARCHIVER $GH_USERNAME --gogs -o $GOGS_CLONE_DIR --exclude linux"
+if [[ -z "${1:-}" ]]; then
+  # Run the section below script.
+  $LMK --only-errors "timeout -s9 $JOB_TIMEOUT $0 --porcelain"
+else
+  cd /home/cec/phd
+  bazel run //datasets/github/mirror_user -- \
+      --gogs \
+      --user=ChrisCummins \
+      --dst=/home/cec/git/cec \
+      --excludes=linux
+fi
