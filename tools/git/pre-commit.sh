77a70b33edf47803e910388487c2d6d4406bad11
Chris Cummins <chrisc.101@gmail.com>
2019-01-03 12:22:37 +0000

    Make precommit hook support non-master branches.

--- tools/git/pre-commit.sh
+++ tools/git/pre-commit.sh
@@ -6,6 +6,8 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 # The name of the current branch, or '(no branch)' if currently headless (such
 # as while rebasing).
 BRANCH_NAME="$(git -C "$ROOT" branch | grep '*' | sed 's/* //')"
+# The name of the remote, e.g. "origin".
+REMOTE_NAME="$(git rev-parse --abbrev-ref $BRANCH_NAME@{upstream} | sed -r 's,/.+,,')"
 
 main() {
   source "$ROOT/.env"
@@ -13,10 +15,10 @@ main() {
 
   git -C "$ROOT" fetch origin
 
-  COMMITS_BEHIND_UPSTREAM="$(git rev-list --left-right --count origin/master...@ | cut -f1)"
+  COMMITS_BEHIND_UPSTREAM="$(git rev-list --left-right --count $REMOTE_NAME/$BRANCH_NAME...@ | cut -f1)"
 
   if [[ "$COMMITS_BEHIND_UPSTREAM" > 0 ]]; then
-    echo "Current branch is $COMMITS_BEHIND_UPSTREAM commits behind origin/master, creating stash ..." >&2
+    echo "Current branch is $COMMITS_BEHIND_UPSTREAM commits behind $REMOTE_NAME/$BRANCH_NAME, creating stash ..." >&2
     git -C "$ROOT" stash
     echo "Rebasing ..."
     git -C "$ROOT" pull --rebase
