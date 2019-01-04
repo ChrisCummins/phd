5f557aae8381949c869530b9ab5c150fd9b93946
Chris Cummins <chrisc.101@gmail.com>
2019-01-04 21:19:27 +0000

    Work in progress on inst2vec refactor.

--- deeplearning/ncc/task_utils.py
+++ deeplearning/ncc/task_utils.py
@@ -27,7 +27,6 @@ import pickle
 import re
 import struct
 import zipfile
-from collections import defaultdict
 
 import numpy as np
 import wget
@@ -35,9 +34,7 @@ from absl import app
 from absl import flags
 from absl import logging
 
-from deeplearning.ncc import rgx_utils as rgx
 from deeplearning.ncc import vocabulary
-from deeplearning.ncc.inst2vec import inst2vec_preprocess as i2v_prep
 
 
 # Embedding and vocabulary file paths
@@ -91,198 +88,31 @@ def ReadEmbeddingFileFromFlags() -> np.ndarray:
   return ReadEmbeddingFile(pathlib.Path(FLAGS.embeddings_file))
 
 
-########################################################################################################################
-# Utils (Preprocess files)
-########################################################################################################################
-def inline_struct_types_in_file(data, dic):
+def read_data_files_from_folder(foldername):
   """
-  Inline structure types in the whole file
-  :param data: list of strings representing the content of one file
-  :param dic: dictionary ["structure name", "corresponding literal structure"]
-  :return: modified data
+  Read all source files in folder
+  Return a list of file contents, whereby each file content is a list of strings, each string representing a line
+  :param foldername: name of the folder in which the data files to be read are located
+  :return: a list of files where each file is a list of strings
   """
-  # Remove all "... = type {..." statements since we don't need them anymore
-  data = [stmt for stmt in data if not re.match('.* = type ', stmt)]
+  # Helper variables
+  data = list()
+  file_names = list()
 
-  # Inline the named structures throughout the file
-  for i in range(len(data)):
+  files_in_folder = os.listdir(foldername + '/')
 
-    possible_struct = re.findall('(' + rgx.struct_name + ')', data[i])
-    if len(possible_struct) > 0:
-      for s in possible_struct:
-        if s in dic and not re.match(s + r'\d* = ', data[i]):
-          # Replace them by their value in dictionary
-          data[i] = re.sub(re.escape(s) + rgx.struct_lookahead, dic[s], data[i])
+  # Loop over files in folder
+  for path in files_in_folder:
+    if path[0] != '.' and path[-3:] == '.ll':
+      # If this isn't a hidden file and it is an LLVM IR file ('.ll' extension),
+      # open file and import content
+      with open(os.path.join(foldername, path)) as f:
+        data.append(f.read())
 
-  return data
+      # Add file name to dictionary
+      file_names.append(path)
 
-
-def inline_struct_types_txt(data, data_with_structure_def):
-  """
-  Inline structure types so that the code has no more named structures but only explicit aggregate types
-  And construct a dictionary of these named structures
-  :param data: input data as a list of files where each file is a list of strings
-  :return: data: modified input data
-           dictio: list of dictionaries corresponding to source files,
-                   where each dictionary has entries ["structure name", "corresponding literal structure"]
-  """
-  print('\tConstructing dictionary of structures and inlining structures...')
-  dictio = defaultdict(list)
-
-  # Loop on all files in the dataset
-  for i in range(len(data)):
-    # Construct a dictionary ["structure name", "corresponding literal structure"]
-    data_with_structure_def[i], dict_temp = \
-      i2v_prep.construct_struct_types_dictionary_for_file(
-          data_with_structure_def[i])
-
-    # If the dictionary is empty
-    if not dict_temp:
-      found_type = False
-      for l in data[i]:
-        if re.match(rgx.struct_name + ' = type (<?\{ .* \}|opaque|{})', l):
-          found_type = True
-          break
-      assert not found_type, "Structures' dictionary is empty for file containing type definitions: \n" + \
-                             data[i][0] + '\n' + data[i][1] + '\n' + data[
-                               i] + '\n'
-
-    # Use the constructed dictionary to substitute named structures
-    # by their corresponding literal structure throughout the program
-    data[i] = inline_struct_types_in_file(data[i], dict_temp)
-
-    # Add the entries of the dictionary to the big dictionary
-    for k, v in dict_temp.items():
-      dictio[k].append(v)
-
-  return data, dictio
-
-
-def abstract_statements_from_identifiers_txt(data):
-  """
-  Simplify lines of code by stripping them from their identifiers,
-  unnamed values, etc. so that LLVM IR statements can be abstracted from them
-  :param data: input data as a list of files where each file is a list of strings
-  :return: modified input data
-  """
-  data = remove_local_identifiers(data)
-  data = remove_global_identifiers(data)
-  data = remove_labels(data)
-  data = replace_unnamed_values(data)
-  data = remove_index_types(data)
-
-  return data
-
-
-def remove_local_identifiers(data):
-  """
-  Replace all local identifiers (%## expressions) by "<%ID>"
-  :param data: input data as a list of files where each file is a list of strings
-  :return: modified input data
-  """
-  print('\tRemoving local identifiers ...')
-  for i in range(len(data)):
-    for j in range(len(data[i])):
-      data[i][j] = re.sub(rgx.local_id, "<%ID>", data[i][j])
-
-  return data
-
-
-def remove_global_identifiers(data):
-  """
-  Replace all local identifiers (@## expressions) by "<@ID>"
-  :param data: input data as a list of files where each file is a list of strings
-  :return: modified input data
-  """
-  print('\tRemoving global identifiers ...')
-  for i in range(len(data)):
-    for j in range(len(data[i])):
-      data[i][j] = re.sub(rgx.global_id, "<@ID>", data[i][j])
-
-  return data
-
-
-def remove_labels(data):
-  """Replace label declarations by token '<LABEL>'.
-
-  Args:
-    data: A list of list of strings to modify.
-
-  Returns:
-     The list of list of strings.
-  """
-  logging.info('Removing labels')
-  for i in range(len(data)):
-    for j in range(len(data[i])):
-      if re.match(r'; <label>:\d+:?(\s+; preds = )?', data[i][j]):
-        data[i][j] = re.sub(r":\d+", ":<LABEL>", data[i][j])
-        data[i][j] = re.sub("<%ID>", "<LABEL>", data[i][j])
-      elif re.match(rgx.local_id_no_perc + r':(\s+; preds = )?', data[i][j]):
-        data[i][j] = re.sub(rgx.local_id_no_perc + ':', "<LABEL>:", data[i][j])
-        data[i][j] = re.sub("<%ID>", "<LABEL>", data[i][j])
-      if '; preds = ' in data[i][j]:
-        s = data[i][j].split('  ')
-        if s[-1][0] == ' ':
-          data[i][j] = s[0] + s[-1]
-        else:
-          data[i][j] = s[0] + ' ' + s[-1]
-
-  return data
-
-
-def replace_unnamed_values(data):
-  """Replace unnamed_values with abstract tokens.
-
-  Abstract tokens map:
-    integers: <INT>
-    floating points: <FLOAT> (whether in decimal or hexadecimal notation)
-    string: <STRING>
-
-  Args:
-    data: A list of list of strings to modify.
-
-  Returns:
-     The list of list of strings.
-  """
-  logging.info('Removing immediate values')
-  for i in range(len(data)):
-    for j in range(len(data[i])):
-      # Hexadecimal notation.
-      data[i][j] = re.sub(
-          r' ' + rgx.immediate_value_float_hexa, " <FLOAT>", data[i][j])
-      # Decimal / scientific notation.
-      data[i][j] = re.sub(
-          r' ' + rgx.immediate_value_float_sci, " <FLOAT>", data[i][j])
-      if (re.match("<%ID> = extractelement", data[i][j]) is None and
-          re.match("<%ID> = extractvalue", data[i][j]) is None and
-          re.match("<%ID> = insertelement", data[i][j]) is None and
-          re.match("<%ID> = insertvalue", data[i][j]) is None):
-        data[i][j] = re.sub(r'(?<!align)(?<!\[) ' + rgx.immediate_value_int,
-                            " <INT>", data[i][j])
-
-      data[i][j] = re.sub(rgx.immediate_value_string, " <STRING>", data[i][j])
-
-  return data
-
-
-def remove_index_types(data):
-  """Replace the index type in expressions containing "extractelement" or
-  "insertelement" by token <TYP>.
-
-  Args:
-    data: A list of list of strings to modify.
-
-  Returns:
-     The list of list of strings.
-  """
-  logging.info('Removing index types ...')
-  for i in range(len(data)):
-    for j in range(len(data[i])):
-      if (re.match("<%ID> = extractelement", data[i][j]) is not None or
-          re.match("<%ID> = insertelement", data[i][j]) is not None):
-        data[i][j] = re.sub(r'i\d+ ', '<TYP> ', data[i][j])
-
-  return data
+  return data, file_names
 
 
 def CreateSeqDirFromIr(folder_ir: str,
@@ -324,109 +154,30 @@ def CreateSeqDirFromIr(folder_ir: str,
     folders_seq = [folder_seq]
 
   # Loop over sub-folders
-  num_folders = len(folders_ir)
   for i, raw_ir_folder in enumerate(folders_ir):
 
     l = folders_seq[i] + '/'
     if not os.path.exists(l) or not os.listdir(l):
-      # Read files
-
       # Read data from folder
-      logging.info('Reading data from folder %s', raw_ir_folder)
-      raw_data, file_names = i2v_prep.read_data_files_from_folder(raw_ir_folder)
-
-      # Print data statistics and release memory
-      source_data_list, source_data = i2v_prep.data_statistics(
-          raw_data, descr="reading data from source files")
-      del source_data_list
-
-      # Source code transformation: simple pre-processing
-      logging.info('Pre-processing code')
-      preprocessed_data, functions_declared_in_files = i2v_prep.preprocess(
-          raw_data)
-      preprocessed_data_with_structure_def = raw_data
-
-      # Load dictionary and cutoff statements from vocabulary files.
-      dictionary = vocab.dictionary
-      stmts_cut_off = vocab.cutoff_stmts
-
-      # IR processing (inline structures, abstract statements)
-
-      # Source code transformation: inline structure types
-      logging.info('Inlining structure types')
-      processed_data, _ = inline_struct_types_txt(
-          preprocessed_data, preprocessed_data_with_structure_def)
-
-      # Source code transformation: identifier processing (abstract statements)
-      logging.info('Creating abstract statements from identifiers')
-      processed_data = abstract_statements_from_identifiers_txt(processed_data)
+      raw_data, file_names = read_data_files_from_folder(raw_ir_folder)
 
       # Write indexed sequence of statements
       seq_folder = folders_seq[i]
       if not os.path.exists(seq_folder):
         os.makedirs(seq_folder)
 
-      # Write indexed sequence of statements to file
-      unknown_counter_folder = list()
-      seq_length_folder = list()
-      file_counter = 0
-      for file in processed_data:
-
-        stmt_indexed = list()  # Construct indexed sequence
-        unknown_counter = 0  # Reset unknown counter
-        for stmt in file:
-
-          # check whether this is a label, in which case we ignore it
-          if re.match(r'((?:<label>:)?(<LABEL>):|; <label>:<LABEL>)', stmt):
-            continue
-
-          # check whether this is an unknown
-          if stmt in stmts_cut_off:
-            stmt = rgx.unknown_token
-            unknown_counter += 1
-
-          # lookup and add to list
-          if stmt not in dictionary.keys():
-            logging.error("NOT IN DICTIONARY: %s", stmt)
-            stmt = rgx.unknown_token
-            unknown_counter += 1
-
-          stmt_indexed.append(dictionary[stmt])
+      # Write indexed sequence of statements to files.
+      for i, file in enumerate(raw_data):
+        result = vocab.EncodeLlvmBytecode(file)
 
         # Write to csv
         file_name_csv = os.path.join(
-            seq_folder, file_names[file_counter][:-3] + '_seq.csv')
+            seq_folder, file_names[i][:-3] + '_seq.csv')
         file_name_rec = os.path.join(
-            seq_folder, file_names[file_counter][:-3] + '_seq.rec')
+            seq_folder, file_names[i][:-3] + '_seq.rec')
         with open(file_name_csv, 'w') as csv, open(file_name_rec, 'wb') as rec:
-          for ind in stmt_indexed:
+          for ind in result.encoded:
             csv.write(str(ind) + '\n')
             rec.write(struct.pack('I', int(ind)))
-        logging.info('Created data pair %s %s', file_name_csv, file_name_rec)
-        logging.info('#UNKS: %d', unknown_counter)
-
-        # Increment counter
-        unknown_counter_folder.append(unknown_counter)
-        seq_length_folder.append(len(stmt_indexed))
-        file_counter += 1
-
-      # Print stats for folder.
-      logging.info('Processed %s (%d / %d)', raw_ir_folder, i + 1, num_folders)
-      logging.info('Number of files processed in %s: %d',
-                   raw_ir_folder, len(seq_length_folder))
-      logging.info('Sequence lengths in %s: min=%d avg=%.2f max=%d',
-                   raw_ir_folder, min(seq_length_folder),
-                   sum(seq_length_folder) / len(seq_length_folder),
-                   max(seq_length_folder))
-      logging.info('Total number stmts in %s: %d', raw_ir_folder,
-                   sum(seq_length_folder))
-      logging.info('#UNKS in sequences in %s: min=%d, avg=%.2f, max=%d',
-                   raw_ir_folder, min(unknown_counter_folder),
-                   sum(unknown_counter_folder) / len(unknown_counter_folder),
-                   max(unknown_counter_folder))
-      logging.info('Sum #UNKS in all sequences in %s: %d / %d, %.2f %%',
-                   raw_ir_folder, sum(unknown_counter_folder),
-                   sum(seq_length_folder),
-                   sum(unknown_counter_folder) * 100 / sum(seq_length_folder))
 
   return folder_seq
--- deeplearning/ncc/vocabulary_test.py
+++ deeplearning/ncc/vocabulary_test.py
@@ -3,6 +3,7 @@
 import pytest
 from absl import flags
 
+from deeplearning.ncc import inst2vec_pb2
 from deeplearning.ncc import vocabulary
 from labm8 import bazelutil
 from labm8 import test
@@ -12,6 +13,59 @@ FLAGS = flags.FLAGS
 
 VOCABULARY_PATH = bazelutil.DataPath(
     'phd/deeplearning/ncc/published_results/vocabulary.zip')
+
+# LLVM IR for the following C function:
+#
+#   int FizzBuzz(int i) {
+#     if (i % 15 == 0) {
+#       return 1;
+#     }
+#     return 0;
+#   }
+#
+# Generated using:
+#
+#   bazel run //compilers/llvm:clang -- /tmp/fizzbuzz.c -- \
+#       -emit-llvm -S -o /tmp/fizzbuzz.ll
+#
+FIZZBUZZ_IR = """\
+; ModuleID = '/tmp/fizzbuzz.c'
+source_filename = "/tmp/fizzbuzz.c"
+target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx10.12.0"
+
+; Function Attrs: noinline nounwind optnone ssp uwtable
+define i32 @FizzBuzz(i32) #0 {
+  %2 = alloca i32, align 4
+  %3 = alloca i32, align 4
+  store i32 %0, i32* %3, align 4
+  %4 = load i32, i32* %3, align 4
+  %5 = srem i32 %4, 15
+  %6 = icmp eq i32 %5, 0
+  br i1 %6, label %7, label %8
+
+; <label>:7:                                      ; preds = %1
+  store i32 1, i32* %2, align 4
+  br label %9
+
+; <label>:8:                                      ; preds = %1
+  store i32 0, i32* %2, align 4
+  br label %9
+
+; <label>:9:                                      ; preds = %8, %7
+  %10 = load i32, i32* %2, align 4
+  ret i32 %10
+}
+
+attributes #0 = { noinline nounwind optnone ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+fxsr,+mmx,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.module.flags = !{!0, !1}
+!llvm.ident = !{!2}
+
+!0 = !{i32 1, !"wchar_size", i32 4}
+!1 = !{i32 7, !"PIC Level", i32 2}
+!2 = !{!"clang version 6.0.0 (tags/RELEASE_600/final)"}
+"""
 
 
 @pytest.fixture(scope='session')
@@ -63,6 +117,48 @@ def test_VocabularyZipFile_unknown_token_index_value(
     vocab: vocabulary.VocabularyZipFile):
   """Test that unknown token index is positive."""
   assert vocab.unknown_token_index > 0
+
+
+def test_VocabularyZipFile_EncodeLlvmBytecode_bytecode(
+    vocab: vocabulary.VocabularyZipFile):
+  """Test that bytecode is set in return value."""
+  result = vocab.EncodeLlvmBytecode(FIZZBUZZ_IR)
+  assert result.input_bytecode == FIZZBUZZ_IR
+
+
+def test_VocabularyZipFile_EncodeLlvmBytecode_preprocessing(
+    vocab: vocabulary.VocabularyZipFile):
+  """Test output of pre-processing bytecode."""
+  options = inst2vec_pb2.EncodeBytecodeOptions(
+      set_bytecode_after_preprocessing=True)
+  result = vocab.EncodeLlvmBytecode(
+      FIZZBUZZ_IR, options)
+
+  assert result.bytecode_after_preprocessing == """\
+define i32 <@ID>(i32)
+<%ID> = alloca i32, align 4
+<%ID> = alloca i32, align 4
+store i32 <%ID>, i32* <%ID>, align 4
+<%ID> = load i32, i32* <%ID>, align 4
+<%ID> = srem i32 <%ID>, <INT>
+<%ID> = icmp eq i32 <%ID>, <INT>
+br i1 <%ID>, label <%ID>, label <%ID>
+; <label>:<LABEL>: ; preds = <LABEL>
+store i32 <INT>, i32* <%ID>, align 4
+br label <%ID>
+; <label>:<LABEL>: ; preds = <LABEL>
+store i32 <INT>, i32* <%ID>, align 4
+br label <%ID>
+; <label>:<LABEL>: ; preds = <LABEL>, <LABEL>
+<%ID> = load i32, i32* <%ID>, align 4
+ret i32 <%ID>"""
+
+
+def test_VocabularyZipFile_EncodeLlvmBytecode_sequence(
+    vocab: vocabulary.VocabularyZipFile):
+  # Function contains 14 statements.
+  result = vocab.EncodeLlvmBytecode(FIZZBUZZ_IR)
+  assert len(result.encoded) == 14
 
 
 if __name__ == '__main__':
