6196646fa193213b8685bda0b70f4d480396068f
Chris Cummins <chrisc.101@gmail.com>
2019-01-07 21:00:27 +0000

    Remove unused code.

--- deeplearning/deeptune/opencl/heterogeneous_mapping/utils.py
+++ deeplearning/deeptune/opencl/heterogeneous_mapping/utils.py
@@ -15,36 +15,6 @@ from deeplearning.clgen.corpuses import atomizers
 
 FLAGS = flags.FLAGS
 
-
-def platform2str(platform: str) -> str:
-  """Get full platform name."""
-  if platform == "amd":
-    return "AMD Tahiti 7970"
-  elif platform == "nvidia":
-    return "NVIDIA GTX 970"
-  else:
-    raise LookupError
-
-
-def escape_suite_name(g: str) -> str:
-  """Format benchmark suite name for display."""
-  c = g.split('-')
-  if c[0] == "amd" or c[0] == "nvidia":
-    return c[0].upper() + " SDK"
-  if c[0] == "npb" or c[0] == "shoc":
-    return c[0].upper()
-  elif c[0] == "parboil" or c[0] == "polybench" or c[0] == "rodinia":
-    return c[0].capitalize()
-  else:
-    raise LookupError
-
-
-def escape_benchmark_name(g: str) -> str:
-  """Escape benchmark name for display."""
-  c = g.split('-')
-  return escape_suite_name(c[0]).split()[0] + "." + c[-2]
-
-
 # Taken from the C99 spec, OpenCL spec 1.2, and bag-of-words analysis of
 # GitHub corpus:
 OPENCL_ATOMS = set(
@@ -244,7 +214,6 @@ def evaluate(model: 'HeterogemeousMappingModel', df: pd.DataFrame, atomizer,
                  np.mean([r['Speedup'] for r in split_data]))
 
     data += split_data
-
 
   return pd.DataFrame(
       data, index=range(1, len(data) + 1), columns=[
