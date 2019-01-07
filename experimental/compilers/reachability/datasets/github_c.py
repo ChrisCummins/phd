1b78b0aa175c0f8a4d8052b0f535f0a702d0a3b9
Chris Cummins <chrisc.101@gmail.com>
2019-01-07 00:42:58 +0000

    Work in progress on bytecode dataset.

--- experimental/compilers/reachability/datasets/github_c.py
+++ experimental/compilers/reachability/datasets/github_c.py
@@ -9,6 +9,7 @@ from compilers.llvm import clang
 from datasets.github.scrape_repos import contentfiles
 from experimental.compilers.reachability import database
 from experimental.compilers.reachability import reachability_pb2
+from labm8 import lockfile
 
 
 FLAGS = flags.FLAGS
@@ -17,51 +18,48 @@ flags.DEFINE_string('db', None, 'Path of database to populate.')
 flags.DEFINE_string('cf', None, 'Path of contentfiles database.')
 
 
-def BytecodeFromSrc(src: str) -> str:
-  """Create bytecode from a C source file.
+def GetBytecodesFromContentFiles(
+    source_name: str,
+    content_files: typing.List[typing.Tuple[int, str]]
+    ) -> typing.List[reachability_pb2.LlvmBytecode]:
+  """Extract LLVM bytecode from a contentfile, or return None.
 
   Args:
-    src: The source string.
+    source_name: The name of the content file database.
+    content_files: A list of <id,text> tuples, where each tuple is the ID and
+      text of a row in the content file database.
 
   Returns:
-    The bytecode as a string.
-
-  Raises:
-    ClangException: If compiling to bytecode fails.
+    A list of zero LlvmBytecode protos, one for each contentfile which was
+    successfully processed.
   """
+  protos = []
+
   clang_args = [
     '-xc', '-S', '-emit-llvm', '-', '-o', '-', '-O0',
     '-ferror-limit=1', '-Wno-everything',  # No warnings please.
   ]
-  process = clang.Exec(clang_args, stdin=src)
-  if process.returncode:
-    raise clang.ClangException(
-        returncode=process.returncode, stderr=process.stderr,
-        command=clang_args)
-  return process.stdout, clang_args
 
+  for content_file_id, text in content_files:
+    process = clang.Exec(clang_args, stdin=text)
+    if process.returncode:
+      continue
 
-def ProcessContentFile(cf_url: str, cf_id: int,
-                       text: str) -> reachability_pb2.LlvmBytecode:
-  try:
-    bytecode, cflags = BytecodeFromSrc(text)
-    clang_returncode = 0
-
-    return reachability_pb2.LlvmBytecode(
-      source_name=cf_url,
-      relpath=str(cf_id),
+    protos.append(reachability_pb2.LlvmBytecode(
+      source_name=source_name,
+      relpath=str(content_file_id),
       lang='C',
-      cflags=' '.join(cflags),
-      bytecode=bytecode,
-      clang_returncode=clang_returncode,
+      cflags=' '.join(clang_args),
+      bytecode=process.stdout,
+      clang_returncode=0,
       error_message='',
-    )
-  except clang.ClangException as e:
-    return None
+    ))
+
+  return protos
 
 
-def BatchedQuery(query, yield_per: int = 1000):
-  i = 0
+def BatchedQuery(query, start_at: int = 0, yield_per: int = 1000):
+  i = start_at
   batch = []
   while True:
     batch = query.offset(i).limit(yield_per).all()
@@ -74,22 +72,52 @@ def BatchedQuery(query, yield_per: int = 1000):
 
 def PopulateBytecodeTable(
     cf: contentfiles.ContentFiles,
-    db: database.Database, commit_every: int = 1000):
-  pool = multiprocessing.Pool()
+    db: database.Database,
+    pool: typing.Optional[multiprocessing.Pool] = None):
+  pool = pool or multiprocessing.Pool()
 
-  with cf.Session() as cf_s:
+  # Only one process at a time can run this method.
+  mutex = lockfile.AutoLockFile()
+
+  # Read source files from the contenfiles database, process them into
+  # bytecodes, and, if successful, write them into the database. We process
+  # files sorted by their numeric ID in the contentfiles database, so that if
+  with mutex, cf.Session() as cf_s, db.Session(commit=True) as s:
+    # Get the ID of the last-processed bytecode file to resume from.
+    resume_from = (s.query(database.LlvmBytecode.relpath)\
+        .filter(database.LlvmBytecode.source_name == cf.url)\
+        .order_by(database.LlvmBytecode.relpath.desc())\
+        .limit(1).first() or (0,))[0]
+    logging.info('Starting at ID %s', resume_from)
+
+    # Get the IDs and C srcs of files to process.
     q = cf_s.query(
         contentfiles.ContentFile.id, contentfiles.ContentFile.text)\
+        .filter(contentfiles.ContentFile.id > resume_from)\
+        .order_by(contentfiles.ContentFile.id)
 
-    chunk_size = 100
-    for i, batch in enumerate(BatchedQuery(q, yield_per=chunk_size)):
-      logging.info('processing file %d', (i + 1) * chunk_size)
-      process_args = [(cf.url, cf_id, text) for cf_id, text in batch]
-      with db.Session(commit=True) as s:
-        for proto in pool.starmap(ProcessContentFile, process_args):
-          if proto:
-            s.add(database.LlvmBytecode
-                **database.LlvmBytecode.FromProto(proto)))
+    # The ID of the last contentfile to process.
+    n = (cf_s.query(contentfiles.ContentFile.id)\
+        .order_by(contentfiles.ContentFile.id.desc())\
+        .one_or_none() or (0,))[0]
+
+    # Process the soure files in batches. For each batch: convert to bytecode
+    # and store in the database. Files in batches are processed in parallel.
+    # Database commits are per-batch.
+    batch_size = 256
+    for batch in BatchedQuery(q, yield_per=batch_size):
+      i = batch[0][0]  # The ID of the first content file in the batch.
+      logging.info('Processing contentfiles to bytecode %d / %d', batch[0][0], n)
+      content_files_per_process = 5
+      process_args = [
+          (cf.url, batch[i:i+content_files_per_process])
+          for i in range(0, len(batch), content_files_per_process)
+      ]
+      for protos in pool.starmap(GetBytecodesFromContentFiles, process_args):
+        for proto in protos:
+          s.add(database.LlvmBytecode(
+              **database.LlvmBytecode.FromProto(proto)))
+      s.commit()
 
 
 def main(argv):
