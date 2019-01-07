aa75d6fb48a31bf340a7b4ba49ea2e0e535465fd
Chris Cummins <chrisc.101@gmail.com>
2019-01-07 14:46:44 +0000

    Add support for multiple languages.

--- experimental/compilers/reachability/datasets/github.py
+++ experimental/compilers/reachability/datasets/github.py
@@ -10,6 +10,7 @@ from absl import logging
 
 from compilers.llvm import clang
 from datasets.github.scrape_repos import contentfiles
+from deeplearning.clgen.preprocessors import opencl
 from experimental.compilers.reachability import database
 from experimental.compilers.reachability import reachability_pb2
 from labm8 import lockfile
@@ -20,16 +21,31 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('db', None, 'Path of database to populate.')
 flags.DEFINE_string('cf', None, 'Path of contentfiles database.')
+flags.DEFINE_string('lang', None,
+                    'Name of the language to process. One of: {c,opencl}.')
+
+# A dictionary mapping language name to a list of language-specific arguments
+# to pass to clang.
+LANGUAGE_TO_CLANG_ARGS = {
+  'c': [
+    '-xc', '-O0',
+    '-ferror-limit=1', '-Wno-everything',  # No warnings please.
+  ],
+  'opencl': opencl.GetClangArgs(use_shim=True),
+}
 
 
 def GetBytecodesFromContentFiles(
-    source_name: str,
+    source_name: str, language: str,
     content_files: typing.List[typing.Tuple[int, str]]
 ) -> typing.List[reachability_pb2.LlvmBytecode]:
-  """Extract LLVM bytecode from a contentfile, or return None.
+  """Extract LLVM bytecodes from contentfiles.
 
   Args:
-    source_name: The name of the content file database.
+    source_name: The name of the content file database. This is the same across
+      all content files.
+    language: The source code language. This is the same across all content
+      files.
     content_files: A list of <id,text> tuples, where each tuple is the ID and
       text of a row in the content file database.
 
@@ -38,10 +54,8 @@ def GetBytecodesFromContentFiles(
     successfully processed.
   """
   protos = []
-
-  clang_args = [
-    '-xc', '-S', '-emit-llvm', '-', '-o', '-', '-O0',
-    '-ferror-limit=1', '-Wno-everything',  # No warnings please.
+  clang_args = LANGUAGE_TO_CLANG_ARGS[language] + [
+    '-S', '-emit-llvm', '-', '-o', '-'
   ]
 
   for content_file_id, text in content_files:
@@ -52,7 +66,7 @@ def GetBytecodesFromContentFiles(
     protos.append(reachability_pb2.LlvmBytecode(
         source_name=source_name,
         relpath=str(content_file_id),
-        lang='C',
+        lang=language,
         cflags=' '.join(clang_args),
         bytecode=process.stdout,
         clang_returncode=0,
@@ -64,10 +78,14 @@ def GetBytecodesFromContentFiles(
 
 def PopulateBytecodeTable(
     cf: contentfiles.ContentFiles,
+    language: str,
     db: database.Database,
     pool: typing.Optional[multiprocessing.Pool] = None):
   # Only one process at a time can run this method.
-  lockfile.AutoLockFile().acquire()
+  lockfile.AutoLockFile(granularity='function').acquire()
+
+  # We use the database URL as the name of the source.
+  source_name = cf.url
 
   # Read source files from the contenfiles database, process them into
   # bytecodes, and, if successful, write them into the database. We process
@@ -77,6 +95,7 @@ def PopulateBytecodeTable(
     resume_from = int((
         s.query(database.LlvmBytecode.relpath)
         .filter(database.LlvmBytecode.source_name == cf.url)
+        .filter(database.LlvmBytecode.language == language)
         # Note the cast to integer: relpath is a string column, sorting by it
         # in its native type would sort the string (e.g. '9' > '10'.
         .order_by(sql.cast(database.LlvmBytecode.relpath, sql.Integer).desc())
@@ -86,16 +105,17 @@ def PopulateBytecodeTable(
 
     # Get the ID of the last contentfile to process.
     n = (cf_s.query(contentfiles.ContentFile.id)
+         .filter(contentfiles.ContentFile.language == language)
          .order_by(contentfiles.ContentFile.id.desc())
          .limit(1).one_or_none() or (0,))[0]
     logging.info('Starting at row %s / %s',
                  humanize.intcomma(resume_from), humanize.intcomma(n))
 
-    # A query to return the IDs and sources of files to process.
-    q = cf_s.query(
-        contentfiles.ContentFile.id, contentfiles.ContentFile.text) \
-      .filter(contentfiles.ContentFile.id > resume_from) \
-      .order_by(contentfiles.ContentFile.id)
+    # A query to return the <id,text> tuples of files to process.
+    q = (cf_s.query(contentfiles.ContentFile.id, contentfiles.ContentFile.text)
+         .filter(contentfiles.ContentFile.id > resume_from)
+         .filter(contentfiles.ContentFile.language == language)
+         .order_by(contentfiles.ContentFile.id))
 
     def _AddProtosToDatabase(
         protos: typing.List[reachability_pb2.LlvmBytecode]) -> None:
@@ -114,7 +134,7 @@ def PopulateBytecodeTable(
 
     ppar.MapDatabaseRowBatchProcessor(
         GetBytecodesFromContentFiles, q,
-        generate_work_unit_args=lambda rows: (cf.url, rows),
+        generate_work_unit_args=lambda rows: (source_name, language, rows),
         work_unit_result_callback=_AddProtosToDatabase,
         start_of_batch_callback=_StartBatch,
         batch_size=256,
@@ -128,9 +148,15 @@ def main(argv):
   if len(argv) > 1:
     raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
 
+  language = FLAGS.lang
+  if not language in LANGUAGE_TO_CLANG_ARGS:
+    raise app.UsageError(
+        f'Language `{language}` not supported. '
+        f'Must be one of: {LANGUAGE_TO_CLANG_ARGS.keys()}')
+
   db = database.Database(FLAGS.db)
   cf = contentfiles.ContentFiles(FLAGS.cf)
-  PopulateBytecodeTable(cf, db)
+  PopulateBytecodeTable(cf, language, db)
 
 
 if __name__ == '__main__':
