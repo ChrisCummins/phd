1c12dc26475e3ddf7d2696a7863cf16e1c792222
Chris Cummins <chrisc.101@gmail.com>
2019-01-21 22:10:32 +0000

    Add programming club exercises.

---
+++ learn/pc/serialize_binary_tree.py
@@ -0,0 +1,84 @@
+# Given the root to a binary tree, implement serialize(root), which serializes
+# the tree into a string, and deserialize(s), which deserializes the string
+# back into the tree.
+import typing
+
+
+class Node:
+  """Example class to serialize."""
+
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+# This implementation is very suboptimal, and overly complex. I was given a hint
+# to look at a reverse polish parser and see if that could be applied here.
+
+
+def set_id(node: Node, counter: int):
+  counter += 1
+  node.id = counter
+  if node.left:
+    counter = set_id(node.left, counter)
+  if node.right:
+    counter = set_id(node.right, counter)
+  return counter
+
+
+def serialize_dfs(node: Node, s: typing.List[str]):
+  s.append(' '.join(str(x) for x in [
+    len(node.val),
+    node.val,
+    node.left.id if node.left else -1,
+    node.right.id if node.right else -1,
+  ]))
+  if node.left:
+    s = serialize_dfs(node.left, s)
+  if node.right:
+    s = serialize_dfs(node.right, s)
+  return s
+
+
+def read_line(s: str):
+  space = s.index(' ')
+  val_len = int(s[:space])
+  s = s[space + 1:]
+  val = s[:val_len]
+
+  s = s[val_len + 1:]
+  left = int(s[:s.index(' ')])
+  s = s[s.index(' ') + 1:]
+  right = int(s[:s.index('\n')])
+  s = s[s.index('\n'):]
+
+  return s, val, left, right
+
+
+# Serialize and deserialize functions.
+
+def serialize(node: Node):
+  set_id(node, 0)
+  return '\n'.join(serialize_dfs(node, [])) + '\n'
+
+
+def deserialize(s: str) -> Node:
+  nodes = []
+  while s.strip():
+    s, val, left, right = read_line(s)
+    nodes.append(Node(val, left, right))
+
+  for n in nodes:
+    if n.left != -1:
+      n.left = nodes[n.left - 1]
+    if n.right != -1:
+      n.right = nodes[n.right - 1]
+
+  return nodes[0]
+
+
+if __name__ == '__main__':
+  node = Node('root', Node('left', Node('left.left')), Node('right'))
+  print(deserialize(serialize(node)))
+  assert deserialize(serialize(node)).left.left.val == 'left.left'
---
+++ learn/pc/vector_of_products.cc
@@ -0,0 +1,99 @@
+#include "phd/test.h"
+
+#include <vector>
+
+// Given an array of integers, return a new array such that each element at
+// index i of the new array is the product of all the numbers in the original
+// array except the one at i.
+//
+// For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+// [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would
+// be [2, 3, 6].
+std::vector<int> VectorOfProducts(const std::vector<int>& vec) {
+  std::vector<int> out(vec.size());
+
+  int product = 1;
+  for (auto i : vec) {
+    product *= i;
+  }
+
+  for (int i = 0; i < out.size(); ++i) {
+    out[i] = product / vec[i];
+  }
+
+  return out;
+}
+
+
+TEST(VectorOfProducts, EmptyListReturnsEmptyList) {
+  std::vector<int> v;
+  EXPECT_EQ(VectorOfProducts(v), std::vector<int>({}));
+}
+
+
+TEST(VectorOfProducts, SingleElementListReturnsTheSame) {
+  std::vector<int> v{1};
+  EXPECT_EQ(VectorOfProducts(v), std::vector<int>({1}));
+}
+
+
+TEST(VectorOfProducts, OneTwoThreeList) {
+  std::vector<int> v{1, 2, 3};
+  EXPECT_EQ(VectorOfProducts(v), std::vector<int>({6, 3, 2}));
+}
+
+
+// Follow-up: what if you can't use division?
+
+std::vector<int> VectorOfProductsWithoutDivision(const std::vector<int>& vec) {
+  std::vector<int> prefixes(vec.size());
+  std::vector<int> suffixes(vec.size());
+
+  // Create a vector of prefixes products, where element i is the product of
+  // all elements up to and including i:
+  //   a[i] * a[i-1] * a[i-2] * ... * a[0].
+  int product = 1;
+  for (int i = 0; i < prefixes.size(); ++i) {
+    product *= vec[i];
+    prefixes[i] = product;
+  }
+
+  // Create a vector of suffix products, where element i is the product of all
+  // elements from i to the end of the list:
+  //   a[i] * a[i+1] * a[i+2] * ... * a[n].
+  product = 1;
+  for (int i = suffixes.size() - 1; i >= 0; --i) {
+    product *= vec[i];
+    suffixes[i] = product;
+  }
+
+  std::vector<int> out(vec.size());
+  for (int i = 0; i < vec.size(); ++i) {
+    int prefix = i ? prefixes[i - 1] : 1;  // bounds checked access.
+    int suffix = i < vec.size() - 1 ? suffixes[i + 1] : 1;
+    out[i] = prefix * suffix;
+  }
+
+  return out;
+}
+
+
+TEST(VectorOfProductsWithoutDivision, EmptyListReturnsEmptyList) {
+  std::vector<int> v;
+  EXPECT_EQ(VectorOfProductsWithoutDivision(v), std::vector<int>({}));
+}
+
+
+TEST(VectorOfProductsWithoutDivision, SingleElementListReturnsTheSame) {
+  std::vector<int> v{1};
+  EXPECT_EQ(VectorOfProductsWithoutDivision(v), std::vector<int>({1}));
+}
+
+
+TEST(VectorOfProductsWithoutDivision, OneTwoThreeList) {
+  std::vector<int> v{1, 2, 3};
+  EXPECT_EQ(VectorOfProductsWithoutDivision(v), std::vector<int>({6, 3, 2}));
+}
+
+
+TEST_MAIN();
