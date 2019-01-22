d9afd2e3dd51281d859cbcff4ca046a8de691f3e
Chris Cummins <chrisc.101@gmail.com>
2019-01-22 15:49:30 +0000

    Fixes for benchmark model.

--- talks/2019_02_msr/benchmark_clgen.py
+++ talks/2019_02_msr/benchmark_clgen.py
@@ -21,157 +21,152 @@ FLAGS = flags.FLAGS
 INSTANCE_TO_BENCHMARK = pbutil.FromString("""
 # File: //deeplearning/clgen/proto/clgen.proto
 # Proto: clgen.Instance
-message Instance {
-  // The path to the clgen working directory. This directory stores persistent
-  // caches files, including the corpus, model checkpoints, and sampler outputs.
-  working_dir: "/tmp/phd/deeplearning/clgen/working_dir"
+working_dir: "/tmp/phd/deeplearning/clgen/working_dir"
+model {
   corpus {
-      local_directory: "$HOME/data/datasets/github/corpuses/opencl"
-      greedy_multichar_atomizer {
-        tokens: "  "
-        tokens: "__assert"
-        tokens: "__attribute"
-        tokens: "__builtin_astype"
-        tokens: "__clc_fabs"
-        tokens: "__clc_fma"
-        tokens: "__inline"
-        tokens: "abs"
-        tokens: "alignas"
-        tokens: "alignof"
-        tokens: "atomic_add"
-        tokens: "auto"
-        tokens: "barrier"
-        tokens: "bool"
-        tokens: "break"
-        tokens: "case"
-        tokens: "char"
-        tokens: "clamp"
-        tokens: "complex"
-        tokens: "const"
-        tokens: "constant"
-        tokens: "continue"
-        tokens: "default"
-        tokens: "defined"
-        tokens: "do"
-        tokens: "double"
-        tokens: "else"
-        tokens: "enum"
-        tokens: "error"
-        tokens: "event_t"
-        tokens: "extern"
-        tokens: "fabs"
-        tokens: "false"
-        tokens: "float"
-        tokens: "for"
-        tokens: "get_global_id"
-        tokens: "get_global_size"
-        tokens: "get_local_id"
-        tokens: "get_local_size"
-        tokens: "get_num_groups"
-        tokens: "global"
-        tokens: "goto"
-        tokens: "half"
-        tokens: "if"
-        tokens: "image1d_array_t"
-        tokens: "image1d_buffer_t"
-        tokens: "image1d_t"
-        tokens: "image2d_array_t"
-        tokens: "image2d_t"
-        tokens: "image3d_t"
-        tokens: "imaginary"
-        tokens: "include"
-        tokens: "inline"
-        tokens: "int"
-        tokens: "into"
-        tokens: "kernel"
-        tokens: "line"
-        tokens: "local"
-        tokens: "long"
-        tokens: "noreturn"
-        tokens: "pragma"
-        tokens: "private"
-        tokens: "quad"
-        tokens: "read_only"
-        tokens: "read_write"
-        tokens: "register"
-        tokens: "restrict"
-        tokens: "return"
-        tokens: "sampler_t"
-        tokens: "short"
-        tokens: "shuffle"
-        tokens: "signed"
-        tokens: "size_t"
-        tokens: "sizeof"
-        tokens: "sqrt"
-        tokens: "static"
-        tokens: "struct"
-        tokens: "switch"
-        tokens: "true"
-        tokens: "typedef"
-        tokens: "u32"
-        tokens: "uchar"
-        tokens: "uint"
-        tokens: "ulong"
-        tokens: "undef"
-        tokens: "union"
-        tokens: "unsigned"
-        tokens: "void"
-        tokens: "volatile"
-        tokens: "while"
-        tokens: "wide"
-        tokens: "write_only"
-      }
-      contentfile_separator: "\n\n"
-      preprocessor: "deeplearning.clgen.preprocessors.opencl:ClangPreprocessWithShim"
-      preprocessor: "deeplearning.clgen.preprocessors.opencl:Compile"
-      preprocessor: "deeplearning.clgen.preprocessors.opencl:NormalizeIdentifiers"
-      preprocessor: "deeplearning.clgen.preprocessors.opencl:StripDoubleUnderscorePrefixes"
-      preprocessor: "deeplearning.clgen.preprocessors.common:StripDuplicateEmptyLines"
-      preprocessor: "deeplearning.clgen.preprocessors.opencl:SanitizeKernelPrototype"
-      preprocessor: "deeplearning.clgen.preprocessors.common:StripTrailingWhitespace"
-      preprocessor: "deeplearning.clgen.preprocessors.opencl:ClangFormat"
-      preprocessor: "deeplearning.clgen.preprocessors.common:MinimumLineCount3"
-      preprocessor: "deeplearning.clgen.preprocessors.opencl:Compile"
+    local_directory: "$HOME/data/datasets/github/corpuses/opencl"
+    greedy_multichar_atomizer {
+      tokens: "  "
+      tokens: "__assert"
+      tokens: "__attribute"
+      tokens: "__builtin_astype"
+      tokens: "__clc_fabs"
+      tokens: "__clc_fma"
+      tokens: "__inline"
+      tokens: "abs"
+      tokens: "alignas"
+      tokens: "alignof"
+      tokens: "atomic_add"
+      tokens: "auto"
+      tokens: "barrier"
+      tokens: "bool"
+      tokens: "break"
+      tokens: "case"
+      tokens: "char"
+      tokens: "clamp"
+      tokens: "complex"
+      tokens: "const"
+      tokens: "constant"
+      tokens: "continue"
+      tokens: "default"
+      tokens: "defined"
+      tokens: "do"
+      tokens: "double"
+      tokens: "else"
+      tokens: "enum"
+      tokens: "error"
+      tokens: "event_t"
+      tokens: "extern"
+      tokens: "fabs"
+      tokens: "false"
+      tokens: "float"
+      tokens: "for"
+      tokens: "get_global_id"
+      tokens: "get_global_size"
+      tokens: "get_local_id"
+      tokens: "get_local_size"
+      tokens: "get_num_groups"
+      tokens: "global"
+      tokens: "goto"
+      tokens: "half"
+      tokens: "if"
+      tokens: "image1d_array_t"
+      tokens: "image1d_buffer_t"
+      tokens: "image1d_t"
+      tokens: "image2d_array_t"
+      tokens: "image2d_t"
+      tokens: "image3d_t"
+      tokens: "imaginary"
+      tokens: "include"
+      tokens: "inline"
+      tokens: "int"
+      tokens: "into"
+      tokens: "kernel"
+      tokens: "line"
+      tokens: "local"
+      tokens: "long"
+      tokens: "noreturn"
+      tokens: "pragma"
+      tokens: "private"
+      tokens: "quad"
+      tokens: "read_only"
+      tokens: "read_write"
+      tokens: "register"
+      tokens: "restrict"
+      tokens: "return"
+      tokens: "sampler_t"
+      tokens: "short"
+      tokens: "shuffle"
+      tokens: "signed"
+      tokens: "size_t"
+      tokens: "sizeof"
+      tokens: "sqrt"
+      tokens: "static"
+      tokens: "struct"
+      tokens: "switch"
+      tokens: "true"
+      tokens: "typedef"
+      tokens: "u32"
+      tokens: "uchar"
+      tokens: "uint"
+      tokens: "ulong"
+      tokens: "undef"
+      tokens: "union"
+      tokens: "unsigned"
+      tokens: "void"
+      tokens: "volatile"
+      tokens: "while"
+      tokens: "wide"
+      tokens: "write_only"
     }
+    contentfile_separator: "\\n\\n"
+    preprocessor: "deeplearning.clgen.preprocessors.opencl:ClangPreprocessWithShim"
+    preprocessor: "deeplearning.clgen.preprocessors.opencl:Compile"
+    preprocessor: "deeplearning.clgen.preprocessors.opencl:NormalizeIdentifiers"
+    preprocessor: "deeplearning.clgen.preprocessors.opencl:StripDoubleUnderscorePrefixes"
+    preprocessor: "deeplearning.clgen.preprocessors.common:StripDuplicateEmptyLines"
+    preprocessor: "deeplearning.clgen.preprocessors.opencl:SanitizeKernelPrototype"
+    preprocessor: "deeplearning.clgen.preprocessors.common:StripTrailingWhitespace"
+    preprocessor: "deeplearning.clgen.preprocessors.opencl:ClangFormat"
+    preprocessor: "deeplearning.clgen.preprocessors.common:MinimumLineCount3"
+    # TODO(cec): Temporarily disabling this: preprocessor: "deeplearning.clgen.preprocessors.opencl:Compile"
   }
-  model {
-    architecture {
-      backend: TENSORFLOW
-      neuron_type: LSTM
-      neurons_per_layer: 512
-      num_layers: 2
-      post_layer_dropout_micros: 0
-    }
-    training {
-      num_epochs: 50
-      sequence_length: 64
-      batch_size: 64
-      shuffle_corpus_contentfiles_between_epochs: true
-      adam_optimizer {
-        initial_learning_rate_micros: 2000
-        learning_rate_decay_per_epoch_micros: 50000
-        beta_1_micros: 900000
-        beta_2_micros: 999000
-        normalized_gradient_clip_micros: 5000000
-      }
-    }
+  architecture {
+    backend: TENSORFLOW
+    neuron_type: LSTM
+    neurons_per_layer: 512
+    num_layers: 2
+    post_layer_dropout_micros: 0
   }
-  sampler {
-    # File: //deeplearning/clgen/proto/sampler.proto
-    # Proto: clgen.Sampler
-    start_text: "kernel void "
+  training {
+    num_epochs: 50
+    sequence_length: 64
     batch_size: 64
-    temperature_micros: 1000000  # = 1.0 real value
-    termination_criteria {
-      symtok {
-        depth_increase_token: "{"
-        depth_decrease_token: "}"
-      }
+    shuffle_corpus_contentfiles_between_epochs: true
+    adam_optimizer {
+      initial_learning_rate_micros: 2000
+      learning_rate_decay_per_epoch_micros: 50000
+      beta_1_micros: 900000
+      beta_2_micros: 999000
+      normalized_gradient_clip_micros: 5000000
     }
-    termination_criteria {
-      maxlen {
-        maximum_tokens_in_sample: 5000
-      }
+  }
+}
+sampler {
+  # File: //deeplearning/clgen/proto/sampler.proto
+  # Proto: clgen.Sampler
+  start_text: "kernel void "
+  batch_size: 64
+  temperature_micros: 1000000  # = 1.0 real value
+  termination_criteria {
+    symtok {
+      depth_increase_token: "{"
+      depth_decrease_token: "}"
+    }
+  }
+  termination_criteria {
+    maxlen {
+      maximum_tokens_in_sample: 5000
     }
   }
 }
