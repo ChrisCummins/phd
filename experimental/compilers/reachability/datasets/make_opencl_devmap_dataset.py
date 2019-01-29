554329ec6bd685c513556c1d8ab43e1f7a1ca4b6
Chris Cummins <chrisc.101@gmail.com>
2019-01-29 10:15:09 +0000

    Add dataset creation scripts.

---
+++ experimental/compilers/reachability/datasets/make_opencl_devmap_dataset.py
@@ -0,0 +1,95 @@
+"""Generate the CFG datasets for OpenCL device mapping."""
+import pathlib
+
+import numpy as np
+import pandas as pd
+from absl import app
+from absl import flags
+from absl import logging
+
+from datasets.opencl.device_mapping import opencl_device_mapping_dataset
+from deeplearning.deeptune.opencl.heterogeneous_mapping import utils
+from deeplearning.deeptune.opencl.heterogeneous_mapping.models import models
+from labm8 import prof
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('outdir', '/tmp/phd/docs/wip_graph/datasets/opencl_devmap',
+                    'The directory to write the output dataframes to.')
+
+
+def SplitToDataFrame(split):
+  split.train_df['split:type'] = ['training'] * len(split.train_df)
+  split.valid_df['split:type'] = ['validation'] * len(split.valid_df)
+  split.test_df['split:type'] = ['test'] * len(split.test_df)
+  return pd.concat((split.train_df, split.valid_df, split.test_df))
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+
+  outdir = pathlib.Path(FLAGS.outdir)
+  outdir.mkdir(exist_ok=True, parents=True)
+
+  with prof.Profile('read dataset'):
+    dataset = opencl_device_mapping_dataset.OpenClDeviceMappingsDataset()
+    df = utils.AddClassificationTargetToDataFrame(dataset.df, 'amd_tahiti_7970')
+
+  model = models.Lda()
+  model.init(seed=0, atomizer=None)
+
+  with prof.Profile('extracted graphs'):
+    extracted_graphs = list(model.ExtractGraphs(df))
+
+  with prof.Profile('encoded graphs'):
+    encoded_graphs = list(model.EncodeGraphs(extracted_graphs))
+
+  unknowns = np.array(
+      [g.graph['num_unknown_statements'] / g.number_of_nodes() for _, g in
+       encoded_graphs])
+
+  logging.info(f'{unknowns.mean():.1%} of statements are unknown '
+               f'(min={unknowns.min():.1%}, max={unknowns.max():.1%})')
+
+  with prof.Profile('input target graphs'):
+    input_target_graphs = list(model.GraphsToInputTargets(encoded_graphs))
+    input_graphs, target_graphs = zip(*input_target_graphs)
+
+  # Add the graph representations to the dataframe they were extracted from.
+  df['networkx:graph'] = [x[1] for x in encoded_graphs]
+  df['networkx:input_graph'] = input_graphs
+  df['networkx:target_graph'] = target_graphs
+
+  # Add the dynamic functions.
+  df['graphnet:loss_op'] = 'GlobalsSoftmaxCrossEntropy'
+  df['graphnet:accuracy_evaluator'] = 'OneHotGlobals'
+
+  # Split into train/test/validation datasets:
+  splits = list(
+      utils.TrainValidationTestSplits(df, np.random.RandomState(0xCEC)))
+  assert len(splits) == 2
+
+  # Re-combine into dataframes.
+  amd_split, nvidia_split = splits
+
+  assert amd_split.gpu_name == "amd_tahiti_7970"
+  amd_df = SplitToDataFrame(amd_split)
+
+  assert nvidia_split.gpu_name == "nvidia_gtx_960"
+  nvidia_df = SplitToDataFrame(nvidia_split)
+
+  # Sanity check
+  assert len(input_graphs) == len(target_graphs) == len(encoded_graphs) == len(
+      amd_df) == len(nvidia_df)
+
+  logging.info("Writing %s", outdir / 'amd.pkl')
+  amd_df.to_pickle(str(outdir / 'amd.pkl'))
+  logging.info("Writing %s", outdir / 'nvidia.pkl')
+  nvidia_df.to_pickle(str(outdir / 'nvidia.pkl'))
+
+
+if __name__ == '__main__':
+  app.run(main)
