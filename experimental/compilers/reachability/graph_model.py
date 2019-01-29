0e632d445765e7aa97419b972ddabd102d992124
Chris Cummins <chrisc.101@gmail.com>
2019-01-29 09:23:23 +0000

    Merge training sript into graph_model.

--- experimental/compilers/reachability/graph_model.py
+++ experimental/compilers/reachability/graph_model.py
@@ -1,7 +1,9 @@
 """Implementation of graph networks for compilers."""
 import collections
+import contextlib
 import json
 import pathlib
+import pickle
 import time
 import typing
 
@@ -10,6 +12,7 @@ import numpy as np
 import pandas as pd
 import sonnet as snt
 import tensorflow as tf
+from absl import app
 from absl import flags
 from absl import logging
 from graph_nets import graphs
@@ -17,12 +20,22 @@ from graph_nets import modules
 from graph_nets import utils_np
 from graph_nets import utils_tf
 
+from experimental.compilers.reachability import graph_model
 from labm8 import labdate
 from labm8 import prof
 
 
 FLAGS = flags.FLAGS
 
+flags.DEFINE_string(
+    'df', '/tmp/phd/docs/wip_graph/lda_opencl_device_mapping_dataset.pkl',
+    'Path of the dataframe to load')
+flags.DEFINE_string(
+    'outdir', '/tmp/phd/docs/wip_graph/model_files',
+    'Path of directory to generate files')
+flags.DEFINE_bool(
+    'profile_tensorflow', False,
+    'Enable profiling of tensorflow.')
 flags.DEFINE_integer(
     'model_seed', 0, 'Seed to use for reproducible results')
 flags.DEFINE_integer(
@@ -604,3 +617,72 @@ class CompilerGraphNeuralNetwork(object):
 
     # Return the output graphs from the final epoch.
     return output_graphs
+
+
+class NoOpProfileContext():
+  """A profiling context which does no profiling.
+
+  This is used a return value of ProfileContext() which allows the
+  unconditional execution of profiling code, irrespective of profiling being
+  enabled.
+  """
+
+  def add_auto_profiling(self, *args, **kwargs):
+    """No-op."""
+    pass
+
+
+@contextlib.contextmanager
+def GetProfileContext(outdir: pathlib.Path, profile: bool = True):
+  """Return a profile context."""
+  if profile:
+    yield tf.contrib.tfprof.ProfileContext(str(outdir))
+  else:
+    yield NoOpProfileContext()
+
+
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+
+  logging.info('Starting evaluating graph model')
+
+  # Load graphs from file.
+  df_path = pathlib.Path(FLAGS.df)
+  assert df_path.is_file()
+
+  assert FLAGS.outdir
+  outdir = pathlib.Path(FLAGS.outdir)
+  # Make the output directories.
+  (outdir / 'values').mkdir(exist_ok=True, parents=True)
+
+  with prof.Profile('load dataframe'):
+    df = pd.read_pickle(df_path)
+  logging.info('Loaded %s dataframe from %s', df.shape, df_path)
+
+  # Prepare TensorFlow profiler.
+  builder = tf.profiler.ProfileOptionBuilder
+  opts = builder(builder.time_and_memory()).order_by('micros').build()
+  opts2 = tf.profiler.ProfileOptionBuilder.trainable_variables_parameter()
+  profile_context = GetProfileContext(
+      outdir / 'profile', FLAGS.profile_tensorflow)
+
+  # Create the model.
+  model = graph_model.CompilerGraphNeuralNetwork(df, outdir)
+
+  with profile_context as pctx, tf.Session() as sess:
+    # Set up profiling. Note that if not FLAGS.profile_tensorflow, these
+    # methods are no-ops.
+    pctx.add_auto_profiling('op', opts, [15, 50, 100])
+    pctx.add_auto_profiling('scope', opts2, [14, 49, 99])
+
+    outputs_path = outdir / 'outputs.pkl'
+    if not outputs_path.is_file():
+      outputs = model.TrainAndEvaluate(sess)
+      with open(outdir / 'outputs.pkl', 'wb') as f:
+        pickle.dump(outputs, f)
+
+
+if __name__ == '__main__':
+  app.run(main)
