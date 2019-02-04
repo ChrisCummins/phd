436c14613ff28733587ed5f4fcbf12493696f3fc
Chris Cummins <chrisc.101@gmail.com>
2019-02-04 10:30:56 +0000

    Rename lstm_model to lstm_reachability_model.

--- experimental/compilers/reachability/models/lstm_reachability_model_test.py
+++ experimental/compilers/reachability/models/lstm_reachability_model_test.py
@@ -7,7 +7,7 @@ from absl import flags
 
 from deeplearning.clgen.corpuses import atomizers
 from experimental.compilers.reachability import control_flow_graph
-from experimental.compilers.reachability.models import lstm_model
+from experimental.compilers.reachability.models import lstm_reachability_model
 from labm8 import test
 
 
@@ -49,7 +49,7 @@ def df() -> pd.DataFrame:
 
 def test_BuildKerasModel_smoke_test(atomizer: atomizers.AtomizerBase):
   """Test that BuildKerasModel() doesn't blow up."""
-  assert lstm_model.BuildKerasModel(
+  assert lstm_reachability_model.BuildKerasModel(
       sequence_length=10, num_classes=3, lstm_size=16, dnn_size=4,
       atomizer=atomizer)
 
@@ -57,7 +57,7 @@ def test_BuildKerasModel_smoke_test(atomizer: atomizers.AtomizerBase):
 def test_LstmReachabilityModel_TrainAndEvaluate_smoke_test(
     df: pd.DataFrame, tempdir: pathlib.Path):
   """Test that training doesn't blow up."""
-  model = lstm_model.LstmReachabilityModel(
+  model = lstm_reachability_model.LstmReachabilityModel(
       df, tempdir, num_classes=2, lstm_size=8, dnn_size=4)
   model.TrainAndEvaluate(num_epochs=2)
 
