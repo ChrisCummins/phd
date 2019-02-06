d4be74aa44ce1b25d609cf4d6b042021840a9cbd
Chris Cummins <chrisc.101@gmail.com>
2019-02-06 16:19:39 +0000

    Add a GPGPU smoke test and dummy benchmark suite.

---
+++ datasets/benchmarks/gpgpu/dummy_benchmark.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+  std::cout << "Hello, world!";
+  std::cerr << "[CECL] Hello, benchmarks!";
+  return 0;
+}
---
+++ datasets/benchmarks/gpgpu/gpgpu_smoke_test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Test that `gpgpu` runs the dummy benchmark suite without catching fire.
+#
+set -eux
+
+TMP_LOGDIR="/tmp/phd/gpgpu"
+
+datasets/benchmarks/gpgpu/gpgpu \
+    --gpgpu_benchmark_suites=dummy_just_for_testing \
+    --gpgpu_device_types=oclgrind \
+    --gpgpu_outdir="$TMP_LOGDIR"
+
+rm -rf "$TMP_LOGDIR"
