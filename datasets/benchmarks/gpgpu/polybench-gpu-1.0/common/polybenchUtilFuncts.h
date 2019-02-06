85f4f20fb910b449f9388a081ae60cd52504999c
Chris Cummins <chrisc.101@gmail.com>
2019-02-06 11:08:19 +0000

    Add missing Polybench header.

---
+++ datasets/benchmarks/gpgpu/polybench-gpu-1.0/common/polybenchUtilFuncts.h
@@ -0,0 +1,49 @@
+//polybenchUtilFuncts.h
+//Scott Grauer-Gray (sgrauerg@gmail.com)
+//Functions used across hmpp codes
+
+#ifndef POLYBENCH_UTIL_FUNCTS_H
+#define POLYBENCH_UTIL_FUNCTS_H
+
+//define a small float value
+#define SMALL_FLOAT_VAL 0.00000001f
+
+double rtclock()
+{
+    struct timezone Tzp;
+    struct timeval Tp;
+    int stat;
+    stat = gettimeofday (&Tp, &Tzp);
+    if (stat != 0) printf("Error return from gettimeofday: %d",stat);
+    return(Tp.tv_sec + Tp.tv_usec*1.0e-6);
+}
+
+
+float absVal(float a)
+{
+	if(a < 0)
+	{
+		return (a * -1);
+	}
+   	else
+	{ 
+		return a;
+	}
+}
+
+
+
+float percentDiff(double val1, double val2)
+{
+	if ((absVal(val1) < 0.01) && (absVal(val2) < 0.01))
+	{
+		return 0.0f;
+	}
+
+	else
+	{
+    		return 100.0f * (absVal(absVal(val1 - val2) / absVal(val1 + SMALL_FLOAT_VAL)));
+	}
+} 
+
+#endif //POLYBENCH_UTIL_FUNCTS_H
