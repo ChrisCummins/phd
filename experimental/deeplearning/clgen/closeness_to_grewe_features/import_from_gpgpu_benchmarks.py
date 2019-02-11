de9f4b2e78eb5406a467a249c64ed4f6a1021a2e
Chris Cummins <chrisc.101@gmail.com>
2019-02-11 17:32:08 +0000

    Work in progress on benchmark import.

--- experimental/deeplearning/clgen/closeness_to_grewe_features/import_from_gpgpu_benchmarks.py
+++ experimental/deeplearning/clgen/closeness_to_grewe_features/import_from_gpgpu_benchmarks.py
@@ -1,4 +1,5 @@
 """Import kernels from GPGPU benchmark suites."""
+import hashlib
 import typing
 
 from absl import app
@@ -17,20 +18,39 @@ flags.DEFINE_string(
     'URL of the database to import OpenCL kernels to.')
 
 
+def RowToOpenCLKernelWithRawGreweFeatures(
+    row: typing.Dict[str, typing.Any], datafolder
+) -> grewe_features_db.OpenCLKernelWithRawGreweFeatures:
+  path = ncc.DataFrameRowToKernelSrcPath(row, datafolder)
+  with open(path, 'rb') as f:
+    src = f.read().decode('unicode_escape')
+  src = src.encode('ascii', 'ignore').decode('ascii')
+
+  return grewe_features_db.OpenCLKernelWithRawGreweFeatures(
+      src_sha256=hashlib.sha256(src).hexdigest(),
+      origin=f'benchmarks_{row["program:benchmark_suite_name"]}',
+      grewe_compute_operation_count=row["feature:comp"],
+      grewe_rational_operation_count=row["feature:rational"],
+      grewe_global_memory_access_count=row["feature:mem"],
+      grewe_local_memory_access_count=row["feature:localmem"],
+      grewe_coalesced_memory_access_count=row["feature:coalesced"],
+      grewe_atomic_operation_count=row["feature:atomic"],
+      src=src,
+  )
+
+
 def main(argv: typing.List[str]):
   """Main entry point."""
   if len(argv) > 1:
     raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
 
   db = grewe_features_db.Database(FLAGS.db)
+  df = opencl_device_mapping_dataset.OpenClDeviceMappingsDataset().df
 
-  df = opencl_device_mapping_dataset.OpenClDeviceMappingsDataset()
   with ncc.DEEPTUNE_INST2VEC_DATA_ARCHIVE as datafolder:
-    src_paths = list(set(
-        ncc.DataFrameRowToKernelSrcPath(row, datafolder) for _, row in
-        df.iterrows()))
-
-    db.ImportFromPaths(src_paths, 'benchmarks')
+    with db.Session(commit=True) as sess:
+      for _, row in df.iterrows():
+        sess.GetOrAdd(RowToOpenCLKernelWithRawGreweFeatures(row, datafolder))
 
 
 if __name__ == '__main__':
