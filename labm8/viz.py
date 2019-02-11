d180fdfc43b6a056e557a18390e25e1cc46b7bba
Chris Cummins <chrisc.101@gmail.com>
2019-02-11 22:40:11 +0000

    Add more viz utilities.

--- labm8/viz.py
+++ labm8/viz.py
@@ -2,10 +2,13 @@
 import pathlib
 import typing
 
+import numpy as np
+import seaborn as sns
 from absl import flags
 from absl import logging
 from matplotlib import axes
 from matplotlib import pyplot as plt
+from scipy import stats
 
 
 FLAGS = flags.FLAGS
@@ -73,3 +76,115 @@ def RotateYLabels(rotation: int = 90, ax: axes.Axes = None):
   """
   ax = ax or plt.gca()
   plt.setp(ax.get_yticklabels(), rotation=rotation)
+
+
+def Distplot(x=None, hue=None, data=None, kde=False, bins=None,
+             norm_hist=False, hue_order=None, ax=None):
+  """An extension of seaborn distribution plots for grouped data.
+
+  Args:
+    x: The x label for dataframe.
+    hue: An optional categorical grouping for data.
+    data: Dataset for plotting.
+    kde: Whether to plot a gaussian kernel density estimate.
+    bins: Specification of hist bins, or None to use Freedman-Diaconis rule.
+    norm_hist: If True, the histogram height shows a density rather than a
+      count. This is implied if a KDE or fitted density is plotted.
+    hue_order: Order to plot the categorical levels in, otherwise the levels are
+      inferred from the data objects.
+    ax: If provided, plot on this axis.
+
+  Returns:
+    Returns the Axes object with the plot for further tweaking.
+  """
+
+  # Utility code taken from seaborn. See:
+  # https://github.com/mwaskom/seaborn/blob/master/seaborn/distributions.py
+  #
+  # Copyright (c) 2012-2018, Michael L. Waskom
+  # All rights reserved.
+  #
+  # Redistribution and use in source and binary forms, with or without
+  # modification, are permitted provided that the following conditions are met:
+  #
+  # * Redistributions of source code must retain the above copyright notice,
+  #   this list of conditions and the following disclaimer.
+  #
+  # * Redistributions in binary form must reproduce the above copyright notice,
+  #   this list of conditions and the following disclaimer in the documentation
+  #   and/or other materials provided with the distribution.
+  #
+  # * Neither the name of the project nor the names of its
+  #   contributors may be used to endorse or promote products derived from
+  #   this software without specific prior written permission.
+  #
+  # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  # ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  # LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  # CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  # SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  # INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  # POSSIBILITY OF SUCH DAMAGE.
+
+  def iqr(a):
+    """Calculate the IQR for an array of numbers."""
+    a = np.asarray(a)
+    q1 = stats.scoreatpercentile(a, 25)
+    q3 = stats.scoreatpercentile(a, 75)
+    return q3 - q1
+
+  def _freedman_diaconis_bins(a):
+    """Calculate number of hist bins using Freedman-Diaconis rule."""
+    # From https://stats.stackexchange.com/questions/798/
+    a = np.asarray(a)
+    if len(a) < 2:
+      return 1
+    h = 2 * iqr(a) / (len(a) ** (1 / 3))
+    # fall back to sqrt(a) bins if iqr is 0
+    if h == 0:
+      return int(np.sqrt(a.size))
+    else:
+      return int(np.ceil((a.max() - a.min()) / h))
+
+  # End of seaborn utility code.
+
+  if bins is None:
+    nbins = min(_freedman_diaconis_bins(data[x]), 50)
+    bins = np.linspace(min(data[x]), max(data[x]), nbins)
+
+  if ax is None:
+    ax = plt.gca()
+
+  if hue is None:
+    sns.distplot(data[x], kde=kde, bins=bins, label=x, norm_hist=norm_hist,
+                 ax=ax)
+  else:
+    hue_order = hue_order or sorted(set(data[hue]))
+
+    for h in hue_order:
+      sns.distplot(data[data[hue] == h][x], kde=kde, bins=bins, label=h,
+                   norm_hist=norm_hist, ax=ax)
+
+  return ax
+
+
+def SummarizeFloats(floats: typing.Iterable[float]) -> str:
+  """Summarize a sequence of floats."""
+  arr = np.array(floats, dtype=np.float32)
+  percs = " ".join(
+      [f'{p}%={np.percentile(arr, p):.2f}' for p in [0, 50, 95, 99, 100]])
+  return (f"n={len(arr)}, mean={arr.mean():.2f}, stdev={arr.std():.2f}, "
+          f"percentiles=[{percs}]")
+
+
+def SummarizeInts(ints: typing.Iterable[int]) -> str:
+  """Summarize a sequence of ints."""
+  arr = np.array(ints, dtype=np.int32)
+  percs = " ".join(
+      [f'{p}%={np.percentile(arr, p):.0f}' for p in [0, 50, 95, 99, 100]])
+  return (f"n={len(arr)}, mean={arr.mean():.2f}, stdev={arr.std():.2f}, "
+          f"percentiles=[{percs}]")
--- labm8/viz_test.py
+++ labm8/viz_test.py
@@ -3,6 +3,7 @@
 import pathlib
 
 import matplotlib
+import pandas as pd
 
 
 matplotlib.use('Agg')
@@ -30,7 +31,7 @@ def test_plot():
 @pytest.mark.parametrize('extension', ('.png', '.pdf'))
 def test_Finalize_produces_a_file(test_plot, tempdir: pathlib.Path,
                                   extension: str):
-  """"""
+  """That file is produced."""
   del test_plot
   viz.Finalize(tempdir / f'plot{extension}')
   assert (tempdir / f'plot{extension}').is_file()
@@ -38,6 +39,7 @@ def test_Finalize_produces_a_file(test_plot, tempdir: pathlib.Path,
 
 @pytest.mark.parametrize('extension', ('.png', '.pdf'))
 def test_Finalize_tight(test_plot, tempdir: pathlib.Path, extension: str):
+  """That tight keyword."""
   del test_plot
   viz.Finalize(tempdir / f'plot{extension}', tight=True)
   assert (tempdir / f'plot{extension}').is_file()
@@ -45,9 +47,28 @@ def test_Finalize_tight(test_plot, tempdir: pathlib.Path, extension: str):
 
 @pytest.mark.parametrize('extension', ('.png', '.pdf'))
 def test_Finalize_figsize(test_plot, tempdir: pathlib.Path, extension: str):
+  """That figsize keyword."""
   del test_plot
   viz.Finalize(tempdir / f'plot{extension}', figsize=(10, 5))
   assert (tempdir / f'plot{extension}').is_file()
+
+
+def test_Distplot_dataframe():
+  """Test plotting dataframe."""
+  df = pd.DataFrame({'x': 1, 'group': 'foo'}, {'x': 2, 'group': 'bar'})
+  viz.Distplot(x='x', hue='group', data=df)
+
+
+def test_Distplot_with_hue_order():
+  """Test plotting with hue order."""
+  df = pd.DataFrame({'x': 1, 'group': 'foo'}, {'x': 2, 'group': 'bar'})
+  viz.Distplot(x='x', hue='group', hue_order=['foo', 'bar'], data=df)
+
+
+def test_Distplot_with_missing_hue_order_values():
+  """Plotting with missing hue order is not an error."""
+  df = pd.DataFrame({'x': 1, 'group': 'foo'}, {'x': 2, 'group': 'bar'})
+  viz.Distplot(x='x', hue='group', hue_order=['foo', 'bar', 'car'], data=df)
 
 
 if __name__ == '__main__':
