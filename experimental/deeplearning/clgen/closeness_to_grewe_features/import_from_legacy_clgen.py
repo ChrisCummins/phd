de63638057b9b2e29946b34f2e0a1cf7d9bb5f19
Chris Cummins <chrisc.101@gmail.com>
2019-02-11 19:14:12 +0000

    Begin on import from legacy CLgen.

---
+++ experimental/deeplearning/clgen/closeness_to_grewe_features/import_from_legacy_clgen.py
@@ -0,0 +1,77 @@
+"""Import from legacy CLgen sampler database format."""
+import pathlib
+import sqlite3
+import tempfile
+import typing
+
+from absl import app
+from absl import flags
+from absl import logging
+
+from experimental.deeplearning.clgen.closeness_to_grewe_features import \
+  grewe_features_db
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string(
+    'db',
+    'sqlite:///tmp/phd/experimental/deplearning/clgen/closeness_to_grewe_features/db.db',
+    'URL of the database to import OpenCL kernels to.')
+flags.DEFINE_string(
+    'legacy_clgen_db', None,
+    'Path of the legacy CLgen sqlite database.')
+flags.DEFINE_string(
+    'origin', None,
+    'Name of the origin of the kernels, e.g. "github".')
+flags.DEFINE_integer(
+    'batch_size', 256,
+    'The number of kernels to process in a batch.')
+
+
+def BatchQueryResults(query):
+  """Batch results of a query."""
+  i = 0
+  batch = []
+  for row in query:
+    batch.append(row)
+    i += 1
+    if i >= FLAGS.batch_size:
+      yield batch
+      batch = []
+
+
+def CreateTempFileFromTestcase(
+    tempdir: pathlib.Path, src: str, number: int) -> pathlib.Path:
+  """Write testcase to a file in directory."""
+  path = tempdir / f'{number}.cl'
+  with open(path, 'w') as f:
+    f.write(src)
+  return path
+
+
+def main(argv: typing.List[str]):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
+
+  db = grewe_features_db.Database(FLAGS.db)
+
+  conn = sqlite3.connect(FLAGS.legacy_clgen_db)
+  c = conn.cursor()
+
+  batches = BatchQueryResults(c.execute('SELECT contents FROM ContentFiles'))
+
+  prefix = 'phd_experimental_deeplearning_clgen_'
+  for i, batch in enumerate(batches):
+    with tempfile.TemporaryDirectory(prefi=prefix) as d:
+      logging.info('Batch %03d', i)
+      d = pathlib.Path(d)
+      paths_to_import = [
+        CreateTempFileFromTestcase(d, src, i) for i, (src,) in enumerate(batch)
+      ]
+      db.ImportFromPaths(paths_to_import)
+
+
+if __name__ == '__main__':
+  app.run(main)
