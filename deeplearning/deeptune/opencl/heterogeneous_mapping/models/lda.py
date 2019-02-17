Column normalization fixes.

deeplearning/deeptune/opencl/heterogeneous_mapping/models/lda.py
@@ -134,8 +134,11 @@ def _ExtractGraphBatchOrDie(
   return batch
 
 
-def NoramlizedColumn(df: pd.DataFrame, column: str) -> np.array:
+def NormalizedColumn(df: pd.DataFrame, column: str) -> np.array:
   """Return the values of a column normalized to range [-0.5,0.5]."""
+  if column not in df.columns.values:
+    raise TypeError(f"Dataframe missing expected column: {column}. Actual "
+                    f"columns: {df.columns.values}")
   return (df[column] / df[column].max()) - .5
 
 
@@ -149,10 +152,12 @@ def SetNormalizedColumns(df: pd.DataFrame) -> pd.DataFrame:
     df: The dataframe, with additional '_norm' suffix columns set.
   """
   for gpu_name in ['amd_tahiti_7970', 'nvidia_gtx_960']:
-    df[f'feature:{gpu_name}:transfer_norm'] = NormalizedColumn(
-        df, f'feature:{gpu_name}:transfer')
-    df[f'feature:{gpu_name}:wgsize_norm'] = NormalizedColumn(
-        df, f'feature:{gpu_name}:wgsize')
+    columns_to_normalize = [
+      f'feature:{gpu_name}:transfer',
+      f'param:{gpu_name}:wgsize'
+    ]
+    for column in columns_to_normalize:
+      df[f'{column}_norm'] = NormalizedColumn(df, column)
   return df
 
 

deeplearning/deeptune/opencl/heterogeneous_mapping/models/lda_test.py
@@ -36,7 +36,8 @@ def test_Lda_ExtractGraphs_returns_cfgs(classify_df: pd.DataFrame):
 
 def test_Lda_ExtractGraphs_cfgs_have_bytecode(single_program_df: pd.DataFrame):
   """Test that CFG has bytecode set."""
-  rows, graphs = zip(*lda.Lda.ExtractGraphs(single_program_df))
+  rows, graphs = zip(
+      *lda.Lda.ExtractGraphs(lda.SetNormalizedColumns(single_program_df)))
   assert len(rows) == 1
   assert graphs[0].graph['llvm_bytecode']
 
@@ -45,7 +46,7 @@ def test_Lda_EncodeGraphs_inst2vec_vectors(single_program_df: pd.DataFrame):
   """Test that CFG has inst2vec attribute set."""
   model = lda.Lda()
   rows, graphs = zip(*model.EncodeGraphs(
-      model.ExtractGraphs(single_program_df)))
+      model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df))))
   assert len(rows) == 1
   assert len(graphs[0].nodes)
 
@@ -59,7 +60,7 @@ def test_Lda_EncodeGraphs_inst2vec_encoded(single_program_df: pd.DataFrame):
   """Test that CFG has inst2vec_encoded attribute set."""
   model = lda.Lda()
   rows, graphs = zip(*model.EncodeGraphs(
-      model.ExtractGraphs(single_program_df)))
+      model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df))))
   assert len(rows) == 1
   assert len(graphs[0].nodes)
 
@@ -74,7 +75,7 @@ def test_Lda_EncodeGraphs_num_unknown_statements(
   """Test that num_unknown_statements is set on graph."""
   model = lda.Lda()
   rows, graphs = zip(*model.EncodeGraphs(
-      model.ExtractGraphs(single_program_df)))
+      model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df))))
   assert len(rows) == 1
 
   assert graphs[0].graph['num_unknown_statements'] >= 0
@@ -84,7 +85,7 @@ def test_Lda_EncodeGraphs_unique_encoded(single_program_df: pd.DataFrame):
   """Test that CFG has multiple unique encoded nodes."""
   model = lda.Lda()
   rows, graphs = zip(*model.EncodeGraphs(
-      model.ExtractGraphs(single_program_df)))
+      model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df))))
   assert len(rows) == 1
   assert len(graphs[0].nodes)
 
@@ -178,7 +179,8 @@ def test_Lda_GraphsToInputTargets_node_features_shape(
   """Test that node features have correct shape."""
   model = lda.Lda()
   input_graphs, target_graphs = zip(*model.GraphsToInputTargets(
-      model.EncodeGraphs(model.ExtractGraphs(single_program_df))))
+      model.EncodeGraphs(
+          model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df)))))
   assert len(input_graphs) == 1
   assert input_graphs[0].nodes[0]['features'].shape == (
     model.embedding_dim + 2,)
@@ -190,7 +192,8 @@ def test_Lda_GraphsToInputTargets_node_features_entry_exit_blocks(
   """Assert that only a single entry node is set."""
   model = lda.Lda()
   input_graphs, target_graphs = zip(*model.GraphsToInputTargets(
-      model.EncodeGraphs(model.ExtractGraphs(single_program_df))))
+      model.EncodeGraphs(
+          model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df)))))
   assert len(input_graphs) == 1
   node_features = np.vstack(
       [d['features'] for _, d in input_graphs[0].nodes(data=True)])
@@ -205,7 +208,8 @@ def test_Lda_GraphsToInputTargets_node_features_dtype(
   """Test that node features have correct type."""
   model = lda.Lda()
   input_graphs, target_graphs = zip(*model.GraphsToInputTargets(
-      model.EncodeGraphs(model.ExtractGraphs(single_program_df))))
+      model.EncodeGraphs(
+          model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df)))))
   assert len(input_graphs) == 1
   assert input_graphs[0].nodes[0]['features'].dtype == np.float32
   assert target_graphs[0].nodes[0]['features'].dtype == np.float32
@@ -216,7 +220,8 @@ def test_Lda_GraphsToInputTargets_global_features_shape(
   """Test that node features have correct shape."""
   model = lda.Lda()
   input_graphs, target_graphs = zip(*model.GraphsToInputTargets(
-      model.EncodeGraphs(model.ExtractGraphs(single_program_df))))
+      model.EncodeGraphs(
+          model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df)))))
   assert len(input_graphs) == 1
   assert input_graphs[0].graph['features'].shape == (2,)
   assert target_graphs[0].graph['features'].shape == (2,)
@@ -227,7 +232,8 @@ def test_Lda_GraphsToInputTargets_global_features_dtype(
   """Test that graph features have correct type."""
   model = lda.Lda()
   input_graphs, target_graphs = zip(*model.GraphsToInputTargets(
-      model.EncodeGraphs(model.ExtractGraphs(single_program_df))))
+      model.EncodeGraphs(
+          model.ExtractGraphs(lda.SetNormalizedColumns(single_program_df)))))
   assert len(input_graphs) == 1
   assert input_graphs[0].graph['features'].dtype == np.float32
   assert target_graphs[0].graph['features'].dtype == np.float32