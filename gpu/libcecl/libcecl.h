63f67d1ed7301cca694e32ad3d8dd48caf919c9e
Chris Cummins <chrisc.101@gmail.com>
2019-02-18 21:28:28 -0500

    Add libcecl license headers.

--- gpu/libcecl/libcecl.h
+++ gpu/libcecl/libcecl.h
@@ -1,22 +1,21 @@
-/*
- * Libcecl interface.
- *
- * Provides a blocking, verbose wrappers around a subset of the OpenCL
- * API.
- *
- * libcecl is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * libcecl is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with libcecl.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Libcecl implementation.
+//
+// Provides a blocking, verbose wrappers around a subset of the OpenCL API.
+//
+// Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
+//
+// libcecl is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libcecl is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libcecl.  If not, see <https://www.gnu.org/licenses/>.
 #pragma once
 
 #ifdef __APPLE__
--- gpu/libcecl/libcecl_runtime.py
+++ gpu/libcecl/libcecl_runtime.py
@@ -1,3 +1,19 @@
+# This file is part of libcecl.
+#
+# Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
+#
+# libcecl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# libcecl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with libcecl.  If not, see <https://www.gnu.org/licenses/>.
 """Runtime utility code for libcecl binaries."""
 import os
 import subprocess
