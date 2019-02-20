498d7c0ddf4c67200dfd95d7b2428e6f1bffe6b8
Chris Cummins <chrisc.101@gmail.com>
2019-02-20 10:04:03 -0500

    Add new overview query.

--- experimental/deeplearning/clgen/closeness_to_grewe_features/snippets.sql
+++ experimental/deeplearning/clgen/closeness_to_grewe_features/snippets.sql
@@ -13,3 +13,49 @@ FROM  (
 ) t 
 GROUP BY origin 
 ORDER BY origin DESC;
+
+# Overview of dynamic feature results.
+SELECT
+	origin,
+	result,
+    COUNT(*) as n,
+    COUNT(*) / (SELECT COUNT(*) FROM driver_result) as ratio
+FROM
+	driver_result AS df
+LEFT JOIN
+	static_features AS sf
+ON sf.id=df.static_features_id
+GROUP BY
+	origin,
+	result;
+
+# Combined static and raw features values.
+SELECT
+    sf.origin,
+    sf.src_sha256,
+    sf.src,
+    sf.grewe_compute_operation_count as compute_operation_count,
+    sf.grewe_global_memory_access_count as global_memory_access_count,
+    sf.grewe_local_memory_access_count as local_memory_access_count,
+    sf.grewe_coalesced_memory_access_count as coalesced_memory_access_count,
+    df.wgsize,
+    df.transferred_bytes, 
+	df.opencl_env,
+    AVG(df.runtime_ms) as avg_runtime,
+    (MAX(df.runtime_ms) - MIN(df.runtime_ms)) / AVG(df.runtime_ms) as norm_range
+FROM
+	dynamic_features AS df
+LEFT JOIN
+	static_features AS sf
+ON sf.id=df.static_features_id
+GROUP BY
+	sf.src_sha256,
+    sf.origin,
+    sf.grewe_compute_operation_count,
+    sf.grewe_global_memory_access_count,
+    sf.grewe_local_memory_access_count,
+    sf.grewe_coalesced_memory_access_count,
+    df.wgsize,
+    df.transferred_bytes,
+    df.dataset,
+    df.opencl_env;
