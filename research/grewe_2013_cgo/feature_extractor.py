d9e58478957f5cd7d4f314b6e1f45ebc5a9e6c9d
Chris Cummins <chrisc.101@gmail.com>
2019-02-25 21:58:27 +0000

    Move inlined header.

--- research/grewe_2013_cgo/feature_extractor.py
+++ research/grewe_2013_cgo/feature_extractor.py
@@ -17,20 +17,17 @@ from absl import logging
 
 from labm8 import bazelutil
 
-
-FEATURE_EXTRACTOR_BINARY = bazelutil.DataPath(
-    'phd/research/grewe_2013_cgo/'
-    'feature_extractor_binary')
+FEATURE_EXTRACTOR_BINARY = bazelutil.DataPath('phd/research/grewe_2013_cgo/'
+                                              'feature_extractor_binary')
 
 INLINED_OPENCL_HEADER = bazelutil.DataPath(
-    'phd/third_party/opencl/inlined/include/cl.h')
+    'phd/third_party/opencl/inlined/cl.h')
 
 # On Linux we must preload the LLVM shared libraries.
 FEATURE_EXTRACTOR_ENV = os.environ.copy()
 _LIBCLANG_SO = bazelutil.DataPath(
     'llvm_linux/lib/libclang.so', must_exist=False)
-_LIBLTO_SO = bazelutil.DataPath(
-    'llvm_linux/lib/libLTO.so', must_exist=False)
+_LIBLTO_SO = bazelutil.DataPath('llvm_linux/lib/libLTO.so', must_exist=False)
 if _LIBCLANG_SO.is_file() and _LIBLTO_SO.is_file():
   FEATURE_EXTRACTOR_ENV['LD_PRELOAD'] = f'{_LIBCLANG_SO}:{_LIBLTO_SO}'
 
@@ -40,18 +37,20 @@ flags.DEFINE_string('feature_extractor_opencl_src_path', None,
                     'Path of OpenCL file to extract features of.')
 
 # The definition of features extracted by the feature extractor.
-GreweEtAlFeatures = collections.namedtuple('GreweEtAlFeatures', [
-  'file',  # str: the base name of the file path.
-  'kernel_name',  # str: name of the kernel, e.g. `kernel void A() {}` -> `A`.
-  'compute_operation_count',  # int: compute operation count
-  'rational_operation_count',  # int: rational operation count
-  'global_memory_access_count',  # int: accesses to global memory
-  'local_memory_access_count',  # int: accesses to local memory
-  'coalesced_memory_access_count',  # int: coalesced memory accesses
-  'atomic_operation_count',  # int: atomic operations
-  'coalesced_memory_access_ratio',  # float: derived feature
-  'compute_to_memory_access_ratio',  # float: derived feature
-])
+GreweEtAlFeatures = collections.namedtuple(
+    'GreweEtAlFeatures',
+    [
+        'file',  # str: the base name of the file path.
+        'kernel_name',  # str: name of the kernel, e.g. `kernel void A() {}` -> `A`.
+        'compute_operation_count',  # int: compute operation count
+        'rational_operation_count',  # int: rational operation count
+        'global_memory_access_count',  # int: accesses to global memory
+        'local_memory_access_count',  # int: accesses to local memory
+        'coalesced_memory_access_count',  # int: coalesced memory accesses
+        'atomic_operation_count',  # int: atomic operations
+        'coalesced_memory_access_ratio',  # float: derived feature
+        'compute_to_memory_access_ratio',  # float: derived feature
+    ])
 
 
 def _GreweEtAlFeatures_from_binary_output(
@@ -78,10 +77,9 @@ class FeatureExtractionError(ValueError):
 
 
 # TODO(polyglot): Add support for multiple languages.
-def ExtractFeaturesFromPath(
-    path: pathlib.Path,
-    extra_args: typing.Optional[typing.List[str]] = None
-) -> typing.Iterator[GreweEtAlFeatures]:
+def ExtractFeaturesFromPath(path: pathlib.Path,
+                            extra_args: typing.Optional[typing.List[str]] = None
+                           ) -> typing.Iterator[GreweEtAlFeatures]:
   """Print CSV format features of file.
 
   Args:
@@ -100,12 +98,16 @@ def ExtractFeaturesFromPath(
   if not path.is_file():
     raise FileNotFoundError(f"File not found: {path}")
 
-  cmd = [str(FEATURE_EXTRACTOR_BINARY), str(INLINED_OPENCL_HEADER),
+  cmd = [str(FEATURE_EXTRACTOR_BINARY),
+         str(INLINED_OPENCL_HEADER),
          str(path)] + [f'-extra_arg={arg}' for arg in extra_args]
   logging.debug('$ %s', ' '.join(cmd))
   process = subprocess.Popen(
-      cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-      env=FEATURE_EXTRACTOR_ENV, universal_newlines=True)
+      cmd,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      env=FEATURE_EXTRACTOR_ENV,
+      universal_newlines=True)
   stdout, stderr = process.communicate()
 
   lines = [line.split(',') for line in stdout.split('\n')]
