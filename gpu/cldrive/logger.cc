62b515222270209ead3700701ed23540121ebf6a
Chris Cummins <chrisc.101@gmail.com>
2019-02-28 17:48:31 +0000

    Begin on logger interface.

---
+++ gpu/cldrive/logger.cc
@@ -0,0 +1,56 @@
+// Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
+// This file is part of cldrive.
+//
+// cldrive is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// cldrive is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with cldrive.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "gpu/cldrive/logger.h"
+#include "logger.h"
+
+Logger::Logger(std::ostream& ostream)
+    : ostream_(ostream), instances_(nullptr), instance_num_(-1) {}
+
+/*virtual*/ phd::Status Logger::Init(const ClDriveInstances* const instances) {
+  CHECK(instances_ == nullptr);
+  instances_ = instances;
+}
+
+/*virtual*/ phd::Status StartNewInstance() { ++instance_num_; }
+
+/*virtual*/ phd::Status Logger::RecordLog(CldriveInstance* instance) {
+  CHECK(instance_num() >= 0);
+}
+
+/*virtual*/ phd::Status Logger::End() {
+  CHECK(instances_ != nullptr);
+  instances_ = nullptr;
+}
+
+ClDriveInstances* Logger::instances() { return instances_; }
+
+std::ostream& Logger::ostream() { return ostream_; }
+
+int Logger::instance_num() const { return instance_num_; }
+
+ProtocolBufferLogger::ProtocolBufferLogger(std::ostream& ostream,
+                                           bool text_format)
+    : Logger(ostream), text_format_(text_format) {}
+
+/*virtual*/ phd::Status ProtocolBufferLogger::End() {
+  if (text_format_) {
+    ostream() << instances()->SerializeToString();
+  } else {
+    ostream() << instances()->DebugString();
+  }
+  return Logger::End();
+}
---
+++ gpu/cldrive/logger.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
+// This file is part of cldrive.
+//
+// cldrive is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// cldrive is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with cldrive.  If not, see <https://www.gnu.org/licenses/>.
+#pragma once
+
+#include "phd/status.h"
+
+#include "gpu/cldrive/proto/cldrive.pb.h"
+
+#include <iostream>
+
+// Abstract logging interface for producing consumable output.
+class Logger {
+ public:
+  Logger(std::ostream& ostream);
+
+  virtual phd::Status Init(const ClDriveInstances* const instances);
+
+  virtual phd::Status StartNewInstance();
+
+  virtual phd::Status RecordLog(CldriveInstance* instance);
+
+  virtual phd::Status End();
+
+ protected:
+  ClDriveInstances* instances();
+  std::ostream& ostream();
+  int instance_num() const;
+
+ private:
+  std::ostream& ostream_;
+  const ClDriveInstances* const instances_;
+  int instance_num_;
+};
+
+// Logging interface for producing protocol buffers.
+class ProtocolBufferLogger : public Logger {
+ public:
+  ProtocolBufferLogger(std::ostream& ostream, bool text_format);
+
+  virtual phd::Status End() override;
+
+ private:
+  bool text_format_ = text_format_;
+};
