a2a3e8c4a93957f2208dc3a69ca32b34fb87508b
Chris Cummins <chrisc.101@gmail.com>
2019-02-28 14:57:25 +0000

    Auto format.

--- gpu/cldrive/api.py
+++ gpu/cldrive/api.py
@@ -15,10 +15,11 @@ FLAGS = flags.FLAGS
 
 _NATIVE_DRIVER = bazelutil.DataPath('phd/gpu/cldrive/native_driver')
 
+
 def _GetCommand(instances):
   if (len(instances.instance) and
       instances.instance[0].device.name == _env.OclgrindOpenCLEnvironment().name
-  ):
+     ):
     return [str(oclgrind.OCLGRIND_PATH), str(_NATIVE_DRIVER)]
   else:
     return [str(_NATIVE_DRIVER)]
@@ -32,7 +33,8 @@ def Drive(instances: cldrive_pb2.CldriveInstances,
 
 
 def DriveToDataFrame(instances: cldrive_pb2.CldriveInstances,
-                     timeout_seconds:int = 360) -> pd.DataFrame:
-  stdout = pbutil.RunProcessMessage(_GetCommand(instances), instances, timeout_seconds=timeout_seconds)
-  return pd.read_csv(io.StringIO(stdout.decode('utf-8')), sep=',', quoting=csv.QUOTE_NONE)
-
+                     timeout_seconds: int = 360) -> pd.DataFrame:
+  stdout = pbutil.RunProcessMessage(
+      _GetCommand(instances), instances, timeout_seconds=timeout_seconds)
+  return pd.read_csv(
+      io.StringIO(stdout.decode('utf-8')), sep=',', quoting=csv.QUOTE_NONE)
