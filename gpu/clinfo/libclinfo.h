fa089b1152c21956cf9e7c2097c1b7e35ea76119
Chris Cummins <chrisc.101@gmail.com>
2019-02-28 17:10:31 +0000

    Tidy up docstring.

--- gpu/clinfo/libclinfo.h
+++ gpu/clinfo/libclinfo.h
@@ -40,8 +40,9 @@ StatusOr<::gpu::clinfo::OpenClDevice> GetOpenClDeviceProto(const string& name);
 // std::invalid_argument if not found.
 cl::Device GetOpenClDevice(const ::gpu::clinfo::OpenClDevice& device);
 
-// Return the "default" OpenCL device, where "default" will attempt to pick some
-// safe device. This is not hemetic.
+// Return a "default" OpenCL device, where "default" will attempt to pick some
+// safe device. If available, Oclgrind will be used. Else, the first device
+// as it appears in the enumerated list.
 // TODO(cec): It would be nice if we could link against oclgrind to ensure that
 // this function could always return the oclgrind device.
 cl::Device GetDefaultOpenClDeviceOrDie();
