b17425a78c564d1e9ef47159e05a97213855a827
Chris Cummins <chrisc.101@gmail.com>
2019-03-01 07:41:50 +0000

    InitApp() in TEST_MAIN()

--- phd/test.h
+++ phd/test.h
@@ -4,6 +4,8 @@
 // gtest and benchmark libraries.
 #pragma once
 
+#include "phd/app.h"
+
 #include "benchmark/benchmark.h"
 #include "gtest/gtest.h"
 
@@ -12,11 +14,12 @@
 #endif
 
 // Inserts a main() function which runs google benchmarks and gtest suite.
-#define TEST_MAIN()                                                            \
-  int main(int argc, char **argv) {                                            \
-    testing::InitGoogleTest(&argc, argv);                                      \
-    const auto ret = RUN_ALL_TESTS();                                          \
-    benchmark::Initialize(&argc, argv);                                        \
-    benchmark::RunSpecifiedBenchmarks();                                       \
-    return ret;                                                                \
+#define TEST_MAIN()                                    \
+  int main(int argc, char **argv) {                    \
+    testing::InitGoogleTest(&argc, argv);              \
+    phd::InitApp(&argc, &argv, "Test suite program."); \
+    const auto ret = RUN_ALL_TESTS();                  \
+    benchmark::Initialize(&argc, argv);                \
+    benchmark::RunSpecifiedBenchmarks();               \
+    return ret;                                        \
   }
