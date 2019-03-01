1aebe91768ff7a8cd7d935af323eb4d6ed771ba2
Chris Cummins <chrisc.101@gmail.com>
2019-03-01 18:19:52 +0000

    Add missing headers for parboil benchmarks.

--- datasets/benchmarks/gpgpu/parboil-0.2/benchmarks/lbm/src/opencl_base/lbm.h
+++ datasets/benchmarks/gpgpu/parboil-0.2/benchmarks/lbm/src/opencl_base/lbm.h
@@ -1,4 +1,6 @@
 #include <libcecl.h>
+#include "lbm_macros.h"
+#include "ocl.h"
 /***************************************************************************
  *cr
  *cr            (C) Copyright 2010 The Board of Trustees of the
@@ -14,23 +16,26 @@
 
 /*############################################################################*/
 
-void LBM_allocateGrid( float** ptr );
-void LBM_freeGrid( float** ptr );
-void LBM_initializeGrid( LBM_Grid grid );
-void LBM_initializeSpecialCellsForLDC( LBM_Grid grid );
-void LBM_loadObstacleFile( LBM_Grid grid, const char* filename );
-void LBM_swapGrids( cl_mem* grid1, cl_mem* grid2 );
-void LBM_showGridStatistics( LBM_Grid Grid );
-void LBM_storeVelocityField( LBM_Grid grid, const char* filename,
-                           const BOOL binary );
+void LBM_allocateGrid(float** ptr);
+void LBM_freeGrid(float** ptr);
+void LBM_initializeGrid(LBM_Grid grid);
+void LBM_initializeSpecialCellsForLDC(LBM_Grid grid);
+void LBM_loadObstacleFile(LBM_Grid grid, const char* filename);
+void LBM_swapGrids(cl_mem* grid1, cl_mem* grid2);
+void LBM_showGridStatistics(LBM_Grid Grid);
+void LBM_storeVelocityField(LBM_Grid grid, const char* filename,
+                            const BOOL binary);
 
 /* OpenCL *********************************************************************/
 
-void OpenCL_LBM_allocateGrid( const OpenCL_Param* prm, cl_mem* ptr );
-void OpenCL_LBM_freeGrid( cl_mem ptr );
-void OpenCL_LBM_initializeGrid( const OpenCL_Param* prm, cl_mem d_grid, LBM_Grid h_grid );
-void OpenCL_LBM_getDeviceGrid( const OpenCL_Param* prm, cl_mem d_grid, LBM_Grid h_grid );
-void OpenCL_LBM_performStreamCollide( const OpenCL_Param* prm, cl_mem srcGrid, cl_mem dstGrid );
+void OpenCL_LBM_allocateGrid(const OpenCL_Param* prm, cl_mem* ptr);
+void OpenCL_LBM_freeGrid(cl_mem ptr);
+void OpenCL_LBM_initializeGrid(const OpenCL_Param* prm, cl_mem d_grid,
+                               LBM_Grid h_grid);
+void OpenCL_LBM_getDeviceGrid(const OpenCL_Param* prm, cl_mem d_grid,
+                              LBM_Grid h_grid);
+void OpenCL_LBM_performStreamCollide(const OpenCL_Param* prm, cl_mem srcGrid,
+                                     cl_mem dstGrid);
 
 /*############################################################################*/
 
