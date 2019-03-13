1d3dc1a8d7af67688a02cf6182c67dac114ef19d
Chris Cummins <chrisc.101@gmail.com>
2019-03-13 11:44:47 +0000

    Work in progress on test view.

--- tools/continuous_integration/buildbot/report_viewer/report_viewer.py
+++ tools/continuous_integration/buildbot/report_viewer/report_viewer.py
@@ -5,6 +5,7 @@ import flask
 import portpicker
 import sqlalchemy as sql
 
+from config import build_info
 from labm8 import app
 from labm8 import bazelutil
 from labm8 import humanize
@@ -16,6 +17,8 @@ FLAGS = app.FLAGS
 app.DEFINE_string(
     "db", "sqlite:////tmp/phd/tools/continuous_integration/buildbot.db",
     "Path to testlogs summary database.")
+app.DEFINE_string("buildbot_url", "http://localhost:8010/#/builders",
+                  "URL of buildbot server.")
 app.DEFINE_integer('port', portpicker.pick_unused_port(),
                    'The port to launch the server on.')
 app.DEFINE_boolean('debug_flask_server', True,
@@ -34,57 +37,83 @@ def DeltaToTargets(delta: db.TestDelta) -> List[Dict[str, Any]]:
       'bazel_target': result.bazel_target,
       'runtime': humanize.Duration(result.runtime_ms / 1000),
       'test_count': result.test_count,
+      'git_branch': result.git_branch,
+      'git_commit': result.git_commit,
       'log': result.log,
       'changed': True,
       'fail': True,
+      'runtime_ms': result.runtime_ms,
   } for result in delta.broken] + [
       {
           'bazel_target': result.bazel_target,
           'runtime': humanize.Duration(result.runtime_ms / 1000),
           'test_count': result.test_count,
+          'git_branch': result.git_branch,
+          'git_commit': result.git_commit,
           'log': result.log,
           'changed': False,
           'fail': True,
+          'runtime_ms': result.runtime_ms,
       } for result in delta.still_broken
   ] + [{
       'bazel_target': result.bazel_target,
       'runtime': humanize.Duration(result.runtime_ms / 1000),
       'test_count': result.test_count,
+      'git_branch': result.git_branch,
+      'git_commit': result.git_commit,
       'log': result.log,
       'changed': True,
       'fail': False,
+      'runtime_ms': result.runtime_ms,
   } for result in delta.fixed
       ] + [{
           'bazel_target': result.bazel_target,
           'runtime': humanize.Duration(result.runtime_ms / 1000),
           'test_count': result.test_count,
+          'git_branch': result.git_branch,
+          'git_commit': result.git_commit,
           'log': result.log,
           'changed': False,
           'fail': False,
+          'runtime_ms': result.runtime_ms,
       } for result in delta.still_pass]
 
 
 def RenderInvocation(host, session, invocation):
   app.Log(1, 'Fetching for invocation %s', invocation)
   delta = db.GetTestDelta(session, invocation, to_return=[db.TestTargetResult])
+  targets = DeltaToTargets(delta)
 
   urls = {
-      "cache_tag": 1,
-      "styles_css": flask.url_for('static', filename='bootstrap.css'),
-      "site_js": flask.url_for('static', filename='site.js'),
+      "cache_tag":
+      1,
+      "styles_css":
+      flask.url_for('static', filename='bootstrap.css'),
+      "site_js":
+      flask.url_for('static', filename='site.js'),
+      "buildbot":
+      FLAGS.buildbot_url,
+      "github_commit": ('https://github.com/ChrisCummins/phd-priv/commit/'
+                        f'{targets[0]["git_commit"]}'),
   }
 
   return flask.render_template(
-      "index.html",
+      "test_view.html",
       host=host,
-      targets=DeltaToTargets(delta),
+      targets=targets,
+      test_count=humanize.Commas(sum(t['test_count'] for t in targets)),
+      test_duration=humanize.Duration(
+          sum(t['runtime_ms'] for t in targets) / 1000),
+      git_commit=targets[0]['git_commit'],
+      git_branch=targets[0]['git_branch'],
       delta=db.TestDelta(
           broken=len(delta.broken),
           fixed=len(delta.fixed),
           still_broken=len(delta.still_broken),
           still_pass=len(delta.still_pass)),
       invocation_datetime=invocation,
-      urls=urls)
+      urls=urls,
+      build_info=build_info.GetBuildInfo())
 
 
 @flask_app.route("/<host>")
