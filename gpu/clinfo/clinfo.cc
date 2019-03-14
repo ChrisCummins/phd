83a745ca7d9b881198021360537b41b5b2f22bed
Chris Cummins <chrisc.101@gmail.com>
2019-03-14 13:26:40 +0000

    Add license headers to //gpu.

--- gpu/clinfo/clinfo.cc
+++ gpu/clinfo/clinfo.cc
@@ -1,16 +1,28 @@
 // Print a OpenClDevices proto describing available OpenCL devices.
+//
+// Copyright 2017, 2018, 2019 Chris Cummins <chrisc.101@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <iostream>
 
-#include "gpu/clinfo/proto/clinfo.pb.h"
 #include "gpu/clinfo/libclinfo.h"
-
+#include "gpu/clinfo/proto/clinfo.pb.h"
 
 int PrintHelp(char **argv) {
   std::cerr << "Usage: " << argv[0] << " [-p <platform_id> -d <device_id>]\n";
   return 1;
 }
-
 
 int main(int argc, char **argv) {
   int platform_id = -1, device_id = -1;
--- gpu/oclgrind/oclgrind.py
+++ gpu/oclgrind/oclgrind.py
@@ -1,3 +1,16 @@
+# Copyright 2018, 2019 Chris Cummins <chrisc.101@gmail.com>.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """A python wrapper around oclgrind, an OpenCL device simulator and debugger.
 
 Oclgrind is developed by James Price <j.price@bristol.ac.uk>.
@@ -19,7 +32,6 @@ from gpu.clinfo.proto import clinfo_pb2
 from labm8 import app
 from labm8 import bazelutil
 from labm8 import system
-
 
 FLAGS = app.FLAGS
 
