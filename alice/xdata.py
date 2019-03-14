18b7c5b3598622b19579b0ebf41a8dbdb3fb38aa
Chris Cummins <chrisc.101@gmail.com>
2019-03-14 12:45:57 +0000

    GPL license alice.

--- alice/xdata.py
+++ alice/xdata.py
@@ -1,7 +1,18 @@
-"""This file contains TODO: one line summary.
-
-TODO: Detailed explanation of the file.
-"""
+# Copyright (c) 2019 Chris Cummins <chrisc.101@gmail.com>.
+#
+# alice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# alice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with alice.  If not, see <https://www.gnu.org/licenses/>.
+"""TODO: Experimental data API."""
 import contextlib
 import os
 import pathlib
