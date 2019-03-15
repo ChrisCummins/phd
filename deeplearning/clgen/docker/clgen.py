aa1ae579052db23a2709f03354c5b4c8c401bbdb
Chris Cummins <chrisc.101@gmail.com>
2019-03-15 15:26:00 +0000

    Add version flag to clgen docker image.

--- deeplearning/clgen/docker/clgen.py
+++ deeplearning/clgen/docker/clgen.py
@@ -5,17 +5,38 @@ set some default arg values.
 """
 import sys
 
+from config import build_info
+from config import getconfig
 from deeplearning.clgen import clgen
 from labm8 import app
 
 FLAGS = app.FLAGS
 
+app.DEFINE_boolean("version", False,
+                   "Print CLgen version information and exit.")
+
 
 def main():
+  if FLAGS.version:
+    config = getconfig.GetGlobalConfig()
+    info = build_info.GetBuildInfo()
+    if info:
+      version = '{info.short_hash}{"*" if info.dirty else ""}'
+    else:
+      version = 'standalone'
+    print(f"""
+CLgen {version} ({config.uname}-{"gpu" if config.with_cuda else "cpu"}).
+
+Made with \033[1;31m♥\033[0;0m by Chris Cummins <chrisc.101@gmail.com>.
+https://chriscummins.cc/clgen
+""".strip())
+    return
+
   # Set default value for config flag. Set the default value before the true
   # args, so that if the user provides a --config value, it will override
   # the default.
   FLAGS([sys.argv[0], '--config=/clgen/config.pbtxt'] + sys.argv[1:])
+
   clgen.RunWithErrorHandling(clgen.DoFlagsAction)
 
 
