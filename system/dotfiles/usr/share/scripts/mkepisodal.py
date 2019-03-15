d224e2fb753501316265058bf7451279a9d8c9ce
Chris Cummins <chrisc.101@gmail.com>
2019-03-15 08:50:02 +0000

    Spruce up.

--- system/dotfiles/usr/share/scripts/mkepisodal.py
+++ system/dotfiles/usr/share/scripts/mkepisodal.py
@@ -16,19 +16,22 @@ def mkepisodal(show_name: str,
                season_num: int,
                directory: Path,
                start_at: int = 1):
-  files = [f for f in sorted(directory.iterdir(), key=lambda s: s.name.lower())
-           if f.is_file()]
+  files = [
+      f for f in sorted(directory.iterdir(), key=lambda s: s.name.lower())
+      if f.is_file()
+  ]
 
+  commands = []
   # Consider each file extension seperately. This is because in most cases,
   # the file types for a set of videos is homogeneous, but there may be
   # subtitle (.srt) files running in tamden.
   extensions = set({f.suffix for f in files})
   for extension in extensions:
     # Ignore files that begin with a "." (i.e. hidden files on Unix).
-    files = [
-        x for x in files if x.suffix == extension and not str(x).startswith(".")
+    extension_files = [
+        x for x in files if x.suffix == extension and not x.name.startswith(".")
     ]
-    for i, episode in enumerate(files):
+    for i, episode in enumerate(extension_files):
       episode_num = i + start_at
       ext = episode.suffix
       newname = f"{show_name} S{season_num:02d}E{episode_num:02d}{ext}"
@@ -36,7 +39,12 @@ def mkepisodal(show_name: str,
       src_path = escape_path(str(directory / episode.name))
       dst_path = escape_path(str(directory / newname))
 
-      print(f"mv -v {src_path} {dst_path}")
+      # Record the command to be emmitted later.
+      commands.append(f"mv -v {src_path} {dst_path}")
+
+  # Emit all commands at once so that they may be sorted alphabetically by file
+  # name, not grouped by file extension.
+  print('\n'.join(sorted(commands)))
 
 
 def main():
