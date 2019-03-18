db75ccb397dbcbe8d4f62540895997497979a0b2
Chris Cummins <chrisc.101@gmail.com>
2019-03-18 17:14:17 +0000

    Add tests.

--- deeplearning/clgen/sample_observers_test.py
+++ deeplearning/clgen/sample_observers_test.py
@@ -1,14 +1,59 @@
 """Unit tests for //deeplearning/clgen:sample_observers."""
+import pathlib
+
 from deeplearning.clgen import sample_observers
+from deeplearning.clgen.proto import model_pb2
 from labm8 import app
+from labm8 import crypto
+from labm8 import fs
 from labm8 import test
 
 FLAGS = app.FLAGS
 
 
-def test_TODO():
-  """Short summary of test."""
-  assert sample_observers
+def test_MaxSampleCountObserver():
+  observer = sample_observers.MaxSampleCountObserver(3)
+  assert observer.OnSample(None)
+  assert observer.OnSample(None)
+  assert not observer.OnSample(None)
+
+
+def test_SaveSampleTextObserver(tempdir: pathlib.Path):
+  observer = sample_observers.SaveSampleTextObserver(tempdir)
+  contents = "Hello, world!"
+  sample = model_pb2.Sample(text=contents)
+
+  assert observer.OnSample(sample)
+  path = tempdir / f'{crypto.sha256_str(contents)}.txt'
+  assert path.is_file()
+  assert fs.Read(path) == contents
+
+
+def test_PrintSampleObserver(capsys):
+  observer = sample_observers.PrintSampleObserver()
+  sample = model_pb2.Sample(text="Hello, world!")
+
+  assert observer.OnSample(sample)
+  captured = capsys.readouterr()
+  assert captured.out == """\
+=== CLGEN SAMPLE ===
+
+Hello, world!
+
+"""
+
+
+def test_InMemorySampleSaver():
+  observer = sample_observers.InMemorySampleSaver()
+  sample = model_pb2.Sample(text="Hello, world!")
+
+  assert observer.OnSample(sample)
+  assert len(observer.samples) == 1
+  assert observer.samples[-1].text == "Hello, world!"
+
+  assert observer.OnSample(sample)
+  assert len(observer.samples) == 2
+  assert observer.samples[-1].text == "Hello, world!"
 
 
 if __name__ == '__main__':
