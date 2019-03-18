93181fd77df1706b5aa32f065beac2b53d925a7c
Chris Cummins <chrisc.101@gmail.com>
2019-03-18 16:32:18 +0000

    Use sudo to clean up docker-created tempfiles.

--- deeplearning/clgen/docker/smoke_test/smoke_test.sh
+++ deeplearning/clgen/docker/smoke_test/smoke_test.sh
@@ -7,7 +7,9 @@ workdir="$(mktemp -d)"
 
 # Tidy up.
 cleanup() {
-  rm -rvf "$workdir"
+  # FIXME(cec): The docker container runs as root, so the generated files in
+  # the mapped directory are owned by root. I should fix this.
+  sudo rm -rvf "$workdir"
 }
 trap cleanup EXIT
 
@@ -15,7 +17,7 @@ cp deeplearning/clgen/tests/data/tiny/corpus.tar.bz2 "$workdir"
 cp deeplearning/clgen/tests/data/tiny/config.pbtxt "$workdir"
 # Set working directory in config file.
 sed -i 's,working_dir: ".*",working_dir: "/clgen",' "$workdir"/config.pbtxt
-
+# Reduce the runtime by training for fewer epochs.
 sed -i 's/num_epochs: 32/num_epochs: 2/' "$workdir"/config.pbtxt
 
 # FIXME(cec): Presently the clang_rewriter doesn't work when testing
