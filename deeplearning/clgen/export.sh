5f2f7f4d47265fd164e81061cd747e62366050aa
Chris Cummins <chrisc.101@gmail.com>
2019-03-18 16:34:42 +0000

    Export all CLgen targets.

--- deeplearning/clgen/export.sh
+++ deeplearning/clgen/export.sh
@@ -4,9 +4,7 @@ destination=$HOME/tmp/clgen
 
 targets=/tmp/targets.txt
 
-bazel query 'kind(cc_test,//deeplearning/clgen/...)' > "$targets"
-bazel query 'kind(py_test,//deeplearning/clgen/...)' >> "$targets"
-echo '//deeplearning/clgen' >> "$targets"
+bazel query '//deeplearning/clgen/...' > "$targets"
 
 bazel run //tools/source_tree:export_source_tree -- \
     --target=$(tr '\n' ',' < $targets | sed 's/,$//') \
