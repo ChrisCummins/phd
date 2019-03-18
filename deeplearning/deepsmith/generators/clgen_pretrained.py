b35963f88a58ece7c0f402c131ad6a56ec8104be
Chris Cummins <chrisc.101@gmail.com>
2019-03-18 16:02:36 +0000

    Implement sample observer for clgen generator.

--- deeplearning/deepsmith/generators/clgen_pretrained.py
+++ deeplearning/deepsmith/generators/clgen_pretrained.py
@@ -18,8 +18,6 @@ By supporting only pre-trained models, this module does not depend on the CLgen
 corpus implementation, allowing for a much smaller dependency set, i.e. without
 pulling all of the LLVM libraries required by CLgen's corpus preprocessors.
 """
-import math
-import sys
 import typing
 
 from deeplearning.clgen import sample
@@ -43,6 +41,40 @@ def ClgenInstanceToGenerator(
   g.opts['model'] = str(instance.model.path)
   g.opts['sampler'] = instance.sampler.hash
   return g
+
+
+class SampleObserver(sample_observers.SampleObserver):
+  """A CLgen model sample observer."""
+
+  def __init__(self, config: generator_pb2.ClgenGenerator,
+               response: generator_pb2.GenerateTestcasesResponse,
+               num_testcases: int):
+    self.config = config
+    self.response = response
+    self.num_testcases = num_testcases
+    self.sample_count = 0
+
+  def OnSample(self, sample: model_pb2.Sample) -> bool:
+    """Sample observer."""
+    self.sample_count += 1
+    app.Log(1, 'Generated sample %d.', self.sample_count)
+    self.response.testcases.extend(self.SampleToTestcases(sample))
+    return len(self.response.testcases) >= self.num_testcases
+
+  def SampleToTestcases(
+      self, sample_: model_pb2.Sample) -> typing.List[deepsmith_pb2.Testcase]:
+    """Convert a CLgen sample to a list of DeepSmith testcase protos."""
+    testcases = []
+    for skeleton in self.config.testcase_skeleton:
+      t = deepsmith_pb2.Testcase()
+      t.CopyFrom(skeleton)
+      p = t.profiling_events.add()
+      p.type = 'generation'
+      p.duration_ms = sample_.wall_time_ms
+      p.event_start_epoch_ms = sample_.sample_start_epoch_ms_utc
+      t.inputs['src'] = sample_.text
+      testcases.append(t)
+    return testcases
 
 
 class ClgenGenerator(generator.GeneratorServiceBase,
@@ -83,34 +115,11 @@ class ClgenGenerator(generator.GeneratorServiceBase,
     response = services.BuildDefaultResponse(
         generator_pb2.GenerateTestcasesResponse)
     with self.instance.Session():
-      num_programs = math.ceil(
-          request.num_testcases / len(self.config.testcase_skeleton))
-      samples = samples_observers.InMemorySampleSaver()
-      for i, sample_ in enumerate(samples.samples)
-          self.instance.model.Sample(
-              self.instance.sampler,
-              [sample_observers.MaxSampleCountObserver(num_programs)])):
-        app.Log(1, 'Generated sample %d.', i + 1)
-        response.testcases.extend(self.SampleToTestcases(sample_))
+      sample_observer = SampleObserver(self.config, response.testcases,
+                                       request.num_testcases)
+      self.instance.model.Sample(self.instance.sampler, [sample_observer])
 
-    # Flush any remaining output generated during Sample().
-    sys.stdout.flush()
     return response
-
-  def SampleToTestcases(
-      self, sample_: model_pb2.Sample) -> typing.List[deepsmith_pb2.Testcase]:
-    """Convert a CLgen sample to a list of DeepSmith testcase protos."""
-    testcases = []
-    for skeleton in self.config.testcase_skeleton:
-      t = deepsmith_pb2.Testcase()
-      t.CopyFrom(skeleton)
-      p = t.profiling_events.add()
-      p.type = 'generation'
-      p.duration_ms = sample_.wall_time_ms
-      p.event_start_epoch_ms = sample_.sample_start_epoch_ms_utc
-      t.inputs['src'] = sample_.text
-      testcases.append(t)
-    return testcases
 
 
 if __name__ == '__main__':
