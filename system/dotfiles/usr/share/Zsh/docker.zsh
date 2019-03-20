7552d4bdd4fb0ea280211f4bfbdadc2da9c3a069
Chris Cummins <chrisc.101@gmail.com>
2019-03-20 22:54:31 +0000

    New docker aliases.

--- system/dotfiles/usr/share/Zsh/docker.zsh
+++ system/dotfiles/usr/share/Zsh/docker.zsh
@@ -1,6 +1,12 @@
 # Aliases and configuration for Docker.
 
 alias d='docker'
+alias dp='docker push'
 alias dls='docker image ls'
 alias drm='docker rmi'
 alias dpr='docker system prune'
+
+dtp() {
+  docker tag $1 $2;
+  docker push $2;
+}
