a46854c4d5d9754b3f5672b5c995668e9edc13fc
Chris Cummins <chrisc.101@gmail.com>
2019-03-20 23:58:23 +0000

    Updated queries.

--- experimental/deeplearning/clgen/closeness_to_grewe_features/snippets.sql
+++ experimental/deeplearning/clgen/closeness_to_grewe_features/snippets.sql
@@ -14,25 +14,41 @@ ORDER BY origin DESC;
 # Overview of dynamic feature results.
 
 SELECT origin,
-       RESULT,
+       outcome,
        count(*) AS n,
        count(*) /
   (SELECT count(*)
-   FROM driver_result) AS ratio
-FROM driver_result AS df
+   FROM dynamic_features) AS ratio
+FROM dynamic_features AS df
 LEFT JOIN static_features AS sf ON sf.id=df.static_features_id
 GROUP BY origin,
-         RESULT;
+         outcome;
 
-# Combined static and raw features values.
+# Dynamic features summary: Grouped by device and outcome.
+
+SELECT opencl_env,
+       outcome,
+       count(*) AS n
+FROM
+  (SELECT opencl_env,
+          outcome,
+          static_features_id
+   FROM dynamic_features AS df
+   GROUP BY opencl_env,
+            outcome,
+            static_features_id) AS t
+GROUP BY opencl_env,
+         outcome;
+
+# Combined static and dynamic features values.
 
 SELECT sf.origin,
        sf.src_sha256,
-       sf.src,
        sf.grewe_compute_operation_count AS compute_operation_count,
        sf.grewe_global_memory_access_count AS global_memory_access_count,
        sf.grewe_local_memory_access_count AS local_memory_access_count,
        sf.grewe_coalesced_memory_access_count AS coalesced_memory_access_count,
+       df.gsize,
        df.wgsize,
        df.transferred_bytes,
        df.opencl_env,
@@ -40,54 +56,23 @@ SELECT sf.origin,
        avg(df.runtime_ms) AS avg_runtime,
        (max(df.runtime_ms) - min(df.runtime_ms)) / avg(df.runtime_ms) AS norm_range
 FROM dynamic_features AS df
-LEFT JOIN static_features AS sf ON sf.id=df.static_features_id
-GROUP BY sf.src_sha256,
-         sf.origin,
+WHERE df.outcome = 'PASS'
+  LEFT JOIN static_features AS sf ON sf.id=df.static_features_id
+GROUP BY sf.origin,
+         sf.src_sha256,
          sf.grewe_compute_operation_count,
          sf.grewe_global_memory_access_count,
          sf.grewe_local_memory_access_count,
          sf.grewe_coalesced_memory_access_count,
          df.wgsize,
+         df.gsize,
          df.transferred_bytes,
-         df.dataset,
          df.opencl_env;
 
 # OpenCL sources containing kernels with unsupported arguments.
 
-SELECT src
-FROM driver_result AS dr
-LEFT JOIN static_features AS sf ON dr.static_features_id=sf.id
-WHERE RESULT = 'UNSUPPORTED_ARGUMENTS';
+SELECT distinct(src)
+FROM dynamic_features AS df
+LEFT JOIN static_features AS sf ON df.static_features_id=sf.id
+WHERE outcome = 'UNSUPPORTED_ARGUMENTS';
 
-# Throwaway code when adding the '_<origin>' suffix to benchmark origins.
-
-SELECT count(*)
-FROM static_features
-WHERE origin LIKE 'benchmarks%';
-
-
-UPDATE static_features
-SET origin='tmp_to_delete_benchmarks'
-WHERE origin='benchmarks';
-
-
-ALTER TABLE static_features MODIFY COLUMN origin varchar(128);
-
-
-DELETE
-FROM static_features
-WHERE origin='tmp_to_delete_benchmarks';
-
-
-UPDATE static_features
-SET origin='clgen_vrae'
-WHERE origin='clgen_legacy';
-
-
-SELECT count(*)
-FROM static_features
-WHERE origin='clgen_legacy';
-
-
-SELECT distinct(origin)
-FROM static_features;
