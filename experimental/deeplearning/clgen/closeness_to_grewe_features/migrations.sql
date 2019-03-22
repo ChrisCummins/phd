806dd4d5c16395a91bb459891dd3209581c5e04a
Chris Cummins <chrisc.101@gmail.com>
2019-03-22 16:09:43 +0000

    Add migrations SQL.

---
+++ experimental/deeplearning/clgen/closeness_to_grewe_features/migrations.sql
@@ -0,0 +1,8 @@
+# Adding a driver ENUM column to dynamic features.
+
+ALTER TABLE `dynamic_features` ADD COLUMN `driver` enum( 'CLDRIVE', 'LIBCECL' ) NOT NULL AFTER `static_features_id`;
+
+
+UPDATE `dynamic_features`
+SET `driver` = 'CLDRIVE';
+
