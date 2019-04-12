dfe0f28085007645ba4ffe523472b185239a9cd3
Chris Cummins <chrisc.101@gmail.com>
2019-04-12 09:53:18 +0100

    One extra smoke test.

--- tools/docker/phd_base/smoke_test/smoke_test.sh
+++ tools/docker/phd_base/smoke_test/smoke_test.sh
@@ -16,6 +16,11 @@ trap cleanup EXIT
 main() {
   cp tools/docker/phd_base/Dockerfile "$workdir"/Dockerfile
   docker build -t phd_base "$workdir"
+
+  # Run docker container.
   test $(docker run --entrypoint /usr/bin/id phd_base -un) = "docker"
+
+  # Check that python interpreter runs.
+  docker run --entrypoint /usr/bin/python phd_base -c "import sys; print(sys.version)"
 }
 main $@
