5887af3ab3f5e539524313a8bd9fdd78bbd96a84
Chris Cummins <chrisc.101@gmail.com>
2019-04-20 09:56:09 +0100

    Disable coverage.

--- third_party/py/git/tests/git_test.py
+++ third_party/py/git/tests/git_test.py
@@ -9,6 +9,8 @@ from labm8 import test
 FLAGS = app.FLAGS
 
 
+MODULE_UNDER_TEST = None  # No coverage.
+
 def test_Git(tempdir: pathlib.Path):
   """Test that git module can be imported."""
   import git
