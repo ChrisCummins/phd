49e68c731faf68eb163236618ea6801dde3caf68
Chris Cummins <chrisc.101@gmail.com>
2019-04-23 12:06:49 +0100

    Add spell check script.

---
+++ scripts/spell_check.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eu
+
+main() {
+  test -f custom_spelling_words.txt
+  for f in $(find . -name '*.tex'); do
+    aspell --home-dir=. --personal=custom_spelling_words.txt -t -c $f;
+  done
+}
+main $@
