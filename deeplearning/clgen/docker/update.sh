4a3251cd489934b691f3e06c7d2eca3cc92dd43c
Chris Cummins <chrisc.101@gmail.com>
2019-06-17 16:56:37 +0100

    Export clgen_preprocess image.

--- deeplearning/clgen/docker/update.sh
+++ deeplearning/clgen/docker/update.sh
@@ -4,10 +4,19 @@
 set -eux
 
 main() {
+  # clgen
   set +e
   ./tools/docker/phd_build/run.sh bazel run //deeplearning/clgen/docker:clgen
   set -e
   docker tag bazel/deeplearning/clgen/docker:clgen chriscummins/clgen:latest
   docker push chriscummins/clgen:latest
+
+  # clgen_preprocess
+  ./tools/docker/phd_build/run.sh bazel build //deeplearning/clgen/docker:clgen_preprocess.tar
+  ENTRYPOINT=docker ./tools/docker/phd_build/run.sh load -i \
+      bazel-bin/deeplearning/clgen/docker/clgen_preprocess.tar
+  docker tag bazel/deeplearning/clgen/docker:clgen_preprocess chriscummins/clgen_preprocess
+  docker rmi bazel/deeplearning/clgen/docker:clgen_preprocess
+  docker push chriscummins/clgen_preprocess
 }
 main $@
