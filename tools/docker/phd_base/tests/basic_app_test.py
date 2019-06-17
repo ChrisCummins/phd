3cf9b8e7730ec23166bc2b9dcf1741eda0942eeb
Chris Cummins <chrisc.101@gmail.com>
2019-06-17 10:13:38 +0100

    Add libmariadbclient18 to phd_base.

---
+++ tools/docker/phd_base/tests/basic_app_test.py
@@ -0,0 +1,14 @@
+"""An example app to run in a docker image."""
+
+from labm8 import app
+
+FLAGS = app.FLAGS
+
+
+def main():
+  """Main entry point."""
+  app.Log(1, "Tests pass.")
+
+
+if __name__ == '__main__':
+  app.Run(main)
---
+++ tools/docker/phd_base/tests/mysql_test.py
@@ -0,0 +1,7 @@
+"""Test MySQL package import."""
+
+if __name__ == '__main__':
+  import _mysql
+  _ = _mysql
+
+  print("test passes.")
