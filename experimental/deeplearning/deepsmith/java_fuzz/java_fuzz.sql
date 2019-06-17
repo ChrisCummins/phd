48b97aeae84c0c12738ae78ad944e9a02440cf9f
Chris Cummins <chrisc.101@gmail.com>
2019-06-17 14:12:07 +0100

    Add a SQL snippets file.

---
+++ experimental/deeplearning/deepsmith/java_fuzz/java_fuzz.sql
@@ -0,0 +1,24 @@
+# MySQL snippets for working with java_fuzz contentfiles database.
+ # Top 10 most popular repos scraped.
+
+SELECT name AS repo,
+       OWNER AS USER,
+                num_stars AS stars,
+                num_forks AS forks,
+                count(*) AS file_count
+FROM contentfiles
+LEFT JOIN repositories ON contentfiles.clone_from_url=repositories.clone_from_url
+GROUP BY contentfiles.clone_from_url
+ORDER BY file_count DESC
+LIMIT 10;
+
+# How many contentfiles if we require minimum 10 stars?
+
+SELECT sum(file_count)
+FROM
+  (SELECT num_stars AS stars,
+          count(*) AS file_count
+   FROM contentfiles
+   LEFT JOIN repositories ON contentfiles.clone_from_url=repositories.clone_from_url
+   WHERE num_stars >= 10
+   GROUP BY contentfiles.clone_from_url) t;
