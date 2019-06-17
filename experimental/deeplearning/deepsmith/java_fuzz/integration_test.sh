c0f86429fc2dbdd741a026115da63e6359a77f41
Chris Cummins <chrisc.101@gmail.com>
2019-06-17 10:43:18 +0100

    Update docker images for java_fuzz.

--- experimental/deeplearning/deepsmith/java_fuzz/integration_test.sh
+++ experimental/deeplearning/deepsmith/java_fuzz/integration_test.sh
@@ -12,26 +12,38 @@ cleanup() {
 trap cleanup EXIT
 
 main() {
+  docker load -i experimental/deeplearning/deepsmith/java_fuzz/scrape_java_files_image.tar
+  docker load -i experimental/deeplearning/deepsmith/java_fuzz/split_contentfiles_image.tar
+  docker load -i experimental/deeplearning/deepsmith/java_fuzz/export_java_corpus_image.tar
+
   # Scrape a single repository.
-  experimental/deeplearning/deepsmith/java_fuzz/scrape_java_files \
-      --n=1 --db="sqlite:///$workdir/java.db"
+  # We share /var/phd so that /var/phd/github_access_token.txt is available
+  # to the scraper image.
+  docker run -v"$workdir":/workdir -v/var/phd:/var/phd bazel/experimental/deeplearning/deepsmith/java_fuzz:scrape_java_files_image \
+      --n=1 --db="sqlite:////workdir/java.db"
 
   # Check that database has been created.
   test -f "$workdir/java.db"
 
   # Export a subset of the content files.
-  experimental/deeplearning/deepsmith/java_fuzz/split_contentfiles \
-      --n=1 --input="sqlite:///$workdir/java.db" --output="sqlite:///$workdir/subset.db"
+  docker run -v"$workdir":/workdir bazel/experimental/deeplearning/deepsmith/java_fuzz:split_contentfiles_image \
+      --n=1 --input="sqlite:////workdir/java.db" --output="sqlite:////workdir/subset.db"
 
   # Check that a new database has been created.
   test -f "$workdir/java.db"
   test -f "$workdir/subset.db"
 
   # Pre-process.
-  experimental/deeplearning/deepsmith/java_fuzz/export_java_corpus \
-      --db="sqlite:///$workdir/java.db" --outdir="$workdir"/corpus \
+  docker run -v"$workdir":/workdir \
+      bazel/experimental/deeplearning/deepsmith/java_fuzz:export_java_corpus_image \
+      --db="sqlite:////workdir/java.db" --outdir=/workdir/corpus \
       --preprocessors="datasets.github.scrape_repos.preprocessors.extractors:JavaStaticMethods"
 
   test -d "$workdir/corpus"
+
+  # Remove the docker images.
+  docker rmi bazel/experimental/deeplearning/deepsmith/java_fuzz:scrape_java_files_image
+  docker rmi bazel/experimental/deeplearning/deepsmith/java_fuzz:split_contentfiles_image
+  docker rmi bazel/experimental/deeplearning/deepsmith/java_fuzz:export_java_corpus_image
 }
 main $@
--- experimental/deeplearning/deepsmith/java_fuzz/publish_docker_images.sh
+++ experimental/deeplearning/deepsmith/java_fuzz/publish_docker_images.sh
@@ -1,39 +1,52 @@
 #!/usr/bin/env bash
 #
 # Build and update the docker image and push it to dockerhub.
-set -eu
+set -eux
 
-# Fully qualified path, WITHOUT the '//' prefix
-BAZEL_TARGET="experimental/deeplearning/deepsmith/java_fuzz:scrape_java_files_image"
-IMAGE_NAME="java_fuzz_scraper"
 DOCKERHUB_USER="chriscummins"
 
 publish_docker_image() {
-  local bazel_target="$1"
-  local image_name="$2"
+  # The fully qualified path, WITHOUT the leading '//'.
+  local package="$1"
+  local target="$2"
+  # The name of the image tag to push to dockerhub.
+  local image_name="$3"
 
-  echo "Building bazel target $bazel_target"
-  ./tools/docker/phd_build/run.sh bazel build "$bazel_target"
-  set +e
-  ./tools/docker/phd_build/run.sh bazel run "$bazel_target"
-  set -e
+  # The fully qualified target to build.
+  local tar_target="//$package:$target.tar"
+  # The path of the tar archive built by bazel.
+  local tar_path="bazel-bin/$package/$target.tar"
+
+  echo "Building $tar_target image"
+  ./tools/docker/phd_build/run.sh bazel build "$tar_target"
+  ENTRYPOINT=docker ./tools/docker/phd_build/run.sh load -i "$tar_path"
   echo "Pushing docker image $image_name"
-  docker tag bazel/"$bazel_target" "$DOCKERHUB_USER"/"$image_name":latest
-  docker push "$DOCKERHUB_USER"/"$image_name":latest
+
+  # The name of docker image that is built by bazel.
+  local load_image_name="bazel/$package:$target"
+  # The name of the docker image to export.
+  local export_image_name="$DOCKERHUB_USER/$image_name:latest"
+
+  docker tag "$load_image_name" "$export_image_name"
+  docker rmi "$load_image_name"
+  # docker push "$export_image_name"
 }
 
 
 main() {
   publish_docker_image \
-      "experimental/deeplearning/deepsmith/java_fuzz:scrape_java_files_image" \
+      "experimental/deeplearning/deepsmith/java_fuzz" \
+      "scrape_java_files_image" \
       "java_fuzz_scraper"
 
   publish_docker_image \
-      "experimental/deeplearning/deepsmith/java_fuzz:split_contentfiles_image" \
+      "experimental/deeplearning/deepsmith/java_fuzz" \
+      "split_contentfiles_image" \
       "java_fuzz_split_contentfiles"
 
   publish_docker_image \
-      "experimental/deeplearning/deepsmith/java_fuzz:export_java_corpus_image" \
+      "experimental/deeplearning/deepsmith/java_fuzz" \
+      "export_java_corpus_image" \
       "java_fuzz_export_corpus"
 }
 main $@
