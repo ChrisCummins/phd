8c9036bdd52ad10eca36d8a9c8a8041958b20986
Chris Cummins <chrisc.101@gmail.com>
2019-06-25 16:36:12 +0100

    Update SQL snippets.

--- experimental/deeplearning/deepsmith/java_fuzz/java_fuzz.sql
+++ experimental/deeplearning/deepsmith/java_fuzz/java_fuzz.sql
@@ -1,60 +1,73 @@
 # MySQL snippets for working with java_fuzz contentfiles database.
- # How many active repos?
- # Reset preprocessor.
-
-DROP DATABASE `github_java_methods_pp`;
-
-
-UPDATE github_java_methods.repositories
-SET exported = 0;
-
-# Count repos.
-
-SELECT count(*)
-FROM repositories
-WHERE active = 0;
-
-
-SELECT count(*)
-FROM repositories
-WHERE exported = 1;
-
-
-SELECT count(*)
-FROM github_java_methods.contentfiles
-WHERE active = 1;
-
-
-SELECT count(*)
-FROM repositories
-WHERE exported = 0;
-
-
-UPDATE repositories
-SET exported = 0;
-
+ # What timestamp have files been scraped over?
 
 SELECT *
 FROM github_java.contentfiles
-WHERE clone_from_url = 'https://github.com/kerstin/fhtw-hamster.git';
-
-
-SELECT count(*)
-FROM github_java_methods.contentfiles
-WHERE active = 1;
-
-
-SELECT *
-FROM github_java.contentfiles
-WHERE text LIKE '%-----BEGIN RSA PRIVATE KEY-----%'
+ORDER BY id
 LIMIT 1;
 
 
-ALTER TABLE github_java_methods.contentfiles ADD active bool NOT NULL DEFAULT 1 AFTER artifact_index;
+SELECT *
+FROM github_java.contentfiles
+ORDER BY id DESC
+LIMIT 1;
+
+# How many repos did we scrape?
+
+SELECT count(*)
+FROM github_java.repositories;
+
+# How many users' data did we scrape?
+
+SELECT count(*)
+FROM
+  (SELECT distinct(OWNER)
+   FROM github_java.repositories) t;
+
+# What is the total line and character counts of the scraped Java?
+
+SELECT sum(linecount),
+       sum(charcount)
+FROM github_java.contentfiles;
+
+# How many .java files did we scrape?
+
+SELECT count(*)
+FROM github_java.contentfiles;
+
+# Reset repositories masks.alter
+
+UPDATE github_java.repositories
+SET active = 1,
+    exported = 0;
+
+# After pruning, how many GitHub repos are active?
+
+SELECT count(*)
+FROM github_java.repositories
+WHERE active = 1;
+
+# How many `.java` files is that?
+
+SELECT count(*)
+FROM github_java.contentfiles
+LEFT JOIN github_java.repositories ON github_java.contentfiles.clone_from_url=github_java.repositories.clone_from_url
+WHERE github_java.repositories.active = 1;
+
+# How many methods have been pre-processed?
+
+SELECT count(*)
+FROM `github_java_methods_2019.06.25`.repositories
+WHERE exported=1;
+
+# Reset pre-processed.
+
+DELETE
+FROM `github_java_methods_pp_2019.06.25`.preprocessed_contentfiles;
 
 
-UPDATE repositories
-SET active = 0;
+UPDATE `github_java_methods_2019.06.25`.repositories
+SET exported=0;
 
 # Top 10 most popular repos scraped.
 
@@ -63,20 +76,35 @@ SELECT name AS repo,
                 num_stars AS stars,
                 num_forks AS forks,
                 count(*) AS file_count
-FROM contentfiles
-LEFT JOIN repositories ON contentfiles.clone_from_url=repositories.clone_from_url
-GROUP BY contentfiles.clone_from_url
+FROM `github_java_methods_2019.06.25`.contentfiles AS cf
+LEFT JOIN `github_java_methods_2019.06.25`.repositories AS repo ON cf.clone_from_url=repo.clone_from_url
+GROUP BY cf.clone_from_url
 ORDER BY file_count DESC
 LIMIT 10;
 
-# How many contentfiles if we require minimum 10 stars?
+# What are the cuases of failures?
 
-SELECT sum(file_count)
-FROM
-  (SELECT num_stars AS stars,
-          count(*) AS file_count
-   FROM contentfiles
-   LEFT JOIN repositories ON contentfiles.clone_from_url=repositories.clone_from_url
-   WHERE num_stars >= 10
-   GROUP BY contentfiles.clone_from_url) t;
+SELECT text, count(*) AS COUNT,
+             (count(*) /
+                (SELECT count(*)
+                 FROM `github_java_methods_pp_2019.06.25`.preprocessed_contentfiles)) * 100 AS "% of total"
+FROM `github_java_methods_pp_2019.06.25`.preprocessed_contentfiles
+WHERE preprocessing_succeeded = 0
+GROUP BY text;
 
+# How many successfully preprocessed methods are there?
+
+SELECT count(*)
+FROM `github_java_methods_pp_2019.06.25`.preprocessed_contentfiles
+WHERE preprocessing_succeeded = 1;
+
+# Select some preprocessed files for inspection.
+
+SELECT *
+FROM `github_java_methods_pp_2019.06.25`.preprocessed_contentfiles
+WHERE preprocessing_succeeded = 1;
+
+# Reset encoded.
+
+DELETE
+FROM `github_java_methods_enc_2019.06.25`.encoded_contentfiles;
