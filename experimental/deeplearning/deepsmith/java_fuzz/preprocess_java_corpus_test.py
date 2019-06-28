4969b3527c6e49493b5cfb434a5c7c46c25711d9
Chris Cummins <chrisc.101@gmail.com>
2019-06-28 20:36:36 +0100

    Add extra preprocess test.

--- experimental/deeplearning/deepsmith/java_fuzz/preprocess_java_corpus_test.py
+++ experimental/deeplearning/deepsmith/java_fuzz/preprocess_java_corpus_test.py
@@ -19,7 +19,6 @@ private static int Foobar(int foo) {
         }
 """)
   ])
-
   assert len(pp_cfs) == 1
   assert pp_cfs[0].text == """\
 private static int fn_A(int a){
@@ -29,6 +28,31 @@ private static int fn_A(int a){
   return a + 10;
 }
 """
+  assert pp_cfs[0].preprocessing_succeeded
+
+
+def test_PreprocessContentFiles_method_depends_on_java_util():
+  """Test that a method which uses java.util.ArrayList works."""
+  pp_cfs = preprocess_java_corpus.PreprocessContentFiles([
+      contentfiles.ContentFile(text="""
+private static int Foobar(int a, ArrayList<Integer> _) {
+  int b=10 + 1;
+  a+=b;
+  a*=2;
+  return a + 10;
+}
+""")
+  ])
+  assert len(pp_cfs) == 1
+  assert pp_cfs[0].text == """\
+private static int fn_A(int a,ArrayList<Integer> b){
+  int c=10 + 1;
+  a+=c;
+  a*=2;
+  return a + 10;
+}
+"""
+  assert pp_cfs[0].preprocessing_succeeded
 
 
 if __name__ == '__main__':
