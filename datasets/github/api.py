bfbef30b5010e5a8becbc81bfe5459542b6e3d97
Chris Cummins <chrisc.101@gmail.com>
2019-07-19 08:38:41 +0100

    Add some GitHub utility functions.

--- datasets/github/api.py
+++ datasets/github/api.py
@@ -18,11 +18,13 @@ GetGithubConectionFromFlagsOrDie() function defined in this module. Don't write
 your own credentials handling code.
 """
 import configparser
-import pathlib
-from datasets.github import github_pb2
-
 import github
+import pathlib
+import subprocess
+
+from datasets.github import github_pb2
 from labm8 import app
+
 
 FLAGS = app.FLAGS
 
@@ -83,3 +85,67 @@ def GetGithubConectionFromFlagsOrDie() -> github.Github:
       return github.Github(credentials.username, credentials.password)
   except Exception as e:  # Deliberately broad catch-all.
     app.FatalWithoutStackTrace('Failed to create GitHub API connection: %s', e)
+
+
+class RepoNotFoundError(ValueError):
+  """Error thrown if a github repo is not found."""
+  pass
+
+
+def GetUserRepo(connection: github.Github, repo_name: str) -> github.Repository:
+  """Get and return a github repository owned by the user.
+
+  Args:
+    connection: A github API connection.
+    repo_name: The name of the repo to get.
+  """
+  try:
+    return connection.get_user().get_repo(repo_name)
+  except github.UnknownObjectException as e:
+    if e.status != 404:
+      raise OSError(f"Github API raised error: {e}")
+    raise RepoNotFoundError(f"Github repo `{repo_name}` not found")
+
+
+def GetOrCreateUserRepo(connection: github.Github, repo_name: str,
+                        description: str=None, homepage: str=None,
+                        has_wiki: bool=True, has_issues: bool=True,
+                        private: bool=True) -> github.Repository:
+  """Get and return a github repository owned by the user.
+
+  Create it if it doesn't exist.
+
+  Args:
+    connection: A github API connection.
+    repo_name: The name of the repo to get.
+    description: The repo description.
+    homepage: The repo homepage.
+    has_wiki: Whether the repo has a wiki.
+    has_issues: Whether the repo has an issue tracker.
+    private: Whether the repo is private.
+  """
+  try:
+    return GetUserRepo(connection, repo_name)
+  except RepoNotFoundError:
+    app.Log(1, "Creating repo %s", repo_name)
+    connection.get_user().create_repo(
+        repo_name,
+        description=description,
+        homepage=homepage,
+        has_wiki=has_wiki,
+        has_issues=has_issues,
+        private=private)
+    return GetUserRepo(connection, repo_name)
+
+
+class RepoCloneFailed(OSError):
+  """Error raised if repo fails to clone."""
+  pass
+
+
+def CloneRepoToDestination(repo: github.Repository, destination: pathlib.Path):
+  """Clone repo from github."""
+  subprocess.check_call(['git', 'clone', repo.ssh_url, str(destination)])
+  if not (destination / '.git').is_dir():
+    raise RepoCloneFailed(
+        f"Cloned repo `{repo.ssh_url}` but `{destination}/.git` not found")
