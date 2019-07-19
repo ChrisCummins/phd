0d54ae9e56abc72a19b7cd8f369c667313690d48
Chris Cummins <chrisc.101@gmail.com>
2019-07-19 10:13:12 +0100

    Add labm8 export script.

---
+++ labm8/EXPORT.py
@@ -0,0 +1,7 @@
+from tools.source_tree import export_source_tree
+export_source_tree.EXPORT(github_repo='labm8',
+                          targets=['//labm8:all'],
+                          move_file_mapping={
+                              'labm8/README.md': 'README.md',
+                              'labm8/LICENSE': 'LICENSE',
+                          })
