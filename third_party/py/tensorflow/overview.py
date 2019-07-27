12fd84534865369978458ee251f457217305975e
Chris Cummins <chrisc.101@gmail.com>
2019-07-27 12:19:55 +0100

    Add a tensorflow overview script.

---
+++ third_party/py/tensorflow/overview.py
@@ -0,0 +1,21 @@
+"""Debugging script to check installed tensorflow version and GPU status.
+
+Usage: python ./overview.py 2>/dev/null
+"""
+import os
+
+import tensorflow as tf
+
+
+def main():
+  print("""
+TENSORFLOW
+====================
+""")
+  print("tf.__version__             ", tf.__version__)
+  print("$CUDA_VISIBLE_DEVICES      ", os.environ.get("CUDA_VISIBLE_DEVICES"))
+  print("tf.test.is_gpu_available() ", tf.test.is_gpu_available())
+
+
+if __name__ == '__main__':
+  main()
