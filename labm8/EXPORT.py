a50dc88b1c8bedc144648dc9a04d1d1198ed047a
Chris Cummins <chrisc.101@gmail.com>
2019-07-31 01:25:56 +0100

    Also export labm8 logo.

--- labm8/EXPORT.py
+++ labm8/EXPORT.py
@@ -1,8 +1,10 @@
 from tools.source_tree import export_source_tree
-export_source_tree.EXPORT(github_repo='labm8',
-                          targets=['//labm8:all'],
-                          move_file_mapping={
-                              'labm8/README.md': 'README.md',
-                              'labm8/LICENSE': 'LICENSE',
-                              'labm8/travis.yml': '.travis.yml',
-                          })
+export_source_tree.EXPORT(
+    github_repo='labm8',
+    targets=['//labm8:all'],
+    extra_files=['labm8/labm8.jpg'],
+    move_file_mapping={
+        'labm8/README.md': 'README.md',
+        'labm8/LICENSE': 'LICENSE',
+        'labm8/travis.yml': '.travis.yml',
+    })
