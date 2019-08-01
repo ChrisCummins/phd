3fd288e7551d82696f43e4d947105d3011ca1a9d
Chris Cummins <chrisc.101@gmail.com>
2019-08-01 15:50:46 +0100

    Fix weird python module import failure

--- tools/git/pre_commit.py
+++ tools/git/pre_commit.py
@@ -23,10 +23,10 @@ _PHD_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..')
 # Ad-hoc import of //tools/code_style/linters:linters_lib.py
 LINTERS_LIB = os.path.join(_PHD_ROOT, 'tools/code_style/linters/linters_lib.py')
 if sys.version_info >= (3, 5):
-  import importlib
+  from importlib import util
 
-  spec = importlib.util.spec_from_file_location("linters_lib", LINTERS_LIB)
-  linters_lib = importlib.util.module_from_spec(spec)
+  spec = util.spec_from_file_location("linters_lib", LINTERS_LIB)
+  linters_lib = util.module_from_spec(spec)
   spec.loader.exec_module(linters_lib)
 else:
   import imp
