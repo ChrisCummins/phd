c60a899349e869c2afe908843de8a43ad9f22016
Chris Cummins <chrisc.101@gmail.com>
2019-08-05 18:16:41 +0100

    Update ryangosling sync options.

--- system/machines/ryangosling/ryangosling.zsh
+++ system/machines/ryangosling/ryangosling.zsh
@@ -1,15 +1,31 @@
 #!/usr/bin/env zsh
 
 ryan_gosling_give_me_photos() {
-  (~/.local/bin/machines \
-     --machine=$HOME/.local/var/machines/ryangosling.pbtxt \
-     --pull photos --delete --nodry_run $@)
+  (
+    ~/.local/bin/machines \
+       --machine=$HOME/.local/var/machines/ryangosling.pbtxt \
+       --pull staging --delete --nodry_run $@ && \
+    ~/.local/bin/machines \
+       --machine=$HOME/.local/var/machines/ryangosling.pbtxt \
+       --pull photos --delete --nodry_run $@ && \
+    ~/.local/bin/machines \
+       --machine=$HOME/.local/var/machines/ryangosling.pbtxt \
+       --pull photo_catalog --delete --nodry_run $@
+  )
 }
 
 ryan_gosling_have_my_photos() {
-  (~/.local/bin/machines \
-     --machine=$HOME/.local/var/machines/ryangosling.pbtxt \
-     --push photos --delete --nodry_run $@)
+  (
+    ~/.local/bin/machines \
+       --machine=$HOME/.local/var/machines/ryangosling.pbtxt \
+       --push staging --delete --nodry_run $@ && \
+    ~/.local/bin/machines \
+       --machine=$HOME/.local/var/machines/ryangosling.pbtxt \
+       --push photos --delete --nodry_run $@ && \
+    ~/.local/bin/machines \
+       --machine=$HOME/.local/var/machines/ryangosling.pbtxt \
+       --push photo_catalog --delete --nodry_run $@
+  )
 }
 
 ryan_gosling_have_my_music() {
