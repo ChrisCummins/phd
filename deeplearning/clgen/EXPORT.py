8896a5721a5c2edf7ebd78873a55befe9691133a
Chris Cummins <chrisc.101@gmail.com>
2019-08-05 16:55:05 +0100

    Replace EXPORT scripts with exports_repo rules.

--- deeplearning/clgen/EXPORT.py
+++
@@ -1,9 +0,0 @@
-from tools.source_tree import export_source_tree
-export_source_tree.EXPORT(
-    github_repo='clgen',
-    targets=['//deeplearning/clgen', 'tests(//deeplearning/clgen/...)'],
-    move_file_mapping={
-        'deeplearning/clgen/README.md': 'README.md',
-        'deeplearning/clgen/LICENSE': 'LICENSE',
-        'deeplearning/clgen/travis.yml': '.travis.yml',
-    })
--- system/dotfiles/EXPORT.py
+++
@@ -1,8 +0,0 @@
-from tools.source_tree import export_source_tree
-export_source_tree.EXPORT(github_repo='dotfiles',
-                          targets=[
-                              '//system/dotfiles',
-                          ],
-                          move_file_mapping={
-                              'system/dotfiles/README.md': 'README.md',
-                          })
