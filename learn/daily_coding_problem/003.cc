b70e60603eaf5412029a87c29e5395611e5e2226
Chris Cummins <chrisc.101@gmail.com>
2019-08-14 01:05:55 +0100

    Add a solution for part 3.

--- learn/daily_coding_problem/003.cc
+++ learn/daily_coding_problem/003.cc
@@ -7,5 +7,39 @@
 // should give 3.
 //
 // You can modify the input array in-place.
+#include <iostream>
+#include <unordered_set>
+#include <vector>
 
-int main(int argc, char** argv) {}
+// Time: O(n)
+// Space: O(n)
+int Solution(const std::vector<int>& a) {
+  std::unordered_set<int> set;
+  for (auto x : a) {
+    if (x > 0) {
+      set.insert(x);
+    }
+  }
+
+  int sum = 0;
+  for (auto x : set) {
+    sum += x;
+  }
+
+  int expectedSum = 0;
+  for (int i = 1; i <= set.size() + 1; ++i) {
+    expectedSum += i;
+  }
+
+  return expectedSum - sum;
+}
+
+int main(int argc, char** argv) {
+  std::cout << Solution({0}) << std::endl;
+  std::cout << Solution({0, 1, 3}) << std::endl;
+  std::cout << Solution({0, 1}) << std::endl;
+  std::cout << Solution({0, 1, 2}) << std::endl;
+  std::cout << Solution({0, 1, 2, 2, 4}) << std::endl;
+  std::cout << Solution({3, 4, -1, 1}) << std::endl;
+  return 0;
+}
