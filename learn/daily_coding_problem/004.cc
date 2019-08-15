0a10e2a67333515a53b3461f80cbeed3821582a8
Chris Cummins <chrisc.101@gmail.com>
2019-08-15 13:44:09 +0100

    Add coding challenge solution.

--- learn/daily_coding_problem/004.cc
+++ learn/daily_coding_problem/004.cc
@@ -9,5 +9,44 @@
 //         return f(a, b)
 //     return pair
 // Implement car and cdr.
+#include <functional>
+#include <iostream>
+#include <tuple>
 
-int main(int argc, char** argv) {}
+// Time: O(1)
+// Space: O(1)
+template <typename T, typename Pair>
+std::function<T()> car(const Pair& pair) {
+  return [&]() {
+    std::cout << "eval car\n";
+    return std::get<0>(pair());
+  };
+}
+
+// Time: O(1)
+// Space: O(1)
+template <typename T, typename Pair>
+std::function<T()> cdr(const Pair& pair) {
+  return [&]() {
+    std::cout << "eval cdr\n";
+    return std::get<1>(pair());
+  };
+}
+
+// Time: O(1)
+// Space: O(1)
+template <typename T>
+std::function<std::tuple<T, T>()> cons(const T& a, const T& b) {
+  return [&]() {
+    std::cout << "eval cons\n";
+    return std::make_tuple(a, b);
+  };
+}
+
+int main(int argc, char** argv) {
+  auto a = car<int>(cons(3, 4));
+  auto b = cdr<int>(cons(3, 4));
+  std::cout << a() << std::endl;
+  std::cout << b() << std::endl;
+  return 0;
+}
