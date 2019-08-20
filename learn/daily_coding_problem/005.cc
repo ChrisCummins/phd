49f2b94fd36fd58c615313995572a3a40cd316ea
Chris Cummins <chrisc.101@gmail.com>
2019-08-20 15:44:17 +0100

    Add solution for coding problem.

--- learn/daily_coding_problem/005.cc
+++ learn/daily_coding_problem/005.cc
@@ -8,4 +8,71 @@
 // have access to get_pointer and dereference_pointer functions that converts
 // between nodes and memory addresses.
 
-int main(int argc, char** argv) {}
+#include <cstdint>
+#include <iostream>
+
+class XorLinkedList {
+ public:
+  int value_;
+  XorLinkedList* both_;
+
+  XorLinkedList(const int& value) : value_(value), both_(nullptr) {}
+
+  static XorLinkedList* Xor(const XorLinkedList* a, const XorLinkedList* b) {
+    return (XorLinkedList*)((uintptr_t)(void*)a ^ (uintptr_t)(void*)b);
+  }
+
+  void add(XorLinkedList* element) {
+    element->both_ = Xor(this, NULL);
+    auto next = Xor(both_, NULL);
+    both_ = Xor(element, next);
+  }
+
+  const XorLinkedList* get(const size_t n) {
+    const XorLinkedList* curr = this;
+    const XorLinkedList* prev = nullptr;
+
+    for (int i = 0; i < n; ++i) {
+      const XorLinkedList* next = XorLinkedList::Xor(prev, curr->both_);
+      if (next == nullptr) {
+        return nullptr;
+      }
+      prev = curr;
+      curr = next;
+    }
+
+    return curr;
+  }
+
+  static void Print(const XorLinkedList* list) {
+    const XorLinkedList* curr = list;
+    const XorLinkedList* prev = nullptr;
+
+    std::cout << "{ ";
+    while (curr != nullptr) {
+      std::cout << curr->value_ << ", ";
+
+      const XorLinkedList* next = XorLinkedList::Xor(prev, curr->both_);
+      prev = curr;
+      curr = next;
+    }
+    std::cout << '}' << std::endl;
+  }
+};
+
+int main(int argc, char** argv) {
+  XorLinkedList A(1);
+  XorLinkedList B(2);
+  XorLinkedList C(3);
+
+  A.add(&B);
+  B.add(&C);
+
+  XorLinkedList::Print(&A);
+  XorLinkedList::Print(A.get(0));
+  XorLinkedList::Print(A.get(1));
+  XorLinkedList::Print(&B);
+  XorLinkedList::Print(&C);
+
+  return 0;
+}
