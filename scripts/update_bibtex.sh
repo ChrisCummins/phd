4002d7818c2f1b33c51a87080bd19fda0cc453f6
Chris Cummins <chrisc.101@gmail.com>
2019-08-21 22:10:38 +0100

    Tidy up LaTex sources.

--- scripts/update_bibtex.sh
+++ scripts/update_bibtex.sh
@@ -5,12 +5,12 @@ BIBTEX_FILE_TO_IMPORT="$HOME/docs/library.bib"
 PHD_REPO="$HOME/phd"
 
 main() {
-  test -f refs.bib
+  test -f biliography.bib
   test -f "$BIBTEX_FILE_TO_IMPORT"
   test -f "$PHD_REPO/WORKSPACE"
-  cp "$BIBTEX_FILE_TO_IMPORT" refs.bib
+  cp "$BIBTEX_FILE_TO_IMPORT" biliography.bib
   cwd="$(pwd)"
-  cd "$PHD_REPO" && bazel run //docs:minimize_bibtex -- --bibtex_path="$cwd/refs.bib"
-  cd "$PHD_REPO" && bazel run //docs:deacronym_bibtex -- --bibtex_path="$cwd/refs.bib"
+  cd "$PHD_REPO" && bazel run //docs:minimize_bibtex -- --bibtex_path="$cwd/biliography.bib"
+  cd "$PHD_REPO" && bazel run //docs:deacronym_bibtex -- --bibtex_path="$cwd/biliography.bib"
 }
 main $@
