3395284f0827db2f7a57d245726e493292316ae0
Chris Cummins <chrisc.101@gmail.com>
2019-10-11 23:29:15 +0200

    Add LlvmBytecodeToContextualFlowGraph() API call.

--- deeplearning/ncc/inst2vec/api.py
+++ deeplearning/ncc/inst2vec/api.py
@@ -1,17 +1,27 @@
 """My (Chris's) API for inst2vec codebase."""
+import copy
+import networkx as nx
+import numpy as np
 import typing
 
-import numpy as np
-
 from deeplearning.ncc import vocabulary
+from deeplearning.ncc.inst2vec import inst2vec_preprocess as preprocess
 from labm8 import app
+
 
 FLAGS = app.FLAGS
 
 
+# Type hints for inst2vec parameters and return values.
+MultiEdges = typing.Dict[str, typing.List[str]]
+
+
 def PreprocessLlvmBytecode(bytecode: str) -> str:
   """Pre-process an LLVM bytecode for encoding."""
-  raise NotImplementedError
+  bytecode_lines = bytecode.split('\n')
+  preprocessed, functions_declared_in_files = preprocess.preprocess(
+      [bytecode_lines])
+  return '\n'.join(preprocessed[0])
 
 
 def EncodeLlvmBytecode(bytecode: str,
@@ -26,7 +36,7 @@ def EmbedEncoded(encoded: typing.List[int], embedding_matrix) -> np.ndarray:
 
 
 def Inst2Vec(bytecode: str, vocab: vocabulary.VocabularyZipFile,
-             embedding) -> np.ndarry:
+             embedding) -> np.ndarray:
   """Transform an LLVM bytecode to an array of embeddings.
 
   Args:
@@ -40,3 +50,38 @@ def Inst2Vec(bytecode: str, vocab: vocabulary.VocabularyZipFile,
   embed = lambda x: EmbedEncoded(x, embedding)
   encode = lambda x: EncodeLlvmBytecode(x, vocab)
   return embed(encode(PreprocessLlvmBytecode(bytecode)))
+
+
+def LlvmBytecodeToContextualFlowGraph(bytecode: str) -> nx.DiGraph:
+  """
+  Given a file of source code, construct a context graph.
+
+  Args:
+    bytecode: The input bytecode.
+    vocab: The vocabulary.
+
+  Returns:
+    A <digraph, multi_edge_list> tuple, where <digraph> is a directed graph in
+    which nodes are identifiers or ad-hoc and edges are statements which is
+    meant as a representation of both data and flow control of the code
+    capturing the notion of context; and <multi_edge_list> is a dictionary of
+    edges that have parallel edges.
+  """
+  preprocessed_bytecode = PreprocessLlvmBytecode(bytecode)
+  llvm_lines = preprocessed_bytecode.split('\n')
+  functions_declared_in_file = preprocess.GetFunctionsDeclaredInFile(llvm_lines)
+  # File name is required by BuildContextualFlowGraph(), but is used only to
+  # produce descriptive error messages, so any value will do.
+  filename = '[input]'
+  xfg, multi_edges = preprocess.BuildContextualFlowGraph(
+      llvm_lines, functions_declared_in_file, filename)
+  del multi_edges  # unused
+  return xfg
+
+
+def XfgToDot(xfg, dotpath):
+  xfg = copy.deepcopy(xfg)
+  for edge in xfg.edges:
+    xfg.edges[edge]['label'] = xfg.edges[edge]['stmt'].encode('ascii', 'ignore').decode('ascii').replace('\0', '0')
+    del xfg.edges[edge]['stmt']
+  nx.drawing.nx_pydot.write_dot(xfg, dotpath)
--- deeplearning/ncc/inst2vec/api_test.py
+++ deeplearning/ncc/inst2vec/api_test.py
@@ -1,12 +1,13 @@
 """Unit tests for //deeplearning/ncc/inst2vec:api."""
 import pathlib
-
 import pytest
 
 from deeplearning.ncc import vocabulary
+from deeplearning.ncc.inst2vec import api
 from labm8 import app
 from labm8 import bazelutil
 from labm8 import test
+
 
 FLAGS = app.FLAGS
 
@@ -81,12 +82,38 @@ def vocab() -> vocabulary.VocabularyZipFile:
     yield v
 
 
-MODULE_UNDER_TEST = None  # TODO(cec): Remove once a test is added.
-
-
-def test_TODO():
+def test_Inst2Vec(vocab: vocabulary.VocabularyZipFile):
   """Short summary of test."""
+  # TODO: Add unit tests for Inst2Vec() once implemented.
   assert True
+
+
+def test_LlvmBytecodeToContextualFlowGraph_simple_function():
+  """Test XFG creation for simple bytecode."""
+  # Input bytecode corresponds to C function: `int main() { return 10; }`.
+  xfg = api.LlvmBytecodeToContextualFlowGraph("""
+; ModuleID = 'foo.c'
+source_filename = "foo.c"
+target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx10.14.0"
+
+; Function Attrs: noinline nounwind optnone ssp uwtable
+define i32 @main() #0 {
+  %1 = alloca i32, align 4
+  store i32 0, i32* %1, align 4
+  ret i32 10
+}
+
+attributes #0 = { noinline nounwind optnone ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+fxsr,+mmx,+sahf,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.module.flags = !{!0, !1, !2}
+!llvm.ident = !{!3}
+
+!0 = !{i32 2, !"SDK Version", [2 x i32] [i32 10, i32 14]}
+!1 = !{i32 1, !"wchar_size", i32 4}
+!2 = !{i32 7, !"PIC Level", i32 2}
+!3 = !{!"Apple LLVM version 10.0.1 (clang-1001.0.46.4)"}
+""")
 
 
 if __name__ == '__main__':
