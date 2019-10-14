e2c2260084529aeb2544e593c858cc609a3dd496
Chris Cummins <chrisc.101@gmail.com>
2019-10-14 02:04:39 +0200

    Set url to labm8.

--- labm8/app.py
+++ labm8/app.py
@@ -81,7 +81,7 @@ def GetVersionInformationString() -> str:
   except ModuleNotFoundError:
     import pkg_resources
     version = f'version: {pkg_resources.get_distribution("labm8").version}'
-    url = 'https://github.com/ChrisCummins/phd'
+    url = 'https://github.com/ChrisCummins/labm8'
   return '\n'.join([
     version,
     'Copyright (C) 2014-2019 Chris Cummins <chrisc.101@gmail.com>',
