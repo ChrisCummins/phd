3b4b958f445a9c8873a3c02c1af68c9a86979f21
Chris Cummins <chrisc.101@gmail.com>
2019-10-16 01:08:33 +0200

    Add CFG-only dataset generator.

--- experimental/compilers/reachability/ggnn/make_reachability_dataset.py
+++ experimental/compilers/reachability/ggnn/make_reachability_dataset.py
@@ -1,6 +1,7 @@
 """This module prepares a dataset for learning reachability analysis from a
 database of control flow graph protocol buffers.
 """
+
 import multiprocessing
 
 import collections
@@ -14,9 +15,12 @@ import typing
 from experimental.compilers.reachability import \
   control_and_data_flow_graph as cdfg
 from experimental.compilers.reachability import database
+from experimental.compilers.reachability import llvm_util
+from experimental.compilers.reachability import reachability_pb2
 from labm8 import app
 from labm8 import humanize
 from labm8 import labtypes
+from labm8 import pbutil
 from labm8 import prof
 
 
@@ -30,7 +34,7 @@ app.DEFINE_output_path(
     '/var/phd/experimental/compilers/reachability/ggnn/dataset',
     'Path to generate GGNN dataset in.',
     is_dir=True)
-app.DEFINE_string('reachability_dataset_type', 'poj-104',
+app.DEFINE_string('reachability_dataset_type', 'poj104_cfg_only',
                   'The type of dataset to export.')
 app.DEFINE_integer(
     'reachability_dataset_max_train_bytecodes', 0,
@@ -141,62 +145,9 @@ def MakeReachabilityAnnotatedGraphs(g: nx.MultiDiGraph,
     yield reachable
 
 
-def BytecodeIdToJob(
-    s: database.Database.SessionType,
-    bytecode_id: id) -> typing.Tuple[str, str, str, str, int]:
-
-  def GetConstantColumn(rows, column_idx, column_name):
-    values = {r[column_idx] for r in rows}
-    if len(values) != 1:
-      raise ValueError(f'Bytecode ID {bytecode_id} should have the same '
-                       f'{column_name} value across its {len(rows)} CFGs, '
-                       f'found these values: `{values}`')
-    return list(values)[0]
-
-  q = s.query(database.LlvmBytecode.bytecode,
-              database.LlvmBytecode.source_name,
-              database.LlvmBytecode.relpath,
-              database.LlvmBytecode.language) \
-    .filter(database.LlvmBytecode.it == bytecode_id).one()
-  bytecode, source, relpath, language = q
-  return bytecode, source, relpath, language, bytecode_id
-
-
-def BytecodeJobToDictionaries(
-    packed_args: typing.Tuple[typing.List[str], str, str, str, int]
-) -> typing.List[typing.Dict[str, typing.Any]]:
-  """
-
-  Args:
-    packed_args: A packed arguments tuple consisting of a list serialized,
-     protos, the source name, the relpath of the bytecode, and the bytecode ID.
-
-  Returns:
-    A list of reachability-annotated dictionaries.
-  """
-  bytecode, source_name, relpath, language, bytecode_id = packed_args
-  builder = cdfg.ControlAndDataFlowGraphBuilder(
-      dataflow='edges_only',
-      preprocess_text=True,
-      discard_unknown_statements=False,
-  )
-
-  try:
-    graph = builder.Build(bytecode)
-    graph.name = f'{source_name}:{relpath}'
-    graph.bytecode_id = str(bytecode_id)
-    graph.language = language
-    annotated_graphs = MakeReachabilityAnnotatedGraphs(
-        graph, FLAGS.reachability_dataset_max_instances_per_graph)
-    return [AnnotatedGraphToDictionary(graph) for graph in annotated_graphs]
-  except Exception as e:
-    app.Error('Failed to create graph for bytecode %d: %s (%s)', bytecode_id, e,
-              type(e).__name__)
-    return []
-
-
 def ExportBytecodeIdsToFileFragments(
     db: database.Database,
+    make_job_cb, process_job_cb,
     bytecode_ids: typing.List[int],
     outpath: pathlib.Path,
     pool: typing.Optional[multiprocessing.Pool] = None
@@ -206,7 +157,6 @@ def ExportBytecodeIdsToFileFragments(
   data = []
 
   def EmitFragment(start_idx, end_idx) -> str:
-    end_idx = i * FLAGS.reachability_dataset_bytecode_batch_size + len(chunk)
     fragment_path = pathlib.Path(str(outpath) + f'.{start_idx}_{end_idx}')
     with prof.Profile(
         f'Wrote {humanize.Commas(len(data))} graphs to {fragment_path}'):
@@ -228,11 +178,11 @@ def ExportBytecodeIdsToFileFragments(
                len(bytecode_ids)) * 100)
       # Run the database queries from the master thread.
       with db.Session() as s:
-        jobs = [BytecodeIdToJob(s, bytecode_id) for bytecode_id in chunk]
+        jobs = [make_job_cb(s, bytecode_id) for bytecode_id in chunk]
 
       # Process database query results in parallel.
       for dicts in pool.imap_unordered(
-          BytecodeJobToDictionaries,
+          process_job_cb,
           jobs,
           chunksize=max(FLAGS.reachability_dataset_bytecode_batch_size // 16,
                         1)):
@@ -247,6 +197,65 @@ def ExportBytecodeIdsToFileFragments(
 
     if data:
       end_idx = i * FLAGS.reachability_dataset_bytecode_batch_size + len(chunk)
+      fragment_paths.append(EmitFragment(start_idx, end_idx))
+
+  return fragment_paths
+
+
+def BuildAndRunJobsOnBytecodeIds(
+    db: database.Database,
+    bytecode_ids: typing.List[int],
+    outpath: pathlib.Path,
+    make_job_cb,
+    process_job_cb,
+    pool: typing.Optional[multiprocessing.Pool] = None
+) -> typing.List[pathlib.Path]:
+  pool = pool or multiprocessing.Pool()
+  fragment_paths = []
+  data = []
+
+  def EmitFragment(start_idx, end_idx) -> str:
+    end_idx = i * FLAGS.reachability_dataset_bytecode_batch_size + len(chunk)
+    fragment_path = pathlib.Path(str(outpath) + f'.{start_idx}_{end_idx}')
+    with prof.Profile(
+        f'Wrote {humanize.Commas(len(data))} graphs to {fragment_path}'):
+      with open(fragment_path, 'wb') as f:
+        pickle.dump(data, f)
+    return fragment_path
+
+  with prof.Profile(lambda t: f'Processed {len(bytecode_ids)} bytecodes '
+                              f'({len(bytecode_ids) / t:.2f} bytecode / s)'):
+    start_idx = 0
+    for i, chunk in enumerate(
+        labtypes.Chunkify(bytecode_ids,
+                          FLAGS.reachability_dataset_bytecode_batch_size)):
+      app.Log(1, 'Processing %s-%s of %s bytecodes (%.2f%%)',
+              i * FLAGS.reachability_dataset_bytecode_batch_size,
+              i * FLAGS.reachability_dataset_bytecode_batch_size + len(chunk),
+              humanize.Commas(len(bytecode_ids)),
+              ((i * FLAGS.reachability_dataset_bytecode_batch_size) /
+               len(bytecode_ids)) * 100)
+      # Run the database queries from the master thread.
+      with db.Session() as s:
+        jobs = [make_job_cb(s, bytecode_id) for bytecode_id in chunk]
+
+      # Process database query results in parallel.
+      for dicts in pool.imap_unordered(
+          process_job_cb,
+          jobs,
+          chunksize=max(FLAGS.reachability_dataset_bytecode_batch_size // 16,
+                        1)):
+        data += dicts
+
+      if len(data) >= FLAGS.reachability_dataset_file_fragment_size:
+        end_idx = i * FLAGS.reachability_dataset_bytecode_batch_size + len(
+            chunk)
+        fragment_paths.append(EmitFragment(start_idx, end_idx))
+        start_idx = end_idx
+        data = []
+
+    if data:
+      end_idx = len(bytecode_ids)
       fragment_paths.append(EmitFragment(start_idx, end_idx))
 
   return fragment_paths
@@ -279,12 +288,139 @@ def GetPoj104BytecodeIds(
   ]
 
 
-def ExportDataset(db: database.Database, train_ids: typing.List[int],
+def ExportDataset(db: database.Database,
+                  make_job_cb, process_job_cb,
+                  train_ids: typing.List[int],
                   val_ids: typing.List[int], test_ids: typing.List[int],
                   outdir: pathlib.Path):
-  ExportBytecodeIdsToFileFragments(db, train_ids, outdir / 'train.pickle')
-  ExportBytecodeIdsToFileFragments(db, val_ids, outdir / 'val.pickle')
-  ExportBytecodeIdsToFileFragments(db, test_ids, outdir / 'test.pickle')
+  ExportBytecodeIdsToFileFragments(db, make_job_cb, process_job_cb, train_ids, outdir / 'train.pickle')
+  ExportBytecodeIdsToFileFragments(db, make_job_cb, process_job_cb, val_ids, outdir / 'val.pickle')
+  ExportBytecodeIdsToFileFragments(db, make_job_cb, process_job_cb, test_ids, outdir / 'test.pickle')
+
+
+def MakePoj104CfgOnlyJob(
+    s: database.Database.SessionType,
+    bytecode_id: id) -> typing.Tuple[typing.List[str], str, str, str, int]:
+
+  def GetConstantColumn(rows, column_idx, column_name):
+    values = {r[column_idx] for r in rows}
+    if len(values) != 1:
+      raise ValueError(f'Bytecode ID {bytecode_id} should have the same '
+                       f'{column_name} value across its {len(rows)} CFGs, '
+                       f'found these values: `{values}`')
+    return list(values)[0]
+
+  q = s.query(database.ControlFlowGraphProto.proto,
+              database.LlvmBytecode.source_name,
+              database.LlvmBytecode.relpath,
+              database.LlvmBytecode.language) \
+    .join(database.LlvmBytecode) \
+    .filter(database.ControlFlowGraphProto.bytecode_id == bytecode_id) \
+    .filter(database.ControlFlowGraphProto.status == 0).all()
+  # A bytecode may have failed to produce any CFGs.
+  if not q:
+    app.Error('Bytecode %s has no CFGs', bytecode_id)
+    return None, None, None, None, None
+  proto_strings = [r[0] for r in q]
+  source = GetConstantColumn(q, 1, 'source')
+  relpath = GetConstantColumn(q, 2, 'relpath')
+  language = GetConstantColumn(q, 3, 'language')
+  return proto_strings, source, relpath, language, bytecode_id
+
+def ProcessPoj104CfgOnlyJob(
+    packed_args: typing.Tuple[typing.List[str], str, str, str, int]
+) -> typing.List[typing.Dict[str, typing.Any]]:
+  """
+  Args:
+    packed_args: A packed arguments tuple consisting of a list serialized,
+     protos, the source name, the relpath of the bytecode, and the bytecode ID.
+  Returns:
+    A list of reachability-annotated dictionaries.
+  """
+  proto_strings, source_name, relpath, language, bytecode_id = packed_args
+  if not proto_strings:
+    return []
+  builder = cdfg.ControlAndDataFlowGraphBuilder(
+      dataflow='none',
+      preprocess_text=False,
+      discard_unknown_statements=False,
+  )
+
+  try:
+    # Create CFGs from the serialized protos.
+    cfgs = []
+    proto = reachability_pb2.ControlFlowGraph()
+    for proto_string in proto_strings:
+      proto.Clear()
+      pbutil.FromString(proto_string, proto)
+      cfgs.append(llvm_util.LlvmControlFlowGraph.FromProto(proto))
+
+    graphs = [builder.BuildFromControlFlowGraph(cfg) for cfg in cfgs]
+    annotated_graphs = []
+    for graph in graphs:
+      graph.name = f'{source_name}:{relpath}'
+      graph.bytecode_id = str(bytecode_id)
+      graph.language = language
+      annotated_graphs += MakeReachabilityAnnotatedGraphs(
+          graph, FLAGS.reachability_dataset_max_instances_per_graph)
+    return [AnnotatedGraphToDictionary(graph) for graph in annotated_graphs]
+  except Exception as e:
+    app.Error('Failed to create CDFG for bytecode %d: %s (%s)', bytecode_id, e,
+              type(e).__name__)
+    return []
+
+def MakePoj104Job(
+    s: database.Database.SessionType,
+    bytecode_id: id) -> typing.Tuple[str, str, str, str, int]:
+
+  def GetConstantColumn(rows, column_idx, column_name):
+    values = {r[column_idx] for r in rows}
+    if len(values) != 1:
+      raise ValueError(f'Bytecode ID {bytecode_id} should have the same '
+                       f'{column_name} value across its {len(rows)} CFGs, '
+                       f'found these values: `{values}`')
+    return list(values)[0]
+
+  q = s.query(database.LlvmBytecode.bytecode,
+              database.LlvmBytecode.source_name,
+              database.LlvmBytecode.relpath,
+              database.LlvmBytecode.language) \
+    .filter(database.LlvmBytecode.it == bytecode_id).one()
+  bytecode, source, relpath, language = q
+  return bytecode, source, relpath, language, bytecode_id
+
+
+def ProcessPoj104Job(
+    packed_args: typing.Tuple[str, str, str, str, int]
+) -> typing.List[typing.Dict[str, typing.Any]]:
+  """
+
+  Args:
+    packed_args: A packed arguments tuple consisting of a list serialized,
+     protos, the source name, the relpath of the bytecode, and the bytecode ID.
+
+  Returns:
+    A list of reachability-annotated dictionaries.
+  """
+  bytecode, source_name, relpath, language, bytecode_id = packed_args
+  builder = cdfg.ControlAndDataFlowGraphBuilder(
+      dataflow='edges_only',
+      preprocess_text=True,
+      discard_unknown_statements=False,
+  )
+
+  try:
+    graph = builder.Build(bytecode)
+    graph.name = f'{source_name}:{relpath}'
+    graph.bytecode_id = str(bytecode_id)
+    graph.language = language
+    annotated_graphs = MakeReachabilityAnnotatedGraphs(
+        graph, FLAGS.reachability_dataset_max_instances_per_graph)
+    return [AnnotatedGraphToDictionary(graph) for graph in annotated_graphs]
+  except Exception as e:
+    app.Error('Failed to create graph for bytecode %d: %s (%s)', bytecode_id, e,
+              type(e).__name__)
+    return []
 
 
 def main():
@@ -302,13 +438,17 @@ def main():
   outdir.mkdir(exist_ok=True, parents=True)
 
   with prof.Profile('Read bytecode IDs from database'):
-    if FLAGS.reachability_dataset_type == 'poj-104':
+    if FLAGS.reachability_dataset_type == 'poj104-cfg':
       train, test, val = GetPoj104BytecodeIds(db)
+      make_job, process_job = MakePoj104CfgOnlyJob, ProcessPoj104CfgOnlyJob
+    elif FLAGS.reachability_dataset_type == 'poj104':
+      train, test, val = GetPoj104BytecodeIds(db)
+      make_job, process_job = MakePoj104Job, ProcessPoj104Job
     else:
       raise app.UsageError('Unknown value for --reachability_dataset_type')
 
   with prof.Profile('Exported dataset files'):
-    ExportDataset(db, train, test, val, outdir)
+    ExportDataset(db, make_job, process_job, train, test, val, outdir)
 
 
 if __name__ == '__main__':
