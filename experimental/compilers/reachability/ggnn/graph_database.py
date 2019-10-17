4b5fed6821a657fefbf1583fcd37c6791fa3cd73
Chris Cummins <chrisc.101@gmail.com>
2019-10-17 14:08:03 +0200

    Add a database of graphs.

---
+++ experimental/compilers/reachability/ggnn/graph_database.py
@@ -0,0 +1,85 @@
+"""Database backend for GGNN graphs."""
+import datetime
+import sqlalchemy as sql
+from sqlalchemy.dialects import mysql
+from sqlalchemy.ext import declarative
+
+from labm8 import app
+from labm8 import labdate
+from labm8 import sqlutil
+
+
+FLAGS = app.FLAGS
+
+Base = declarative.declarative_base()
+
+
+class Meta(Base, sqlutil.TablenameFromClassNameMixin):
+  """Key-value database metadata store."""
+  key: str = sql.Column(sql.String(64), primary_key=True)
+  value: str = sql.Column(sql.String(64), nullable=False)
+
+
+class GraphMeta(Base, sqlutil.PluralTablenameFromCamelCapsClassNameMixin):
+  """A table of graph metadata.
+
+  For every GraphMeta, there should be a corresponding Graph row containing the
+  actual data.
+  """
+  id: int = sql.Column(sql.Integer, primary_key=True)
+
+  # No foreign key relationship because they are separate databases.
+  bytecode_id: int = sql.Column(sql.Integer, nullable=False)
+
+  # The source of the graph. This is duplicates of information stored in the
+  # LlvmBytecode table.
+  source_name: str = sql.Column(sql.String(256), nullable=False)
+  relpath: str = sql.Column(sql.String(256), nullable=False)
+  language: str = sql.Column(sql.String(16), nullable=False)
+
+  node_count: int = sql.Column(sql.Integer, nullable=False)
+  edge_count: int = sql.Column(sql.Integer, nullable=False)
+  node_type_count: int = sql.Column(sql.Integer, default=1, nullable=False)
+  edge_type_count: int = sql.Column(sql.Integer, default=1, nullable=False)
+
+  node_features_dimensionality: int = sql.Column(sql.Integer,default=0, nullable=False)
+  edge_features_dimensionality: int = sql.Column(sql.Integer, default=0, nullable=False)
+  graph_features_dimensionality: int = sql.Column(sql.Integer, default=0, nullable=False)
+
+  node_labels_dimensionality: int = sql.Column(sql.Integer, default=0, nullable=False)
+  edge_labels_dimensionality: int = sql.Column(sql.Integer, default=0, nullable=False)
+  graph_labels_dimensionality: int = sql.Column(sql.Integer, default=0, nullable=False)
+
+  # The minimum number of message passing steps that are be required to produce
+  # the labels from the features. E.g. for graph flooding problems, this value
+  # will be the diameter of the graph.
+  data_flow_max_steps_required: int = sql.Column(
+      sql.Integer, default=0, nullable=False)
+
+  date_added: datetime.datetime = sql.Column(
+      sql.DateTime().with_variant(mysql.DATETIME(fsp=3), 'mysql'),
+      nullable=False,
+      default=labdate.GetUtcMillisecondsNow)
+
+  graph: 'Graph' = sql.orm.relationship(
+      'Graph', uselist=False, back_populates="meta")
+
+
+class Graph(Base, sqlutil.PluralTablenameFromCamelCapsClassNameMixin):
+  """The data for a graph.
+
+  This is an opaque byte array that can be used as needed, e.g. for pickled
+  dictionaries, networkx graphs, etc.
+  """
+  id: int = sql.Column(sql.Integer, sql.ForeignKey('graph_metas.id'),
+                       primary_key=True)
+  data: bytes = sql.Column(
+      sql.LargeBinary().with_variant(sql.LargeBinary(2**31), 'mysql'),
+      nullable=False)
+  meta: GraphMeta = sql.orm.relationship('GraphMeta', back_populates="graph")
+
+
+class Database(sqlutil.Database):
+
+  def __init__(self, url: str, must_exist: bool = False):
+    super(Database, self).__init__(url, Base, must_exist=must_exist)
---
+++ experimental/compilers/reachability/ggnn/graph_database_test.py
@@ -0,0 +1,73 @@
+"""Unit tests for //experimental/compilers/reachability/ggnn:graph_database."""
+import networkx as nx
+import pathlib
+import pickle
+import pytest
+
+from experimental.compilers.reachability.ggnn import graph_database
+from labm8 import app
+from labm8 import test
+
+
+FLAGS = app.FLAGS
+
+@pytest.fixture(scope='function')
+def db(tempdir: pathlib.Path) -> graph_database.Database:
+  yield graph_database.Database(f'sqlite:///{tempdir}/db')
+
+
+def test_Graph_pickled_networkx_graph(db: graph_database.Database):
+  """Test saving and loading graph with a pickled networkx graph of data."""
+  g = nx.MultiDiGraph()
+  g.add_edge('A', 'B')
+  g.add_edge('B', 'C')
+
+  with db.Session(commit=True) as s:
+    s.add(graph_database.GraphMeta(
+        bytecode_id = 1,
+        source_name = 'foo',
+        relpath = 'bar',
+        language = 'c',
+        node_count = g.number_of_nodes(),
+        edge_count = g.number_of_edges(),
+        graph=graph_database.Graph(data=pickle.dumps(g))))
+  with db.Session() as s:
+    gm = s.query(graph_database.GraphMeta).first()
+    assert gm.bytecode_id == 1
+    assert gm.source_name == 'foo'
+    assert gm.relpath == 'bar'
+    assert gm.language == 'c'
+    assert gm.node_count == 3
+    assert gm.edge_count == 2
+
+    g = pickle.loads(gm.graph.data)
+    assert g.number_of_nodes() == 3
+    assert g.number_of_edges() == 2
+
+    assert gm.id == gm.graph.id
+
+
+def test_Graph_pickled_dictionary(db: graph_database.Database):
+  """Test saving and loading graph with a pickled dictionary of data."""
+  with db.Session(commit=True) as s:
+    s.add(graph_database.GraphMeta(
+        bytecode_id = 1,
+        source_name = 'foo',
+        relpath = 'bar',
+        language = 'c',
+        node_count = 1,
+        edge_count = 2,
+        graph=graph_database.Graph(data=pickle.dumps({"a": 1, "b": 2}))))
+  with db.Session() as s:
+    gm = s.query(graph_database.GraphMeta).first()
+    assert gm.bytecode_id == 1
+
+    d = pickle.loads(gm.graph.data)
+    assert d["a"] == 1
+    assert d["b"] == 2
+
+    assert gm.id == gm.graph.id
+
+
+if __name__ == '__main__':
+  test.Main()
