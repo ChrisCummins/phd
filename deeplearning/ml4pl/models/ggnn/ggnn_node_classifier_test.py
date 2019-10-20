9e90ddc9c23f75586ddd6ba29c6d7827b889f9d1
Chris Cummins <chrisc.101@gmail.com>
2019-10-20 13:15:43 +0200

    Update test data representation.

--- deeplearning/ml4pl/models/ggnn/ggnn_node_classifier_test.py
+++ deeplearning/ml4pl/models/ggnn/ggnn_node_classifier_test.py
@@ -8,100 +8,108 @@ from deeplearning.ml4pl.models.ggnn import ggnn_node_classifier
 from labm8 import app
 from labm8 import test
 
+
 FLAGS = app.FLAGS
 
 
 @pytest.fixture(scope='function')
-def db_3(tempdir: pathlib.Path) -> graph_database.Database:
-  """A test fixture that yields a database with 3 rows.
+def db(tempdir: pathlib.Path) -> graph_database.Database:
+  """A test fixture that yields a database with 3 rows."""
+  return graph_database.Database(f'sqlite:///{tempdir}/db')
 
-  One GraphMeta and Graph each for train/val/test.
+
+def test_Train_2d_node_features_2d_node_labels(db: graph_database.Database):
+  """Test training a model with a graph dataset with 2D node features and
+  labels and a single edge type.
+
+  This is similar to the type of dataset that would be used for learning control
+  flow reachability.
   """
-  db = graph_database.Database(f'sqlite:///{tempdir}/db')
+  FLAGS.num_epochs = 2
+
   with db.Session(commit=True) as session:
     session.add_all([
-        graph_database.GraphMeta(
-            group="train",
-            bytecode_id=0,
-            source_name="source",
-            relpath="relpath",
-            language="c",
-            node_count=3,
-            edge_count=2,
-            node_features_dimensionality=5,
-            node_labels_dimensionality=3,
-            data_flow_max_steps_required=2,
-            graph=graph_database.Graph(
-                data=pickle.dumps({
-                    'adjacency_list': [(0, 1), (1, 2)],
-                    'node_features': [
-                        (1, 0, 0, 0, 0),
-                        (0, 1, 0, 0, 0),
-                        (0, 0, 1, 0, 0),
-                    ],
-                    'targets': [
-                        (1, 0, 0),
-                        (0, 1, 0),
-                        (0, 0, 1),
-                    ],
-                }))),
-        graph_database.GraphMeta(
-            group="val",
-            bytecode_id=0,
-            source_name="source",
-            relpath="relpath",
-            language="c",
-            node_count=3,
-            edge_count=2,
-            node_features_dimensionality=5,
-            node_labels_dimensionality=3,
-            data_flow_max_steps_required=2,
-            graph=graph_database.Graph(
-                data=pickle.dumps({
-                    'adjacency_list': [(0, 1), (1, 2)],
-                    'node_features': [
-                        (1, 0, 0, 0, 0),
-                        (0, 1, 0, 0, 0),
-                        (0, 0, 1, 0, 0),
-                    ],
-                    'targets': [
-                        (1, 0, 0),
-                        (0, 1, 0),
-                        (0, 0, 1),
-                    ],
-                }))),
-        graph_database.GraphMeta(
-            group="test",
-            bytecode_id=0,
-            source_name="source",
-            relpath="relpath",
-            language="c",
-            node_count=3,
-            edge_count=2,
-            node_features_dimensionality=5,
-            node_labels_dimensionality=3,
-            data_flow_max_steps_required=2,
-            graph=graph_database.Graph(
-                data=pickle.dumps({
-                    'adjacency_list': [(0, 1), (1, 2)],
-                    'node_features': [
-                        (1, 0, 0, 0, 0),
-                        (0, 1, 0, 0, 0),
-                        (0, 0, 1, 0, 0),
-                    ],
-                    'targets': [
-                        (1, 0, 0),
-                        (0, 1, 0),
-                        (0, 0, 1),
-                    ],
-                }))),
+      graph_database.GraphMeta(
+          group="train",
+          bytecode_id=0,
+          source_name="source",
+          relpath="relpath",
+          language="c",
+          edge_type_count=1,
+          node_count=3,
+          edge_count=2,
+          node_features_dimensionality=2,
+          node_labels_dimensionality=2,
+          data_flow_max_steps_required=2,
+          graph=graph_database.Graph(
+              data=pickle.dumps({
+                'adjacency_lists': [[(0, 1), (1, 2)]],
+                'node_x': [
+                  (1, 0),
+                  (0, 1),
+                  (0, 0),
+                ],
+                'node_y': [
+                  (1, 0),
+                  (0, 1),
+                  (0, 0),
+                ],
+              }))),
+      graph_database.GraphMeta(
+          group="val",
+          bytecode_id=0,
+          source_name="source",
+          relpath="relpath",
+          language="c",
+          edge_type_count=1,
+          node_count=3,
+          edge_count=2,
+          node_features_dimensionality=2,
+          node_labels_dimensionality=2,
+          data_flow_max_steps_required=2,
+          graph=graph_database.Graph(
+              data=pickle.dumps({
+                'adjacency_lists': [[(0, 1), (1, 2)]],
+                'node_x': [
+                  (1, 0),
+                  (0, 1),
+                  (0, 0),
+                ],
+                'node_y': [
+                  (1, 0),
+                  (0, 1),
+                  (0, 0),
+                ],
+              }))),
+      graph_database.GraphMeta(
+          group="test",
+          bytecode_id=0,
+          source_name="source",
+          relpath="relpath",
+          language="c",
+          edge_type_count=1,
+          node_count=3,
+          edge_count=2,
+          node_features_dimensionality=2,
+          node_labels_dimensionality=2,
+          data_flow_max_steps_required=2,
+          graph=graph_database.Graph(
+              data=pickle.dumps({
+                'adjacency_lists': [[(0, 1), (1, 2)]],
+                'node_x': [
+                  (1, 0),
+                  (0, 1),
+                  (0, 0),
+                ],
+                'node_y': [
+                  (1, 0),
+                  (0, 1),
+                  (0, 0),
+                ],
+              }))),
     ])
-  return db
 
-
-def test_Train(db_3: graph_database.Database):
-  FLAGS.num_epochs = 2
-  model = ggnn_node_classifier.GgnnNodeClassifierModel(db_3)
+  model = ggnn_node_classifier.GgnnNodeClassifierModel(db)
   model.Train()
 
 
