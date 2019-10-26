d57f51fcbfb0b8f474ba9f68741362f722f5ea1b
Chris Cummins <chrisc.101@gmail.com>
2019-10-26 04:02:55 +0200

    Update SQL file.

--- deeplearning/ml4pl/ml4pl.sql
+++ deeplearning/ml4pl/ml4pl.sql
@@ -1,3 +1,12 @@
+-- How many bytecodes do we have, and where have they come from?
+
+SELECT `reachability`.`llvm_bytecode`.source_name AS SOURCE,
+       count(*) AS COUNT
+FROM `reachability`.`llvm_bytecode`
+WHERE clang_returncode = 0
+GROUP BY `reachability`.`llvm_bytecode`.source_name
+ORDER BY COUNT DESC;
+
 -- How many CFGs do we have, and where have they come from?
 
 SELECT `reachability`.`llvm_bytecode`.source_name AS SOURCE,
@@ -7,4 +16,27 @@ LEFT JOIN `reachability`.`llvm_bytecode` ON bytecode_id=`reachability`.`llvm_byt
 WHERE status = 0
 GROUP BY `reachability`.`llvm_bytecode`.source_name
 ORDER BY COUNT DESC;
+
+-- Labelled graphs database overview.
+
+SELECT graphs.source_name AS SOURCE,
+       graphs.language,
+       graphs.group,
+       count(*) AS COUNT
+FROM ml4pl_reachability_cfg_all.graph_metas AS graphs
+GROUP BY graphs.source_name,
+         graphs.group,
+         graphs.language
+ORDER BY graphs.group,
+         COUNT DESC;
+
+
+SELECT graphs.language,
+       graphs.group,
+       count(*) AS COUNT
+FROM ml4pl_reachability_cfg_all.graph_metas AS graphs
+GROUP BY graphs.group,
+         graphs.language
+ORDER BY graphs.group,
+         COUNT DESC;
 
