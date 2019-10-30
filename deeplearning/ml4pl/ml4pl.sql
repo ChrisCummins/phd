e2a09405d845e621eebee3e25750585fd613ea4e
Chris Cummins <chrisc.101@gmail.com>
2019-10-30 17:05:58 +0100

    Update SQL queries.

--- deeplearning/ml4pl/ml4pl.sql
+++ deeplearning/ml4pl/ml4pl.sql
@@ -18,6 +18,47 @@ WHERE status=0
 GROUP BY source_name,
          LANGUAGE;
 
+-- Agregate stats about the graph database.
+
+SELECT count(*)
+FROM ml4pl_graphs.graph_metas;
+
+
+SELECT source_name,
+       `language`,
+       `group`,
+       count(*) AS graph_count,
+       sum(node_count) AS node_count,
+       sum(edge_count) AS edge_count,
+       round(avg(node_count)) AS avg_node_count,
+       round(avg(edge_count)) AS avg_edge_count
+FROM ml4pl_graphs.graph_metas
+GROUP BY source_name,
+         `language`,
+         `group`
+ORDER BY source_name,
+         `language`,
+         `group`;
+
+-- Compare file and graph counts, indicating how many failed.
+
+SELECT graphs.source_name,
+       graph_count,
+       bytecode_count,
+       bytecode_count - graph_count AS 'failed',
+       (graph_count / bytecode_count) * 100 AS 'ratio'
+FROM
+  (SELECT source_name,
+          count(*) AS graph_count
+   FROM ml4pl_graphs.graph_metas
+   GROUP BY source_name) AS graphs
+LEFT JOIN
+  (SELECT source_name,
+          count(*) AS bytecode_count
+   FROM reachability.llvm_bytecode
+   GROUP BY source_name) AS bytecodes ON graphs.source_name = bytecodes.source_name
+ORDER BY ratio ASC;
+
 -- How many CFGs do we have, and where have they come from?
 
 SELECT `reachability`.`llvm_bytecode`.source_name AS SOURCE,
