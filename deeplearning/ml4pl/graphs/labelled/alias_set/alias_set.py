1d2f0265dfe9d194c4f20c03b12fb68735980e21
Chris Cummins <chrisc.101@gmail.com>
2019-11-06 13:34:35 +0100

    Implement alias set construction.

--- deeplearning/ml4pl/graphs/labelled/alias_set/alias_set.py
+++ deeplearning/ml4pl/graphs/labelled/alias_set/alias_set.py
@@ -3,6 +3,7 @@ import random
 import typing
 
 import networkx as nx
+import numpy as np
 from labm8 import app
 from labm8 import decorators
 
@@ -11,79 +12,136 @@ from compilers.llvm import opt_util
 FLAGS = app.FLAGS
 
 app.DEFINE_integer(
-    'max_instances_per_alias_set', 3,
-    'The maximum number of instances to produce for a single '
-    'alias set. Multiple instances are produced by changing '
-    'the root identifier.')
+    'alias_set_min_size', 2,
+    'The minimum number of pointers in an alias set to be used as a labelled '
+    'example.')
 
 
 @decorators.timeout(seconds=120)
 def AnnotateAliasSet(g: nx.MultiDiGraph,
-                     alias_set: opt_util.AliasSet,
                      root_identifier: str,
+                     identifiers_in_set: typing.List[str],
                      x_label: str = 'x',
                      y_label: str = 'y',
-                     true: typing.Any = True,
-                     false: typing.Any = False) -> int:
-  if root_identifier not in alias_set:
-    raise ValueError(f"Root identifier {root_identifier} not in alias set")
-  if root_identifier not in g.nodes:
-    raise ValueError(f"Root identifier {root_identifier} not in graph")
+                     false=False,
+                     true=True) -> int:
+  """
 
-  for _, data in g.nodes():
-    data[x_label] = false
+  Args:
+    g: The graph.
+    root_identifier: A name of a node in the alias set.
+    identifiers_in_set: The names of the nodes in the alias set.
+    x_label: The graph 'x' attribute property attribute name.
+    y_label: The graph 'y' attribute property attribute name.
+    false: The value to set for nodes not in the alias set.
+    true: The value to set for nodes in the alias set.
+
+  Returns:
+    The number of identifiers in the alias set.
+  """
+  # Set all of the nodes as not the root identifier and not part of the alias
+  # set.
+  for _, data in g.nodes(data=True):
+    data[x_label] = 0
     data[y_label] = false
+  g.nodes[root_identifier][x_label] = 1
 
-  g.nodes[root_identifier][x_label] = true
+  # Mark the nodes in the alias set.
+  for pointer in identifiers_in_set:
+    if pointer not in g.nodes:
+      identifier_nodes = [
+          node for node, type_ in g.nodes(data='type') if type_ == 'identifier'
+      ]
+      raise ValueError(f"Pointer `{pointer}` not in function with identifiers "
+                       f"{identifier_nodes}")
+    g.nodes[pointer][y_label] = true
 
-  for pointer in alias_set.pointers:
-    if pointer.identifier not in g.nodes:
-      raise ValueError(f"Identifier `{pointer.identifier}` from alias set not "
-                       "found in graph")
-    g.nodes[pointer.identifier][y_label] = true
-
-  return len(alias_set.pointers)
+  return len(identifiers_in_set)
 
 
 def MakeAliasSetGraphs(
     g: nx.MultiDiGraph,
-    alias_sets: typing.List[opt_util.AliasSet],
+    bytecode: str,
     n: typing.Optional[int] = None,
     false=False,
     true=True,
 ) -> typing.Iterable[nx.MultiDiGraph]:
-  """Produce up to `n` dependency graphs from the given unlabelled graph.
+  """Produce up to `n` alias set graphs.
 
   Args:
     g: The unlabelled input graph.
-    alias_sets: A list of alias sets for this graph.
+    bytecode: The bytecode which produced the input graph.
     n: The maximum number of graphs to produce. Multiple graphs are produced by
-      selecting different root nodes for creating labels. If `n` is provided,
+      selecting different root pointers for alias sets. If `n` is provided,
       the number of graphs generated will be in the range
-      1 <= x <= min(num_statements, n). Else, the number of graphs will be equal
-      to num_statements (i.e. one graph for each statement in the input graph).
-    false: The value to set for binary false values on X and Y labels.
-    true: The value to set for binary true values on X and Y labels.
+      1 <= x <= min(num_alias_sets, n), where num_alias_sets is the number of
+      alias sets larger than --alias_set_min_size. If n is None, num_alias_sets
+      graphs will be produced.
+    false: TODO(cec): Unused. This method is hardcoded to use 3-class 1-hots.
+    true: TODO(cec): Unused. This method is hardcoded to use 3-class 1-hots.
 
   Returns:
     A generator of annotated graphs, where each graph has 'x' and 'y' labels on
-    the statement nodes, and additionally 'dominated_node_count' and
-    'data_flow_max_steps_required' attributes.
+    the statement nodes, and additionally a 'data_flow_max_steps_required'
+    attribute which is set to the number of pointers in the alias set.
   """
-  alias_sets = [s for s in alias_sets if len(s.pointers) > 1]
-  if len(alias_sets) > n:
-    random.shuffle(alias_sets)
+  # TODO(cec): Replace true/false args with a list of class values for all
+  # graph annotator functions.
+  del false
+  del true
 
-  for alias_set in alias_sets[:n]:
-    root_pointers = alias_set.pointers
-    if len(root_pointers) > FLAGS.max_instances_per_alias_set:
-      random.shuffle(root_pointers)
+  # Build the alias sets for the given bytecode.
+  alias_sets_by_function = opt_util.GetAliasSetsByFunction(bytecode)
 
-    for root_pointer in root_pointers[:FLAGS.max_instances_per_alias_set]:
-      labelled = g.copy()
-      labelled.alias_set_size = (AnnotateAliasSet(labelled,
-                                                  alias_set,
-                                                  root_pointer,
-                                                  false=false,
-                                                  true=true))
-      yield labelled
+  functions = {
+      function for node, function in g.nodes(data='function')
+      # Not all nodes have a 'function' attribute, e.g. the magic root node.
+      if function
+  }
+  app.Log(1, '%s', alias_sets_by_function)
+  if functions != alias_sets_by_function.keys():
+    raise ValueError(f"Expected functions `{functions}`, found "
+                     f"`{set(alias_sets_by_function.keys())}`")
+
+  function_alias_set_pairs: typing.List[
+      typing.Tuple[str, opt_util.AliasSet]] = []
+  # Flatten the alias set dictionary and ignore any alias sets that are smaller
+  # than the threshold size.
+  for function, alias_sets in alias_sets_by_function.items():
+    function_alias_set_pairs += [
+        (function, alias_set)
+        for alias_set in alias_sets
+        if len(alias_set.pointers) >= FLAGS.alias_set_min_size
+    ]
+
+  # Select `n` random alias sets to generate labelled graphs for.
+  if n and len(function_alias_set_pairs) > n:
+    random.shuffle(function_alias_set_pairs)
+    function_alias_set_pairs = function_alias_set_pairs[:n]
+
+  for function, alias_set in function_alias_set_pairs:
+    # Translate the must/may alias property into 3-class 1-hot labels.
+    if alias_set.type == 'may alias':
+      false = np.array([1, 0, 0], np.int32)
+      true = np.array([0, 1, 0], np.int32)
+    elif alias_set.type == 'must alias':
+      false = np.array([1, 0, 0], np.int32)
+      true = np.array([0, 0, 1], np.int32)
+    else:
+      raise ValueError(f"Unknown alias set type `{alias_set.type}`")
+
+    # Transform pointer name into the node names produced by the ComposeGraphs()
+    # method in the graph builder. When we compose multiple graphs, we add the
+    # function name as a prefix, and `_operand` suffix to identifier nodes.
+    pointers = [
+        f'{function}_{p.identifier}_operand' for p in alias_set.pointers
+    ]
+
+    root_pointer = random.choice(pointers)
+    labelled = g.copy()
+    labelled.data_flow_max_steps_required = AnnotateAliasSet(labelled,
+                                                             root_pointer,
+                                                             pointers,
+                                                             false=false,
+                                                             true=true)
+    yield labelled
---
+++ deeplearning/ml4pl/graphs/labelled/alias_set/alias_set_test.py
@@ -0,0 +1,116 @@
+"""Unit tests for //deeplearning/ml4pl/graphs/labelled/alias_set."""
+import typing
+
+import networkx as nx
+import numpy as np
+import pytest
+from labm8 import app
+from labm8 import test
+
+from compilers.llvm import clang
+from compilers.llvm import opt
+from deeplearning.ml4pl.graphs.labelled.alias_set import alias_set
+from deeplearning.ml4pl.graphs.unlabelled.cdfg import \
+  control_and_data_flow_graph as cdfg
+
+FLAGS = app.FLAGS
+
+
+class InputPair(typing.NamedTuple):
+  # A <graph, bytecode> tuple for inputting to alias_set.MakeAliasSetGraphs().
+  graph: nx.MultiDiGraph
+  bytecode: str
+
+
+def CSourceToBytecode(source: str) -> str:
+  """Build LLVM bytecode for the given C code."""
+  process = clang.Exec(['-xc', '-O0', '-S', '-emit-llvm', '-', '-o', '-'],
+                       stdin=source)
+  assert not process.returncode
+  return process.stdout
+
+
+def CSourceToInputPair(source: str) -> InputPair:
+  """Create a graph and bytecode for the given C source string.
+  This is a convenience method for generating test inputs. If this method fails,
+  it is because graph construction or clang is broken.
+  """
+  bytecode = CSourceToBytecode(source)
+  builder = cdfg.ControlAndDataFlowGraphBuilder()
+  graph = builder.Build(bytecode)
+  return InputPair(graph=graph, bytecode=bytecode)
+
+
+def test_MakeAliasSetGraphs_invalid_bytecode():
+  graph = nx.MultiDiGraph()
+  bytecode = "invalid bytecode!"
+  with pytest.raises(opt.OptException):
+    list(alias_set.MakeAliasSetGraphs(graph, bytecode))
+
+
+def test_MakeAliasSetGraphs_may_alias_set():
+  # https://llvm.org/docs/AliasAnalysis.html
+  input_pair = CSourceToInputPair("""
+void A() {
+  char C[2];
+  char A[10];
+  for (int i = 0; i != 10; ++i) {
+    C[0] = A[i];          /* One byte store */
+    C[1] = A[9-i];        /* One byte store */
+  }
+}
+""")
+  graphs = list(alias_set.MakeAliasSetGraphs(*input_pair))
+  assert len(graphs) == 1
+  # Computed manually.
+  identifiers_in_alias_set = {'A_%16_operand', 'A_%10_operand'}
+  for node, data in graphs[0].nodes(data=True):
+    # Test the 'selector' node.
+    assert data['x'] in {0, 1}
+    # Test the labels.
+    if node in identifiers_in_alias_set:
+      assert np.array_equal(data['y'], [0, 1, 0])
+    else:
+      assert np.array_equal(data['y'], [1, 0, 0])
+
+
+def test_MakeAliasSetGraphs_multiple_functions():
+  """Test alias """
+  # https://llvm.org/docs/AliasAnalysis.html
+  input_pair = CSourceToInputPair("""
+void A() {
+  char C[2];
+  char A[10];
+  for (int i = 0; i != 10; ++i) {
+    C[0] = A[i];          /* One byte store */
+    C[1] = A[9-i];        /* One byte store */
+  }
+}
+
+void B() {
+  char C[2];
+  char A[10];
+  for (int i = 0; i != 10; ++i) {
+    C[0] = A[i];          /* One byte store */
+    C[1] = A[9-i];        /* One byte store */
+  }
+}
+""")
+  graphs = list(alias_set.MakeAliasSetGraphs(*input_pair))
+  identifiers_in_alias_sets = [{'A_%16_operand', 'A_%10_operand'},
+                               {'B_%16_operand', 'B_%10_operand'}]
+  assert len(graphs) == 2
+  # Computed manually.
+  for identifiers_in_alias_set, graph in zip(identifiers_in_alias_sets, graphs):
+    for node, data in graph.nodes(data=True):
+      # Test the 'selector' node.
+      assert data['x'] in {0, 1}
+      # Test the labels.
+      if node in identifiers_in_alias_set:
+        assert np.array_equal(data['y'], [0, 1, 0])
+      else:
+        assert np.array_equal(data['y'], [1, 0, 0])
+
+
+if __name__ == '__main__':
+  test.Main()
