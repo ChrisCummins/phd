efcd0da3caa2617156a335003eb4cb87bfaae702
Chris Cummins <chrisc.101@gmail.com>
2019-11-15 17:18:12 +0000

    LSTM node classifier.

--- deeplearning/ml4pl/models/lstm/lstm_node_classifier.py
+++ deeplearning/ml4pl/models/lstm/lstm_node_classifier.py
@@ -1,5 +1,6 @@
 """Train and evaluate a model for graph-level classification."""
 import typing
+import warnings
 
 import keras
 import numpy as np
@@ -13,6 +14,7 @@ from deeplearning.ml4pl.models.lstm import bytecode2seq
 from deeplearning.ml4pl.models.lstm import graph2seq
 from deeplearning.ml4pl.models.lstm import lstm_utils as utils
 from labm8 import app
+from labm8 import prof
 
 FLAGS = app.FLAGS
 
@@ -81,11 +83,6 @@ class LstmNodeClassifierModel(classifier_base.ClassifierBase):
         dtype='int32',
         name="model_in_segments")
 
-    # input_graph_node_list = keras.Input(
-    #     batch_shape=(FLAGS.batch_size, self.encoder.max_sequence_length),
-    #     dtype='int32',
-    #     name='graph_node_list_input')
-
     # lookup token embeddings
     encoded_inputs = keras.layers.Embedding(
         input_dim=self.encoder.bytecode_encoder.
@@ -121,26 +118,29 @@ class LstmNodeClassifierModel(classifier_base.ClassifierBase):
 
       return tf.stack(segment_sums, axis=0)
 
+    # Encoded sequences input.
     x = keras.layers.Lambda(segment_sum_wrapper)(
         [encoded_inputs, input_segments])
 
-    # vanilla
+    # Concatenate the "selector vector" input.
+    selector_vector = keras.Input(batch_shape=(FLAGS.batch_size, None, 2),
+                                  name="selector_vector")
+    x = keras.layers.Concatenate()([x, selector_vector],)
+
     x = utils.MakeLstm(FLAGS.hidden_size, return_sequences=True,
                        name="lstm_1")(x)
 
     x = utils.MakeLstm(FLAGS.hidden_size,
-                       name="lstm_2",
                        return_sequences=True,
-                       return_state=False)(x)
+                       return_state=False,
+                       name="lstm_2")(x)
 
     # map to number of classes with a dense layer
-    out = keras.layers.Dense(self.stats.node_labels_dimensionality,
-                             activation="sigmoid",
-                             name="langmodel_out")(x)
+    out = keras.layers.Dense(2, activation="sigmoid", name="out")(x)
 
     # pass both inputs to the model class.
-    self.model = keras.Model(inputs=[input_layer, input_segments],
-                             outputs=[out])
+    self.model = keras.Model(
+        inputs=[input_layer, input_segments, selector_vector], outputs=[out])
     self.model.summary()
 
     self.model.compile(optimizer="adam",
@@ -155,8 +155,8 @@ class LstmNodeClassifierModel(classifier_base.ClassifierBase):
     sequences."""
     if FLAGS.batch_size > 1024:
       raise ValueError(
-          f"Here batch size counts number of graphs, so {FLAGS.batch_size} is too many."
-      )
+          f"Here batch size counts number of graphs, so {FLAGS.batch_size} is "
+          f"too many.")
 
     options = graph_batcher.GraphBatchOptions(max_graphs=FLAGS.batch_size,
                                               groups=groups)
@@ -173,18 +173,56 @@ class LstmNodeClassifierModel(classifier_base.ClassifierBase):
           batch.graph_nodes_list[:-1] != batch.graph_nodes_list[1:])[0] + 1
       all_node_y_per_graph = np.split(batch.node_y, split_indices)
 
-      assert len(node_masks) == len(all_node_y_per_graph)
+      all_node_x_per_graph = np.split(batch.node_x_indices, split_indices)
+
+      if len(node_masks) != len(all_node_y_per_graph):
+        raise OSError(f"len(node_masks)={len(node_masks)} != "
+                      f"len(all_node_y_per_graph)={len(all_node_y_per_graph)}")
+      if len(node_masks) != len(all_node_x_per_graph):
+        raise OSError(f"len(node_masks)={len(node_masks)} != "
+                      f"len(all_node_x_per_graph)={len(all_node_x_per_graph)}")
 
       # Mask only the "active" node labels.
+      # Shape (batch_size, ?, 2)
       node_y_per_graph = [
           node_y[node_mask]
           for node_y, node_mask in zip(all_node_y_per_graph, node_masks)
       ]
+      max_nodes_in_graph = max(max(m) for m in grouping_ids) + 1
+      app.Log(2, "Padding graph batch to %s nodes", max_nodes_in_graph)
+
+      # Shape (batch_size, ?, 2)
+      node_y_truncated = np.array(
+          keras.preprocessing.sequence.pad_sequences(
+              node_y_per_graph,
+              maxlen=max_nodes_in_graph,
+              value=np.array([0, 0], dtype=np.int32),
+          ))
+
+      # Select the second node x embedding indices as a one hot vector.
+      # Shape (batch_size, ?, 2)
+      selector_vectors = [
+          node_x[node_mask]
+          for node_x, node_mask in zip(all_node_x_per_graph, node_masks)
+      ]
+
+      # Shape (batch_size, max_nodes_in_graph, 2)
+      selector_vectors = np.array(
+          keras.preprocessing.sequence.pad_sequences(
+              selector_vectors,
+              maxlen=max_nodes_in_graph,
+              value=np.array([0, 0], dtype=np.int32),
+          ))
+
+      assert selector_vectors.shape == node_y_truncated.shape
 
       yield batch.log, {
           'encoded_sequences': np.vstack(encoded_sequences),
           'segment_ids': np.vstack(grouping_ids),
-          'node_x_indices': np.vstack(batch.node_x_indices),
+          'selector_vectors': selector_vectors,
+          # The y values as fed to the model.
+          'node_y_truncated': node_y_truncated,
+          # The "true" y values without padding/truncation.
           'node_y': node_y_per_graph,
       }
 
@@ -196,38 +234,51 @@ class LstmNodeClassifierModel(classifier_base.ClassifierBase):
     x = [
         batch['encoded_sequences'],
         batch['segment_ids'],
+        batch['selector_vectors'],
     ]
-    # TODO(cec): Mask node_y
-    y = [batch['node_y']]
-
-    losses = []
-
-    def _RecordLoss(epoch, data):
-      """Callback to record training/prediction loss."""
-      del epoch
-      app.Log(2, 'Loss %s', data['loss'])
-      losses.append(data['loss'])
-
-    callbacks = [keras.callbacks.LambdaCallback(on_epoch_end=_RecordLoss)]
+    y = [batch['node_y_truncated']]
 
     if log.type == 'train':
-      self.model.fit(x,
-                     y,
-                     epochs=1,
-                     batch_size=log.graph_count,
-                     callbacks=callbacks,
-                     verbose=False,
-                     shuffle=False)
+      with prof.Profile(f'model.train_on_batch() {len(y[0])} instances',
+                        print_to=lambda x: app.Log(2, x)):
+        loss = self.model.train_on_batch(x, y)[0]
 
-    log.loss = sum(losses) / max(len(losses), 1)
+    log.loss = loss
 
-    # TODO
     # Run the same input again through the LSTM to get the raw predictions.
     # This is obviously wasteful when training, but I don't know of a way to
     # get the raw predictions from self.model.fit().
-    pred_y = self.model.predict(x)[0]
+    with prof.Profile(f'model.predict_on_batch() {len(y[0])} instances',
+                      print_to=lambda x: app.Log(2, x)):
+      pred_y = self.model.predict_on_batch(x)
 
-    return batch.graph_y, pred_y
+    assert len(pred_y) == len(batch['node_y'])
+
+    num_classes = pred_y.shape[-1]
+
+    # To handle the fact that LSTMs can't always receive the entire input
+    # sequence, we pad the predictions.
+    padded_y_pred = []
+    for y_pred_for_graph, true_node_y in zip(pred_y, batch['node_y']):
+      pad_count = max(len(true_node_y) - len(y_pred_for_graph), 0)
+      if pad_count > 0:
+        app.Log(3, 'Inserting %s padding predictions from %s to %s', pad_count,
+                len(y_pred_for_graph), len(true_node_y))
+        padding = np.zeros((pad_count, num_classes))
+        padded_y_pred.append(np.concatenate([y_pred_for_graph, padding]))
+      else:
+        padded_y_pred.append(y_pred_for_graph[:len(true_node_y)])
+      app.Log(3, 'Padded lengths %s %s', padded_y_pred[-1].shape,
+              true_node_y.shape)
+
+    # Flatten the per-graph predictions and labels.
+    y_true = np.reshape(batch['node_y'], (-1, num_classes))
+    pred_y = np.reshape(np.array(padded_y_pred), (-1, num_classes))
+
+    # TODO(cec): Here there is an error when broadcasting arrays, not sure
+    # what's going on?
+
+    return self.MinibatchResults(y_true_1hot=y_true, y_pred_1hot=pred_y)
 
   def ModelDataToSave(self):
     model_path = self.working_dir / f'{self.run_id}_keras_model.h5'
@@ -241,6 +292,8 @@ class LstmNodeClassifierModel(classifier_base.ClassifierBase):
 
 def main():
   """Main entry point."""
+  # TODO(cec): Only filter https://scikit-learn.org/stable/modules/generated/sklearn.exceptions.UndefinedMetricWarning.html
+  warnings.filterwarnings("ignore")
   classifier_base.Run(LstmNodeClassifierModel)
 
 
