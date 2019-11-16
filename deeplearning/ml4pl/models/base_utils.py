added a DSC loss func that is not recommended, added learning rate schedule that is recommended, fixed bugs in position embeddings padding and print recall and precision during training

deeplearning/ml4pl/models/base_utils.py
@@ -18,7 +18,8 @@ def pos_emb(positions: Union[int, List[int], np.array], demb: int = 200, dpad: i
     pos_emb = np.hstack((np.sin(sinusoid_inp), np.cos(sinusoid_inp)))
 
     if dpad > 0:
-        pad = np.zeros([dpad, demb])
-        pos_emb = np.vstack([pos_emb, pad])
-        assert np.all(pos_emb[-1] == np.zeros([demb])), f"test failed. pos_emb: \n{pos_emb}"
+        in_length = 1 if type(positions) == int else len(positions)
+        pad = np.zeros([in_length, dpad])
+        pos_emb = np.hstack([pos_emb, pad])
+        assert np.all(pos_emb[:,-1] == np.zeros(in_length)), f"test failed. pos_emb: \n{pos_emb}"
     return pos_emb

deeplearning/ml4pl/models/ggnn/ggnn.py
@@ -67,6 +67,15 @@ app.DEFINE_integer(
     "Size for MLP that combines graph_x and GGNN output features")
 classifier_base.MODEL_FLAGS.add("auxiliary_inputs_dense_layer_size")
 
+app.DEFINE_boolean("use_dsc_loss", False,
+    "Whether to use the DSC loss instead of Cross Entropy."
+    "DSC loss help with class imbalances. Refer to "
+    "https://arxiv.org/pdf/1911.02855.pdf"
+)
+
+app.DEFINE_boolean("use_lr_schedule", False,
+    "whether to use a warmup-train-finetune learning rate schedule."
+)
 
 ###########################
 app.DEFINE_boolean('kfold', False, "Set to do automatic kfold validation on devmap.")
@@ -414,12 +423,32 @@ class GgnnClassifier(ggnn.GgnnBaseModel):
                                               predictions)
       loss = _loss + FLAGS.intermediate_loss_discount_factor * graph_only_loss
     elif self.stats.node_labels_dimensionality:
-      loss = tf.losses.softmax_cross_entropy(self.placeholders["node_y"],
+      if FLAGS.use_dsc_loss:
+        # self.placeholders['node_y'] have shape (num_nodes_in_batch, 2)
+        p1 = tf.nn.softmax(predictions[:, 0])
+        y1 = tf.cast(self.placeholders['node_y'][:, 0], tf.float32)
+
+        # we fix class 2 bc here 0 is the dominant mode!
+        p2 = 1.0 - tf.nn.softmax(predictions[:, 1])
+        y2 = 1.0 - tf.cast(self.placeholders['node_y'][:, 1], tf.float32)
+
+        loss = (self.make_dsc_loss(p1, y1) + self.make_dsc_loss(p2, y2)) / 2.0
+      else:
+        loss = tf.losses.softmax_cross_entropy(self.placeholders["node_y"],
                                              predictions)
+        #loss = 0.0
+
     else:
       raise ValueError("No graph labels and no node labels!")
 
     return loss, accuracies, accuracy, predictions
+
+  def make_dsc_loss(self, p1: tf.Tensor, y1: tf.Tensor):
+    normalization = tf.cast(self.placeholders['node_count'], tf.float32)
+    numerator = (1. - p1) * p1 * y1
+    denominator = ((1.0 - p1) * p1 + y1) * normalization
+    neg_loss = tf.reduce_sum(numerator / (denominator + utils.SMALL_NUMBER))
+    return 1.0 - neg_loss
 
   def MakeMinibatchIterator(
       self, epoch_type: str, groups: typing.List[str]
@@ -448,6 +477,8 @@ class GgnnClassifier(ggnn.GgnnBaseModel):
             FLAGS.output_layer_dropout_keep_prob,
             self.placeholders["is_training"]:
             True,
+            self.placeholders['learning_rate_multiple']:
+            utils.WarmUpAndFinetuneLearningRateSchedule(self.epoch_num, FLAGS.num_epochs) if FLAGS.use_lr_schedule else 1.0
         })
       else:
         feed_dict.update({

deeplearning/ml4pl/models/ggnn/ggnn_base.py
@@ -212,7 +212,7 @@ class GgnnBaseModel(classifier_base.ClassifierBase):
     inst2vec_embeddings = tf.Variable(initial_value=embeddings[0],
                                       trainable=trainable,
                                       dtype=tf.float32)
-    selector_embeddings = tf.Variable(initial_value=embeddings[1],
+    selector_embeddings = tf.Variable(initial_value=embeddings[1] * 50,
                                       trainable=False,
                                       dtype=tf.float32)
     return inst2vec_embeddings, selector_embeddings
@@ -445,7 +445,7 @@ class GgnnBaseModel(classifier_base.ClassifierBase):
         else:
           app.Log(1, "Freezing weights of variable `%s`.", var.name)
       trainable_vars = filtered_vars
-    optimizer = tf.compat.v1.train.AdamOptimizer(FLAGS.learning_rate)
+    optimizer = tf.compat.v1.train.AdamOptimizer(FLAGS.learning_rate * self.placeholders['learning_rate_multiple'])
     grads_and_vars = optimizer.compute_gradients(self.ops["loss"],
                                                  var_list=trainable_vars)
     clipped_grads = []