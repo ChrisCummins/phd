54add13dd1152dba6aab72f2034f867661d6c0c9
Chris Cummins <chrisc.101@gmail.com>
2019-11-16 21:45:02 +0000

    Update graph batcher tests.

--- deeplearning/ml4pl/graphs/labelled/graph_tuple/graph_batcher_test.py
+++ deeplearning/ml4pl/graphs/labelled/graph_tuple/graph_batcher_test.py
@@ -1,11 +1,12 @@
 """Unit tests for //deeplearning/ml4pl/graphs/labelled/graph_batcher."""
 import pytest
-from labm8 import app
-from labm8 import test
 
 from deeplearning.ml4pl.graphs import graph_database
 from deeplearning.ml4pl.graphs.labelled.graph_tuple import graph_batcher
 from deeplearning.ml4pl.models import log_database
+from labm8 import app
+from labm8 import test
+
 
 FLAGS = app.FLAGS
 
@@ -39,14 +40,20 @@ def test_NextGraph_larger_than_batch_size():
 def test_GraphBatchOptions_ShouldAddToBatch_no_filters():
   options = graph_batcher.GraphBatchOptions(max_graphs=0, max_nodes=0)
   log = log_database.BatchLogMeta(node_count=0, graph_count=0)
-  graph = graph_database.GraphMeta(node_count=0)
+  graph = graph_database.GraphMeta(node_count=0, graph=graph_database.Graph())
   assert options.ShouldAddToBatch(graph, log)
 
+
+def test_GraphBatchOptions_ShouldAddToBatch_no_data():
+  options = graph_batcher.GraphBatchOptions(max_graphs=0, max_nodes=0)
+  log = log_database.BatchLogMeta(node_count=0, graph_count=0)
+  graph = graph_database.GraphMeta(node_count=0, graph=None)
+  assert not options.ShouldAddToBatch(graph, log)
 
 def test_GraphBatchOptions_ShouldAddToBatch_node_count_filter():
   options = graph_batcher.GraphBatchOptions(max_graphs=0, max_nodes=100)
   log = log_database.BatchLogMeta(node_count=30, graph_count=0)
-  graph = graph_database.GraphMeta(node_count=50)
+  graph = graph_database.GraphMeta(node_count=50, graph=graph_database.Graph())
   assert options.ShouldAddToBatch(graph, log)
 
   graph = graph_database.GraphMeta(node_count=80)
@@ -56,7 +63,7 @@ def test_GraphBatchOptions_ShouldAddToBatch_node_count_filter():
 def test_GraphBatchOptions_ShouldAddToBatch_graph_count_filter():
   options = graph_batcher.GraphBatchOptions(max_graphs=10, max_nodes=0)
   log = log_database.BatchLogMeta(node_count=0, graph_count=0)
-  graph = graph_database.GraphMeta(node_count=0)
+  graph = graph_database.GraphMeta(node_count=0, graph=graph_database.Graph())
   assert options.ShouldAddToBatch(graph, log)
 
   log.graph_count = 10
