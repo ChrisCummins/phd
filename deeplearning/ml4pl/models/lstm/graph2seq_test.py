20341758f482165759d2f28a06e3eeaf7f6d1bbd
Chris Cummins <chrisc.101@gmail.com>
2019-11-17 13:29:38 +0000

    Work on graph tests.

--- deeplearning/ml4pl/models/lstm/graph2seq_test.py
+++ deeplearning/ml4pl/models/lstm/graph2seq_test.py
@@ -1,43 +1,97 @@
 """Unit tests for //deeplearning/ml4pl/models/lstm:graph2seq."""
-import numpy as np
 import pathlib
+import pickle
+
+import networkx as nx
 import pytest
 
 from deeplearning.ml4pl.bytecode import bytecode_database
 from deeplearning.ml4pl.graphs import graph_database
+from deeplearning.ml4pl.models.lstm import bytecode2seq
 from deeplearning.ml4pl.models.lstm import graph2seq
 from labm8 import app
 from labm8 import test
-
 
 FLAGS = app.FLAGS
 
 
 @pytest.fixture(scope='function')
 def bytecode_db(tempdir: pathlib.Path) -> bytecode_database.Database:
-  yield bytecode_database.Database(f'sqlite:///{tempdir}/bytecodes.db')
+  db = bytecode_database.Database(f'sqlite:///{tempdir}/bytecodes.db')
+  with db.Session(commit=True) as session:
+    session.add(
+        bytecode_database.LlvmBytecode(
+            id=1,
+            source_name='',
+            relpath='',
+            language='',
+            cflags='',
+            charcount=len('Hello, world!'),
+            linecount=1,
+            bytecode='Hello, world!',
+            clang_returncode=0,
+            error_message='',
+            bytecode_sha1='',
+        ))
+  yield db
 
 
 @pytest.fixture(scope='function')
 def graph_db(tempdir: pathlib.Path) -> graph_database.Database:
-  yield graph_database.Database(f'sqlite:///{tempdir}/graphs.db')
+  db = graph_database.Database(f'sqlite:///{tempdir}/graphs.db')
+  # Add a program graph, which is used by GraphToBytecodeGroupingsEncoder.
+  g = nx.MultiDiGraph()
+  g.add_node('root', type='magic')
+  g.add_node('a', type='statement', function='a', text='a')
+  g.add_node('%1', type='statement', function='a', text='%1')
+  g.add_edge('root', 'a', flow='call')
+  g.add_edge('%1', 'a', flow='data')
+  with db.Session(commit=True) as session:
+    session.add(
+        graph_database.GraphMeta(
+            id=1,
+            bytecode_id=1,
+            group='',
+            source_name='',
+            relpath='',
+            language='',
+            node_count=0,
+            edge_count=0,
+            edge_position_max=0,
+            loop_connectedness=0,
+            undirected_diameter=0,
+            graph=graph_database.Graph(pickled_data=pickle.dumps(g))))
+  yield db
 
 
-def test_Encode(graph_db: graph_database.Database):
-  encoder = graph2seq.GraphToSequenceEncoder(graph_db)
-  encoded = encoder.Encode(["Hello, world"])
+def test_GraphToBytecodeEncoder_Encode(graph_db: graph_database.Database,
+                                       bytecode_db: bytecode_database.Database):
+  FLAGS.bytecode_db = lambda: bytecode_db
+  encoder = graph2seq.GraphToBytecodeEncoder(graph_db,
+                                             bytecode2seq.BytecodeEncoder())
+  encoded = encoder.Encode([1])
   assert len(encoded) == 1
-  assert encoded[0] == [124, 7, 6, 6, 3, 51, 1, 71, 80, 6, 4]
+  assert len(encoded[0])
 
 
-def test_EncodeStringsWithGroupings():
-  encoder = graph2seq.GraphToSequenceEncoder(graph_db)
+@pytest.mark.parametrize('group_by', ('statement', 'identifier'))
+def test_GraphToBytecodeGroupingsEncoder_Encode(
+    graph_db: graph_database.Database, bytecode_db: bytecode_database.Database,
+    group_by: str):
+  FLAGS.bytecode_db = lambda: bytecode_db
+  FLAGS.unlabelled_graph_db = lambda: graph_db
 
-  enc, idx = encoder.EncodeStringsWithGroupings(["HH", "H", "H"])
+  encoder = graph2seq.GraphToBytecodeGroupingsEncoder(
+      graph_db, bytecode2seq.BytecodeEncoder(), group_by)
 
-  assert len(enc) == 4
-  assert np.array_equal(enc, [124, 124, 124, 124])
-  assert np.array_equal(idx, [0, 0, 1, 2])
+  encoded_sequences, segment_ids, node_masks = encoder.Encode([1])
+
+  assert len(encoded_sequences) == 1
+  assert len(encoded_sequences[0])
+  assert len(segment_ids) == 1
+  assert len(segment_ids[0])
+  assert len(node_masks) == 1
+  assert len(node_masks[0])
 
 
 if __name__ == '__main__':
