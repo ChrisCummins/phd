dea28aad6d2d10d040a7e2386fd6c9f66c5b2a04
Chris Cummins <chrisc.101@gmail.com>
2019-11-18 13:53:18 +0000

    Fix graph query call.

--- deeplearning/ml4pl/models/lstm/graph2seq.py
+++ deeplearning/ml4pl/models/lstm/graph2seq.py
@@ -5,14 +5,14 @@ import typing
 
 import networkx as nx
 import numpy as np
+from labm8 import app
+from labm8 import labtypes
 
 from deeplearning.ml4pl.graphs import graph_database
 from deeplearning.ml4pl.graphs import graph_query
 from deeplearning.ml4pl.graphs.unlabelled.cdfg import \
   control_and_data_flow_graph as cdfg
 from deeplearning.ml4pl.models.lstm import bytecode2seq
-from labm8 import app
-from labm8 import labtypes
 
 FLAGS = app.FLAGS
 
@@ -368,7 +368,7 @@ class GraphToBytecodeGroupingsEncoder(EncoderBase):
     strings_to_encode = [
         labtypes.flatten([
             graph.nodes[n].get('original_text', '')
-            for n in graph_query.GetStatementsForNode(identifier)
+            for n in graph_query.GetStatementsForNode(graph, identifier)
         ])
         for identifier in identifiers
     ]
