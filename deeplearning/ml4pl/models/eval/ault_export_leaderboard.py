d417b3b02ca96264c5bb32ee3c52a8cf8cf59c3c
Zacharias Fisches <zacharias.vf@gmail.com>
2019-11-21 23:58:01 +0100

    specific leaderboard exporter

--- deeplearning/ml4pl/models/eval/ault_export_leaderboard.py
+++ deeplearning/ml4pl/models/eval/ault_export_leaderboard.py
@@ -3,6 +3,7 @@ from deeplearning.ml4pl.models.eval import google_sheets
 from deeplearning.ml4pl.models.eval import leaderboard
 from labm8 import app
 from labm8 import prof
+import pandas as pd
 
 app.DEFINE_string('worksheet', 'Leaderboard',
                   'The name of the worksheet to export to.')
@@ -17,7 +18,13 @@ def main():
     ws = g.GetOrCreateWorksheet(s, FLAGS.worksheet)
 
   with prof.Profile("Created leaderboard"):
-    df = leaderboard.GetLeaderboard(log_db=FLAGS.log_db())
+    log_db_str = FLAGS.log_db.url
+    dfs = []
+    for i in range(10):
+      FLAGS.log_db.url = log_db_str.replace('.db', f'_{i}.db')
+      df = leaderboard.GetLeaderboard(log_db=FLAGS.log_db())
+      dfs.append(df)
+    df = pd.concat(dfs)
 
   with prof.Profile("Exported dataset"):
     g.ExportDataFrame(ws, df)
