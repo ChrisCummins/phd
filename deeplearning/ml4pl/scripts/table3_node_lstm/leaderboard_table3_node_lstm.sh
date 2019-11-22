07c0e67f7075f36305c53964c2709108c58adceb
Zacharias Fisches <zacharias.vf@gmail.com>
2019-11-22 03:05:17 +0100

    fix node lstm leaderboard command

--- deeplearning/ml4pl/scripts/table3_node_lstm/leaderboard_table3_node_lstm.sh
+++ deeplearning/ml4pl/scripts/table3_node_lstm/leaderboard_table3_node_lstm.sh
@@ -1,7 +1,6 @@
 bazel run //deeplearning/ml4pl/models/eval:export_leaderboard -- \
---google_sheets_credentials='/users/zfisches/google_api.json' \
 --log_db='sqlite:////users/zfisches/logs_node_lstm_series.db' \
---worksheet=lstm_table5 \
+--worksheet=node_lstm_series \
 --extra_model_flags=inst2vec_embeddings,output_layer_dropout_keep_prob \
 --extra_flags=batch_size,\
 max_train_per_epoch,\
@@ -10,5 +9,5 @@ use_lr_schedule,\
 learning_rate,\
 layer_timesteps,\
 edge_weight_dropout_keep_prob,\
-manual_tag \
+manual_tag,\
 bytecode_encoder \
