054b86bdcb1d9ab7772bd3ffe5a484314827cd5f
Chris Cummins <chrisc.101@gmail.com>
2019-11-24 01:16:11 +0000

    Begin implementation of graph database migration.

---
+++ deeplearning/ml4pl/graphs/migrate/migrate_graph_database.py
@@ -0,0 +1,37 @@
+"""Migrate the graph tuple databases.
+
+This updates the graph tuple representation based on my experience in initial
+experiments.
+"""
+from deeplearning.ml4pl.graphs import graph_database
+from deeplearning.ml4pl.graphs.labelled.graph_tuple import graph_tuple
+from labm8 import app
+
+FLAGS = app.FLAGS
+
+
+def RemoveBackwardEdges(graph: graph_tuple.GraphTuple):
+  """Graph tuples store redundant backward edges. Remove those."""
+  return graph_tuple.GraphTuple(
+      adjacency_lists=graph.adjacency_lists[:3],
+      edge_positions=graph.edge_positions[:3],
+      incoming_edge_counts=graph.incoming_edge_counts[:3],
+      node_x_indices=graph.node_x_indices,
+      node_y=graph.node_y,
+      graph_x=graph.graph_x,
+      graph_y=graph.graph_y,
+  )
+
+
+def UpdateGraphMetas(graph: graph_database.GraphMeta):
+  """Update column values on graph metas."""
+  # TODO
+
+
+def main():
+  """Main entry point."""
+  # TODO: Implement!
+
+
+if __name__ == '__main__':
+  app.Run(main)
---
+++ deeplearning/ml4pl/graphs/migrate/migrate_graph_database_test.py
@@ -0,0 +1,15 @@
+"""Unit tests for //deeplearning/ml4pl/graphs/migrate:migrate_graph_database."""
+from deeplearning.ml4pl.graphs.migrate import migrate_graph_database
+from labm8 import app
+from labm8 import test
+
+FLAGS = app.FLAGS
+
+
+def test_TODO():
+  """TODO: Short summary of test."""
+  assert migrate_graph_database
+
+
+if __name__ == '__main__':
+  test.Main()
