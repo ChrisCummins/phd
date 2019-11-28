2bcd724d2d2e9ee7d91eca267d6f4fda07c73127
Chris Cummins <chrisc.101@gmail.com>
2019-11-28 12:52:15 +0000

    Replace yapf with my forked version of black.
    Yapf was producing inconsistent results when run
    on different machines (even with the same config
    file and version), so I am replacing it with black.
    
    Mostly I agree with black's opinions on code
    formatting, with the notable exception of
    indentation level. Black enforces 4 spaces, whereas
    I prefer 2. Since the indentation level is not
    customizable, I have forked black and changed it to
    2 spaces.

--- tools/code_style/linters/linters_lib.py
+++ tools/code_style/linters/linters_lib.py
@@ -13,8 +13,7 @@ import threading
 # The path to the root of the PhD repository, i.e. the directory which this file
 # is in.
 # WARNING: Moving this file may require updating this path!
-_PHD_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                         '../../..')
+_PHD_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../..")
 
 
 def WhichOrDie(name):
@@ -29,33 +28,32 @@ def WhichOrDie(name):
   for path in os.environ["PATH"].split(os.pathsep):
     if os.path.exists(os.path.join(path, name)):
       return os.path.join(path, name)
-  print(sys.argv[0],
-        'error: Could not find required binary:',
-        name,
-        file=sys.stderr)
   print(
-      "You probably haven't installed the development dependencies. "
-      "See INSTALL.md.",
-      file=sys.stderr)
+    sys.argv[0], "error: Could not find required binary:", name, file=sys.stderr
+  )
+  print(
+    "You probably haven't installed the development dependencies. "
+    "See INSTALL.md.",
+    file=sys.stderr,
+  )
   sys.exit(1)
 
 
-BUILDIFIER = WhichOrDie('buildifier')
-CLANG_FORMAT = WhichOrDie('clang-format')
-GO = WhichOrDie('go')
-JAVA = WhichOrDie('java')
-JSBEAUTIFY = WhichOrDie('js-beautify')
-JSON_LINT = WhichOrDie('jsonlint')
-SQLFORMAT = WhichOrDie('sqlformat')
-YAPF = WhichOrDie('yapf')
-REORDER_PYTHON_IMPORTS = WhichOrDie('reorder-python-imports')
+BUILDIFIER = WhichOrDie("buildifier")
+CLANG_FORMAT = WhichOrDie("clang-format")
+GO = WhichOrDie("go")
+JAVA = WhichOrDie("java")
+JSBEAUTIFY = WhichOrDie("js-beautify")
+JSON_LINT = WhichOrDie("jsonlint")
+SQLFORMAT = WhichOrDie("sqlformat")
+BLACK = WhichOrDie("black")
+REORDER_PYTHON_IMPORTS = WhichOrDie("reorder-python-imports")
 
-YAPF_RC = os.path.join(_PHD_ROOT, 'tools/code_style/yapf.yml')
-assert os.path.isfile(YAPF_RC)
-JSBEAUTIFY_RC = os.path.join(_PHD_ROOT, 'tools/code_style/jsbeautifyrc.json')
+JSBEAUTIFY_RC = os.path.join(_PHD_ROOT, "tools/code_style/jsbeautifyrc.json")
 assert os.path.isfile(JSBEAUTIFY_RC)
 GOOGLE_JAVA_FORMAT = os.path.join(
-    _PHD_ROOT, 'tools/code_style/linters/google-java-format-1.7-all-deps.jar')
+  _PHD_ROOT, "tools/code_style/linters/google-java-format-1.7-all-deps.jar"
+)
 assert os.path.isfile(GOOGLE_JAVA_FORMAT)
 
 
@@ -77,17 +75,16 @@ def ExecOrDie(cmd):
   Returns:
      The process stdout as a string.
   """
-  process = subprocess.Popen(cmd,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE,
-                             universal_newlines=True)
+  process = subprocess.Popen(
+    cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
+  )
   stdout, stderr = process.communicate()
   if process.returncode:
-    Print('error')
-    Print('    $ ', ' '.join(cmd), sep='')
+    Print("error")
+    Print("    $ ", " ".join(cmd), sep="")
 
-    for line in stderr.rstrip().split('\n'):
-      Print('   ', line)
+    for line in stderr.rstrip().split("\n"):
+      Print("   ", line)
 
     sys.exit(1)
   return stdout
@@ -95,9 +92,9 @@ def ExecOrDie(cmd):
 
 def GetGitBranchOrDie():
   """Get the name of the current git branch."""
-  branches = ExecOrDie(['git', 'branch'])
-  for line in branches.split('\n'):
-    if line.startswith('* '):
+  branches = ExecOrDie(["git", "branch"])
+  for line in branches.split("\n"):
+    if line.startswith("* "):
       return line[2:]
   print("fatal: Unable to determine git branch", file=sys.stderr)
   sys.exit(1)
@@ -106,14 +103,14 @@ def GetGitBranchOrDie():
 def GetGitRemoteOrDie(branch_name):
   """Get the name of the current git remote."""
   fully_qualified_name = ExecOrDie(
-      ['git', 'rev-parse', '--abbrev-ref', branch_name + '@{upstream}'])
-  components = fully_qualified_name.split('/')
+    ["git", "rev-parse", "--abbrev-ref", branch_name + "@{upstream}"]
+  )
+  components = fully_qualified_name.split("/")
   assert len(components) > 1
   return components[0]
 
 
 class LinterThread(threading.Thread):
-
   def __init__(self, paths):
     super(LinterThread, self).__init__()
     assert paths
@@ -134,72 +131,72 @@ class LinterThread(threading.Thread):
 
 
 class BuildiferThread(LinterThread):
-
   def run(self):
     ExecOrDie([BUILDIFIER] + self._paths)
 
 
 class ClangFormatThread(LinterThread):
-
   def run(self):
     # TODO(cec): Use project-local clang-format style file.
-    ExecOrDie([CLANG_FORMAT, '-style', 'Google', '-i'] + self._paths)
+    ExecOrDie([CLANG_FORMAT, "-style", "Google", "-i"] + self._paths)
 
 
 class PythonThread(LinterThread):
-
   def run(self):
-    ExecOrDie([YAPF, '--style', YAPF_RC, '-i'] + self._paths)
-    ExecOrDie([REORDER_PYTHON_IMPORTS, '--exit-zero-even-if-changed'] +
-              self._paths)
+    ExecOrDie([BLACK, "--line-length=82", "--target-version=py37"] + self._paths)
+    ExecOrDie(
+      [REORDER_PYTHON_IMPORTS, "--exit-zero-even-if-changed"] + self._paths
+    )
 
 
 class SqlFormat(LinterThread):
-
   def run(self):
     for path in self.paths:
       self.Lint(path)
 
   @staticmethod
   def Lint(path):
-    ExecOrDie([
-        SQLFORMAT, '--reindent', '--keywords', 'upper', '--identifiers',
-        'lower', path, '--outfile', path
-    ])
+    ExecOrDie(
+      [
+        SQLFORMAT,
+        "--reindent",
+        "--keywords",
+        "upper",
+        "--identifiers",
+        "lower",
+        path,
+        "--outfile",
+        path,
+      ]
+    )
 
 
 class JsBeautifyThread(LinterThread):
-
   def run(self):
-    ExecOrDie([JSBEAUTIFY, '--replace', '--config', JSBEAUTIFY_RC] +
-              self._paths)
+    ExecOrDie([JSBEAUTIFY, "--replace", "--config", JSBEAUTIFY_RC] + self._paths)
 
 
 class GoFmtThread(LinterThread):
-
   def run(self):
     # Run linter on each file individually because:
     #   1. An error in one file prevents linting in all other files.
     #   2. All files in a single invocation must be in the same directory.
     for path in self._paths:
-      ExecOrDie([GO, 'fmt', path])
+      ExecOrDie([GO, "fmt", path])
 
 
 class GoogleJavaFormatThread(LinterThread):
-
   def run(self):
-    ExecOrDie([JAVA, '-jar', GOOGLE_JAVA_FORMAT, '-i'] + self._paths)
+    ExecOrDie([JAVA, "-jar", GOOGLE_JAVA_FORMAT, "-i"] + self._paths)
 
 
 class JsonlintThread(LinterThread):
-
   def run(self):
     for path in self._paths:
-      ExecOrDie([JSON_LINT, '-i', path])
+      ExecOrDie([JSON_LINT, "-i", path])
 
 
 class LinterActions(object):
-
   def __init__(self, paths):
     self._paths = paths
     self._buildifier = []
@@ -214,24 +211,29 @@ class LinterActions(object):
 
     for path in paths:
       basename = os.path.basename(path)
-      if basename == 'BUILD' or basename == 'WORKSPACE':
+      if basename == "BUILD" or basename == "WORKSPACE":
         self._buildifier.append(path)
 
       _, extension = os.path.splitext(path)
 
-      if extension == '.cc' or extension == '.c' or extension == '.h' or extension == '.ino':
+      if (
+        extension == ".cc"
+        or extension == ".c"
+        or extension == ".h"
+        or extension == ".ino"
+      ):
         self._clang_format.append(path)
-      elif extension == '.py' or extension == '.bzl':
+      elif extension == ".py" or extension == ".bzl":
         self._python.append(path)
-      elif extension == '.sql':
+      elif extension == ".sql":
         self._sqlformat.append(path)
-      elif extension == '.html' or extension == '.css' or extension == '.js':
+      elif extension == ".html" or extension == ".css" or extension == ".js":
         self._jsbeautify.append(path)
-      elif extension == '.go':
+      elif extension == ".go":
         self._gofmt.append(path)
-      elif extension == '.java':
+      elif extension == ".java":
         self._java.append(path)
-      elif extension == '.json':
+      elif extension == ".json":
         self._json.append(path)
 
   @property
@@ -240,9 +242,16 @@ class LinterActions(object):
 
   @property
   def paths_with_actions(self):
-    return (self._buildifier + self._clang_format + self._python +
-            self._sqlformat + self._jsbeautify + self._gofmt + self._java +
-            self._json)
+    return (
+      self._buildifier
+      + self._clang_format
+      + self._python
+      + self._sqlformat
+      + self._jsbeautify
+      + self._gofmt
+      + self._java
+      + self._json
+    )
 
   @property
   def modified_paths(self):
@@ -281,10 +290,10 @@ def GetGitDiffFilesOrDie(staged):
 
   To list both, call this function twice with both staged=True and staged=False.
   """
-  cmd = ['git', 'diff', '--name-only']
+  cmd = ["git", "diff", "--name-only"]
   if staged:
-    cmd.append('--cached')
+    cmd.append("--cached")
   output = ExecOrDie(cmd)
-  lines = output.split('\n')
+  lines = output.split("\n")
   staged_file_relpaths = lines[:-1]  # Last line is blank.
   return staged_file_relpaths
