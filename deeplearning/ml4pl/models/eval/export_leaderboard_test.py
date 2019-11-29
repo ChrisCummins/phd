df8aa2618b22b6425f976c292b6f84831e606aa4
Chris Cummins <chrisc.101@gmail.com>
2019-11-29 14:34:12 +0000

    Work in progress on leaderboard test.

--- deeplearning/ml4pl/models/eval/export_leaderboard_test.py
+++ deeplearning/ml4pl/models/eval/export_leaderboard_test.py
@@ -3,15 +3,18 @@ from labm8.py import test
 
 FLAGS = test.FLAGS
 
+from deeplearning.ml4pl.models.eval import google_sheets
+
 MODULE_UNDER_TEST = None
 
 requires_google_sheets_credentials_file = test.SkipIf(
-  not FLAGS.credentials.is_file(), reason="google sheets credentials not found",
+  not FLAGS.google_sheets_credentials.is_file(),
+  reason="google sheets credentials not found",
 )
 
 
 def test_TODO():
-  pass
+  del google_sheets
 
 
 if __name__ == "__main__":
