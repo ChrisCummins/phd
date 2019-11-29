68aba17a5a060bf383a39d6f415dd52f5f4d57a4
Chris Cummins <chrisc.101@gmail.com>
2019-11-29 23:31:16 +0000

    Replace conftest with parametrized databases.

--- deeplearning/ml4pl/graphs/graph_database_reader_test.py
+++ deeplearning/ml4pl/graphs/graph_database_reader_test.py
@@ -1,9 +1,47 @@
 """Unit tests for //deeplearning/ml4pl/ggnn:graph_database_reader."""
 from deeplearning.ml4pl.graphs import graph_database
 from deeplearning.ml4pl.graphs import graph_database_reader as reader
+from deeplearning.ml4pl.testing import testing_databases
 from labm8.py import test
 
 FLAGS = test.FLAGS
+
+
+@test.Fixture(scope="function", params=testing_databases.TEST_DB_URLS)
+def empty_graph_db(request) -> graph_database.Database:
+  yield from testing_databases.YieldDatabase(
+    graph_database.Database, request.param
+  )
+
+
+@test.Fixture(scope="function")
+def graph_db_512(
+  empty_graph_db: graph_database.Database,
+) -> graph_database.Database:
+  """Fixture which returns a database with 512 graphs, indexed by node_count."""
+
+  def _MakeGraphMeta(i):
+    return graph_database.GraphMeta(
+      group="train",
+      bytecode_id=1,
+      source_name="foo",
+      relpath="bar",
+      language="c",
+      node_count=i,
+      edge_count=2,
+      edge_position_max=0,
+      loop_connectedness=0,
+      undirected_diameter=0,
+      data_flow_max_steps_required=i,
+      graph=graph_database.Graph(
+        pickled_data=pickle.dumps(np.ones(200000 // 4) * i)  # ~200KB of data
+      ),
+    )
+
+  with empty_graph_db.Session(commit=True) as s:
+    s.add_all([_MakeGraphMeta(i) for i in range(512)])
+
+  return empty_graph_db
 
 
 @test.Parametrize("buffer_size", [1, 25, 10000])
