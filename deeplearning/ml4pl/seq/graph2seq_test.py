52a6dee0d2b74c8260953469c47b5d0aeb1f3580
Chris Cummins <chrisc.101@gmail.com>
2019-12-08 04:18:29 +0000

    Use session-level test fixtures for graph2seq.

--- deeplearning/ml4pl/seq/graph2seq_test.py
+++ deeplearning/ml4pl/seq/graph2seq_test.py
@@ -19,7 +19,7 @@ from labm8.py import test
 FLAGS = test.FLAGS
 
 
-@test.Fixture(scope="function", params=testing_databases.GetDatabaseUrls())
+@test.Fixture(scope="session", params=testing_databases.GetDatabaseUrls())
 def ir_db(request) -> ir_database.Database:
   """A test fixture which yields an empty graph proto database."""
   yield from testing_databases.YieldDatabase(
@@ -27,7 +27,7 @@ def ir_db(request) -> ir_database.Database:
   )
 
 
-@test.Fixture(scope="function", params=testing_databases.GetDatabaseUrls())
+@test.Fixture(scope="session", params=testing_databases.GetDatabaseUrls())
 def graph_db(request) -> graph_tuple_database.Database:
   yield from testing_databases.YieldDatabase(
     graph_tuple_database.Database, request.param
@@ -51,46 +51,52 @@ def opencl_relpaths() -> Set[str]:
   return set(opencl_df.relpath.values)
 
 
-@test.Fixture(scope="function")
+@test.Fixture(scope="session", params=testing_databases.GetDatabaseUrls())
 def populated_graph_db(
-  graph_db: graph_tuple_database.Database, opencl_relpaths: Set[str]
+  request, opencl_relpaths: Set[str]
 ) -> graph_tuple_database.Database:
   """A test fixture which yields a graph database with 256 OpenCL IR entries."""
-  rows = []
-  # Create random rows using OpenCL relpaths.
-  for i, relpath in enumerate(opencl_relpaths):
-    graph_tuple = random_graph_tuple_database_generator.CreateRandomGraphTuple()
-    graph_tuple.ir_id = i + 1
-    graph_tuple.id = len(opencl_relpaths) - i
-    rows.append(graph_tuple)
+  with testing_databases.DatabaseContext(
+    graph_tuple_database.Database, request.param
+  ) as db:
+    rows = []
+    # Create random rows using OpenCL relpaths.
+    for i, relpath in enumerate(opencl_relpaths):
+      graph_tuple = (
+        random_graph_tuple_database_generator.CreateRandomGraphTuple()
+      )
+      graph_tuple.ir_id = i + 1
+      graph_tuple.id = len(opencl_relpaths) - i
+      rows.append(graph_tuple)
 
-  with graph_db.Session(commit=True) as session:
-    session.add_all(rows)
+    with db.Session(commit=True) as session:
+      session.add_all(rows)
 
-  return graph_db
+    yield db
 
 
-@test.Fixture(scope="function")
-def populated_ir_db(
-  ir_db: ir_database.Database, opencl_relpaths: Set[str]
-) -> ir_database.Database:
+@test.Fixture(scope="session", params=testing_databases.GetDatabaseUrls())
+def populated_ir_db(request, opencl_relpaths: Set[str]) -> ir_database.Database:
   """A test fixture which yields an IR database with 256 OpenCL entries."""
-  rows = []
-  # Create random rows using OpenCL relpaths.
-  for i, relpath in enumerate(opencl_relpaths):
-    ir = ir_database.IntermediateRepresentation.CreateFromText(
-      source="pact17_opencl_devmap",
-      relpath=relpath,
-      source_language=ir_database.SourceLanguage.OPENCL,
-      type=ir_database.IrType.LLVM_6_0,
-      cflags="",
-      text=CreateRandomString(),
-    )
-    ir.id = i + 1
-    rows.append(ir)
+  with testing_databases.DatabaseContext(
+    ir_database.Database, request.param
+  ) as db:
+    rows = []
+    # Create random rows using OpenCL relpaths.
+    for i, relpath in enumerate(opencl_relpaths):
+      ir = ir_database.IntermediateRepresentation.CreateFromText(
+        source="pact17_opencl_devmap",
+        relpath=relpath,
+        source_language=ir_database.SourceLanguage.OPENCL,
+        type=ir_database.IrType.LLVM_6_0,
+        cflags="",
+        text=CreateRandomString(),
+      )
+      ir.id = i + 1
+      rows.append(ir)
 
-  with ir_db.Session(commit=True) as session:
-    session.add_all(rows)
+    with ir_db.Session(commit=True) as session:
+      session.add_all(rows)
 
   return ir_db
 
