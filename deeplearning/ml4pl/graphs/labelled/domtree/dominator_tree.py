1acd89db011c504a2099311a07b7b605db4397b1
Chris Cummins <chrisc.101@gmail.com>
2019-12-10 09:30:17 +0000

    Begin port of dominator trees.

--- deeplearning/ml4pl/graphs/labelled/domtree/dominator_tree.py
+++ deeplearning/ml4pl/graphs/labelled/domtree/dominator_tree.py
@@ -1,102 +1,106 @@
 """Module for labelling program graphs with dominator trees information."""
-import typing
+import collections
+from typing import Dict
+from typing import List
+from typing import Set
 
 import networkx as nx
 
-from deeplearning.ml4pl.graphs import graph_query as query
+from deeplearning.ml4pl.graphs import programl_pb2
+from deeplearning.ml4pl.graphs.labelled import data_flow_graphs
 from labm8.py import app
-from labm8.py import decorators
 
 FLAGS = app.FLAGS
 
 
-@decorators.timeout(seconds=60)
-def AnnotateDominatorTree(
-  g: nx.MultiDiGraph,
-  root_node: str,
-  x_label: str = "x",
-  y_label: str = "y",
-  true: typing.Any = True,
-  false: typing.Any = False,
-) -> typing.Tuple[int, int]:
-  # Create a map from nodes to predecessors.
-  predecessors: typing.Dict[str, typing.Set[str]] = {
-    node: set(
-      [
+# The real_y arrays for node reachability:
+NOT_DOMINATED = [1, 0]
+DOMINATED = [0, 1]
+
+
+class DominatorTreeAnnotator(data_flow_graphs.DataFlowGraphAnnotator):
+  """Annotate graphs with dominator analysis.
+
+  Statement node A dominates statement node B iff all control paths to B pass
+  through A.
+  """
+
+  def RootNodeType(self) -> programl_pb2.Node.Type:
+    """Dominator trees are a statement-based analysis."""
+    return programl_pb2.Node.STATEMENT
+
+  def Annotate(
+    self, g: nx.MultiDiGraph, root_node: int
+  ) -> data_flow_graphs.DataFlowAnnotatedGraph:
+    """Annotate nodes in the graph with dominator trees.
+
+    The 'root node' annotation is a [0,1] value appended to node x vectors.
+    The node label is a 1-hot binary vector set to y node vectors.
+
+    Args:
+      g: The graph to annotate.
+      root_node: The root node for building the dominator tree.
+
+    Returns:
+      A data flow annotated graph.
+    """
+    statement_nodes: List[int] = [
+      node
+      for node, type_ in g.nodes(data="type")
+      if type_ == programl_pb2.Node.STATEMENT
+    ]
+    # Create a map from nodes to predecessors.
+    predecessors: Dict[int, Set[int]] = {
+      node: set(
         src
         for src, _, flow in g.in_edges(node, data="flow")
-        if flow == "control"
-      ]
-    )
-    for node in [g for g, type_ in g.nodes(data="type") if type_ == "statement"]
-  }
+        if flow == programl_pb2.Edge.CONTROL
+      )
+      for node in statement_nodes
+    }
 
-  # Initialize the dominator sets.
-  dominators: typing.Dict[str, typing.Set[int]] = {
-    n: set(predecessors.keys()) - set([root_node])
-    for n, type_ in g.nodes(data="type")
-    if type_ == "statement"
-  }
-  dominators[root_node] = set([root_node])
+    # Initialize the dominator sets.
+    dominators: Dict[int, Set[int]] = {
+      n: set(predecessors.keys()) - set([root_node])
+      for n, type_ in g.nodes(data="type")
+      if type_ == "statement"
+    }
+    dominators[root_node] = set([root_node])
 
-  data_flow_steps = 0
-  changed = True
-  while changed:
-    data_flow_steps += 1
-    changed = False
-    for node in dominators.keys():
-      if node == root_node:
-        continue
-      dom_pred = [dominators[p] for p in predecessors[node]]
-      if dom_pred:
-        dom_pred = set.intersection(*dom_pred)
+    changed = True
+    data_flow_steps = 0
+    while changed:
+      changed = False
+      data_flow_steps += 1
+      for node in dominators:
+        if node == root_node:
+          continue
+        dom_pred = [dominators[p] for p in predecessors[node]]
+        if dom_pred:
+          dom_pred = set.intersection(*dom_pred)
+        else:
+          dom_pred = set()
+        new_dom = {node}.union(dom_pred)
+        if new_dom != dominators[node]:
+          dominators[node] = new_dom
+          changed = True
+
+    # Now that we have computed the dominator sets, assign labels and features
+    # to all nodes.
+    dominated_node_count = 0
+    for node, data in g.nodes(data=True):
+      data["x"].append(data_flow_graphs.ROOT_NODE_NO)
+      if node in dominators and root_node in dominators[node]:
+        dominated_node_count += 1
+        data["y"] = DOMINATED
       else:
-        dom_pred = set()
-      new_dom = set([node]).union(dom_pred)
-      if new_dom != dominators[node]:
-        dominators[node] = new_dom
-        changed = True
+        data["y"] = NOT_DOMINATED
 
-  num_dominated = 0
-  for node, data in g.nodes(data=True):
-    data[x_label] = [data[x_label], 0]
-    if node in dominators and root_node in dominators[node]:
-      num_dominated += 1
-      data[y_label] = true
-    else:
-      data[y_label] = false
+    g.nodes[root_node]["x"][-1] = data_flow_graphs.ROOT_NODE_YES
 
-  g.nodes[root_node][x_label] = [g.nodes[root_node][x_label][0], 1]
-
-  return num_dominated, data_flow_steps
-
-
-def MakeDominatorTreeGraphs(
-  g: nx.MultiDiGraph, n: typing.Optional[int] = None, false=False, true=True,
-) -> typing.Iterable[nx.MultiDiGraph]:
-  """Produce up to `n` dominator trees from the given unlabelled graph.
-
-  Args:
-    g: The unlabelled input graph.
-    n: The maximum number of graphs to produce. Multiple graphs are produced by
-      selecting different root nodes for creating labels. If `n` is provided,
-      the number of graphs generated will be in the range
-      1 <= x <= min(num_statements, n). Else, the number of graphs will be equal
-      to num_statements (i.e. one graph for each statement in the input graph).
-    false: The value to set for binary false values on X and Y labels.
-    true: The value to set for binary true values on X and Y labels.
-
-  Returns:
-    A generator of annotated graphs, where each graph has 'x' and 'y' labels on
-    the statement nodes, and additionally 'dominated_node_count' and
-    'data_flow_max_steps_required' attributes.
-  """
-  root_statements = query.SelectRandomNStatements(g, n)
-
-  for root_node in root_statements[:n]:
-    domtree = g.copy()
-    (
-      domtree.dominated_node_count,
-      domtree.data_flow_max_steps_required,
-    ) = AnnotateDominatorTree(domtree, root_node, false=false, true=true)
-    yield domtree
+    return data_flow_graphs.DataFlowAnnotatedGraph(
+      g=g,
+      root_node=root_node,
+      data_flow_steps=data_flow_steps,
+      positive_node_count=dominated_node_count,
+    )
