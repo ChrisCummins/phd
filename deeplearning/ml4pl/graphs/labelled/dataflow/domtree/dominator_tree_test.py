dce0c325b0a5a3f209a03e946befeee16faa0cb3
Chris Cummins <chrisc.101@gmail.com>
2019-12-10 19:08:23 +0000

    Update dominator tree tests.

--- deeplearning/ml4pl/graphs/labelled/dataflow/domtree/dominator_tree_test.py
+++ deeplearning/ml4pl/graphs/labelled/dataflow/domtree/dominator_tree_test.py
@@ -1,49 +1,71 @@
 """Unit tests for //deeplearning/ml4pl/graphs/labelled/dataflow/domtree:dominator_tree."""
 import networkx as nx
 
+from deeplearning.ml4pl.graphs import programl
+from deeplearning.ml4pl.graphs import programl_pb2
 from deeplearning.ml4pl.graphs.labelled.dataflow.domtree import dominator_tree
 from labm8.py import test
 
 FLAGS = test.FLAGS
 
+###############################################################################
+# Fixtures.
+###############################################################################
 
-@test.XFail("TODO(github.com/ChrisCummins/ProGraML/issues/22): Fix me")
-def test_AnnotateDominatorTree():
-  g = nx.MultiDiGraph()
-  g.add_node("A", type="statement", x=-1)
-  g.add_node("B", type="statement", x=-1)
-  g.add_node("C", type="statement", x=-1)
-  g.add_node("D", type="statement", x=-1)
-  g.add_node("E", type="statement", x=-1)
-  g.add_node("%1", type="identifier", x=-1)
-  g.add_edge("A", "B", flow="control")
-  g.add_edge("A", "C", flow="control")
-  g.add_edge("B", "D", flow="control")
-  g.add_edge("C", "D", flow="control")
-  g.add_edge("%1", "C", flow="data")
-  g.add_edge("A", "C", flow="data")
-  g.add_edge("E", "B", flow="control")
 
-  dominated_node_count, max_steps = dominator_tree.AnnotateDominatorTree(g, "A")
-  assert dominated_node_count == 2
+@test.Fixture(scope="session")
+def graph() -> nx.MultiDiGraph():
+  builder = programl.GraphBuilder()
+  a = builder.AddNode(x=[-1])
+  b = builder.AddNode(x=[-1])
+  c = builder.AddNode(x=[-1])
+  d = builder.AddNode(x=[-1])
+  e = builder.AddNode(x=[-1])
+  v1 = builder.AddNode(type=programl_pb2.Node.IDENTIFIER, x=[-1])
+  builder.AddEdge(a, b)
+  builder.AddEdge(a, c)
+  builder.AddEdge(b, d)
+  builder.AddEdge(c, d)
+  builder.AddEdge(v1, c)
+  builder.AddEdge(a, c)
+  builder.AddEdge(e, b)
+  return builder.g
+
+
+@test.Fixture(scope="function")
+def annotator() -> dominator_tree.DominatorTreeAnnotator:
+  return dominator_tree.DominatorTreeAnnotator()
+
+
+###############################################################################
+# Tests.
+###############################################################################
+
+
+def test_AnnotateDominatorTree(
+  graph: nx.MultiDiGraph, annotator: dominator_tree.DominatorTreeAnnotator
+):
+  annotated = annotator.Annotate(graph, root_node=0)
+
+  assert annotated.data_flow_positive_node_count == 2
   # TODO(cec): What is the correct value here?
-  assert max_steps in {2, 3}
+  assert annotated.data_flow_steps in {2, 3}
 
   # Features
-  assert g.nodes["A"]["x"] == [-1, 1]
-  assert g.nodes["B"]["x"] == [-1, 0]
-  assert g.nodes["C"]["x"] == [-1, 0]
-  assert g.nodes["D"]["x"] == [-1, 0]
-  assert g.nodes["E"]["x"] == [-1, 0]
-  assert g.nodes["%1"]["x"] == [-1, 0]
+  assert annotated.node[0].x == [-1, 1]
+  assert annotated.node[1].x == [-1, 0]
+  assert annotated.node[2].x == [-1, 0]
+  assert annotated.node[3].x == [-1, 0]
+  assert annotated.node[4].x == [-1, 0]
+  assert annotated.node[5].x == [-1, 0]
 
   # Labels
-  assert g.nodes["A"]["y"]
-  assert not g.nodes["B"]["y"]
-  assert g.nodes["C"]["y"]
-  assert not g.nodes["D"]["y"]
-  assert not g.nodes["E"]["y"]
-  assert not g.nodes["%1"]["y"]
+  assert annotated.node[0].y == dominator_tree.DOMINATED
+  assert annotated.node[1].y == dominator_tree.NOT_DOMINATED
+  assert annotated.node[2].y == dominator_tree.DOMINATED
+  assert annotated.node[3].y == dominator_tree.NOT_DOMINATED
+  assert annotated.node[4].y == dominator_tree.NOT_DOMINATED
+  assert annotated.node[5].y == dominator_tree.NOT_DOMINATED
 
 
 if __name__ == "__main__":
