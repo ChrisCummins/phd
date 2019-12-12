6279491c7c340b6e674bade2da4a178adfe5f515
Chris Cummins <chrisc.101@gmail.com>
2019-12-12 00:05:41 +0000

    Add a flaky dataset generator wrapper script.

---
+++ deeplearning/ml4pl/graphs/labelled/dataflow/flaky_make_data_flow_analysis_dataset.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# A wrapper around
+# //deeplearning/ml4pl/graphs/labelled/dataflow:make_data_flow_analysis_dataset
+# for running on flaky, heavily loaded systems.
+#
+# Usage:
+#
+#   bazel run //deeplearning/ml4pl/graphs/labelled/dataflow:flaky_make_data_flow_analysis -- \
+#       <make_data_flow_analysis_args...>
+#
+set -u
+
+main() {
+  i=0
+  while true; do
+    i=$((i+1))
+    echo "Beginning run $i of dataset generator"
+    # Run the dataset generator for 30 minutes.
+    timesout -s9 1800 deeplearning/ml4pl/graphs/labelled/dataflow/make_data_flow_analysis_dataset $@
+    # If the dataset generator completed with a zero return code, terminate.
+    if $@ ; then
+      echo "Dataset generator terminated gracefully after $i iterations"
+      break
+    fi
+    # Pause for a second so that the user can C-c twice to break the loop.
+    if ! sleep 1 ; then
+      break
+    fi
+  done
+}
+main $@
