985ee6afc361e8e4bd620db5326cc7bf10a8a864
Chris Cummins <chrisc.101@gmail.com>
2019-12-12 03:28:34 +0000

    Default to verbose execution with flaky script.

--- deeplearning/ml4pl/graphs/labelled/dataflow/flaky_make_data_flow_analysis_dataset.sh
+++ deeplearning/ml4pl/graphs/labelled/dataflow/flaky_make_data_flow_analysis_dataset.sh
@@ -17,7 +17,10 @@ main() {
     i=$((i+1))
     echo "Beginning run $i of dataset generator"
     # Run the dataset generator for 30 minutes.
-    timeout -s9 1800 deeplearning/ml4pl/graphs/labelled/dataflow/make_data_flow_analysis_dataset --max_instances=10000 $@
+    timeout -s9 1800 deeplearning/ml4pl/graphs/labelled/dataflow/make_data_flow_analysis_dataset \
+        --max_instances=10000 \
+        --vmodule='*'=3 \
+        $@
     # If the dataset generator completed with a zero return code, terminate.
     if $@ ; then
       echo "Dataset generator terminated gracefully after $i iterations"
