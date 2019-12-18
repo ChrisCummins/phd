7ccaa3d807172faa6ba93674da7b83a10b51e270
Chris Cummins <chrisc.101@gmail.com>
2019-12-18 11:21:22 +0000

    Re-implement nx iterators/queries for new schema.
    github.com/ChrisCummins/ProGraML/issues/2

---
+++ deeplearning/ml4pl/graphs/nx_utils.py
@@ -0,0 +1,229 @@
+"""Functions for iterating over and querying networkx program graphs."""
+import collections
+import typing
+from typing import List
+from typing import Tuple
+
+import networkx as nx
+
+from deeplearning.ml4pl.graphs import programl_pb2
+from labm8.py import app
+
+FLAGS = app.FLAGS
+
+###############################################################################
+# Iterators.
+###############################################################################
+
+
+def NodeTypeIterator(g: nx.DiGraph, node_type: programl_pb2.Node.Type):
+  """Iterate over nodes in a graph of a given type."""
+  for node, data in g.nodes(data=True):
+    if data["type"] == node_type:
+      yield node, data
+
+
+def StatementNodeIterator(g: nx.DiGraph):
+  """Iterate over the statement nodes in a graph."""
+  yield from NodeTypeIterator(g, programl_pb2.Node.STATEMENT)
+
+
+def IdentifierNodeIterator(g: nx.DiGraph):
+  """Iterate over identifier nodes in a graph."""
+  yield from NodeTypeIterator(g, programl_pb2.Node.IDENTIFIER)
+
+
+def EdgeTypeIterator(g: nx.DiGraph, flow: programl_pb2.Edge.Flow):
+  """Iterate over edges of the given flow type."""
+  for src, dst, data in g.edges(data=True):
+    if data["flow"] == flow:
+      yield src, dst, data
+
+
+def ControlFlowEdgeIterator(g: nx.DiGraph):
+  """Iterate over the control flow edges of a graph."""
+  yield from EdgeTypeIterator(g, programl_pb2.Edge.CONTROL)
+
+
+def DataFlowEdgeIterator(g: nx.DiGraph):
+  """Iterate over the data flow edges of a graph."""
+  yield from EdgeTypeIterator(g, programl_pb2.Edge.DATA)
+
+
+def CallEdgeIterator(g: nx.DiGraph):
+  """Iterate over the call edges of a graph."""
+  yield from EdgeTypeIterator(g, programl_pb2.Edge.CALL)
+
+
+def EntryBlockIterator(g: nx.DiGraph):
+  """Iterate over the entry blocks of a graph.
+  An entry block is a statement with no control predecessor.
+  """
+  for node, data in StatementNodeIterator(g):
+    # Filter out the root node.
+    if data["function"] is None:
+      continue
+
+    for _, _, flow in g.in_edges(node, data="flow"):
+      if flow == programl_pb2.Edge.CONTROL:
+        break
+    else:
+      yield node, data
+
+
+def ExitBlockIterator(g: nx.Graph):
+  """Iterate over the exit blocks of a graph.
+  An exit block is a statement with no control successor.
+  """
+  for node, data in StatementNodeIterator(g):
+    # Filter out the root node.
+    if data["function"] is None:
+      continue
+
+    for _, _, flow in g.out_edges(node, data="flow"):
+      if flow == programl_pb2.Edge.CONTROL:
+        break
+    else:
+      yield node, data
+
+
+###############################################################################
+# Queries.
+###############################################################################
+
+
+def GetDefsAndSuccessors(
+  g: nx.MultiDiGraph, node: int
+) -> Tuple[List[int], List[int]]:
+  """Get the list of data elements that this statement defines, and the control
+  successor statements."""
+  defs, successors = [], []
+  for src, dst, flow in g.out_edges(node, data="flow"):
+    if flow == programl_pb2.Edge.DATA:
+      defs.append(dst)
+    elif flow == programl_pb2.Edge.CONTROL:
+      successors.append(dst)
+  return defs, successors
+
+
+def GetUsesAndPredecessors(
+  g: nx.MultiDiGraph, node: int
+) -> Tuple[List[int], List[int]]:
+  """Get the list of data elements which this statement uses, and control
+  predecessor statements."""
+  uses, predecessors = [], []
+  for src, dst, flow in g.in_edges(node, data="flow"):
+    if flow == programl_pb2.Edge.DATA:
+      uses.append(src)
+    elif flow == programl_pb2.Edge.CONTROL:
+      predecessors.append(src)
+  return uses, predecessors
+
+
+def IsExitStatement(g: nx.MultiDiGraph, node: int):
+  """Determine if a statement is an exit node."""
+  for _, _, flow in g.out_edges(node, data="flow"):
+    if flow == programl_pb2.Edge.CONTROL:
+      break
+  else:
+    return True
+
+
+def StatementIsSuccessor(
+  g: nx.MultiDiGraph,
+  src: int,
+  dst: int,
+  flow: programl_pb2.Edge.Flow = programl_pb2.Edge.CONTROL,
+) -> bool:
+  """Return True if `dst` is successor to `src`."""
+  visited = set()
+  q = collections.deque([src])
+  while q:
+    current = q.popleft()
+    if current == dst:
+      return True
+    visited.add(current)
+    for _, next_node, edge_flow in g.out_edges(current, data="flow"):
+      if edge_flow != flow:
+        continue
+      node_type = g.nodes[next_node]["type"]
+      if node_type != programl_pb2.Node.STATEMENT:
+        continue
+      if next_node in visited:
+        continue
+      q.append(next_node)
+  return False
+
+
+def StatementNeighbors(
+  g: nx.Graph,
+  node: int,
+  flow=programl_pb2.Edge.CONTROL,
+  direction: typing.Optional[
+    typing.Callable[[typing.Any, typing.Any], typing.Any]
+  ] = None,
+) -> typing.Set[int]:
+  """Return the neighboring statements connected by the given flow type."""
+  direction = direction or (lambda src, dst: dst)
+  neighbors = set()
+  neighbor_edges = direction(g.in_edges, g.out_edges)
+  for src, dst, edge_flow in neighbor_edges(node, data="flow"):
+    neighbor = direction(src, dst)
+    if edge_flow == flow:
+      if g.nodes[neighbor]["type"] == programl_pb2.Node.STATEMENT:
+        neighbors.add(neighbor)
+      else:
+        neighbors = neighbors.union(StatementNeighbors(g, neighbor, flow=flow))
+  return neighbors
+
+
+def SuccessorNodes(
+  g: nx.DiGraph,
+  node: int,
+  direction: typing.Optional[
+    typing.Callable[[typing.Any, typing.Any], typing.Any]
+  ] = None,
+  ignored_nodes: typing.Optional[typing.Iterable[int]] = None,
+) -> typing.List[int]:
+  """Find the successor nodes of a node."""
+  direction = direction or (lambda src, dst: dst)
+  ignored_nodes = ignored_nodes or set()
+  real = []
+  for src, dst in direction(g.in_edges, g.out_edges)(node):
+    if direction(src, dst) not in ignored_nodes:
+      real.append(direction(src, dst))
+    else:
+      # The node is ignored, so skip over it and look for the next
+      real += SuccessorNodes(g, direction(src, dst), ignored_nodes, direction)
+  return real
+
+
+def GetStatementsForNode(
+  graph: nx.MultiDiGraph, node: int
+) -> typing.Iterable[str]:
+  """Return the statements which are connected to the given node.
+
+  Args:
+    graph: The graph to fetch the statements from.
+    node: The node to fetch the statements for. If the node is a statement, it
+      returns itself. If the node is an identifier, it returns all statements
+      which define/use this identifier.
+
+  Returns:
+    An iterator over statement nodes.
+  """
+  root = graph.nodes[node]
+  if root["type"] == programl_pb2.Node.STATEMENT:
+    yield node
+  elif (
+    root["type"] == programl_pb2.Node.IDENTIFIER
+    or root["type"] == programl_pb2.Node.IMMEDIATE
+  ):
+    for src, dst in graph.in_edges(node):
+      if graph.nodes[src]["type"] == programl_pb2.Node.STATEMENT:
+        yield src
+    for src, dst in graph.out_edges(node):
+      if graph.nodes[dst]["type"] == programl_pb2.Node.STATEMENT:
+        yield dst
+  else:
+    raise NotImplementedError(f"Unknown node type {root['type']}")
---
+++ deeplearning/ml4pl/graphs/nx_utils_test.py
@@ -0,0 +1,132 @@
+"""Unit tests for //deeplearning/ml4pl/graphs:nx_utils."""
+from deeplearning.ml4pl.graphs import nx_utils
+from deeplearning.ml4pl.graphs import programl
+from deeplearning.ml4pl.graphs import programl_pb2
+from labm8.py import test
+
+FLAGS = test.FLAGS
+
+
+@test.Fixture(scope="function")
+def graph():
+  builder = programl.GraphBuilder()
+  fn1 = builder.AddFunction()
+  root = builder.AddNode()
+  A = builder.AddNode(function=fn1)
+  B = builder.AddNode(function=fn1)
+  C = builder.AddNode(function=fn1)
+  D = builder.AddNode(function=fn1)
+  v1 = builder.AddNode(type=programl_pb2.Node.IDENTIFIER, function=fn1)
+  builder.AddEdge(A, B, flow=programl_pb2.Edge.CONTROL)
+  builder.AddEdge(B, C, flow=programl_pb2.Edge.CONTROL)
+  builder.AddEdge(C, D, flow=programl_pb2.Edge.CONTROL)
+  builder.AddEdge(root, A, flow=programl_pb2.Edge.CALL)
+  builder.AddEdge(A, v1, flow=programl_pb2.Edge.DATA)
+  builder.AddEdge(v1, D, flow=programl_pb2.Edge.DATA)
+  return builder.g
+
+
+def test_StatementNodeIterator(graph):
+  assert len(list(nx_utils.StatementNodeIterator(graph))) == 5
+
+
+def test_IdentifierNodeIterator(graph):
+  assert len(list(nx_utils.IdentifierNodeIterator(graph))) == 1
+
+
+def test_EntryBlockIterator(graph):
+  assert len(list(nx_utils.EntryBlockIterator(graph))) == 1
+
+
+def test_ExitBlockIterator(graph):
+  assert len(list(nx_utils.ExitBlockIterator(graph))) == 1
+
+
+def test_ControlFlowEdgeIterator(graph):
+  assert len(list(nx_utils.ControlFlowEdgeIterator(graph))) == 3
+
+
+def test_DataFlowEdgeIterator(graph):
+  assert len(list(nx_utils.DataFlowEdgeIterator(graph))) == 2
+
+
+def test_StatementNeighbors(graph):
+  assert nx_utils.StatementNeighbors(graph, 1) == {2}
+
+
+def test_StatementNeighbors_data_flow(graph):
+  assert nx_utils.StatementNeighbors(graph, 1, flow=programl_pb2.Edge.DATA) == {
+    4
+  }
+
+
+def test_StatementIsSuccessor(graph):
+  assert nx_utils.StatementIsSuccessor(graph, 1, 2)
+  assert not nx_utils.StatementIsSuccessor(graph, 2, 1)
+
+
+def test_StatementIsSuccessor_linear_control_path():
+  # A -> B -> C
+  builder = programl.GraphBuilder()
+  a = builder.AddNode()
+  b = builder.AddNode()
+  c = builder.AddNode()
+  builder.AddEdge(a, b)
+  builder.AddEdge(b, c)
+  g = builder.g
+  assert nx_utils.StatementIsSuccessor(g, a, a)
+  assert nx_utils.StatementIsSuccessor(g, a, b)
+  assert nx_utils.StatementIsSuccessor(g, a, c)
+  assert nx_utils.StatementIsSuccessor(g, b, c)
+  assert not nx_utils.StatementIsSuccessor(g, c, a)
+  assert not nx_utils.StatementIsSuccessor(g, b, a)
+  assert not nx_utils.StatementIsSuccessor(g, a, -1)  # Destination not in graph
+  with test.Raises(Exception):
+    nx_utils.StatementIsSuccessor(g, -1, a)  # Source not in graph
+
+
+def test_StatementIsSuccessor_branched_control_path():
+  # A -> B -> D
+  # A -> C -> D
+  builder = programl.GraphBuilder()
+  a = builder.AddNode()
+  b = builder.AddNode()
+  c = builder.AddNode()
+  d = builder.AddNode()
+  builder.AddEdge(a, b)
+  builder.AddEdge(a, c)
+  builder.AddEdge(b, d)
+  builder.AddEdge(c, d)
+  g = builder.g
+  assert nx_utils.StatementIsSuccessor(g, a, b)
+  assert nx_utils.StatementIsSuccessor(g, a, c)
+  assert nx_utils.StatementIsSuccessor(g, a, b)
+  assert not nx_utils.StatementIsSuccessor(g, b, a)
+  assert not nx_utils.StatementIsSuccessor(g, b, c)
+  assert nx_utils.StatementIsSuccessor(g, b, d)
+
+
+def test_GetStatementsForNode_node():
+  """Test the nodes returned when root is a statementt."""
+  builder = programl.GraphBuilder()
+  foo = builder.AddNode()
+
+  nodes = list(nx_utils.GetStatementsForNode(builder.g, foo))
+  assert nodes == [foo]
+
+
+def test_GetStatementsForNode_identifier():
+  """Test the nodes returned when root is an identifier."""
+  builder = programl.GraphBuilder()
+  foo = builder.AddNode()
+  bar = builder.AddNode()
+  v1 = builder.AddNode(type=programl_pb2.Node.IDENTIFIER)
+  builder.AddEdge(foo, v1, flow=programl_pb2.Edge.DATA)
+  builder.AddEdge(v1, bar, flow=programl_pb2.Edge.DATA)
+
+  nodes = list(nx_utils.GetStatementsForNode(builder.g, v1))
+  assert nodes == [foo, bar]
+
+
+if __name__ == "__main__":
+  test.Main()
