a3ad22d0af217a4e2804e198d4380f31bdae40af
Chris Cummins <chrisc.101@gmail.com>
2019-12-19 20:13:43 +0000

    License //deeplearning/ml4pl/ under Apache v2.0.

--- deeplearning/ml4pl/ir/create/import_from_npb.py
+++ deeplearning/ml4pl/ir/create/import_from_npb.py
@@ -1,3 +1,18 @@
+# Copyright 2019 the ProGraML authors.
+#
+# Contact Chris Cummins <chrisc.101@gmail.com>.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Import bytecodes from a serial C implementation of NPB benchmarks.
 
 This uses ﻿Aleksandr Maramzin's source tree:
@@ -33,6 +48,7 @@ from deeplearning.ml4pl.bytecode import bytecode_database
 from labm8.py import app
 from labm8.py import fs
 from labm8.py import sqlutil
+
 
 FLAGS = app.FLAGS
 
--- deeplearning/ml4pl/models/export_logs.py
+++ deeplearning/ml4pl/models/export_logs.py
@@ -1,3 +1,18 @@
+# Copyright 2019 the ProGraML authors.
+#
+# Contact Chris Cummins <chrisc.101@gmail.com>.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """A module for exporting summaries of log databases.
 
 Example usage:
@@ -39,6 +54,7 @@ from labm8.py import app
 from labm8.py import google_sheets
 from labm8.py import pdutil
 from labm8.py import prof
+
 
 # During table export, we be able to de-pickle any of the class objects from
 # this project. List those classes / modules here to make the dependency
