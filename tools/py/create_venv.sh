1e9b21148c2c0bc8d85faace12bb365325445ad5
Chris Cummins <chrisc.101@gmail.com>
2019-12-25 19:27:49 +0000

    Spring-clean python toolchain configuration.
    This change add a new package //tools/py for housing python toolchain
    files. The virtualenv directory is moved out of the source tree, from
    tools/venv/phd to ~/.cache/phd/tools/py/venv/phd.
    
    github.com/ChrisCummins/phd/issues/49

---
+++ tools/py/create_venv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Create a python virtualenv and install the project dependencies.
+#
+# Usage:
+#     $ ./tools/py/create_venv.sh
+set -eux
+DIR="$HOME/.cache/tools/py/venv/phd"
+
+rm -rf "$DIR"
+mkdir -pv "$(dirname $DIR)"
+python3 -m venv "$DIR"
+set +u  # The virtualenv activation script references potentially unbound variables.
+source "$DIR/bin/activate"
+python -m pip install -r requirements.txt
---
+++ tools/py/python_version.py
@@ -0,0 +1,14 @@
+# A script which prints the python interpreter path and version.
+#
+# Use this to verify that bazel's python toolchain is set up correctly.
+# Example:
+#
+#     $ bazel run //tools/py:python_version
+#     /usr/bin/python3
+#     3.7.5 (default, Nov 20 2019, 09:21:52)
+#     [GCC 9.2.1 20191008]
+#
+import sys
+
+print(sys.executable)
+print(sys.version)
