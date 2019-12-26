175d49fc3f118d81bea1b7e96dc5a99683fa7e77
Chris Cummins <chrisc.101@gmail.com>
2019-12-26 08:51:59 +0000

    Add node version manager config.

---
+++ system/dotfiles/usr/share/Zsh/nvm.zsh
@@ -0,0 +1,8 @@
+# Support for node version manager.
+# https://github.com/nvm-sh/nvm
+
+if [[ -d "$HOME/.nvm" ]]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
