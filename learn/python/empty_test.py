19a9b121098e682b4a25e4f509cfca588d14c7ff
Chris Cummins <chrisc.101@gmail.com>
2019-12-26 17:44:31 +0000

    Make an empty test suite not fail.
    This changes the default behaviour of pytest such that empty test
    suites are not considered errors. This is to make it easier to guess
    a good shard_count for test targets, as if you over-approximate the
    number of shards required, then some of them will be empty and
    "fail".
    
    github.com/ChrisCummins/ProGraML/issues/47

---
+++ learn/python/empty_test.py
@@ -0,0 +1,12 @@
+"""An empty test suite.
+
+This test will fail if --error_if_no_tests, else it will pass (but print a
+warning).
+"""
+from labm8.py import test
+
+
+FLAGS = test.FLAGS
+
+if __name__ == "__main__":
+  test.Main()
