c3de9b3ae3c223eb3bd377dedb9c3c9c907ba52f
Chris Cummins <chrisc.101@gmail.com>
2020-01-02 23:31:57 +0000

    Add a test for checkpoint construction.
    github.com/ChrisCummins/phd/issues/65

--- deeplearning/ml4pl/models/checkpoints.py
+++ deeplearning/ml4pl/models/checkpoints.py
@@ -22,6 +22,7 @@ from typing import Any
 from typing import Dict
 from typing import NamedTuple
 from typing import Optional
+from typing import Union
 
 from deeplearning.ml4pl import run_id as run_id_lib
 from deeplearning.ml4pl.models import epoch
@@ -38,23 +39,52 @@ class CheckpointReference(NamedTuple):
   # If epoch_num is None, then load the best epoch.
   epoch_num: Optional[int]
 
-  def __repr__(self):
+  def __eq__(self, rhs: Union["CheckpointReference", "Checkpoint"]) -> bool:
+    """Equality check for checkpoints / checkpoint references."""
+    return rhs.run_id == self.run_id and rhs.epoch_num == self.epoch_num
+
+  def __repr__(self) -> str:
+    """Return the string representation of a checkpoint ref, suitable for
+    parsing with FromString().
+    """
     return (
       f"{self.run_id}@{'best' if self.epoch_num is None else self.epoch_num}"
     )
 
   @classmethod
   def FromString(cls, string: str) -> "CheckpointReference":
+    """Parse a checkpoint reference from string.
+
+    The format for a checkpoint reference is:
+
+      <run_id>[@<epoch_num|"best">]
+
+    If the "@<epoch_num>" suffix is omitted, the best checkpoint is selected.
+    This is the same as using the "@best" suffix.
+
+    Args:
+      string: A checkpoint reference string.
+
+    Returns:
+      A CheckpointRefernece instance.
+
+    Raises:
+      ValueError: If the string is malformed.
+    """
     try:
       if "@" in string:
         components = string.split("@")
         assert len(components) == 2
         run_id_string, epoch_num_string = components
-        run_id = run_id_lib.RunId.FromString(run_id_string)
-        epoch_num = int(epoch_num_string)
+        if epoch_num_string == "best":
+          epoch_num = None
+        else:
+          epoch_num = int(epoch_num_string)
       else:
-        run_id = run_id_lib.RunId.FromString(string)
+        run_id_string = string
         epoch_num = None
+
+      run_id = run_id_lib.RunId.FromString(run_id_string)
 
       return CheckpointReference(run_id, epoch_num)
     except Exception:
