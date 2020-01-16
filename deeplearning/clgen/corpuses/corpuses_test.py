1b3e968890931b5e657c59e7a7dc322c605704a3
Chris Cummins <chrisc.101@gmail.com>
2020-01-16 17:26:46 +0000

    Update paths in test comment headers.
    Signed-off-by: format 2020.01.14 <github.com/ChrisCummins/format>

--- deeplearning/clgen/corpuses/corpuses_test.py
+++ deeplearning/clgen/corpuses/corpuses_test.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with clgen.  If not, see <https://www.gnu.org/licenses/>.
-"""Unit tests for //deeplearning/clgen/corpus.py."""
+"""Unit tests for //deeplearning/clgen/corpuses:corpus."""
 import datetime
 import os
 import pathlib
