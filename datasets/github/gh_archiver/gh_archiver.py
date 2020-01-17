01017433297f4541c859698f6150e8840855be5f
Chris Cummins <chrisc.101@gmail.com>
2020-01-17 16:06:52 +0000

    Remove --gogs behavior from gh_archiver.
    Signed-off-by: format 2020.01.16 <github.com/ChrisCummins/format>

--- datasets/github/gh_archiver/gh_archiver.py
+++ datasets/github/gh_archiver/gh_archiver.py
@@ -50,12 +50,9 @@ import os
 import pathlib
 import shutil
 import sys
-from configparser import ConfigParser
 from typing import Iterator
-from typing import Tuple
 from typing import Union
 
-import requests
 from git import Repo
 from github import AuthenticatedUser
 from github import NamedUser
@@ -85,36 +82,11 @@ app.DEFINE_list(
   "cloned.",
 )
 app.DEFINE_list("exclude", [], "A list of repositories to exclude.")
-app.DEFINE_boolean("gogs", False, "Mirror repositories to gogs instance.")
-app.DEFINE_integer("gogs_uid", 1, "The gogs UID.")
-app.DEFINE_output_path(
-  "gogsrc",
-  pathlib.Path("~/.gogsrc").expanduser(),
-  "File to read Gogs server address and token from.",
-)
 app.DEFINE_boolean(
   "force_https",
   False,
   "If set, force using HTTPS git remotes, even when SSH is available.",
 )
-
-
-def get_gogs_config() -> Tuple[str, str]:
-  """ read gogs configuration """
-  if not FLAGS.gogsrc.is_file():
-    raise app.UsageError(f"--gogsrc file not found: {FLAGS.gogsrc}")
-
-  config = ConfigParser()
-  config.read(FLAGS.gogsrc)
-
-  try:
-    server = config["Server"]["Address"]
-    token = config["User"]["Token"]
-  except KeyError as e:
-    print(f"config variable {e} not set. Check {FLAGS.gogsrc}", file=sys.stderr)
-    sys.exit(1)
-
-  return server, token
 
 
 def get_repos(user) -> Iterator[GithubRepository]:
@@ -136,22 +108,6 @@ def truncate(string: str, maxlen: int, suffix=" ...") -> str:
     return string
 
 
-def sanitize_description(string) -> str:
-  """ make Github repo description compatible with gogs """
-  if string:
-    # the decode/encode dance. We need to get from ascii-encoded UTF-8, to
-    # plain ascii, with unicode symbols stripped.
-    return truncate(
-      string.encode("utf-8")
-      .decode("unicode_escape")
-      .encode("ascii", "ignore")
-      .decode("ascii"),
-      255,
-    )
-  else:
-    return ""
-
-
 def main():
   if not FLAGS.user:
     raise app.UsageError("--user not set")
@@ -165,9 +121,6 @@ def main():
     user: Union[NamedUser, AuthenticatedUser] = g.get_user(FLAGS.user)
 
     repos = list(get_repos(user))
-
-    if FLAGS.gogs:
-      gogs_server, gogs_token = get_gogs_config()
 
     FLAGS.outdir.mkdir(exist_ok=True, parents=True)
 
@@ -190,77 +143,36 @@ def main():
 
     errors = False
     for repo in repos:
-      if FLAGS.gogs:
-        local_path = FLAGS.outdir / pathlib.Path(repo.name.lower() + ".git")
-      else:
-        local_path = FLAGS.outdir / repo.name
+      local_path = FLAGS.outdir / repo.name
 
       # remove any file of the same name
       if local_path.exists() and not local_path.is_dir():
         os.remove(local_path)
 
-      if FLAGS.gogs:
-        # Mirror to gogs instance
-        if not local_path.is_dir():
-          sys.stdout.write(f"mirroring {repo.name} ... ")
-          headers = {
-            "Authorization": f"token {gogs_token}",
-          }
-          data = {
-            "auth_username": github_username,
-            "auth_token": github_token,
-            "repo_name": truncate(repo.name, 255),
-            "clone_addr": repo.clone_url,
-            "uid": FLAGS.gogs_uid,
-            "description": sanitize_description(repo.description),
-            "private": False,
-            "mirror": True,
-          }
-
-          def pretty(d):
-            import json
-
-            print(
-              json.dumps(d, sort_keys=True, indent=4, separators=(",", ": "))
-            )
-
-          r = requests.post(
-            gogs_server + "/api/v1/repos/migrate", headers=headers, data=data
-          )
-          print(r.status_code)
-          if r.status_code < 200 or r.status_code >= 300:
-            pretty(headers)
-            pretty(data)
-            print()
-            print("status", r.status_code)
-            pretty(r.json())
-            print(len(data["description"]))
-            sys.exit(1)
+      # Local clone
+      if local_path.is_dir():
+        print(f"updating {repo.name}")
       else:
-        # Local clone
-        if local_path.is_dir():
-          print(f"updating {repo.name}")
-        else:
-          print(f"cloning {repo.name}:{repo.default_branch}")
-          try:
-            Repo.clone_from(
-              repo.clone_url if FLAGS.force_https else repo.git_url,
-              local_path,
-              branch=repo.default_branch,
-            )
-          except Exception as e:
-            errors = True
-            print(f"ERROR: {local_path} {type(e).__name__}", file=sys.stderr)
-
+        print(f"cloning {repo.name}:{repo.default_branch}")
         try:
-          local_repo = Repo(local_path)
-          for remote in local_repo.remotes:
-            remote.fetch()
-          for submodule in local_repo.submodules:
-            submodule.update(init=True)
+          Repo.clone_from(
+            repo.clone_url if FLAGS.force_https else repo.git_url,
+            local_path,
+            branch=repo.default_branch,
+          )
         except Exception as e:
           errors = True
           print(f"ERROR: {local_path} {type(e).__name__}", file=sys.stderr)
+
+      try:
+        local_repo = Repo(local_path)
+        for remote in local_repo.remotes:
+          remote.fetch()
+        for submodule in local_repo.submodules:
+          submodule.update(init=True)
+      except Exception as e:
+        errors = True
+        print(f"ERROR: {local_path} {type(e).__name__}", file=sys.stderr)
 
     return sys.exit(1 if errors else 0)
   except KeyboardInterrupt:
