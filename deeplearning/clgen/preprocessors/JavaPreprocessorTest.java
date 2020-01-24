2c1bbc6be873eb8e7132677c21a74d8c6dc2a8e7
Chris Cummins <chrisc.101@gmail.com>
2020-01-24 10:52:03 +0000

    Update Java proto names.
    Prototool rewrites the package name and enables an option that
    requires updating Java proto package imports.
    
    Signed-off-by: format 2020.01.16 <github.com/ChrisCummins/format>

--- deeplearning/clgen/preprocessors/JavaPreprocessorTest.java
+++ deeplearning/clgen/preprocessors/JavaPreprocessorTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import deeplearning.clgen.InternalProtos.PreprocessorWorkerJobOutcome;
+import com.clgen.PreprocessorWorkerJobOutcome;
 import labm8.java.bazelutil.BazelRunfiles;
 import org.junit.Test;
 
