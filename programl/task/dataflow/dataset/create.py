63b2a46dd6a0f9d07635754b1e2c77efda5b3a5f
Chris Cummins <chrisc.101@gmail.com>
2020-05-16 04:21:22 +0100

    programl: Don't silence label program stdout.
    Signed-off-by: format 20.02.05 <github.com/ChrisCummins/format>

--- programl/task/dataflow/dataset/create.py
+++ programl/task/dataflow/dataset/create.py
@@ -196,7 +196,7 @@ def Main():
   ExportClassifyAppGraphs(pathlib.Path(FLAGS.classifyapp), path)
 
   app.Log(1, "Creating data flow analysis labels")
-  subprocess.check_output([str(CREATE_LABELS), "--path", str(path)])
+  subprocess.check_call([str(CREATE_LABELS), "--path", str(path)])
 
 
 if __name__ == "__main__":
