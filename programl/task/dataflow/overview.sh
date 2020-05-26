bb6f1ec66be4df20740e4f272d1cbcf98595bad3
Chris Cummins <chrisc.101@gmail.com>
2020-05-26 18:29:33 +0100

    programl: Refactor overview script.
    Signed-off-by: format 2020.01.21 <github.com/ChrisCummins/format>

--- programl/task/dataflow/overview.sh
+++ programl/task/dataflow/overview.sh
@@ -14,6 +14,44 @@ EOF
 
 set -euo pipefail
 
+# Overview of dataset stats.
+# Must be called from the root of the dataflow dataset.
+overview() {
+  echo "=========================="
+  echo "#. runs: " $(find -L logs -mindepth 3 -maxdepth 3 -type d | wc -l)
+  echo
+  echo "Epochs: "
+  for run in $(find -L logs -mindepth 3 -maxdepth 3 -type d | sort); do
+    echo "    $(find $run/epochs -type f | wc -l) $run"
+  done
+
+  echo
+  echo "=========================="
+  echo "Dataset job logs:"
+  find labels -mindepth 1 -maxdepth 1 -type f | sort | xargs wc -l | sed 's/^/    /'
+
+  echo
+  echo "=========================="
+  echo "Directory sizes:"
+  for dir in graphs $(find labels -mindepth 1 -maxdepth 1 -type d | sort); do
+    echo "    $(find $dir -type f | wc -l) $dir"
+  done
+
+  echo
+  echo "=========================="
+  echo "Splits:"
+  for dir in train val test; do
+    echo "    $(find $dir -type l | wc -l) $dir"
+  done
+
+  echo
+  echo "=========================="
+  echo "Graph sources:"
+  find graphs -type f | grep -v poj104 | grep -v github | cut -d'.' -f1 | sort | uniq -c
+  find graphs -type f -name 'poj104*' | cut -d'_' -f1 | sort | uniq -c
+  find graphs -type f -name 'github*' | cut -d'.' -f1,3 | sort | uniq -c
+}
+
 main() {
   set +u
   if [[ -z "$1" ]]; then
@@ -41,31 +79,6 @@ main() {
   fi
   set -u
 
-  echo "=========================="
-  echo "#. runs: " $(find -L logs -mindepth 3 -maxdepth 3 -type d | wc -l)
-  echo
-  echo "Epochs: "
-  for run in $(find -L logs -mindepth 3 -maxdepth 3 -type d | sort); do
-    echo "    $(find $run/epochs -type f | wc -l) $run"
-  done
-
-  echo
-  echo "=========================="
-  echo "Dataset job logs:"
-  find labels -mindepth 1 -maxdepth 1 -type f | sort | xargs wc -l | sed 's/^/    /'
-
-  echo
-  echo "=========================="
-  echo "Directory sizes:"
-  for dir in graphs $(find labels -mindepth 1 -maxdepth 1 -type d | sort); do
-    echo "    $(find $dir -type f | wc -l) $dir"
-  done
-
-  echo
-  echo "=========================="
-  echo "Splits:"
-  for dir in train val test; do
-    echo "    $(find $dir -type l | wc -l) $dir"
-  done
+  overview
 }
 main $@
