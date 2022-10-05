b4a492923b49f36737ab539532034cd799edecd3
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-05 09:19:34 +0000

    Adding tarfile member sanitization to extractall()

--- deeplearning/deeptune/opencl/heterogeneous_mapping/models/deeptune.py
+++ deeplearning/deeptune/opencl/heterogeneous_mapping/models/deeptune.py
@@ -185,7 +185,29 @@ class DeepTune(base.HeterogeneousMappingModel):
 
       # Unpack the tarball to a temporary directory.
       with tarfile.open(inpath) as tar:
-        tar.extractall(d)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, d)
 
       # Restore model properties from files.
       self.model = keras_models.load_model(d / "keras_model.h5")
